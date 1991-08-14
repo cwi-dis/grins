a723d7517e0e9dfac4638e5021408795f361c942
Guido van Rossum <guido@python.org>
1991-08-14 15:51:13 +0000

    Added ExitException -- raised by Quit buttons and the like.

---
+++ mm/lib/MMExc.py
@@ -0,0 +1,17 @@
+# This module only defines exceptions.
+# Suggested usage:
+#	from MMExc import *
+
+TypeError = 'MMExc.TypeError'		# Invalid type in input file
+SyntaxError = 'MMExc.SyntaxError'	# Invalid syntax in input file
+
+CheckError = 'MMExc.CheckError'		# Invalid call from client
+AssertError = 'MMExc.AssertError'	# Internal inconsistency
+
+NoSuchAttrError = 'MMExc.NoSuchAttrError'	# Attribute not found
+NoSuchUIDError = 'MMExc.NoSuchUIDError'		# UID not in UID map
+DuplicateUIDError = 'MMExc.DuplicateUIDError'	# UID already in UID map
+
+ExitException = 'MMExc.ExitException'	# Exit application,
+					# Should be caught at outer level.
+					# Argument is proposed exit status.
