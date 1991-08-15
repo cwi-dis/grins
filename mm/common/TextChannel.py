c2cf67d103d02209f04f879846971b74fe568c3d
Guido van Rossum <guido@python.org>
1991-08-15 13:07:35 +0000

    Use MMAttrdefs.getattr() to get attributes. Changed default point sizes back to 12-ish. When resetting, the font is taken from the attributes. The window is now drawn in RGB mode, to avoid an apparent colormap bug in FORMS, IRIX 4.0 or whereever. The player now *is* the queue. Move the geometry calculations to glwindow.setgeometry().

--- mm/common/TextChannel.py
+++ mm/common/TextChannel.py
@@ -1,69 +1,70 @@
 # Text channel
 
-from Channel import Channel # the base class
-
 import string
+
 import gl, fm
 from GL import *
 from DEVICE import *
-from glwindow import glwindow
 
-class TextWindow() = glwindow():
+import glwindow
+
+from MMExc import *
+import MMAttrdefs
+
+from Channel import Channel # the base class
+
+class TextWindow() = (glwindow.glwindow)():
 	#
 	def init(self, (name, attrdict)):
+		self.name = name
+		self.attrdict = attrdict
 		self.text = '' # Initially, display no text
 		gl.foreground() # Fight silly GL default
-		# Get the default font and point size for the window
-		if attrdict.has_key('font'):
-			self.fontname, self.pointsize = \
-				mapfont(attrdict['font'])
+		self.resetfont()
+		# Get the window size (given in lines and characters!)
+		if self.attrdict.has_key('winsize'):
+			width, height = attrdict['winsize']
+			width = width * self.avgcharwidth
+			height = height * self.fontheight
 		else:
-			self.fontname , self.pointsize = 'Times-Roman', 12
-		if attrdict.has_key('pointsize'):
-			self.pointsize = attrdict['pointsize']
+			width, height = 0, 0
+		# Get the preferred position (in pixels, "hv" coordinates)
+		if attrdict.has_key('winpos'):
+			h, v = attrdict['winpos']
+		else:
+			h, v = 0, 0
+		glwindow.setgeometry(h, v, width, height)
+		# Actually create the window
+		wid = gl.winopen(name)
+		# Immediately register it with the main loop
+		self.register(wid)
+		# Let the user resize it
+		gl.winconstraints()
+		# Use RGB mode; in colormap mode FORMS sometimes
+		# messes up our colormap ?!?
+		gl.RGBmode()
+		gl.gconfig()
+		# Clear it immediately (looks better)
+		gl.RGBcolor(255, 255, 255)
+		gl.clear()
+		return self
+	#
+	def resetfont(self):
+		# Get the default font and point size for the window
+		if self.attrdict.has_key('font'):
+			fontspec = self.attrdict['font']
+		else:
+			fontspec = 'default'
+		self.fontname, self.pointsize = mapfont(fontspec)
+		if self.attrdict.has_key('pointsize'):
+			ps = self.attrdict['pointsize']
+			if ps <> 0: self.pointsize = ps
 		# Create the default font object
 		self.font1 = fm.findfont(self.fontname) # At 1 point...
 		self.font = self.font1.scalefont(self.pointsize)
 		# Find out some parameters of the font
 		self.avgcharwidth, self.baseline, self.fontheight = \
 			getfontparams(self.font)
-#		print 'avg char width:', self.avgcharwidth,
-#		print 'baseline:', self.baseline,
-#		print 'fontheight:', self.fontheight
-		# Get the window size (given in lines and characters!)
-		if attrdict.has_key('winsize'):
-			width, height = attrdict['winsize']
-			width = width * self.avgcharwidth
-			height = height * self.fontheight
-#			print 'winsize', (width, height)
-		else:
-			width, height = 0, 0
-		# Get the preferred position (in pixels, "hv" coordinates)
-		if attrdict.has_key('winpos'):
-			# Make sure we have nonzero width and height
-			if width <= 0: width = 400
-			if height <= 0: height = 300
-			h, v = attrdict['winpos']
-#			print 'winpos', (h, v)
-			scrwidth = gl.getgdesc(GD_XPMAX)
-			scrheight = gl.getgdesc(GD_YPMAX)
-			x, y = h, scrheight-v-height
-#			print 'prefposition', (x, x+width-1, y, y+height-1)
-			gl.prefposition(x, x+width-1, y, y+height-1)
-		elif width <> 0 or height <> 0:
-			# Make sure we have nonzero width and height
-			if width <= 0: width = 400
-			if height <= 0: height = 300
-#			print 'prefsize', (width, height)
-			gl.prefsize(width, height)
-		# Actually create the window
-		wid = gl.winopen(name)
-		self.register(wid)
-		# Clear it immediately (looks better)
-		gl.color(WHITE)
-		gl.clear()
-		gl.winconstraints() # Let the user resize it
-		return self
 	#
 	def settext(self, text):
 		self.text = text
@@ -79,10 +80,10 @@ class TextWindow() = glwindow():
 		gl.scrmask(x0, x1, y0, y1)
 		gl.ortho2(-MASK, width+MASK, height+MASK, -MASK)
 		#
-		gl.color(WHITE)
+		gl.RGBcolor(255, 255, 255)
 		gl.clear()
 		#
-		gl.color(BLACK)
+		gl.RGBcolor(0, 0, 0)
 		gl.cmov2(0, self.baseline)
 		self.font.setfont()
 		fm.prstr(self.text)
@@ -96,7 +97,7 @@ class TextChannel() = Channel():
 		return self
 	#
 	def getduration(self, node):
-		wait = node.GetInherAttrDef('wait_for_close', 0)
+		wait = MMAttrdefs.getattr(node, 'wait_for_close')
 		if wait:
 			# An approximation of infinity for the
 			# time analysis algorithm
@@ -106,14 +107,18 @@ class TextChannel() = Channel():
 	#
 	def play(self, (node, callback, arg)):
 		self.showtext(node)
-		wait = node.GetInherAttrDef('wait_for_close', 0)
+		wait = MMAttrdefs.getattr(node, 'wait_for_close')
 		if wait:
 			self.player.freeze() # Should have another interface
-			now = self.player.queue.timefunc()
-			self.qid = self.player.queue.enter(now + 0.001, 0, \
+			now = self.player.timefunc()
+			self.qid = self.player.enter(now + 0.001, 0, \
 						self.done, (callback, arg))
 		else:
 			Channel.play(self, (node, callback, arg))
+	#
+	def reset(self):
+		self.window.resetfont()
+		self.window.settext('')
 	#
 	def showtext(self, node):
 		self.window.settext(self.getstring(node))
@@ -122,7 +127,7 @@ class TextChannel() = Channel():
 		if node.type = 'imm':
 			return string.join(node.GetValues())
 		elif node.type = 'ext':
-			file = node.GetInherAttrDef('file', '/dev/null')
+			file = MMAttrdefs.getattr(node, 'file')
 			try:
 				fp = open(file, 'r')
 			except:
@@ -147,13 +152,13 @@ def getfontparams(font):
 	return avgcharwidth, baseline, fontheight
 
 fontmap = { \
-	'':		('Times-Roman', 13), \
-	'default':	('Times-Roman', 13), \
-	'plain':	('Times-Roman', 13), \
-	'italic':	('Times-Italic', 13), \
-	'bold':		('Times-Bold', 13), \
-	'courier':	('Courier', 13), \
-	'bigbold':	('Times-Bold', 16), \
+	'':		('Times-Roman', 12), \
+	'default':	('Times-Roman', 12), \
+	'plain':	('Times-Roman', 12), \
+	'italic':	('Times-Italic', 12), \
+	'bold':		('Times-Bold', 12), \
+	'courier':	('Courier', 12), \
+	'bigbold':	('Times-Bold', 14), \
 	'title':	('Times-Bold', 24), \
 	  }
 
@@ -162,4 +167,4 @@ def mapfont(fontname):
 	if fontmap.has_key(fontname):
 		return fontmap[fontname]
 	else:
-		return fontname, 13
+		return fontname, 12
