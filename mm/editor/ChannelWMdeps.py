3a9584c591db49c52d53344d5e6d5d80d2505ba5
Guido van Rossum <guido@python.org>
1991-08-15 13:05:22 +0000

    Added setrate() and reset() methods; removed freeze() and unfreeze(), which are generalized into setrate(). Use MMAttrdefs.getattr() to get attributes. The player now *is* the queue.

--- mm/editor/ChannelWMdeps.py
+++ mm/editor/ChannelWMdeps.py
@@ -2,10 +2,20 @@
 # Most methods should be overridden or at least extended.
 # The default methods defined here are useful for a null channel.
 
+from MMExc import *
+import MMAttrdefs
+
 class Channel():
 	#
 	# Initialization method (returns self!).
 	# Should be extended, not overridden.
+	# NB: this *shares* the attribute dictionary with the context.
+	# A good channel should modify the attribute dictionary
+	# if it changes any of its internal parameters, so the change
+	# in value is saved with the document and can be used as a default
+	# by MMAttrdefs.getattr().  Also, a good channel reads its
+	# parameters out of the attribute dictionary each time it
+	# is reset, so changes made while it was dormant are noted.
 	#
 	def init(self, (name, attrdict, player)):
 		self.name = name
@@ -28,13 +38,13 @@ class Channel():
 	# (This does not depend on the playback rate!)
 	#
 	def getduration(self, node):
-		return float(node.GetInherAttrDef('duration', 2.0))
+		return MMAttrdefs.getattr(node, 'duration')
 	#
 	# Start playing a node.
 	#
 	def play(self, (node, callback, arg)):
 		secs = self.getduration(node)
-		self.qid = self.player.queue.enter(secs, 0, self.done, \
+		self.qid = self.player.enter(secs, 0, self.done, \
 							(callback, arg))
 	#
 	# Don't call done() directly -- it's a callback used internally.
@@ -43,17 +53,23 @@ class Channel():
 		self.qid = None
 		callback(arg)
 	#
-	# Freeze and unfreeze have a dummy implementation here --
-	# they rely on the queue implementing the delay.
+	# Setting the playback rate to 0.0 freezes the channel.
+	# Ignored by null channels -- the timer queue already
+	# takes care of this.  The initial value is 0.0!!!
 	#
-	def freeze(self):
+	def setrate(self, rate):
 		pass
 	#
-	def unfreeze(self):
-		pass
+	# Stop the current event immediately.
 	#
 	def stop(self):
 		if self.qid <> None:
-			self.player.queue.cancel(self.qid)
+			self.player.cancel(self.qid)
 			self.qid = None
+	#
+	# Reset the channel's state.
+	# This should only be called in stopped state.
+	#
+	def reset(self):
+		pass
 	#
