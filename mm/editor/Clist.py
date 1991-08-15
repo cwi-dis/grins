c612bb0897b5a37cdf03321ad2697b7b6eb0970f
Guido van Rossum <guido@python.org>
1991-08-15 13:06:43 +0000

    Initial revision

---
+++ mm/editor/Clist.py
@@ -0,0 +1,45 @@
+# Channel list editor
+
+# This is not the channel view, but lets you edit the collection of
+# channels and their attributes.
+
+import gl
+import fl
+from FL import *
+import glwindow
+from MMExc import *
+import AttrEdit
+
+class Clist():
+	#
+	def init(self, context):
+		self.context = context
+		self.makeform()
+		return self
+	#
+	def makeform(self):
+		nchannels = len(self.context.channelnames)
+		if nchannels = 0:
+			print 'No channels!'
+			raise ExitException, 1
+		#
+		form = fl.make_form(FLAT_BOX, nchannels*100, 100)
+		#
+		self.buttons = []
+		x, y, w, h = 0, 0, 100, 100
+		for name in self.context.channelnames:
+			b = form.add_button(NORMAL_BUTTON,x,y,w,h, name)
+			b.set_call_back(self.callback, name)
+			x = x + 100
+			self.buttons.append(b)
+		#
+		self.editors = {}
+		#
+		form.show_form(PLACE_SIZE, TRUE, 'Channel List Editor')
+		self.form = form
+	#
+	def callback(self, (obj, name)):
+		if AttrEdit.haschannelattreditor(self.context, name):
+			gl.ringbell()
+		else:
+			AttrEdit.showchannelattreditor(self.context, name)
