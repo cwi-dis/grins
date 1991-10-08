db663b1a9043769e39b3446d316fce9f93932992
Guido van Rossum <guido@python.org>
1991-10-08 15:54:58 +0000

    Move try... statement into main() function.

--- mm/editor/main.py
+++ mm/editor/main.py
@@ -1,9 +1,7 @@
-# Integration -- main program for testing
+# Main program for the CMIF editor
 
-import gl
-gl.foreground()
 import sys
-from MMExc import *
+import MMExc
 import TopLevel
 import SoundChannel
 
@@ -15,17 +13,19 @@ def main():
 	#
 	top = TopLevel.TopLevel().init(filename)
 	top.show()
-	top.run()
+	#
+	try:
+		top.run()
+		top.destroy()
+		status = 0
+	except MMExc.ExitException, status:
+		if status <> 0: print 'Exit status', status
+	except KeyboardInterrupt:
+		print 'Interrupt.'
+		status = 1
+	finally:
+		SoundChannel.restore()
+	#
+	sys.exit(status)
 
-try:
-	main()
-	status = 0
-except ExitException, status:
-	if status <> 0: print 'Exit status', status
-except KeyboardInterrupt:
-	print 'Interrupt.'
-	status = 1
-finally:
-	SoundChannel.restore()
-
-sys.exit(status)
+main()
