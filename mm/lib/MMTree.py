02430f75c41ce6d6eb893a79b49c65e5877a73bf
Guido van Rossum <guido@python.org>
1991-10-08 09:14:26 +0000

    Added functions to read a node in an already established context. (See MMRead.py for a problem.)

---
+++ mm/lib/MMTree.py
@@ -0,0 +1,6 @@
+# MMTree -- Multimedia tree interface
+
+from MMExc import *		# Exceptions
+from MMRead import ReadFile, ReadFileContext, \
+	ReadOpenFile, ReadOpenFileContext, ReadString, ReadStringContext
+from MMWrite import WriteFile, WriteOpenFile
