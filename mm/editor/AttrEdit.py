17b43385940a3a81ad3848cbf90b5183b5606db9
Guido van Rossum <guido@python.org>
1991-10-10 16:46:07 +0000

    Minor improvements in the user interface, and added style editors.

--- mm/editor/AttrEdit.py
+++ mm/editor/AttrEdit.py
@@ -88,6 +88,39 @@ def haschannelattreditor(context, name):
 	return dict[name].showing
 
 
+# A similar interface for styles (note different arguments!).
+# Note that the administration is in context.styleattreditors,
+# which is created here if necessary.
+#
+def showstyleattreditor(context, name):
+	try:
+		dict = context.styleattreditors
+	except NameError:
+		dict = context.styleattreditors = {}
+	if not dict.has_key(name):
+		dict[name] = \
+			AttrEditor().init(StyleWrapper().init(context, name))
+	dict[name].open()
+
+def hidestyleattreditor(context, name):
+	try:
+		dict = context.styleattreditors
+	except NameError:
+		return
+	if not dict.has_key(name):
+		return
+	dict[name].close()
+
+def hasstyleattreditor(context, name):
+	try:
+		dict = context.styleattreditors
+	except NameError:
+		return 0
+	if not dict.has_key(name):
+		return 0
+	return dict[name].showing
+
+
 # The "Wrapper" classes encapsulate the differences between attribute
 # editors for nodes and channels.  If you want editors for other
 # attribute collections (styles!) you may want to new wrappers.
@@ -101,21 +134,22 @@ class Wrapper(): # Base class -- common operations
 	def getcontext(self):
 		return self.context
 	def register(self, object):
-		self.context.geteditmgr().register(object)
+		self.editmgr.register(object)
 	def unregister(self, object):
-		self.context.geteditmgr().unregister(object)
+		self.editmgr.unregister(object)
 	def transaction(self):
-		return self.context.geteditmgr().transaction()
+		return self.editmgr.transaction()
 	def commit(self):
-		self.context.geteditmgr().commit()
+		self.editmgr.commit()
 	def rollback(self):
-		self.context.geteditmgr().rollback()
+		self.editmgr.rollback()
 
 class NodeWrapper() = Wrapper():
 	#
 	def init(self, node):
 		self.node = node
 		self.context = node.GetContext()
+		self.editmgr = self.context.geteditmgr()
 		self.root = node.GetRoot()
 		return self
 	#
@@ -124,7 +158,7 @@ class NodeWrapper() = Wrapper():
 	#
 	def maketitle(self):
 		name = MMAttrdefs.getattr(self.node, 'name')
-		return 'Node attributes for ' + name
+		return 'Attributes for node: ' + name
 	#
 	def getattr(self, name): # Return the attribute or a default
 		return MMAttrdefs.getattr(self.node, name)
@@ -132,23 +166,20 @@ class NodeWrapper() = Wrapper():
 	def getvalue(self, name): # Return the raw attribute or None
 		return self.node.GetRawAttrDef(name, None)
 	#
-	def getdefault(self, name):
+	def getdefault(self, name): # Return the default or None
 		return MMAttrdefs.getdefattr(self.node, name)
 	#
 	def setattr(self, (name, value)):
-		self.node.SetAttr(name, value)
+		self.editmgr.setnodeattr(self.node, name, value)
 	#
 	def delattr(self, name):
-		try:
-			self.node.DelAttr(name)
-		except MMExc.NoSuchAttrError:
-			pass
+		self.editmgr.setnodeattr(self.node, name, None)
 	#
 	# Return a list of attribute names that make sense for this node,
 	# in an order that makes sense to the user.
 	#
 	def attrnames(self):
-		namelist = ['name', 'channel', 'duration']
+		namelist = ['name', 'channel']
 		try:
 			# Get the channel class (should be a subroutine!)
 			cname = MMAttrdefs.getattr(self.node, 'channel')
@@ -157,12 +188,17 @@ class NodeWrapper() = Wrapper():
 			cclass = channelmap[ctype]
 			# Add the class's declaration of attributes
 			namelist = namelist + cclass.node_attrs
+			for name in cclass.chan_attrs:
+				if name in namelist: continue
+				if MMAttrdefs.getdef(name)[5] = 'channel':
+					namelist.append(name)
+			namelist = namelist + cclass.node_attrs
 		except:
 			pass # Ignore errors in the above
-		# Merge in nonstandard attributes
+		# Merge in nonstandard attributes (except synctolist!)
 		extras = []
 		for name in self.node.GetAttrDict().keys():
-			if name not in namelist:
+			if name not in namelist and name <> 'synctolist':
 				extras.append(name)
 		extras.sort()
 		return namelist + extras
@@ -173,6 +209,7 @@ class ChannelWrapper() = Wrapper():
 	#
 	def init(self, (context, name)):
 		self.context = context
+		self.editmgr = context.geteditmgr()
 		self.name = name
 		self.attrdict = self.context.channeldict[name]
 		return self
@@ -182,7 +219,7 @@ class ChannelWrapper() = Wrapper():
 			self.context.channeldict[self.name] = self.attrdict
 	#
 	def maketitle(self):
-		return 'Channel attributes for ' + self.name
+		return 'Attributes for channel: ' + self.name
 	#
 	def getattr(self, name):
 		if self.attrdict.has_key(name):
@@ -196,21 +233,20 @@ class ChannelWrapper() = Wrapper():
 		else:
 			return None
 	#
-	def getdefault(self, name):
+	def getdefault(self, name): # Return the default or None
 		return MMAttrdefs.getdef(name)[1]
 	#
 	def setattr(self, (name, value)):
-		self.attrdict[name] = value
+		self.editmgr.setchannelattr(self.name, name, value)
 	#
 	def delattr(self, name):
-		if self.attrdict.has_key(name):
-			del self.attrdict[name]
+		self.editmgr.setchannelattr(self.name, name, None)
 	#
 	# Return a list of attribute names that make sense for this channel,
 	# in an order that makes sense to the user.
 	#
 	def attrnames(self):
-		namelist = ['type', 'duration']
+		namelist = ['type']
 		try:
 			ctype = self.attrdict['type']
 			cclass = channelmap[ctype]
@@ -218,8 +254,69 @@ class ChannelWrapper() = Wrapper():
 			namelist = namelist + cclass.chan_attrs
 			for name in cclass.node_attrs:
 				if name in namelist: continue
-				attrdef = MMAttrdefs.getdef(name)
-				if attrdef[5] = 'channel':
+				if MMAttrdefs.getdef(name)[5] = 'channel':
+					namelist.append(name)
+		except:
+			pass # Ignore errors in the above
+		# Merge in nonstandard attributes
+		extras = []
+		for name in self.attrdict.keys():
+			if name not in namelist:
+				extras.append(name)
+		extras.sort()
+		return namelist + extras
+	#
+
+
+class StyleWrapper() = Wrapper():
+	#
+	def init(self, (context, name)):
+		self.context = context
+		self.editmgr = context.geteditmgr()
+		self.name = name
+		self.attrdict = self.context.styledict[name]
+		return self
+	#
+	def stillvalid(self):
+		return self.context.styledict.has_key(self.name) and \
+			self.context.styledict[self.name] = self.attrdict
+	#
+	def maketitle(self):
+		return 'Attributes for style: ' + self.name
+	#
+	def getattr(self, name):
+		if self.attrdict.has_key(name):
+			return self.attrdict[name]
+		else:
+			return MMAttrdefs.getdef(name)[1]
+	#
+	def getvalue(self, name): # Return the raw attribute or None
+		if self.attrdict.has_key(name):
+			return self.attrdict[name]
+		else:
+			return None
+	#
+	def getdefault(self, name): # Return the default or None
+		return MMAttrdefs.getdef(name)[1]
+	#
+	def setattr(self, (name, value)):
+		self.editmgr.setstyleattr(self.name, name, value)
+	#
+	def delattr(self, name):
+		self.editmgr.setstyleattr(self.name, name, None)
+	#
+	# Return a list of attribute names that make sense for this style,
+	# in an order that makes sense to the user.
+	#
+	def attrnames(self):
+		namelist = ['type', 'channel']
+		try:
+			# Get the channel class (should be a subroutine!)
+			ctype = self.attrdict['type']
+			cclass = channelmap[ctype]
+			# Add the class's declaration of attributes
+			for name in cclass.node_attrs + cclass.chan_attrs:
+				if MMAttrdefs.getdef(name)[5] <> 'raw':
 					namelist.append(name)
 		except:
 			pass # Ignore errors in the above
@@ -290,6 +387,7 @@ class AttrEditor() = Dialog():
 	#
 	def open(self):
 		self.close()
+		self.title = self.wrapper.maketitle()
 		self.getvalues()
 		self.wrapper.register(self)
 		self.show()
@@ -297,44 +395,70 @@ class AttrEditor() = Dialog():
 	def getvalues(self):
 		self.form.freeze_form()
 		for b in self.blist: b.getvalue()
-		self.form.unfreeze_form()
 		self.changed = 0
+		self.fixbuttons()
+		self.form.unfreeze_form()
 	#
 	def setvalues(self):
-		if not self.wrapper.transaction(): return
+		if not self.wrapper.transaction(): return 0
 		self.form.freeze_form()
 		for b in self.blist: b.setvalue()
-		self.form.unfreeze_form()
 		self.changed = 0
+		self.fixbuttons()
+		self.form.unfreeze_form()
 		self.wrapper.commit()
+		return 1
 	#
 	def fixvalues(self):
-		# Get the values that have changed
+		self.fixfocus()
 		changed = 0
 		self.form.freeze_form()
 		for b in self.blist:
 			b.fixvalue()
 			if b.changed: changed = 1
-		self.form.unfreeze_form()
 		self.changed = changed
+		self.fixbuttons()
+		self.form.unfreeze_form()
 	#
 	def close(self):
 		if self.showing:
 			self.wrapper.unregister(self)
 			self.hide()
 	#
+	def fixbuttons(self):
+		if self.changed:
+			boxtype = UP_BOX
+		else:
+			boxtype = FRAME_BOX
+		for b in self.ok_button,self.apply_button,self.restore_button:
+			b.boxtype = boxtype
+	#
 	def cancel_callback(self, dummy):
 		self.close()
 	#
-	def restore_callback(self, dummy):
+	def restore_callback(self, (obj, arg)):
+		obj.set_button(1)
 		self.getvalues()
+		obj.set_button(0)
 	#
-	def apply_callback(self, dummy):
-		self.setvalues()
+	def apply_callback(self, (obj, arg)):
+		obj.set_button(1)
+		self.fixfocus()
+		if self.changed:
+			dummy = self.setvalues()
+		obj.set_button(0)
 	#
-	def ok_callback(self, dummy):
-		self.setvalues()
-		self.close()
+	def ok_callback(self, (obj, arg)):
+		obj.set_button(1)
+		self.fixfocus()
+		if not self.changed or self.setvalues():
+			self.close()
+		obj.set_button(0)
+	#
+	def fixfocus(self):
+		for b in self.blist:
+			if b.value.focus:
+				b.valuecallback(b.value, None)
 	#
 
 
@@ -353,19 +477,19 @@ class ButtonRow():
 		attrdef = MMAttrdefs.getdef(b.name)
 		labeltext = attrdef[2]
 		if labeltext = '': labeltext = b.name
-		b.label = b.form.add_text(NORMAL_TEXT, x, y, w, h, labeltext)
+		b.label = b.form.add_text(NORMAL_TEXT, x, y, w-1, h, labeltext)
 	#
 	def makehelpbutton(b, (x, y, w, h)):
-		b.help = b.form.add_button(HIDDEN_BUTTON, x, y, w, h, '')
+		b.help = b.form.add_button(HIDDEN_BUTTON, x, y, w-1, h, '')
 		b.help.set_call_back(b.helpcallback, None)
 	#
 	def makevalueinput(b, (x, y, w, h)):
-		b.value = b.form.add_input(NORMAL_INPUT, x, y, w, h, '')
+		b.value = b.form.add_input(NORMAL_INPUT, x, y, w-1, h, '')
 		b.value.lstyle = FIXED_STYLE
 		b.value.set_call_back(b.valuecallback, None)
 	#
 	def makeresetbutton(b, (x, y, w, h)):
-		b.reset = b.form.add_button(PUSH_BUTTON, x, y, w, h, 'reset')
+		b.reset = b.form.add_button(PUSH_BUTTON, x, y, w-1, h, 'reset')
 		b.reset.set_call_back(b.resetcallback, None)
 	#
 	def getvalue(b):
@@ -383,8 +507,6 @@ class ButtonRow():
 		b.update()
 	#
 	def setvalue(b):
-		if b.value.focus:
-			b.valuecallback(b.value, None)
 		if b.changed:
 			if b.isdefault:
 				b.wrapper.delattr(b.name)
@@ -393,8 +515,6 @@ class ButtonRow():
 		b.getvalue()
 	#
 	def fixvalue(b):
-		if b.value.focus:
-			b.valuecallback(b.value, None)
 		if b.isdefault and b.changed:
 			b.currentvalue = b.wrapper.getdefault(b.name)
 			b.update()
@@ -429,6 +549,7 @@ class ButtonRow():
 		b.currentvalue = value
 		b.isdefault = 0
 		b.update()
+		b.attreditor.fixbuttons()
 	#
 	def resetcallback(b, dummy):
 		b.changed = b.attreditor.changed = 1
@@ -439,6 +560,7 @@ class ButtonRow():
 		else:
 			b.currentvalue = b.lastvalue
 		b.update()
+		b.attreditor.fixbuttons()
 	#
 	def update(b):
 		b.currenttext = b.valuerepr(b.currentvalue)
@@ -447,6 +569,10 @@ class ButtonRow():
 		if b.isdefault and not b.changed:
 			b.reset.hide_object()
 		else:
+			if b.changed:
+				b.reset.boxtype = UP_BOX
+			else:
+				b.reset.boxtype = FRAME_BOX
 			b.reset.show_object()
 	#
 	def valuerepr(b, value):
@@ -463,8 +589,10 @@ class ButtonRow():
 def closeall(root):
 	closenode(root)
 	context = root.GetContext()
-	for cname in root.GetContext().channelnames:
+	for cname in root.GetContext().channeldict.keys():
 		hidechannelattreditor(context, cname)
+	for cname in root.GetContext().styledict.keys():
+		hidestyleattreditor(context, cname)
 
 
 # Recursively close the attribute editor for this node and its subtree.
