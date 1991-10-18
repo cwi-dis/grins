7e46d8897f312898c10cb04375780832fa52ac11
Guido van Rossum <guido@python.org>
1991-10-18 16:42:54 +0000

    Added movie channel

--- mm/common/ChannelMap.py
+++ mm/common/ChannelMap.py
@@ -5,10 +5,12 @@ from NullChannel import NullChannel
 from TextChannel import TextChannel
 from SoundChannel import SoundChannel
 from ImageChannel import ImageChannel
+from MovieChannel import MovieChannel
 
 channelmap = { \
 	'null': 	NullChannel, \
 	'text': 	TextChannel, \
 	'sound':	SoundChannel, \
 	'image': 	ImageChannel, \
+	'movie': 	MovieChannel, \
 	}
