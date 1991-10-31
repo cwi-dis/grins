c45fde63375ff9d041f3bd0239efe6bda70fc3b3
Guido van Rossum <guido@python.org>
1991-10-31 15:13:50 +0000

    Lotsa new stuff from Jack.

--- mm/editor/BlockView.py
+++ mm/editor/BlockView.py
@@ -1,5 +1,6 @@
 import fl
 from FL import *
+import gl
 import GL
 import MMAttrdefs
 import glwindow
@@ -54,6 +55,8 @@ MMARG = 5 # margin between title button and real buttons
 BH=20	# the height of the open/close button and title
 BW=10   # width of open/close button
 
+MENUH = 20	# Height of menu bar
+
 class BlockView () = ViewDialog(), BasicDialog () :
 	#
 	# init() method compatible with the other views.
@@ -62,10 +65,37 @@ class BlockView () = ViewDialog(), BasicDialog () :
 		self = ViewDialog.init(self, 'blockview_')
 		self.toplevel = toplevel
 		self.root = self.toplevel.root
+		self.editmgr = self.root.context.editmgr
 		width, height = \
 			MMAttrdefs.getattr(self.root, 'blockview_winsize')
 		self = BasicDialog.init(self, (width, height, 'Hierarchy'))
+		self.changing_node = None
+		self.clipboard = None
 		return self.new(width, height, self.root)
+	def show(self):
+		if self.showing: return
+		print 'self.show'
+		self.editmgr.register(self)
+		BasicDialog.show(self)
+	def hide(self):
+		if not self.showing: return
+		print 'self.hide'
+		self.editmgr.unregister(self)
+		BasicDialog.hide(self)
+	def transaction(self):
+		print 'self.transaction'
+		return 1
+	def commit(self):
+		print 'self.commit'
+		modnode = self.rootview
+		self.form.freeze_form ()
+		self.rmBlockview (modnode)
+		self.mkBlockview(modnode.bv_xywh, modnode)
+		self.presentlabels (modnode)
+		self.setfocus(self.focus)
+		self.form.unfreeze_form ()
+	def rollback(self):
+		print 'self.rollback'
 	#
 	# new makes an object of type 'blockview'
 	#
@@ -79,6 +109,7 @@ class BlockView () = ViewDialog(), BasicDialog () :
 	#	6. set the focus to the root.
 	#
 	def new (self, (w, h, root)) :
+		h = h - MENUH
 		self._init(w, h)
 		area = self.form.add_box (UP_BOX, 0, 0, w, h, '')
 
@@ -88,34 +119,59 @@ class BlockView () = ViewDialog(), BasicDialog () :
 		focus = self.form.add_button(HIDDEN_BUTTON, 0, 0, w, h, '')
 		focus.set_call_back(self._change_focus_callback, 0)
 		
+		self.addmenus(0,h,w,MENUH)
 		self.rootview = root
 		self._initcommanddict()
+		self.setopenclose(root, 1)
 		self.mkBlockview((0, 0, w,h), root)
 		self.presentlabels(root)
 		self.setfocus(root)
 
 		return self
+	def setopenclose(self, (node, OC)):
+		node.bv_OC = OC
+		for i in node.GetChildren():
+		    self.setopenclose(i, OC)
 	#
 	# addtocommand adds a command to the commanddictionary.
 	# Anybody can submit their own commands
 	#
-	def addtocommand(self, (key, func, helpstr)) :
-		self.commanddict[key] = (func, helpstr)
+	def addtocommand(self, (key, func)) :
+		self.commanddict[key] = func
+	def addmenus(self, (x,y,w,h)):
+		f = self.form
+		menubar = f.add_box(FLAT_BOX,x,y,w,h,'')
 
+		edit_menu = f.add_menu(PUSH_MENU,x,y,w/3,h,'Edit')
+		edit_menu.set_menu('i Insert before|a Insert after|u Insert child%l|d Delete')
+		cmdmap = 'iaud'
+		edit_menu.set_call_back(self._menu_callback,cmdmap);
+		clipboard_menu = f.add_menu(PUSH_MENU,x+w/3,y,w/3,h,'Clipboard')
+		clipboard_menu.set_menu('I Paste before|A Paste after|U Paste as child%l|D Cut')
+		cmdmap = 'IAUD'
+		clipboard_menu.set_call_back(self._menu_callback,cmdmap);
+		operation_menu = f.add_menu(PUSH_MENU,x+2*w/3,y,w/3,h,'Operation')
+		operation_menu.set_menu('h Help|p Play|+ Zoom|- Unzoom%l|o Open info|e Open attr')
+		cmdmap = 'hp+-oe'
+		operation_menu.set_call_back(self._menu_callback,cmdmap);
 	#
 	# submit a number to default commands.
 	#
 	def _initcommanddict(self) :
-		self.addtocommand('a', attreditfunc, 'attribute editor')
-		self.addtocommand('d', deleteNode, 'delete node')
-		self.addtocommand('h', helpfunc, 'help message')
-		self.addtocommand('o', infofunc, 'open node info window')
-		self.addtocommand('p', playfunc, 'play node')
-		self.addtocommand('r', rotatefunc, 'rotate node')
-		self.addtocommand('u', unzoomfunc, 'unzoom node')
-		self.addtocommand('z', zoomfunc, 'zoom node')
-		self.addtocommand('P', addParallel, 'add parallel node')
-		self.addtocommand('S', addSequential, 'add sequential node')
+		self.addtocommand('e', attreditfunc)
+		self.addtocommand('a', InsertAfterNode)
+		self.addtocommand('i', InsertBeforeNode)
+		self.addtocommand('u', InsertChildNode)
+		self.addtocommand('d', DeleteNode)
+		self.addtocommand('A', CInsertAfterNode)
+		self.addtocommand('I', CInsertBeforeNode)
+		self.addtocommand('U', CInsertChildNode)
+		self.addtocommand('D', CDeleteNode)
+		self.addtocommand('h', helpfunc)
+		self.addtocommand('o', infofunc)
+		self.addtocommand('p', playfunc)
+		self.addtocommand('-', unzoomfunc)
+		self.addtocommand('+', zoomfunc)
 	#
 
 	# blockview gets a region in the form where it recursively
@@ -127,12 +183,14 @@ class BlockView () = ViewDialog(), BasicDialog () :
 		obj = self.form.add_box (UP_BOX, x, y, w, h, '')
 		obj.boxtype = FRAME_BOX
 
-		node.bv_OC 	= 1		# block open/closed toggle
 		node.bv_form 	= self.form
 		node.bv_obj	= obj
 	
 		kids = node.GetChildren()
-		if type in ('seq','par','grp') and len(kids) > 0:
+		if type in ('seq','par','grp'):
+			node.bv_xywh    = (x,y,w,h)
+			if node.bv_OC < 0:
+			     node.bv_OC = 1
 			# Create the open/close button:
 			bx = x + LMARG
 			by = y + h - TMARG - BH
@@ -151,43 +209,51 @@ class BlockView () = ViewDialog(), BasicDialog () :
 			o.align = ALIGN_CENTER
 			node.bv_labeltext = o
 			# Create childrens' boxes
-			h = h - MMARG - BH
-			if type in ('grp', 'seq') :
-				h = h / len(kids)
-				dx, dy = 0, h
-				toosmall = (h < TMARG+BMARG+BH)
-			else: 				 # parallel node
-				w = w / len(kids)
-				dx, dy = w, 0
-				toosmall = (w < LMARG+RMARG+BW)
-			if toosmall:
-			    node.bv_OC = 0
-			    node.bv_openclose.col1 = GL.RED
-			    node.bv_toosmall = 1
-			    x, y = 0, 0
-			    w, h = 1, 1
-			else:
-			    x,y = x+LMARG,y+BMARG
-			    w,h = w-LMARG-RMARG,h-TMARG-BMARG
-			num = 1
-			if node.GetType() = 'seq':
-				kids = kids[:]
-				kids.reverse()
-			for child in kids :
-				self.mkBlockview(((x, y, w, h), child))
-				x, y = x + dx, y + dy
-				num = num + 1
+			if kids:
+			    h = h - MMARG - BH
+			    if type in ('grp', 'seq') :
+				    h = h / len(kids)
+				    dx, dy = 0, h
+			    else: 				 # parallel node
+				    w = w / len(kids)
+				    dx, dy = w, 0
+			    toosmall = ((h < TMARG+BMARG+BH) or \
+					(w < LMARG+RMARG+BW))
+			    if toosmall:
+				node.bv_OC = 0
+				node.bv_openclose.col1 = GL.RED
+				node.bv_toosmall = 1
+				x, y = 0, 0
+				w, h = 1, 1
+			    elif not node.bv_OC:
+				o = node.bv_openclose
+				o.col1, o.col2 = o.col2, o.col1
+			    else:
+				x,y = x+LMARG,y+BMARG
+				w,h = w-LMARG-RMARG,h-TMARG-BMARG
+			    if node.bv_OC:
+				if node.GetType() = 'seq':
+				    kids = kids[:]
+				    kids.reverse()
+				for child in kids :
+				    self.mkBlockview(((x, y, w, h), child))
+				    x, y = x + dx, y + dy
+			    else:
+				self.setopenclose(node,node.bv_OC)
 
 	#
 	# delete all the objects (made by blockview) form the node
 	#
 	def rmBlockview (self, node) :
+		if node.bv_OC = -1:
+		    node.bv_OC = node.GetParent().bv_OC
+		    return
 		if node.GetType () in ('seq', 'par', 'grp'):
-			node.bv_openclose.hide_object ()	# rm_object()
+			node.bv_openclose.delete_object ()
 			del node.bv_openclose
-			node.bv_labeltext.hide_object()
+			node.bv_labeltext.delete_object()
 			del node.bv_labeltext
-		node.bv_obj.hide_object ()	# should be rm_object()
+		node.bv_obj.delete_object ()
 		del node.bv_obj
 		if node.bv_OC:
 		    for child in node.GetChildren () :
@@ -214,7 +280,6 @@ class BlockView () = ViewDialog(), BasicDialog () :
 		self.form.freeze_form()
 		self.focus.bv_obj.boxtype = FRAME_BOX
 
-		import gl
 		gl.winset(self.form.window)
 		mx, my = fl.get_mouse ()
 
@@ -250,26 +315,33 @@ class BlockView () = ViewDialog(), BasicDialog () :
 				return  self._find_node (child, (x, y))
 		return node
 	#
+	def _menu_callback(self, (obj,args)):
+		index = obj.get_menu()
+		if index:
+		    self._do_command(args[index-1])
+	#
 	# command_callback : read which key is clicked and executes
 	# the associated command.
 	#
 	def _command_callback(self, (obj, args)) :
 		key = obj.get_default()
+		self._do_command(key)
+	def _do_command(self,key):
 		if self.commanddict.has_key (key) :
-			self.commanddict[key][0](self)
+			self.commanddict[key](self)
 		else :
 			if fl.show_question ('Unknown command',key, \
 						'Do you want help?'):
 			    self.toplevel.help.givehelp('Hierarchy', \
-					'Keyboard commands')
+					'Commands')
 
 	def _openclose_callback (self, (obj, node)) :
 		if node.bv_toosmall: return
 		node.bv_form.freeze_form ()
 
+		self.rmBlockview(node)
 		node.bv_OC = (not node.bv_OC)		# toggle open/close
-		obj.col1, obj.col2 = obj.col2, obj.col1 # swap: red <--> green
-		self._openclose (node, node.bv_OC)	# recursive
+		self.mkBlockview(node.bv_xywh,node)
 		node.bv_obj.show_object ()		# show this node
 		node.bv_openclose.show_object ()	# show roundbutton
 		node.bv_labeltext.show_object()
@@ -277,28 +349,31 @@ class BlockView () = ViewDialog(), BasicDialog () :
 
 		node.bv_form.unfreeze_form ()		
 		
-	def _openclose (self, (node, toggle)) :
-		if toggle <> 0 :
-			node.bv_obj.show_object ()
-			if node.GetType () in ('seq', 'par', 'grp') :
-				node.bv_openclose.show_object ()
-				node.bv_labeltext.show_object ()
-				if node.bv_OC = 0 : return
-		else :
-			node.bv_obj.hide_object ()
-			if node.GetType () in ('seq', 'par', 'grp') :
-				node.bv_openclose.hide_object ()
-				node.bv_labeltext.hide_object ()
-		
-		for child in node.GetChildren () :
-			self._openclose (child, toggle)
-		
 	def _in_bounds (self, (node, (x, y))) :
 		o = node.bv_obj
 		if  x > o.x and x < o.x+o.w and y > o.y and y < o.y+o.h :
 			return node
 		else :
 			return None
+	#
+	# GetNewNode - Get new node
+	#
+	def GetNewNode(self) :
+		type = self.focus.GetType()
+		child = self.root.context.newnode(type)
+		child.bv_OC = -1	# Signal that it is new
+		return child
+	#
+	def fromclipboard(self):
+		rv = self.clipboard
+		self.clipboard = None
+		if rv <> None:
+		    rv.bv_OC = -1
+		return rv
+	def toclipboard(self, node):
+		if self.clipboard <> None:
+			self.clipboard.Destroy()
+		self.clipboard = node
 
 def helpfunc (bv) :
 	bv.toplevel.help.givehelp('Hierarchy')
@@ -313,208 +388,120 @@ def attreditfunc (bv) :
 # delete the focussed node.
 # focus switches to parent.
 # 
-def deleteNode (bv) :
+def _DeleteNode (bv,cb) :
 	node = bv.focus
-	
+	em = bv.editmgr
 	parent = node.GetParent ()
 	if parent = None :
 		fl.show_message ('sorry, cannot delete root','','')
 		return
-
-	if not node.context.editmgr.transaction(): return
-	# XXX Should use editmgr to make the actual changes as well!
-	# XXX Shouldn't fix the display here!
-
-	pObj = parent.bv_obj
-	x, y, w, h = pObj.x, pObj.y, pObj.w, pObj.h
-	node.Extract ()
-	bv.form.freeze_form ()
-	bv.rmBlockview (parent)	# yes, from the parent !
-	node.Destroy ()
 	#
-	bv.mkBlockview((x, y,w,h),parent)
-	bv.presentlabels (parent)
-	bv.setfocus(parent)
+	if not em.transaction():
+	    print 'Sorry, no trans'
+	    return
 	#
-	# if an interior node has no children the delete it as well.
-	# this is questionable ....
+	# Find out new focus. 
+	# Order of tries: next sibling; previous sibling; parent
 	#
-	if len (parent.GetChildren()) = 0 : deleteNode(bv)
+	children = parent.GetChildren()
+	nf = children.index(node)
+	if nf = len(children)-1:
+		nf = nf - 1
 	#
-	bv.form.unfreeze_form ()
-
+	# And zap the node and move to the clipboard
+	#
+	print 'nf=', nf
+	bv.form.freeze_form()
+	bv.rmBlockview(node)
+	em.delnode(node)
+	if cb:
+	    bv.toclipboard(node)
+	else:
+	    node.Destroy()
+	#
+	if nf:
+		bv.focus = children[nf]
+	else:
+		bv.focus = parent
+	#
+	bv.changing_node = parent
+	bv.form.unfreeze_form()
 	node.context.editmgr.commit()
-
-def mkNode(node) :
-	context = node.GetContext()
-	child = context.newnode('imm')
-	return child
-	
-def addChild (node) :
-	child = mkNode(node)
-	child.AddToTree(node,100000) # at the end of the children
-	return child
-
+	bv.changing_node = None
+def DeleteNode (bv):
+	_DeleteNode(bv,0)
+def CDeleteNode (bv):
+	_DeleteNode(bv,1)
 #
-# rotate a node around with its right anscestor.
+# Insert a node before/after the focus. The node will be sequential without
+# children, by default. Use Node Info to change this.
 #
-
-def rotatefunc (bv) :
+def _doInsertNode (bv, after, cb) :
 	node = bv.focus
 	parent = node.GetParent()
+	em = bv.editmgr
 	if parent = None : return
 
-	if not node.context.editmgr.transaction(): return
-	# XXX Should use editmgr to make the actual changes as well!
-	# XXX Shouldn't fix the display here!
-	
-	nObj = parent.bv_obj
-	x, y, w, h = nObj.x, nObj.y, nObj.w, nObj.h
+	if not em.transaction(): return
+
 	kids = parent.GetChildren()
+	i = kids.index(node)
+	if after:
+		i = i + 1
 	
-	if parent.GetChild(len(kids) - 1) = node :
-		kids[1:] = kids [0:len(kids)-1]
-		kids[0] = node
-	else :
-		for i in range(0, len(kids)) :
-			if kids[i] = node :
-				kids[i], kids[i+1] = kids[i+1], kids[i]
-				break
-	bv.form.freeze_form ()
-	bv.rmBlockview (parent)		# get rid of all FORMS objects
-	#
-	bv.mkBlockview((x, y, w, h), parent)
-	bv.presentlabels (parent)
-	bv.setfocus(node)
-	bv.form.unfreeze_form ()
+	if cb:
+	    newnode = bv.fromclipboard()
+	else:
+	    newnode = bv.GetNewNode()
+	if newnode = None:
+	    gl.ringbell()
+	    node.context.editmgr.rollback()
+	    return
+	em.addnode(parent, i, newnode)
+	bv.focus = newnode
 
+	bv.changing_node = parent
 	node.context.editmgr.commit()
+	bv.changing_node = None
+def InsertBeforeNode(bv):
+	_doInsertNode(bv, 0, 0)
+def CInsertBeforeNode(bv):
+	_doInsertNode(bv, 0, 1)
+def InsertAfterNode(bv):
+	_doInsertNode(bv, 1, 0)
+def CInsertAfterNode(bv):
+	_doInsertNode(bv, 1, 1)
 
 #
-# add a sequential node to the focus.
-# The stratagy is :
-#	0. if the focus is a paralle node-> change the focus to sequntial
-#	1. if the focus is a sequential node-> add node to the focus' children
-#	2. if the focus is a leaf (call it 'a')-> make a tree: ; (a, b)
-#		in which ; is a sequential node, and 'b' is a new node.
+# Add a child node to a (possibly empty) seq/par node.
 #
+def _InsertChildNode (bv,cb) :
+	parent = bv.focus
+	em = bv.editmgr
 
-def addSequential (bv) :
-	node = bv.focus
+	if not parent.GetType() in ('seq','par'):
+	    gl.ringbell()
+	    return
+	if not em.transaction(): return
 
-	if not node.context.editmgr.transaction(): return
-	# XXX Should use editmgr to make the actual changes as well!
-	# XXX Shouldn't fix the display here!
+	if cb:
+	    newnode = bv.fromclipboard()
+	else:
+	    newnode = bv.GetNewNode()
+	if newnode = None:
+	    gl.ringbell()
+	    node.context.editmgr.rollback()
+	    return
+	em.addnode(parent, 0, newnode)
 
-	nObj = node.bv_obj
-	x, y, w, h = nObj.x, nObj.y, nObj.w, nObj.h
-	bv.form.freeze_form ()
-	bv.rmBlockview (node)		# get rid of all FORMS objects
-
-	# case 0
-	if node.GetType () = 'par' :
-		node.SetType('seq')
-		#
-		bv.mkBlockview((x, y, w, h), node)
-		bv.presentlabels (node)
-		bv.setfocus(node)
-		bv.form.unfreeze_form ()
-	# case 1
-	elif len(node.GetChildren()) > 0 :
-		#
-		child = addChild(node)
-		#
-		bv.mkBlockview((x, y, w, h),node)
-		bv.presentlabels (node)
-		bv.setfocus(child)
-		bv.form.unfreeze_form ()
-	# case 2
-	else :
-		#
-		seq = mkNode (node)
-		seq.SetType('seq')
-		parent = node.GetParent ()
-		if parent = None :
-			seq.AddToTree(node, 0)
-		else :
-			for i in range (len(parent.GetChildren ())) :
-				if node = parent.GetChild(i) : break
-			node.Extract ()
-			seq.AddToTree(parent, i)
-			node.AddToTree(seq, 0)
-		#
-		child = addChild (seq)
-		#
-		bv.mkBlockview((x, y, w, h), seq)
-		bv.presentlabels (seq)
-		bv.setfocus(child)
-		bv.form.unfreeze_form ()
-
-	node.context.editmgr.commit()
-
+	bv.changing_node = parent
+	parent.context.editmgr.commit()
+	bv.changing_node = None
+def InsertChildNode(bv):
+	_InsertChildNode(bv,0)
+def CInsertChildNode(bv):
+	_InsertChildNode(bv,1)
 #
-# add a parallel node to the focus.
-# The stratagy is :
-#	0. if the focus is a seq node-> change the focus to par
-#	1. if the focus is a parallel node-> add node to the focus' children
-#	2. if the focus is a leaf (call it 'a')-> make a tree: | (a, b)
-#		in which ; is a parallel node, and 'b' is a new node.
-#
-
-def addParallel (bv) :
-	node = bv.focus
-
-	if not node.context.editmgr.transaction(): return
-	# XXX Should use editmgr to make the actual changes as well!
-	# XXX Shouldn't fix the display here!
-
-	nObj = node.bv_obj
-	x, y, w, h = nObj.x, nObj.y, nObj.w, nObj.h
-
-	bv.form.freeze_form ()
-	bv.rmBlockview (node)		# get rid of all FORMS objects
-
-	# case 0.
-	if node.GetType () = 'seq' :
-		node.SetType('par')
-		#
-		bv.mkBlockview((x, y, w, h), node)
-		bv.presentlabels (node)
-		bv.setfocus(node)
-		bv.form.unfreeze_form ()
-	# case 1
-	elif len(node.GetChildren()) > 0 :
-		#
-		child = addChild(node)
-		#
-		bv.mkBlockview((x, y, w, h), node)
-		bv.presentlabels (node)
-		bv.setfocus(child)
-		bv.form.unfreeze_form ()
-	# case 2
-	else :
-		#
-		par = mkNode (node)
-		par.SetType('par')
-		parent = node.GetParent ()		
-		if parent = None :
-			par.AddToTree(node, 0)
-		else :
-			for i in range (len(parent.GetChildren ())) :
-				if node = parent.GetChild(i) : break
-			node.Extract ()
-			par.AddToTree(parent, i)
-			node.AddToTree(par, 0)
-		#
-		child = addChild (par)
-		#
-		bv.mkBlockview((x, y, w, h), par)
-		bv.presentlabels (parent)
-		bv.setfocus(child)
-		bv.form.unfreeze_form ()
-
-	node.context.editmgr.commit()
-
 def unzoomfunc (bv) :
 	if bv.rootview = bv.root : return
 
@@ -533,7 +520,9 @@ def zoomfunc (bv) :
 
 	while node.GetParent() <> bv.rootview :
 		node = node.GetParent()
-		if node = None : return
+		if node = None :
+		    gl.ringbell()
+		    return
 
 	nObj = node.GetParent().bv_obj
 	x, y, w, h = nObj.x, nObj.y, nObj.w, nObj.h
