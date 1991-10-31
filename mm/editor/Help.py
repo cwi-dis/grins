07c6a3b4509b3d0258b892d50012778cb81fca9c
Guido van Rossum <guido@python.org>
1991-10-31 15:15:04 +0000

    *** empty log message ***

--- mm/editor/Help.py
+++ mm/editor/Help.py
@@ -14,6 +14,7 @@ class HelpWindow() = ViewDialog(), BasicDialog():
     	self.root = toplevel.root # For ViewDialog
 	self.dirname = dirname + '/'
 	self.topics = None
+	self.return_stack = []
 	return self
     def inittopics(self):
 	if self.topics <> None: return
@@ -35,12 +36,20 @@ class HelpWindow() = ViewDialog(), BasicDialog():
     def make_form(self):
 	# This sets self.topic and self.help:
 	self.form = help_form.mk_form_help_form(self)
+	self.button_return.hide_object()
     def givehelp(self, topic):
 	self.inittopics()
 	if type(topic) = type(()):
 	    topic, subtopic = topic
 	else:
 	    subtopic = ''
+	#
+	# Yucky: if the subtopic is numeric we assume that this
+	# is a return, and we don't stack the current help page.
+	if self.curtopic <> '' and type(subtopic) <> type(1):
+	    num = self.help.get_browser()
+	    self.return_stack.insert(0,(self.curtopic,num))
+	    self.button_return.show_object()
 	for i in range(0,len(self.topics)):
 	    if self.topics[i] = topic:
 		self.topic.deselect_browser()
@@ -49,7 +58,10 @@ class HelpWindow() = ViewDialog(), BasicDialog():
 		self.help.load_browser(self.dirname + topic)
 		self.curtopic = topic
 		if subtopic:
-                    j = findtopicline(self.dirname + topic, subtopic)
+		    if type(subtopic) = type(1):
+			j = subtopic
+		    else:
+			j = findtopicline(self.dirname + topic, subtopic)
                     if j:
                         self.help.set_browser_topline(j)
                         self.help.select_browser_line(j)
@@ -62,6 +74,13 @@ class HelpWindow() = ViewDialog(), BasicDialog():
 	else:
 	    self.help.clear_browser()
 	    self.show()
+    def cb_return(self, arg):
+	if self.return_stack:
+	    topic = self.return_stack[0]
+	    del self.return_stack[0]
+	    if not self.return_stack:
+		self.button_return.hide_object()
+	    self.givehelp(topic)
     def cb_topic(self, arg):
 	i = arg[0].get_browser()
 	if i = 0:
