ba582f7002e29952255d6e0d263f6b1f32bd7749
Guido van Rossum <guido@python.org>
1991-11-27 18:13:52 +0000

    Changes again...

--- mm/editor/NodeEditForm.py
+++ mm/editor/NodeEditForm.py
@@ -16,16 +16,19 @@ def mk_form_convertform (mod):
 	button_cancel.set_call_back(mod.callback_cancel, 0)
 	mod.button_cancel = button_cancel
 	#
-	button_convert = convertform.add_button(NORMAL_BUTTON,10.0,50.0,90.0,30.0,'Convert')
-	button_convert.set_call_back(mod.callback_convert, 0)
-	mod.button_convert = button_convert
-	#
-	input_filename = convertform.add_input(NORMAL_INPUT,180.0,50.0,270.0,30.0,'Filename:')
-	input_filename.set_call_back(mod.callback_dummy, 0)
+	input_filename = convertform.add_input(ALWAYS_INPUT,180.0,50.0,270.0,30.0,'Filename:')
+	input_filename.set_call_back(mod.callback_input, 0)
 	mod.input_filename = input_filename
 	#
 	obj = convertform.add_text(NORMAL_TEXT,10.0,90.0,440.0,30.0,'This is an internal node. Convert to external?')
 	obj.align = ALIGN_CENTER
+	#
+	button_convert = convertform.add_button(NORMAL_BUTTON,10.0,50.0,90.0,30.0,'Convert')
+	button_convert.set_call_back(mod.callback_convert, 0)
+	mod.button_convert = button_convert
+	#
+	obj = convertform.add_button(NORMAL_BUTTON,110.0,10.0,90.0,30.0,'Browser...')
+	obj.set_call_back(mod.callback_browser, 0)
 	#
 	return convertform
 
@@ -36,10 +39,10 @@ def mk_form_err (mod,err):
 	#
 	obj = err.add_box(NO_BOX,0.0,0.0,460.0,40.0,'')
 	#
-	obj = err.add_text(NORMAL_TEXT,120.0,10.0,220.0,30.0,'File already exists!')
+	obj = err.add_text(NORMAL_TEXT,200.0,10.0,140.0,30.0,'File already exists!')
 	#
 	button_overwrite = err.add_button(NORMAL_BUTTON,330.0,10.0,120.0,30.0,'Overwrite')
-	button_overwrite.set_call_back(mod.callback_overwrite, 0)
+	button_overwrite.set_call_back(mod.callback_convert, 0)
 	mod.button_overwrite = button_overwrite
 	#
 	return err
