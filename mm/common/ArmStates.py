9e9ea36f4ace789f8bbcb32a79e3b7b151e89a83
Guido van Rossum <guido@python.org>
1992-03-24 17:21:13 +0000

    Initial revision

---
+++ mm/common/ArmStates.py
@@ -0,0 +1,8 @@
+# Symbolic constants for arm states
+
+ARM_NONE      = ''
+ARM_SCHEDULED = 'scheduled'
+ARM_ARMING    = 'arming'
+ARM_ARMED     = 'armed'
+ARM_PLAYING   = 'playing'
+ARM_DONE      = ARM_NONE
