3c3057262fffb92d99352c1fad97b5c17d029785
Guido van Rossum <guido@python.org>
1992-03-30 16:25:09 +0000

    Initial revision

---
+++ mm/editor/AdefDialog.py
@@ -0,0 +1,30 @@
+#
+# AdefDialog - A more-or-less modal dialog that does keep gl events
+# coming in, though, so we can define anchors.
+
+import fl
+import FL
+import flp
+
+class Struct(): pass
+data = Struct()
+data.initialized = 0
+
+def run(str):
+	if not data.initialized:
+		form = flp.parse_form('AdefDialogForm', 'form')
+		flp.create_full_form(data, form)
+		data.initialized = 1
+	data.label.label = str
+	fl.deactivate_all_forms()
+	data.form.show_form(FL.PLACE_MOUSE, 1, '')
+	obj = fl.do_forms()
+	data.form.hide_form()
+	fl.activate_all_forms()
+	if obj == data.cancel_button:
+		return 0
+	elif obj == data.ok_button:
+		return 1
+	else:
+		print 'AdefDialog: funny object returned: ', obj
+		return 0
