3ac134d9e06bbec460310b0fa1f157b218b507c5
Guido van Rossum <guido@python.org>
1992-04-02 18:45:24 +0000

    Create inner try stmt now that except and finally can't be mixed

--- mm/editor/main.py
+++ mm/editor/main.py
@@ -49,14 +49,15 @@ def main():
 	top.show()
 	#
 	try:
-		if playnow:
-			top.player.playsubtree(top.root)
-		top.run()
-		top.destroy()
-	except MMExc.ExitException, status:
-		if status <> 0: print 'Exit status', status
-	except KeyboardInterrupt:
-		print 'Interrupt.'
+		try:
+			if playnow:
+				top.player.playsubtree(top.root)
+			top.run()
+			top.destroy()
+		except MMExc.ExitException, status:
+			if status <> 0: print 'Exit status', status
+		except KeyboardInterrupt:
+			print 'Interrupt.'
 	finally:
 		try:
 			SoundChannel.restore()
