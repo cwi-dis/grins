7c30ae9c2c75f2a11d0b58cc2a2cf0ffb8bd4e93
Guido van Rossum <guido@python.org>
1992-04-02 18:36:47 +0000

    Initial revision

---
+++ mm/lib/FileCache.py
@@ -0,0 +1,32 @@
+from stat import *
+import os
+
+class FileCache:
+
+	def init(self, func):
+		self.cache = {}
+		self.func = func
+		return self
+
+	def flushall(self):
+		self.cache = {}
+
+	def flush(self, file):
+		if self.cache.has_key(file):
+			del self.cache[file]
+
+	def get(self, file):
+		try:
+			st = os.stat(file)
+			mtime, size = st[ST_MTIME], st[ST_SIZE]
+		except os.error:
+			mtime, size = None
+		if self.cache.has_key(file):
+			entry = self.cache[file]
+			mt, sz, res = entry
+			if mt == mtime and sz == size:
+				return res
+			del self.cache[file]
+		result = self.func(file)
+		self.cache[file] = mtime, size, result
+		return result
