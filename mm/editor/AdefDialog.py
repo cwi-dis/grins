10ec0a85fc372c890085507b53b2e2dbb5cc53d9
Guido van Rossum <guido@python.org>
1992-04-10 15:14:19 +0000

    Add whole-node button

--- mm/editor/AdefDialog.py
+++ mm/editor/AdefDialog.py
@@ -10,21 +10,25 @@ class Struct(): pass
 data = Struct()
 data.initialized = 0
 
+cancel = 'AdefDialog.cancel'
+
 def run(str):
 	if not data.initialized:
 		form = flp.parse_form('AdefDialogForm', 'form')
 		flp.create_full_form(data, form)
 		data.initialized = 1
-	data.label.label = str
+	data.label.label = str + '\nor click "whole node" button'
 	fl.deactivate_all_forms()
 	data.form.show_form(FL.PLACE_MOUSE, 1, '')
 	obj = fl.do_forms()
 	data.form.hide_form()
 	fl.activate_all_forms()
 	if obj == data.cancel_button:
-		return 0
+		raise cancel
 	elif obj == data.ok_button:
 		return 1
+	elif obj == data.whole_button:
+		return 0
 	else:
 		print 'AdefDialog: funny object returned: ', obj
 		return 0
