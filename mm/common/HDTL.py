15e9a16f5d9ef2f9c1413df8b7570d3173ffc7a9
Guido van Rossum <guido@python.org>
1992-04-22 18:52:02 +0000

    First release of the 'bag' code; also many little bits of cleanup (e.g., Timing doesn't use long-living global variables any more).

---
+++ mm/common/HDTL.py
@@ -0,0 +1,7 @@
+# The player algorithm treats the head and tail (begin and end) sides
+# of a node as separate events in time; there are separate counters
+# and lists of dependencies, indexed by the symbolic constants
+# HD and TL: e.g., node.counter[HD], node.deps[HD].
+# This is used in modules Player and Timing
+
+HD, TL = 0, 1
