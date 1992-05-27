22a7c3121f3f7ff888ebde6c6d8495ff146bf505
Guido van Rossum <guido@python.org>
1992-05-27 14:13:07 +0000

    Changed main.findfile() function to use $CMIFPATH if possible. Changed cmif.py to import findfile from main instead of duplicating the code.

--- mm/editor/cmif.py
+++ mm/editor/cmif.py
@@ -1,7 +1,12 @@
-def findfile(name):
-	import os
-	if os.environ.has_key('CMIF'):
-		CMIF = os.environ['CMIF']
-	else:
-		CMIF = '/ufs/guido/mm/demo' # Traditional default
-	return os.path.join(CMIF, name)
+# This module provides a public interface to the function findfile
+# defined in main.py.  Dependent on how the program was started (with
+# main.py as standard input or with "import main") we find the
+# function in __main__ or in main.
+#
+# (We can't define the function here and import it from main, because
+# main must *use* it to patch up the Python search path!)
+
+try:
+	from __main__ import findfile
+except ImportError:
+	from main import findfile
