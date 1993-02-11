01ed02eaf1e7f27c48bbe862f4f1570c2512561b
Guido van Rossum <guido@python.org>
1993-02-11 14:21:42 +0000

    Fixed bug in exception handling

--- mm/lib/MovieDuration.py
+++ mm/lib/MovieDuration.py
@@ -5,10 +5,12 @@ import FileCache
 def getduration(filename):
 	fp = open(filename, 'r')
 	import VFile
+	import os
+	VerrorList = VFile.Error, os.error, IOError, RuntimeError, EOFError
 	try:
 		vfile = VFile.RandomVinFile().initfp(fp, filename)
-	except VFile.VerrorList, msg:
-		raise IOError, (0, 'bad movie file')
+	except VerrorList, msg:
+		raise IOError, (0, 'bad movie file: ' + str(msg))
 	vfile.readcache()
 	t, ds, cs = vfile.getrandomframeheader(len(vfile.index)-1)
 	return t * 0.001
