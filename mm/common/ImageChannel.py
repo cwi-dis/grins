a27dabee71d9419555d46a998023558f8f9ab5d4
Jack Jansen <Jack.Jansen@cwi.nl>
1993-05-17 12:53:04 +0000

    Sjoerd's editor with the new channel modules.

--- mm/common/ImageChannel.py
+++ mm/common/ImageChannel.py
@@ -1,466 +1,68 @@
-# Image channel
-
-import MMExc
-import MMAttrdefs
-
-import gl
-import GL
-import DEVICE
-import fl
-
-from Channel import Channel
-from ChannelWindow import ChannelWindow
-
+from Channel import ChannelWindow
+from MMExc import *			# exceptions
 from AnchorDefs import *
 
-import FileCache
-import pipes
+class ImageChannel(ChannelWindow):
+	node_attrs = ChannelWindow.node_attrs + ['scale', 'scalefilter']
 
-
-def between(v, x0, x1):
-	return ((x0 <= v and v <= x1) or (x1 <= v and v <= x0))
-
-class ImageNodeInfo():
-	def init(self):
-		self.node = None
-		self.parray = None
-		self.error = None
-		self.xsize = self.ysize = 0
-		self.xcorner = self.ycorner = 0
-		self.effscale = 1.0
-		self.mousescale = 1.0
-		return self
-
-	def setcolors(self):
-		if not self.node:
-			raise 'setcolors without node'
-		self.bgcolor = MMAttrdefs.getattr(self.node, 'bgcolor')
-		self.hicolor = MMAttrdefs.getattr(self.node, 'hicolor')
-		self.fgcolor = self.hicolor
-		
-	def setdefcolors(self, attrdict):
-		if attrdict.has_key('bgcolor'):
-			self.bgcolor = attrdict['bgcolor']
-		else:
-			self.bgcolor = 255, 255, 255
-
-		if attrdict.has_key('hicolor'):
-			self.hicolor = attrdict['hicolor']
-		else:
-			self.hicolor = 255, 0, 0
-
-		self.fgcolor = self.hicolor
-		
-class ImageWindow(ChannelWindow):
-	#
-	# Initialization function.
-	#
-	def init(self, name, attrdict, channel):
-		self = ChannelWindow.init(self, name, attrdict, channel)
-		self.nonode_ninfo = ImageNodeInfo().init()
-		self.nonode_ninfo.setdefcolors(attrdict)
-		self.clear()
-		self.scheduler = channel.scheduler
-		return self
-	#
-	def __repr__(self):
-		return '<ImageWindow instance, name=' + `self.name` + '>'
-	#
-	def show(self):
-		if self.is_showing():
-			self.pop()
-			return
-		ChannelWindow.show(self)
-		fl.qdevice(DEVICE.LEFTMOUSE)
-		self.render()
-	#
-	def redraw(self):
-		if not self.is_showing():
-			return
-		gl.reshapeviewport()
-		winwidth, winheight = gl.getsize()
-		self.ninfo.xcorner = \
-		     int(winwidth - self.ninfo.xsize*self.ninfo.effscale) / 2
-		self.ninfo.ycorner = \
-		     int(winheight - self.ninfo.ysize*self.ninfo.effscale) / 2
-		gl.ortho2(-0.5, winwidth-0.5, -0.5, winheight-0.5)
-		self.render()
-	#
-	def clear(self):
-		self.anchors = []
-		self.newanchor = None
-		self.ninfo = self.nonode_ninfo
-		if self.is_showing():
-			self.setwin()
-			gl.RGBcolor(self.ninfo.bgcolor)
-			gl.clear()
-	#
-	def getmouse(self):
-		mx, my = fl.get_mouse()
-		return self.convmouse(mx, my)
-
-	def convmouse(self, mx, my):
-		mx = int((mx - self.ninfo.xcorner) / self.ninfo.mousescale)
-		my = int((my - self.ninfo.ycorner) / self.ninfo.mousescale)
-		return mx, my
-	#
-	def mouse(self, dev, val):
-		if dev == DEVICE.RIGHTMOUSE or self.newanchor:
-			self.node = self.ninfo.node # ChannelWindow needs it
-			ChannelWindow.mouse(self, dev, val)
-			return
-		if (dev, val) <> (DEVICE.LEFTMOUSE, 1):
-			return
-		if not self.ninfo.node:
-			gl.ringbell()
-			return
-		if (dev, val) <> (DEVICE.LEFTMOUSE, 1):
-			return
-		if not self.anchors:
-			print 'mouse: no anchors on this node'
-
-		mx, my = self.getmouse()
-		al2 = []
-		for a in self.anchors:
-			args = a[A_ARGS]
-			if not args:
-				al2.append(a)
-				continue
-			x0, y0, x1, y1 = args[0], args[1], args[2], args[3]
-			if between(mx, x0, x1) and between(my, y0, y1):
-				al2.append(a)
-		if not al2:
-			print 'Mouse: No anchor selected'
-			gl.ringbell()
-			return
-		for a in al2:
-			self.drawanchor(a[A_ARGS], 1)
-		rv = self.scheduler.anchorfired(self.ninfo.node, al2)
-		# If this was a paused anchor and it didn't fire
-		# stop showing the node. We also clear the haspauseanchor
-		# field, so we won't execute this code again.
-		if rv == 0 and self.channel.haspauseanchor and \
-			  len(al2) == 1 and al2[0][A_TYPE] == ATYPE_PAUSE:
-			self.channel.haspauseanchor = 0
-			self.channel.pauseanchor_done(0)
-	#
-	def armimage(self, filename_arg, node):
-		self.arm_ninfo = ImageNodeInfo().init()
-		# (Import imgfile here so if it doesn't exist we can
-		# still play documents that don't contain images...)
-		import imgfile
-		import imageop
-		filename = rgbcache.get(filename_arg)
-		self.arm_ninfo.node = node
-		self.arm_ninfo.setcolors()
-		self.arm_ninfo.parray = None
-		self.arm_ninfo.effscale = 1.0 # Scale for lrectwrite
-		self.arm_ninfo.mousescale = 1.0 # Scale for mouse and anchors
-		self.arm_ninfo.xcorner = 0
-		self.arm_ninfo.ycorner = 0
-		self.arm_ninfo.error = None
-		try:
-			self.arm_ninfo.xsize, self.arm_ninfo.ysize, dummy = \
-				imgfile.getsizes(filename)
-		except imgfile.error, msg:
-			print 'Cannot get size of image file', filename_arg, \
-				  ':', msg
-			self.arm_ninfo.error = 'Bad or missing file ' + `filename_arg`
-			return
-		if self.is_showing():
-			self.setwin()
-			winwidth, winheight = gl.getsize()
-		else:
-			winwidth, winheight = 0, 0
-		self.arm_ninfo.effscale = MMAttrdefs.getattr(node, 'scale')
-		scalefilter = MMAttrdefs.getattr(node, 'scalefilter')
-		video = MMAttrdefs.getattr(node, 'video')
-		if self.arm_ninfo.effscale <= 0.0:
-			if not self.is_showing():
-				self.arm_ninfo.effscale = 1.0
-			else:
-				self.arm_ninfo.effscale = \
-					min(float(winwidth)/self.arm_ninfo.xsize, \
-					    float(winheight)/self.arm_ninfo.ysize)
-		self.arm_ninfo.mousescale = self.arm_ninfo.effscale
-		try:
-			if self.arm_ninfo.effscale == \
-				  int(self.arm_ninfo.effscale):
-				self.arm_ninfo.parray = imgfile.read(filename)
-			else:
-				width = int(self.arm_ninfo.xsize * \
-					  self.arm_ninfo.effscale)
-				height = int(self.arm_ninfo.ysize * \
-					  self.arm_ninfo.effscale)
-				if not scalefilter:
-					self.arm_ninfo.parray = \
-						  imgfile.readscaled(filename,\
-						  width, height)
-				else:
-					self.arm_ninfo.parray = \
-						  imgfile.readscaled(filename,\
-						  width, height, scalefilter)
-				self.arm_ninfo.effscale = 1.0
-				self.arm_ninfo.xsize, self.arm_ninfo.ysize = \
-					  width, height
-			if video:
-				self.arm_ninfo.parray = imageop.tovideo( \
-					  self.arm_ninfo.parray, 4, \
-					  width, height)
-		except imgfile.error, msg:
-			print 'Cannot read image file', filename_arg, ':', msg
-			self.arm_ninfo.error = 'Cannot read file ' + \
-				  `filename_arg`
-		self.arm_ninfo.xcorner = int(winwidth - \
-			  self.arm_ninfo.xsize*self.arm_ninfo.effscale) / 2
-		self.arm_ninfo.ycorner = int(winheight - \
-			  self.arm_ninfo.ysize*self.arm_ninfo.effscale) / 2
-	#
-	def showimage(self, node):
-		if node != self.arm_ninfo.node:
-			raise 'Not the armed node'
-		self.ninfo = self.arm_ninfo
-		self.arm_ninfo = None
-		if self.is_showing():
-			self.pop() # Implies gl.winset()
-			self.render()
-	#
-	def setanchors(self, anchors):
-		self.anchors = anchors
-		# NB: this routine must be called before showimage
-	#
-	# Start defining an anchor
-	def setdefanchor(self, anchor):
-		if anchor in self.anchors:
-			self.saveanchor = anchor
-			self.anchors.remove(anchor)
-		else:
-			self.saveanchor = None
-		self.newanchor = anchor
-		self.redraw()
-		self.sqqstart(anchor[2])
-	#
-	# Stop defining an anchor
-	def getdefanchor(self):
-		x0, y0, x1, y1 = self.sqqend()
-		x0, y0 = self.convmouse(x0, y0)
-		x1, y1 = self.convmouse(x1, y1)
-		res = (self.newanchor[0], self.newanchor[1], [x0,y0,x1,y1])
-		self.anchors.append(res)
-		self.saveanchor = None
-		self.newanchor = None
-		self.redraw()
-		return res
-	#
-	# Abort defining an anchor
-	def enddefanchor(self, anchor):
-		dummy = self.sqqend()
-		if self.saveanchor <> None:
-			self.anchors.append(self.saveanchor)
-			self.saveanchor = None
-		self.newanchor = None
-		self.redraw()
-	#
-	def render(self):
-		gl.RGBcolor(self.ninfo.bgcolor)
-		gl.clear()
-		width, height = gl.getsize()
-		if self.ninfo.parray:
-			gl.rectzoom(self.ninfo.effscale, self.ninfo.effscale)
-			gl.lrectwrite(self.ninfo.xcorner, self.ninfo.ycorner, \
-				self.ninfo.xcorner+self.ninfo.xsize-1, \
-				self.ninfo.ycorner+self.ninfo.ysize-1, \
-				self.ninfo.parray)
-		else:
-			if self.ninfo.error:
-				gl.RGBcolor(self.ninfo.fgcolor)
-				width, height = gl.getsize()
-				x, y = width/2, height/2
-				sw = gl.strwidth(self.ninfo.error)
-				sh = gl.getheight()
-				x = max(0, x - sw/2)
-				y = y + sh/2
-				gl.cmov2(x, y)
-				gl.charstr(self.ninfo.error)
-		# Draw anchors even if the image can't be drawn
-		if self.anchors:
-			for dummy, tp, a in self.anchors:
-				self.drawanchor(a, 0)
-		# And redraw current defining anchor, if needed
-		if self.newanchor:
-			self.sqqredraw()
-
-	#
-	# Subroutine to draw an anchor
-	#
-	def drawanchor(self, a, filled):
-		if len(a) <> 4:
-			return
-		gl.RGBcolor(self.ninfo.hicolor)
-		x0, y0, x1, y1 = a[0], a[1], a[2], a[3]
-		x0 = int(x0 * self.ninfo.mousescale + self.ninfo.xcorner)
-		x1 = int(x1 * self.ninfo.mousescale + self.ninfo.xcorner)
-		y0 = int(y0 * self.ninfo.mousescale + self.ninfo.ycorner)
-		y1 = int(y1 * self.ninfo.mousescale + self.ninfo.ycorner)
-		if filled:
-			gl.linewidth(3)
-		gl.bgnclosedline()
-		gl.v2i(x0, y0)
-		gl.v2i(x0, y1)
-		gl.v2i(x1, y1)
-		gl.v2i(x1, y0)
-		gl.endclosedline()
-		if filled:
-			gl.linewidth(1)
-
-
-# XXX Make the image channel class a derived class from ImageWindow?!
-
-class ImageChannel(Channel):
-	#
-	# Declaration of attributes that are relevant to this channel,
-	# respectively to nodes belonging to this channel.
-	#
-	chan_attrs = ['base_window', 'base_winoff']
-	node_attrs = ['file', 'duration', 'scale', 'scalefilter', 'video', \
-		 'bgcolor', 'hicolor']
-##		 'bgcolor', 'fgcolor', 'hicolor']
-	#
 	def init(self, name, attrdict, scheduler, ui):
-		self = Channel.init(self, name, attrdict, scheduler, ui)
-		self.window = ImageWindow().init(name, attrdict, self)
-		self.armed_node = None
-		return self
-	#
-	def __repr__(self):
-		return '<ImageChannel instance, name=' + `self.name` + '>'
-	#
-	def show(self):
-		if self.may_show():
-			self.window.show()
-	#
-	def hide(self):
-		self.window.hide()
-	#
-	def is_showing(self):
-		return self.window.is_showing()
-	#
-	def destroy(self):
-		self.window.destroy()
-	#
-	def save_geometry(self):
-		self.window.save_geometry()
-	#
-	def getduration(self, node):
-		return Channel.getduration(self, node)
-	#
-	# clear is called by Channel.clearnode() to clear remains of
-	# previous node.
-	def clear(self):
-		self.window.clear()
-	#
-	def play(self, node, callback, arg):
-		self.node = node
-		if not self.is_showing():
-			apply(callback, arg)
-			return
-	        if node <> self.armed_node:
-		    print 'ImageChannel: node not armed'
-		    self.arm(node)
-		self.armed_node = None
-		self.showanchors(node)
-		self.window.showimage(node)
-		Channel.play(self, node, callback, arg)
-	#
-	def defanchor(self, node, anchor):
-		fl.show_message('Defining image anchors is broken :-(', \
-			  '(Sorry for this -- will try to fix soon!)', \
-			  '--Guido')
-		return None
-		self.arm(node)
-		self.showanchors(node)
-		self.window.setdefanchor(anchor)
-		self.window.showimage(node)
+		return ChannelWindow.init(self, name, attrdict, scheduler, ui)
 
-		import AdefDialog
-		try:
-			rv = AdefDialog.anchor('Sweep anchor with mouse')
-			a = self.window.getdefanchor()
-			if rv == 0:
-				a = (a[0], a[1], [])
-			return a
-		except AdefDialog.cancel:
-			self.window.enddefanchor(anchor)
-			return None
-	#
-	def showanchors(self, node):
-		self.autoanchor = None
-		self.haspauseanchor = 0
+	def __repr__(self):
+		return '<ImageChannel instance, name=' + `self._name` + '>'
+
+	def do_arm(self, node):
+		f = self.getfilename(node)
+		imbox = self.armed_display.display_image_from_file(f)
 		try:
 			alist = node.GetRawAttr('anchorlist')
-		except MMExc.NoSuchAttrError:
-			self.window.setanchors([])
-			return
-		al2 = []
+		except NoSuchAttrError:
+			alist = []
+		self.armed_display.fgcolor(self.gethicolor(node))
 		for a in alist:
-			if a[A_TYPE] == ATYPE_AUTO:
-				self.autoanchor = a
-			if a[A_TYPE] == ATYPE_PAUSE:
-				self.haspauseanchor = 1
-			if a[A_TYPE] not in (ATYPE_NORMAL, ATYPE_PAUSE):
-				continue
-			if len(a[A_ARGS]) not in (0,4):
+##				print 'anchor:',`a`
+			args = a[A_ARGS]
+			if len(args) == 0:
+				args = [0,0,1,1]
+			elif len(args) == 4:
+				args = self.convert_args(f, args)
+			if len(args) != 4:
 				print 'ImageChannel: funny-sized anchor'
-			else:
-				al2.append(a)
-		self.window.setanchors(al2)
-	#
-	def reset(self):
-		self.window.clear()
-	#
-	def arm(self, node):
-		if not self.is_showing():
-			return
-		self.window.armimage(self.getfilename(node), node)
-		self.armed_node = node
-	#
-	def setwaiting(self):
-		self.window.setwaiting()
-	#
-	def setready(self):
-		self.window.setready()
-	#
+				continue
+			x, y, w, h = args[0], args[1], args[2], args[3]
+			# convert coordinates from image size to window size
+			x = x * imbox[2] + imbox[0]
+			y = y * imbox[3] + imbox[1]
+			w = w * imbox[2]
+			h = h * imbox[3]
+			b = self.armed_display.newbutton(x, y, w, h)
+			b.hiwidth(3)
+##			b.hicolor(self.getfgcolor(node))
+			self.setanchor(a[A_ID], a[A_TYPE], b)
+		return 1
 
-
-temps = []
-
-def makergbfile(filename):
-	import torgb
-	try:
-		result = torgb.torgb(filename)
-	except torgb.error, msg:
-		print torgb.error, msg
-		return filename
-	if result <> filename:
-		temps.append(result)
-	return result
-
-def rmcache(original, cached):
-	import os
-	if cached != original:
-		try:
-			os.unlink(cached)
-		except os.error:
-			pass
-
-def cleanup():
-	import os
-	rgbcache.flushall()
-	for tempname in temps:
-		try:
-			os.unlink(tempname)
-		except (os.error, IOError):
-			pass
-	temps[:] = []
-
-rgbcache = FileCache.FileCache().init(makergbfile, rmcache)
+	# Hack to convert pixel offsets into relative offsets and to make
+	# the coordinates relative to the upper-left corner instead of
+	# lower-left.  This only works for RGB images.
+	# If the offsets are in the range [0..1], we don't need to do
+	# the conversion since the offsets are already fractions of
+	# the image.
+	def convert_args(self, file, args):
+		need_conversion = 0
+		for a in args:
+			if a > 1:
+				need_conversion = 1
+				break
+		if not need_conversion:
+			return args
+		import imgfile
+		xsize, ysize, zsize = imgfile.getsizes(file)
+		x0, y0, x1, y1 = args[0], args[1], args[2], args[3]
+		y0 = ysize - y0
+		y1 = ysize - y1
+		if x0 > x1:
+			x0, x1 = x1, x0
+		if y0 > y1:
+			y0, y1 = y1, y0
+		return float(x0)/float(xsize), float(y0)/float(ysize), \
+			  float(x1-x0)/float(xsize), float(y1-y0)/float(ysize)
--- mm/common/MovieChannel.py
+++ mm/common/MovieChannel.py
@@ -1,259 +1,42 @@
-# Movie channel
-#
-# (This consists mostly of wrappers around the C module "moviechannel".)
+from Channel import ChannelWindowThread
 
-
-import os
-import gl
-import VFile
-import MMAttrdefs
-import GLLock
-
-from Channel import Channel
-from ChannelWindow import ChannelWindow
-
-# Errors that VFile operations may raise
-VerrorList = VFile.Error, os.error, IOError, RuntimeError, EOFError
-
-
-class MovieWindow(ChannelWindow):
-	#
-	def init(self, name, attrdict, channel):
-		self = ChannelWindow.init(self, name, attrdict, channel)
-		self.vfile = None
-		self.clear()
-		return self
-	#
+class MovieChannel(ChannelWindowThread):
 	def __repr__(self):
-		return '<MovieWindow instance, name=' + `self.name` + '>'
-	#
-	def show(self):
-		if self.wid <> 0:
-			self.setwin()
-			return
-		ChannelWindow.show(self)
-		self.clear()
-	#
-	def redraw(self):
-		if self.wid == 0: return
-		gl.reshapeviewport()
-		self.erase()
-		if self.channel.threads and not self.cleared:
-			self.channel.threads.resized()
-	#
-	def clear(self):
-		self.node = None
-		self.cleared = 1
-		if self.wid <> 0:
-			self.setwin()
-			gl.RGBmode()
-			gl.gconfig()
-			self.rgbmode = 1
-			self.erase()
-	#
-	def erase(self):
-		if self.node <> None:
-			r, g, b = MMAttrdefs.getattr(self.node, 'bgcolor')
-		elif self.attrdict.has_key('bgcolor'):
-			r, g, b = self.attrdict['bgcolor']
-		else:
-			r, g, b = 255, 255, 255
-		if not self.cleared:
-			return
-		#
-		if self.rgbmode:
-			gl.RGBcolor(r, g, b)
-		else:
-			INDEX = 31 # Overwrite a "harmless" color index
-			gl.mapcolor(INDEX, r, g, b)
-			gl.color(INDEX)
-			gl.gflush()
-		#
-		gl.clear()
-	#
-	def setfile(self, filename, node):
-		self.node = self.vfile = None
+		return '<MovieChannel instance, name=' + `self._name` + '>'
+
+	def threadstart(self):
+		import moviechannel
+		return moviechannel.init()
+
+	def do_arm(self, node):
+		filename = self.getfilename(node)
 		try:
-			self.vfile = VFile.RandomVinFile().init(filename)
-			try:
-				self.vfile.readcache()
-			except VFile.Error:
-				print filename, ': no cached index'
+			import VFile
+			vfile = VFile.RandomVinFile().init(filename)
 		except EOFError:
 			print 'Empty movie file', `filename`
-			return
-		except VerrorList, msg:
-			print 'Cannot open movie file', `filename`, ':', msg
-			return
-		self.node = node
-		self.rgbmode = (self.vfile.format == 'rgb')
-	#
-	def popup(self):
-		if gl.windepth(self.wid) <> 1:
-			self.pop()
-			if gl.getdisplaymode() in (0, 5):
-				gl.RGBcolor(200, 200, 200)
-				gl.clear()
-				return
-			gl.writemask(0xffffffff)
-			gl.clear()
-
-
-# XXX Make the movie channel class a derived class from MovieWindow?!
-
-##def armdone(arg):
-##	pass
-
-class MovieChannel(Channel):
-	#
-	# Declaration of attributes that are relevant to this channel,
-	# respectively to nodes belonging to this channel.
-	#
-	chan_attrs = ['base_window', 'base_winoff']
-	node_attrs = ['file', 'scale', 'bgcolor']
-	#
-	def init(self, name, attrdict, scheduler, ui):
-		self = Channel.init(self, name, attrdict, scheduler, ui)
-		self.window = MovieWindow().init(name, attrdict, self)
-		self.armed_node = None
-		import mm, moviechannel
-		self.threads = mm.init(moviechannel.init(), \
-			  0, self.deviceno, None)
-##		print 'MovieChannel.init: self.threads = ' + `self.threads`
-		return self
-	#
-	def __repr__(self):
-		return '<MovieChannel instance, name=' + `self.name` + '>'
-	#
-	def show(self):
-		if self.may_show():
-			self.window.show()
-	#
-	def hide(self):
-		self.window.hide()
-	#
-	def is_showing(self):
-		return self.window.is_showing()
-	#
-	def destroy(self):
-##		print 'MovieChannel.destroy'
-		if GLLock.gl_lock:
-			GLLock.gl_lock.release()
-		self.threads.close()
-		self.threads = None
-		if GLLock.gl_lock:
-			GLLock.gl_lock.acquire()
-		self.window.destroy()
-	#
-	def save_geometry(self):
-		self.window.save_geometry()
-	#
-	def do_arm(self, node):
-##		print 'MovieChannel.do_arm: self.threads = ' + `self.threads`
-		if self.window.vfile:
-			import glwindow, mm
-##			glwindow.devregister(`self.deviceno`+':'+`mm.armdone`,\
-##				  armdone, 0)
-			if not GLLock.gl_lock:
-				GLLock.init()
-			# The C code doesn't support all the formats that
-			# VFile recognizes.  If an unsupported format is
-			# found, it raises RuntimeError.
-			try:
-				self.threads.arm(self.window.vfile.fp, 0, 0, \
-				  {'width': self.window.vfile.width, \
-				   'height': self.window.vfile.height, \
-				   'format': self.window.vfile.format, \
-				   'index': self.window.vfile.index, \
-				   'c0bits': self.window.vfile.c0bits, \
-				   'c1bits': self.window.vfile.c1bits, \
-				   'c2bits': self.window.vfile.c2bits, \
-				   'offset': self.window.vfile.offset, \
-				   'scale': MMAttrdefs.getattr(node, 'scale'), \
-				   'wid': self.window.wid, \
-				   'bgcolor': MMAttrdefs.getattr(node, 'bgcolor'), \
-				   'gl_lock': GLLock.gl_lock}, \
-				   None, 1)
-			except RuntimeError, msg:
-				print 'Bad movie file', \
-				      self.window.vfile.filename, msg
-				self.window.vfile = None
-				self.window.node = None
-		self.armed_node = node
-	#
-	def arm(self, node):
-		if not self.is_showing():
-			return
-		filename = self.getfilename(node)
-		self.window.setfile(filename, node)
-		self.do_arm(node)
-	#
-	def did_prearm(self):
-		return (self.armed_node <> None)
-	#
-	def play(self, node, callback, arg):
-		self.node = node
-		self.cb = (callback, arg)
-		if not self.is_showing() or not self.window.vfile:
-			import Duration
-			duration = Duration.get(node)
-			dummy = self.scheduler.enter(duration, 0, \
-				self.done, None)
-			return
-	        if node <> self.armed_node:
-			print 'MovieChannel: node not armed'
-			self.window.popup() # was: .pop(); --Guido
-			self.arm(node)
-		else:
-			self.window.popup()
-		self.armed_node = None
-		import glwindow, mm
-		glwindow.devregister(`self.deviceno`+':'+`mm.playdone`, \
-			  self.done, None)
-		self.window.cleared = 0
-		self.threads.play()
-		self.scheduler.arm_ready(self.name)
-	#
-	#DEBUG: remove dummy entry from queue and call proper done method
-	def done(self, arg):
-		if not self.node:
-			# apparently someone has already called stop()
-			return
-		Channel.done(self, arg)
-	#
-	def stop(self):
-		if self.window.vfile:
-			if GLLock.gl_lock:
-				GLLock.gl_lock.release()
-			self.threads.playstop()
-			if GLLock.gl_lock:
-				GLLock.gl_lock.acquire()
-		Channel.stop(self)
-		self.window.vfile = None
-	#
-	def setrate(self, rate):
-		self.threads.setrate(rate)
-	#
-	def reset(self):
-		self.window.clear()
-		self.node = None # Attempt to fix obscure bug --Guido
-	#
-	def clear(self):
-		self.reset()
-	#
-	def getduration(self, node):
-		import MovieDuration
-		filename = self.getfilename(node)
+			return 1
 		try:
-			return MovieDuration.get(filename)
-		except IOError:
-			print 'cannot open movie file to get duration:',
-			print `filename`
-			return MMAttrdefs.getattr(node, 'duration')
-	#
-	def setwaiting(self):
-		self.window.setwaiting()
-	#
-	def setready(self):
-		self.window.setready()
-	#
+			vfile.readcache()
+		except VFile.Error:
+			print `filename` + ': no cached index'
+		try:
+			import MMAttrdefs, GLLock
+			self.threads.arm(vfile.fp, 0, 0, \
+				  {'width': vfile.width, \
+				   'height': vfile.height, \
+				   'format': vfile.format, \
+				   'index': vfile.index, \
+				   'c0bits': vfile.c0bits, \
+				   'c1bits': vfile.c1bits, \
+				   'c2bits': vfile.c2bits, \
+				   'offset': vfile.offset, \
+				   'scale': MMAttrdefs.getattr(node, 'scale'), \
+				   'wid': self.window._window_id, \
+				   'bgcolor': self.getbgcolor(node), \
+				   'gl_lock': GLLock.gl_lock}, \
+				  None, self.syncarm)
+		except RuntimeError, msg:
+			print 'Bad movie file', `vfile.filename`, msg
+			return 1
+		return 0
