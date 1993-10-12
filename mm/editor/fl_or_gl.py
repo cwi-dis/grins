67e251f0239164360bc8306a46a126745aa8ef7d
Sjoerd Mullender <sjoerdmullender@users.noreply.github.com>
1993-10-12 12:55:05 +0000

    * Added support for X window interface. * Moviechannel now uses colormap mode on 8-bit XS Indigo's instead of   converting the image to RGB32. * Compression Library support is dependent on USE_CL compile-time flag.

--- mm/editor/fl_or_gl.py
+++ mm/editor/fl_or_gl.py
@@ -10,8 +10,14 @@ _last_forms = None
 
 _no_qread = 1				# not allowed to block initially
 
+_lock = None
+
 def mayblock():
 	return _no_qread == 0
+
+def uselock(lock):
+	global _lock
+	_lock = lock
 
 def startmonitormode():
 	global _no_qread
@@ -28,25 +34,33 @@ def endmonitormode():
 	_no_qread = 1
 
 def _qread():
+	if _lock:
+		_lock.acquire()
 	global _last_forms
 	while not _last_forms:
 		_last_forms = fl.do_forms()
 	dev, val = _real_qread()
 	import glwindow
 	glwindow.dispatch(dev, val)
+	if _lock:
+		_lock.release()
 	_last_forms = None
 	return 0, 0
 
 def _qtest():
 	if not mayblock():
 		return None
+	if _lock:
+		_lock.acquire()
+	retval = None
 	global _last_forms
 	if not _last_forms:
 		_last_forms = fl.check_forms()
 	if _last_forms:
 		retval = _real_qtest()
-		return retval
-	return None
+	if _lock:
+		_lock.release()
+	return retval
 
 fl_or_gl.qread = _qread
 fl_or_gl.qtest = _qtest
