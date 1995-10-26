832f15acdbbdb46082a1725cc3ed74860a6fee7a
Sjoerd Mullender <sjoerdmullender@users.noreply.github.com>
1995-10-26 08:41:29 +0000

    Typo in unused code.

--- mm/editor/StyleSheet.py
+++ mm/editor/StyleSheet.py
@@ -20,7 +20,7 @@ class StyleSheet(ViewDialog):
 		self.window = w = windowinterface.Window(title)
 		self.browser = w.Selection(
 			'Styles', 'Name', [], None,
-			left =: None, top = None, right = None)
+			left = None, top = None, right = None)
 		separator = w.Separator(
 			left = None, right = None, top = self.browser)
 		buttons = w.ButtonRow(
