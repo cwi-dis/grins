5352c6448fb253d5d0331bbf3aea7475f50d4772
Sjoerd Mullender <sjoerdmullender@users.noreply.github.com>
1995-11-07 10:39:12 +0000

    Use constants from cl module, not from CL module.

--- mm/lib/MpegDuration.py
+++ mm/lib/MpegDuration.py
@@ -2,15 +2,14 @@
 
 import FileCache
 import cl
-import CL
 
 def getduration(filename):
 	fp = open(filename, 'r')
-	hsize = cl.QueryMaxHeaderSize(CL.MPEG_VIDEO)
+	hsize = cl.QueryMaxHeaderSize(cl.MPEG_VIDEO)
 	hdr = fp.read(hsize)
-	cmp = cl.OpenDecompressor(CL.MPEG_VIDEO)
+	cmp = cl.OpenDecompressor(cl.MPEG_VIDEO)
 	dummy = cmp.ReadHeader(hdr)
-	pbuf = [CL.FRAME_RATE, 0, CL.NUMBER_OF_FRAMES, 0]
+	pbuf = [cl.FRAME_RATE, 0, cl.NUMBER_OF_FRAMES, 0]
 	cmp.GetParams(pbuf)
 ##	print 'mpeg: rate', pbuf[1], 'nframe', pbuf[3]
 	return pbuf[3]/pbuf[1]
