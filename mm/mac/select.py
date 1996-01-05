cda14b2c6aa32542a57b2bb413f56a59c71efaa1
Jack Jansen <Jack.Jansen@cwi.nl>
1996-01-05 18:04:29 +0000

    - Added mac_windowbase and mac_windowinterface - Added MacOS event handling (incomplete) and some hooks for creating   windows.

--- mm/mac/select.py
+++ mm/mac/select.py
@@ -2,12 +2,16 @@
 # select workaround for MacCmif
 #
 
-import time
+import Evt
 
 def select(ifdlist, ofdlist, efdlist, timeout=None):
 	if timeout == None and (ifdlist or ofdlist or efdlist):
 		raise 'Select without timeout', (ifdlist, ofdlist, efdlist)
 	if timeout > 0:
-		time.sleep(timeout)
+		dummy, (what, msg, when, where, modifiers) = Evt.WaitNextEvent(0, 0)
+		stop_when = when + int(timeout*60)
+		while when < stop_when:
+			dummy, (what, msg, when, where, modifiers) = Evt.WaitNextEvent(0,
+					stop_when-when)
 	return [], [], []
 	
