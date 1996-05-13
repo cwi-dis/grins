56d334fb646c289f23a66f9676736cff84174c39
Sjoerd Mullender <sjoerdmullender@users.noreply.github.com>
1996-05-13 14:39:46 +0000

    Significant speedup of player. player/mkcmifcache.py now makes a .cmp file for the player. Old version has tag pre-player-speedup.

--- mm/lib/windowinterface.py
+++ mm/lib/windowinterface.py
@@ -2,21 +2,21 @@ import os
 
 if os.environ.has_key('CMIF_USE_DUMMY'):
 	from dummy_window import *
-elif os.environ.has_key('CMIF_USE_X'):
-	from X_window import *
 elif os.name == 'mac':
 	from mac_window import *
+elif os.environ.has_key('CMIF_USE_GL'):
+	from GL_window import *
 else:
 	try:
-		import gl, fm
+		import Xlib, Xt, Xm
 	except ImportError:
 		import sys
 		sys.last_traceback = None
 		sys.exc_traceback = None
 		try:
-			import Xlib, Xt, Xm
+			import gl, fm
 		except ImportError:
 			raise ImportError, 'No module named windowinterface'
-		from X_window import *
-	else:
 		from GL_window import *
+	else:
+		from X_window import *
