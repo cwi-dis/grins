a17171615a9f2d4e93f2444f843aa3aa0ef03a59
Jack Jansen <Jack.Jansen@cwi.nl>
1996-06-03 15:13:52 +0000

    Program to turn Attrdefs file into a resource, for standalong cmif application.

---
+++ mm/mac/mkattrdefresource.py
@@ -0,0 +1,30 @@
+#
+# Create attrdefs resource file
+#
+import os
+import sys
+import string
+import macfs
+import Res
+import py_resource
+
+# For now:
+progdir=os.path.split(sys.argv[0])[0]
+CMIFDIR=os.path.split(progdir)[0]
+
+ATTRDEFPATH = CMIFDIR + ':lib:Attrdefs.atc'
+
+fp = open(ATTRDEFPATH, 'rb')
+data = fp.read()
+fp.close()
+
+fss, ok = macfs.StandardPutFile('Attrdef resource output file:')
+if not ok: sys.exit(0)
+
+fsid = py_resource.create(fss.as_pathname(), creator='RSED')
+
+py_resource.writemodule('Attrdefs', 512, data, 'CMat')
+
+Res.CloseResFile(fsid)
+print 'Wrote', ATTRDEFPATH, 'to', fss.as_pathname()
+sys.exit(1)
