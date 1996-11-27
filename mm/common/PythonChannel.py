077454244e5e55b3fbcc6f2a2526f4652c46f308
Jack Jansen <Jack.Jansen@cwi.nl>
1996-11-27 16:08:24 +0000

    Made various goodies available in the namespace: channel, player, node, toplevel, anchors and args.

--- mm/common/PythonChannel.py
+++ mm/common/PythonChannel.py
@@ -1,13 +1,37 @@
 from Channel import Channel
 import string
+from MMExc import *			# exceptions
 
 class PythonChannel(Channel):
 	def __repr__(self):
 		return '<PythonChannel instance, name=' + `self._name` + '>'
 
+	def seekanchor(self, node, aid, args):
+		self.seekargs = args
+		
 	def do_play(self, node):
 		if node.GetType() <> 'imm':
 			print 'PythonChannel: imm nodes only'
 		cmds = node.GetValues()
 		cmds = string.join(cmds, '\n')
-		exec cmds
+
+		try:
+			args = self.seekargs
+			self.seekargs = ()
+		except AttributeError:
+			args = ()
+
+		try:
+			alist = node.GetRawAttr('anchorlist')
+		except NoSuchAttrError:
+			alist = []
+			
+		namespace = {
+			"channel": self,
+			"player": self._player,
+			"node": node,
+			"toplevel": self._player.toplevel,
+			"anchors": alist,
+			"args":args}
+		
+		exec cmds in namespace, namespace
