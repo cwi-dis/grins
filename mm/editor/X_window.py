ea596b48037be821a4b3ed7f2f4e541e018cf669
Sjoerd Mullender <sjoerdmullender@users.noreply.github.com>
1996-11-29 14:59:19 +0000

    Make bakcing store default for 8-bit displays.

--- mm/editor/X_window.py
+++ mm/editor/X_window.py
@@ -17,10 +17,14 @@ ARR_HALFWIDTH = 5
 ARR_SLANT = float(ARR_HALFWIDTH) / float(ARR_LENGTH)
 
 class _Toplevel(X_windowbase._Toplevel):
-	def newwindow(self, x, y, w, h, title, pixmap = 0):
+	def newwindow(self, x, y, w, h, title, pixmap = None):
+		if pixmap is None:
+			pixmap = toplevel._depth <= 8
 		return _Window(self, x, y, w, h, title, 0, pixmap)
 
-	def newcmwindow(self, x, y, w, h, title, pixmap = 0):
+	def newcmwindow(self, x, y, w, h, title, pixmap = None):
+		if pixmap is None:
+			pixmap = toplevel._depth <= 8
 		return _Window(self, x, y, w, h, title, 1, pixmap)
 
 	def getsize(self):
