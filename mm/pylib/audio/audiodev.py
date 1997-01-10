d447089d52ef71a67573e030fa8cde2f8ebc04a8
Sjoerd Mullender <sjoerdmullender@users.noreply.github.com>
1997-01-10 13:08:25 +0000

    Audio libraries evolved further.

--- mm/pylib/audio/audiodev.py
+++ mm/pylib/audio/audiodev.py
@@ -2,17 +2,17 @@ Error = 'audiodev.Error'
 
 import sys
 
-if sys.platform in ('irix4', 'irix5'):
+if sys.platform in ('irix4', 'irix5', 'irix6'):
 	import audiodevsgi
-	AudioDev = audiodevsgi.AudioDevSGI
+	writer = audiodevsgi.AudioDevSGI
 	del audiodevsgi
 elif sys.platform in ('sunos4', 'sunos5'):
 	import audiodevsun
-	AudioDev = audiodevsun.AudioDevSUN
+	writer = audiodevsun.AudioDevSUN
 	del audiodevsun
 elif sys.platform == 'mac':
 	import audiodevmac
-	AudioDev = audiodevmac.AudioDevMAC
+	writer = audiodevmac.AudioDevMAC
 	del audiodevmac
 else:
 	raise ImportError, 'No module named audiodev'
