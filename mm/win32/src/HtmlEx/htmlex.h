c179d4dacf4afc2ef36c33b231506f849a23ab40
Sjoerd Mullender <sjoerdmullender@users.noreply.github.com>
1997-03-13 16:18:25 +0000

    Windows NT integrated version.

---
+++ mm/win32/src/HtmlEx/htmlex.h
@@ -0,0 +1,40 @@
+#ifndef _HTMLEX_H_
+#define _HTMLEX_H_
+
+//Win32 Header Files
+#include <process.h>
+
+//MFC Header Files
+#include "StdAfx.h"
+
+//Python Header Files
+#define Py_USE_NEW_NAMES
+#include "allobjects.h"
+#include "modsupport.h"
+#include "abstract.h"
+
+//PythonWin Header Files
+#include "win32ui.h"
+#include "win32assoc.h"
+#include "win32cmd.h"
+#include "win32win.h"
+
+//HtmlEx Header Files
+#include "resource.h"
+#define PyIMPORT  __declspec(dllimport)
+#define PyEXPORT  __declspec(dllexport)
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void PyErr_Print (void);
+void CallbackExErrorFunc(char *str);
+PyEXPORT void initcallbackex();
+
+#ifdef __cplusplus
+}
+#endif
+
+
+
