9baf4d975006f4ba00f4bded89076e3b838cf7fc
Sjoerd Mullender <sjoerdmullender@users.noreply.github.com>
1997-03-13 16:18:29 +0000

    Windows NT integrated version.

---
+++ mm/win32/src/HtmlEx/resource.h
@@ -0,0 +1,35 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by Html.rc
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDC_HTMLCTRL                    101
+#define IDD_CMIF_DIALOG                 102
+#define IDR_MAINFRAME                   128
+#define IDD_PLAYER                      132
+#define IDR_PLAY                        134
+#define IDR_STOP                        135
+#define IDR_PAUSE                       136
+#define IDR_PYTHON                      138
+#define IDR_MENUTEST                    140
+#define IDD_DIALOG1                     142
+#define IDC_STOP                        1000
+#define IDC_PLAY                        1001
+#define IDC_PAUSE                       1002
+#define IDC_BUTTON1                     1007
+#define IDC_BUTTON2                     1008
+#define ID_TEST_CHOICE1                 32771
+#define ID_INDICATOR_STAT               32772
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        143
+#define _APS_NEXT_COMMAND_VALUE         32772
+#define _APS_NEXT_CONTROL_VALUE         1009
+#define _APS_NEXT_SYMED_VALUE           102
+#endif
+#endif
