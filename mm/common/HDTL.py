4c323e2a76376c8f0395d86118300bfe02e971b0
Sjoerd Mullender <sjoerdmullender@users.noreply.github.com>
1997-04-15 12:53:38 +0000

    Added RCS id in __version__ variable.

--- mm/common/HDTL.py
+++ mm/common/HDTL.py
@@ -1,3 +1,5 @@
+__version__ = "$Id$"
+
 # The player algorithm treats the head and tail (begin and end) sides
 # of a node as separate events in time; there are separate counters
 # and lists of dependencies, indexed by the symbolic constants
