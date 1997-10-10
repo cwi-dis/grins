c0b842feeb414d99c2bdf11f36c5672c93d9e6fb
Sjoerd Mullender <sjoerdmullender@users.noreply.github.com>
1997-10-10 14:39:46 +0000

    New ATYPE_WHOLE anchor type to refer to the whole node as a source.

--- mm/common/AnchorDefs.py
+++ mm/common/AnchorDefs.py
@@ -11,16 +11,17 @@ A_ARGS = 2
 # XXX These cannot really be changed, much code relies on these values
 
 # Types:
-ATYPE_DEST  = 0	# The whole node is the anchor
+ATYPE_DEST  = 0	# The whole node is the anchor (destination-only anchor)
 ATYPE_AUTO   = 1	# Auto-firing anchor (fires when the node is done)
 ATYPE_NORMAL = 2	# Normal (e.g. e box drawn in the window)
 ATYPE_PAUSE  = 3	# Pausing (same plus player pauses until choice made)
 # XXX ATYPE_PAUSE should be replaced by a separate 'pause' flag on the node
 ATYPE_COMP   = 4	# Composite anchor (args are other anchors)
 ATYPE_ARGS   = 5	# Pausing, with arguments
+ATYPE_WHOLE  = 6	# Whole-node source anchor
 
 # whole-node (destination only) anchors types
-WholeAnchors = (ATYPE_DEST, ATYPE_AUTO, ATYPE_COMP)
+WholeAnchors = (ATYPE_DEST, ATYPE_AUTO, ATYPE_COMP, ATYPE_WHOLE)
 DestOnlyAnchors = (ATYPE_DEST, ATYPE_COMP)
 
 #
