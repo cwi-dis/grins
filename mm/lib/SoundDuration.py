1df4e79d4ae181e919612b18d9007bcb6c92ada7
Sjoerd Mullender <sjoerdmullender@users.noreply.github.com>
1998-02-02 10:15:50 +0000

    Only store URLs internally, never O/S-dependent file names. Also make this somewhat clearer in the naming of some variables. cmifed and grins can now be called with either a file name or a URL as argument.

--- mm/lib/SoundDuration.py
+++ mm/lib/SoundDuration.py
@@ -3,28 +3,28 @@ __version__ = "$Id$"
 # Cache info about sound files
 
 # Used to get full info
-def getfullinfo(filename):
+def getfullinfo(url):
 	import audio
 	from MMurl import urlretrieve
-	filename = urlretrieve(filename)[0]
 	try:
+		filename = urlretrieve(url)[0]
 		a = audio.reader(filename)
 	except (audio.Error, IOError), msg:
-		print 'error in sound file', filename, ':', msg
+		print 'error in sound file', url, ':', msg
 		return 0, 8000, []
 	return a.getnframes(), a.getframerate(), a.getmarkers()
 
 import FileCache
 allinfo_cache = FileCache.FileCache(getfullinfo)
 
-def get(filename):
-	nframes, framerate, markers = allinfo_cache.get(filename)
+def get(url):
+	nframes, framerate, markers = allinfo_cache.get(url)
 	if nframes == 0: nframes = framerate
 	duration = float(nframes) / framerate
 	return duration
 
-def getmarkers(filename):
-	nframes, framerate, markers = allinfo_cache.get(filename)
+def getmarkers(url):
+	nframes, framerate, markers = allinfo_cache.get(url)
 	if not markers:
 		return []
 	xmarkers = []
