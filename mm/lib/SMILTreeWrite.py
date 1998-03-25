0bc323c261cec61e80136a4352065223952a23c4
Sjoerd Mullender <sjoerdmullender@users.noreply.github.com>
1998-03-25 16:44:06 +0000

    Write proper declaration at start of file. Properly uniquify all id's. Use indefinite in repeat and dur attributes if appropriate. Removed +delay from sync attributes. Write title meta element. Write fewer (if any) attributes that are equal to the default. Renamed scale attribute into fit. Write background-color. Write inline data for immediate nodes. Don't write id's on media elements that don't need it.

--- mm/lib/SMILTreeWrite.py
+++ mm/lib/SMILTreeWrite.py
@@ -14,6 +14,13 @@ import os
 import MMurl
 import regsub
 
+# This string is written at the start of a SMIL file.
+SMILdecl = '''\
+<?xml version="1.0" encoding="ISO-8859-1"?>
+<!DOCTYPE smil PUBLIC "-//W3C//DTD SMIL 1.0//EN"
+                      "http://www.w3.org/AudioVideo/Group/SMIL10.dtd">
+'''
+
 # A fileish object with indenting
 class IndentedFile:
 	def __init__(self, fp):
@@ -73,7 +80,9 @@ def WriteFile(root, filename):
 #
 def getid(writer, node):
 	uid = node.GetUID()
-	return writer.uid2name[uid]
+	name = writer.uid2name[uid]
+	if writer.ids_used[name]:
+		return name
 
 def getcmifattr(writer, node, attr):
 	return node.GetRawAttrDef(attr, None)
@@ -92,25 +101,14 @@ def getduration(writer, node, attr):
 	duration = node.GetRawAttrDef(attr, 0)
 	if duration == 0:
 		return None
-	if duration == -1:		# infinite duration...
-		duration = 60*60*24	# ...1 day long enough?
-	sign, value = encodeduration(duration)
-	if sign != '+':
-		print '** Illegal duration', \
-		      node.GetRawAttrDef('name', '<unnamed>'),\
-		      node.GetUID()
-	return value
-		
-def encodeduration(duration):
-	if not duration:
-		return '+', 0
-	if duration < 0:
-		sign = '-'
-		duration = -duration
+	if duration < 0:		# infinite duration...
+		return 'indefinite'
 	else:
-		sign = '+'
-	return sign, '%.3fs' % duration
-
+		duration = '%.3f' % duration
+		if duration[-4:] == '.000':
+			duration = duration[:-4]
+		return duration + 's'
+		
 def getsyncarc(writer, node, isend):
 	allarcs = node.GetRawAttrDef('synctolist', [])
 	arc = None
@@ -130,7 +128,11 @@ def getsyncarc(writer, node, isend):
 		      node.GetUID()
 		return
 	srcuid, srcside, delay, dstside = arc
-	sign, delay = encodeduration(delay)
+	if delay < 0:
+		print '** Negative delay for', \
+		      node.GetRawAttrDef('name', '<unnamed>'),\
+		      node.GetUID()
+		return
 	parent = node.GetParent()
 	ptype = parent.GetType()
 	siblings = parent.GetChildren()
@@ -141,22 +143,19 @@ def getsyncarc(writer, node, isend):
 	   (ptype == 'seq' and index > 0 and
 	    srcuid == siblings[index-1].GetUID()):
 		# sync arc from parent/previous node
-		if sign == '+':
-			rv = delay
-		else:
-			print '** Negative delay for', \
-			      node.GetRawAttrDef('name', '<unnamed>'),\
-			      node.GetUID()
-			rv = sign + delay
+		rv = '%.3fs' % delay
 	else:
 		srcname = writer.uid2name[srcuid]
 		rv = 'id(%s)'%srcname
 		if srcside:
 			rv = rv+'(end)'
+			if delay:
+				print '** Delay required with end syncarc',\
+				      node.GetRawAttrDef('name', '<unnamed>'),\
+				      node.GetUID()
+## 			rv = rv+'+%.3f'%delay
 		else:
-			rv = rv+'(begin)'
-		if delay:
-			rv = rv + sign + delay
+			rv = rv+'(%.3fs)' % delay
 	return rv
 
 def getterm(writer, node):
@@ -176,6 +175,8 @@ def getrepeat(writer, node):
 	value = node.GetRawAttrDef('loop', 1)
 	if value == 1:
 		return
+	elif value == 0:
+		return 'indefinite'
 	else:
 		return `value`
 
@@ -216,33 +217,43 @@ def mediatype(chtype, error=0):
 
 class SMILWriter:
 	def __init__(self, node, fp, filename):
+		self.__title = None
 		self.root = node
 		self.fp = fp
 
-		self.names_used = {'':1}
-		self.uid2name = {}
-		self.calcnames(node)
+		self.ids_used = {}
 
+		self.uid2name = {}
+		self.calcnames1(node)
+
+		self.ch2name = {}
+		self.bases_used = {}
+		self.calcchnames1(node)
+
+		# second pass
+		self.calcnames2(node)
+		self.calcchnames2(node)
+
+		# must come after second pass
 		self.aid2name = {}
 		self.calcanames(node)
 
-		self.chnames_used = {'':1}
-		self.ch2name = {}
-		self.bases_used = {}
-		self.calcchnames(node)
 		if len(self.bases_used) > 1:
 			print '** Document uses multiple toplevel channels'
 		
 		self.tmpdirname = filename + '.data'
 
 	def write(self):
-		self.fp.write('<?xml encoding="ISO-8859-1"?>\n')
+		self.fp.write(SMILdecl)
 		self.fp.write('<smil>\n')
 		self.fp.push()
 		self.fp.write('<head>\n')
 		self.fp.push()
 		self.fp.write('<meta name="sync" content="soft"/>\n')
 		self.writelayout()
+		if self.__title:
+			self.fp.write('<meta name="title" content=%s/>\n' %
+				      nameencode(self.__title))
 		self.fp.pop()
 		self.fp.write('</head>\n')
 		self.fp.write('<body>\n')
@@ -253,42 +264,74 @@ class SMILWriter:
 		self.fp.pop()
 		self.fp.write('</smil>\n')
 
-	def calcnames(self, node):
-		"""Calculate unique names for nodes"""
+	def calcnames1(self, node):
+		"""Calculate unique names for nodes; first pass"""
 		uid = node.GetUID()
-		name = identify(node.GetRawAttrDef('name', ''))
-		if self.names_used.has_key(name):
-			if not name:
-				name = 'node'
-			name = '%s_%s'%(name, uid)
-		if self.names_used.has_key(name):
-			raise 'DuplicateNameError', name
-		self.names_used[name] = 1
-		self.uid2name[uid] = name
+		name = node.GetRawAttrDef('name', '')
+		if name:
+			name = identify(name)
+			if not self.ids_used.has_key(name):
+				self.ids_used[name] = 1
+				self.uid2name[uid] = name
 		if node.GetType() in interiortypes:
 			for child in node.children:
-				self.calcnames(child)
+				self.calcnames1(child)
 
-	def calcchnames(self, node):
-		"""Calculate unique names for channels"""
+	def calcnames2(self, node):
+		"""Calculate unique names for nodes; second pass"""
+		uid = node.GetUID()
+		name = node.GetRawAttrDef('name', '')
+		if not self.uid2name.has_key(uid):
+			isused = name != ''
+			if isused:
+				name = identify(name)
+			else:
+				name = 'node'
+			# find a unique name by adding a number to the name
+			i = 0
+			nn = '%s-%d' % (name, i)
+			while self.ids_used.has_key(nn):
+				i = i+1
+				nn = '%s-%d' % (name, i)
+			name = nn
+			self.ids_used[name] = isused
+			self.uid2name[uid] = name
+		if node.GetType() in interiortypes:
+			for child in node.children:
+				self.calcnames2(child)
+
+	def calcchnames1(self, node):
+		"""Calculate unique names for channels; first pass"""
 		context = node.GetContext()
 		channels = context.channels
 		for ch in channels:
 			name = identify(ch.name)
-			if self.chnames_used.has_key(name):
-				num = 1
-				while 1:
-					nn = '%s_%d'%(name, num)
-					if not self.chnames_used.has_key(nn):
-						name = nn
-						break
-					num = num + 1
-			self.chnames_used[name] = 1
-			self.ch2name[ch] = name
-
+			if not self.ids_used.has_key(name):
+				self.ids_used[name] = 0
+				self.ch2name[ch] = name
 			if ch.has_key('base_window'):
 				base = ch['base_window']
 				self.bases_used[base] = 1
+
+	def calcchnames2(self, node):
+		"""Calculate unique names for channels; second pass"""
+		context = node.GetContext()
+		channels = context.channels
+		for ch in channels:
+			if not self.ch2name.has_key(ch):
+				name = identify(ch.name)
+				i = 0
+				nn = '%s-%d' % (name, i)
+				while self.ids_used.has_key(nn):
+					i = i+1
+					nn = '%s-%d' % (name, i)
+				name = nn
+				self.ids_used[name] = 0
+				self.ch2name[ch] = name
+			# remove base windows that aren't top-level
+			if self.bases_used.has_key(ch.name) and \
+			   ch.has_key('base_window'):
+				del self.bases_used[ch.name]
 
 	def calcanames(self, node):
 		"""Calculate unique names for anchors"""
@@ -298,16 +341,22 @@ class SMILWriter:
 			aid = (uid, id)
 			if type in SourceAnchors:
 				aname = '%s-%s' % (self.uid2name[uid], id)
-			else:
-				aname = self.uid2name[uid]
-			self.aid2name[aid] = aname
+				if self.ids_used.has_key(aname):
+					i = 0
+					nn = '%s-%d' % (aname, i)
+					while self.ids_used.has_key(nn):
+						i = i+1
+						nn = '%s-%d' % (aname, i)
+					aname = nn
+				self.aid2name[aid] = aname
+				self.ids_used[aname] = 0
 		if node.GetType() in interiortypes:
 			for child in node.children:
 				self.calcanames(child)
 
 	def writelayout(self):
 		"""Write the layout section"""
-		self.fp.write('<layout>\n') # default: type="text/smil-basic"
+		self.fp.write('<layout>\n') # default: type="text/smil-basic-layout"
 		self.fp.push()
 		channels = self.root.GetContext().channels
 		self.channels_defined = {}
@@ -317,32 +366,45 @@ class SMILWriter:
 				    nameencode(self.ch2name[ch])]
 			# if toplevel window, define a channel elt, but
 			# don't define coordinates (i.e., use defaults)
+			if not ch.has_key('base_window'):
+				if not self.__title:
+					self.__title = ch.name
 			if ch.has_key('base_window') and \
 			   ch.has_key('base_winoff'):
 				x, y, w, h = ch['base_winoff']
+				if x+w >= 1.0: w = 0
+				if y+h >= 1.0: h = 0
 				data = ('left', x), ('top', y), ('width', w), ('height', h)
 				for name, value in data:
 					value = int(value*100)
 					if value:
 						attrlist.append('%s="%d%%"'%
 								(name, value))
-			if ch.has_key('z'):
+			if ch.has_key('z') and ch['z'] > 0:
 				attrlist.append('z-index="%d"' % ch['z'])
 			if ch.has_key('scale'):
 				scale = ch['scale']
 			else:
 				scale = 0
 			if scale == 0:
-				scale = 'meet'
+				fit = 'meet'
 			elif scale == -1:
-				scale = 'slice'
+				fit = 'slice'
 			elif scale == 1:
-				scale = 'hidden'
+				fit = 'hidden'
 			else:
-				scale = None
+				fit = None
 				print '** Channel uses unsupported scale value', name
-			if scale is not None:
-				attrlist.append('scale="%s"' % scale)
+			if fit is not None and fit != 'meet':
+				attrlist.append('fit="%s"' % fit)
+
+			if ch.has_key('transparent') and \
+			   ch['transparent'] == 1:
+				# background-color="transparent" is default
+				pass
+			elif ch.has_key('bgcolor'):
+				attrlist.append('background-color="#%02x%02x%02x"' % ch['bgcolor'])
+
 ## 			if len(attrlist) == 1:
 ## 				# Nothing to define
 ## 				continue
@@ -355,7 +417,7 @@ class SMILWriter:
 			
 			
 			
-	def writenode(self, x):
+	def writenode(self, x, root = 0):
 		"""Write a node (possibly recursively)"""
 		type = x.GetType()
 		if type == 'bag':
@@ -375,43 +437,73 @@ class SMILWriter:
 		attrlist = ['<%s'%mtype]
 		if not interior and chtype in ('label', 'text'):
 			attrlist.append('type="text/plain"')
+
+		# if node used as destination, make sure it's id is written
+		uid = x.GetUID()
+		name = self.uid2name[uid]
+		if not self.ids_used[name]:
+			alist = x.GetAttrDef('anchorlist', [])
+			hlinks = x.GetContext().hyperlinks
+			for id, type, args in alist:
+				if hlinks.finddstlinks((uid, id)):
+					self.ids_used[name] = 1
+					break
+
 		imm_href = None
 		if type == 'imm':
-			if chtype == 'html':
-				suff = '.html'
-			else:
-				suff = '.txt'
-			fname = self.smiltempfile(x, suff)
-			fp = open(fname, 'w')
 			data = string.join(x.GetValues(), '\n')
-			if data[-1:] != '\n':
+			if mtype != 'text':
+				import base64
+				data = base64.encodestring(data)
+			elif '<' in data or '&' in data:
+				# "quote" ]]> in string
+				data = string.join(string.split(data, ']]>'),
+						   ']]>]]<![CDATA[>')
+				data = '<![CDATA[%s]]>\n' % data
+			else:
 				data = data + '\n'
-			fp.write(data)
-			fp.close()
-			imm_href = MMurl.pathname2url(fname)
+## 			if chtype == 'html':
+## 				suff = '.html'
+## 			else:
+## 				suff = '.txt'
+## 			fname = self.smiltempfile(x, suff)
+## 			fp = open(fname, 'w')
+## 			data = string.join(x.GetValues(), '\n')
+## 			if data[-1:] != '\n':
+## 				data = data + '\n'
+## 			fp.write(data)
+## 			fp.close()
+## 			imm_href = MMurl.pathname2url(fname)
+		else:
+			data = None
+
 		for name, func in smil_attrs:
-			if name == 'src' and imm_href:
-				value = imm_href
+			if name == 'src' and type == 'imm':
+				value = None
 			else:
 				value = func(self, x)
 			if value:
 				value = nameencode(value)
 				attrlist.append('%s=%s'%(name, value))
-		attrlist = string.join(attrlist, ' ')
-
 		if interior:
-			self.fp.write(attrlist+'>\n')
-			self.fp.push()
+			attrlist = string.join(attrlist, ' ')
+			if not root or len(attrlist) > 1:
+				root = 0
+			if not root:
+				self.fp.write(attrlist+'>\n')
+				self.fp.push()
 			for child in x.GetChildren():
 				self.writenode(child)
-			self.fp.pop()
-			self.fp.write('</%s>\n'%mtype)
+			if not root:
+				self.fp.pop()
+				self.fp.write('</%s>\n'%mtype)
 		elif type in ('imm', 'ext'):
-			self.writemedianode(x, attrlist, mtype)
+			attrlist = string.join(attrlist, ' ')
+			self.writemedianode(x, attrlist, mtype, data)
 		else:
 			raise CheckError, 'bad node type in writenode'
 
-	def writemedianode(self, x, attrlist, mtype):
+	def writemedianode(self, x, attrlist, mtype, data):
 		# XXXX Not correct for imm
 		pushed = 0		# 1 if has whole-node source anchor
 		hassrc = 0		# 1 if has other source anchors
@@ -432,10 +524,23 @@ class SMILWriter:
 			elif type in SourceAnchors:
 				hassrc = 1
 		self.fp.write(attrlist)
-		if not hassrc:
+		if not hassrc and not data:
 			# if no source anchors, make empty element
 			self.fp.write('/')
 		self.fp.write('>\n')
+		if data:
+			self.fp.push()
+			if data[:9] == '<![CDATA[':
+				data = string.split(data, '\n')
+				self.fp.write(data[0])
+				level = self.fp.level
+				self.fp.level = 0
+				for line in data[1:]:
+					self.fp.write(line)
+				self.fp.level = level
+			else:
+				self.fp.write(data)
+			self.fp.pop()
 		if hassrc:
 			self.fp.push()
 			for id, type, args in alist:
@@ -443,6 +548,7 @@ class SMILWriter:
 				   type != ATYPE_WHOLE:
 					self.writelink(x, id, type, args)
 			self.fp.pop()
+		if data or hassrc:
 			self.fp.write('</%s>\n'%mtype)
 		for i in range(pushed):
 			self.fp.pop()
@@ -464,13 +570,13 @@ class SMILWriter:
 				href = '%s#%s' % (uid2[:lastslash], uid2[lastslash+1:])
 		else:
 			href = '#' + self.uid2name[uid2]
-		attrs.append('href="%s"' % href)
+		attrs.append('href=%s' % nameencode(href))
 		return string.join(attrs)
 
 	def writelink(self, x, id, atype, args):
 		items = ["<anchor"]
 		aid = (x.GetUID(), id)
-		items.append('id="%s"'%self.aid2name[aid])
+		items.append('id=%s' % nameencode(self.aid2name[aid]))
 
 		links = x.GetContext().hyperlinks.findsrclinks(aid)
 		if len(links) > 1:
@@ -500,7 +606,7 @@ class SMILWriter:
 			elif args:
 				print '** Unparseable args on', aid, args
 			else:
-				items.append('iid="%s"'%id)
+				items.append('fragment-id=%s'%nameencode(id))
 
 		self.fp.write(string.join(items) + '/>\n')
 
@@ -529,16 +635,15 @@ namechars = string.letters + string.digits + '_-.'
 
 def identify(name):
 	"""Turn a CMIF name into an identifier"""
-	name = string.lower(name)
-	rv = ''
+	rv = []
 	for ch in name:
 		if ch in namechars:
-			rv = rv + ch
+			rv.append(ch)
 		else:
 			if rv and rv[-1] != '_':
-				rv = rv + '_'
+				rv.append('_')
 	# the first character must not be a digit
 	if rv and rv[0] in string.digits:
-		rv = '_' + rv
-	return rv
+		rv.insert(0, '_')
+	return string.join(rv, '')
 
