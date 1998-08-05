432b53097f3767052602e9919a4e784257c4dfd5
Sjoerd Mullender <sjoerdmullender@users.noreply.github.com>
1998-08-05 15:39:00 +0000

    Merged Epsilon version into main branch. Next step is to remove files again that don't need to be in the repository (and of course checking that everything still works!).

---
+++ mm/common/win32/PythonChannel.py
@@ -0,0 +1,50 @@
+__version__ = "$Id$"
+
+from Channel import Channel, PLAYING
+import string
+from MMExc import *			# exceptions
+import sys
+
+class PythonChannel(Channel):
+	def seekanchor(self, node, aid, args):
+		self.seekargs = args
+
+	def do_play(self, node):
+		if node.GetType() <> 'imm':
+			print 'PythonChannel: imm nodes only'
+		cmds = node.GetValues()
+		cmds = string.join(cmds, '\n')
+
+		try:
+			args = self.seekargs
+			self.seekargs = ()
+		except AttributeError:
+			args = ()
+
+		try:
+			alist = node.GetRawAttr('anchorlist')
+		except NoSuchAttrError:
+			alist = []
+
+		namespace = {
+			"channel": self,
+			"player": self._player,
+			"node": node,
+			"toplevel": self._player.toplevel,
+			"anchors": alist,
+			"args":args}
+		try:
+			exec cmds in namespace, namespace
+		except:
+			# XXXX Should be done in editor only, and after
+			# asking the user whether this is ok.
+			#print "EXCEPTION IN PYTHONCHANNEL NODE"
+			#import pdb
+			#pdb.post_mortem(sys.exc_traceback)
+			pass
+
+
+	def play_1(self):
+		# It could be we jumped away so are not playing anymore
+		if self._playstate == PLAYING:
+			Channel.play_1(self)
