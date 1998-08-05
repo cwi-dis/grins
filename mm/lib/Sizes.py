ffd28a313fe884bc5e201830d1a294b1e59ac8f8
Sjoerd Mullender <sjoerdmullender@users.noreply.github.com>
1998-08-05 15:40:33 +0000

    Merged Epsilon version into main branch. Next step is to remove files again that don't need to be in the repository (and of course checking that everything still works!).

--- mm/lib/Sizes.py
+++ mm/lib/Sizes.py
@@ -6,8 +6,12 @@ def GetImageSize(file):
 	try:
 		import img
 	except ImportError:
-		import imageex
-		width, height = imageex.SizeOfImage(file)
+		try:
+			import imageex
+		except ImportError:
+			return 0, 0
+		else:
+			width, height = imageex.SizeOfImage(file)
 	else:
 		rdr = img.reader(None, file)
 		width, height = rdr.width, rdr.height
@@ -20,11 +24,18 @@ def GetVideoSize(file):
 	try:
 		import mv
 	except ImportError:
-		import mpegex
-		width, height = mpegex.SizeOfImage(file)
+		try:
+			import mpegex, cmifex
+		except ImportError:
+			return 0, 0
+		else:
+			h = cmifex.CreateWindow("",10,10,100,100,0)
+			width, height = mpegex.SizeOfImage(h,file)
+			h.DestroyWindow()
 	else:
 		movie = mv.OpenFile(file, mv.MV_MPEG1_PRESCAN_OFF)
 		track = movie.FindTrackByMedium(mv.DM_IMAGE)
-		width, height = track.GetImageWidth(), track.GetImageHeight()
+		width = track.GetImageWidth()
+		height = track.GetImageHeight()
 	cache[file] = width, height
 	return width, height
