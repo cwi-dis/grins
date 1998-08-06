d3a66c0811cecff10c795ed3d577cb688dd53431
Mark Hammond <mhammond@users.noreply.github.com>
1998-08-06 14:30:37 +0000

    New resource definitions from Epsilon.

--- mm/win32/src/CmifEx/resource.h
+++ mm/win32/src/CmifEx/resource.h
@@ -14,6 +14,22 @@
 #define IDR_PAUSE                       136
 #define IDR_PYTHON                      138
 #define IDC_POINT_HAND                  141
+#define IDD_CLOSING_DIALOG              146
+#define IDD_CREATE_BOX                  147
+#define IDC_GRAB_HAND                   148
+#define IDC_GRAB_HAND2                  156
+#define IDC_LD_STRECH                   157
+#define IDC_D_STRECH                    158
+#define IDC_RD_STRECH                   159
+#define IDC_R_STRECH                    160
+#define IDC_PUT                         163
+#define IDC_UL_STRECH                   170
+#define IDC_UR_STRECH                   171
+#define IDC_L_STRECH                    173
+#define IDC_U_STRECH                    174
+#define IDC_POINT_HAND2                 175
+#define IDC_WATCH2                      195
+#define IDI_ICON1                       196
 #define IDC_STOP                        1000
 #define IDC_PLAY                        1001
 #define IDC_PAUSE                       1002
@@ -21,14 +37,19 @@
 #define IDC_PLAYER                      1010
 #define IDC_CLOSE                       1011
 #define IDC_OPEN                        1012
+#define IDC_LABEL                       1013
+#define IDC_SAVE_BUTTON                 1014
+#define IDC_CANCEL_BUTTON               1016
+#define IDC_BOX_LABEL                   1016
+#define IDC_DONTSAVE_BUTTON             1017
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        146
+#define _APS_NEXT_RESOURCE_VALUE        197
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1014
+#define _APS_NEXT_CONTROL_VALUE         1018
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
