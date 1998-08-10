5dbd188b12eeb0010df57276c1f1f53a6c9bf273
Mark Hammond <mhammond@users.noreply.github.com>
1998-08-10 14:05:03 +0000

    Add public symbols are declared as such.

--- mm/win32/src/mci-dll/Mcidll.h
+++ mm/win32/src/mci-dll/Mcidll.h
@@ -1,4 +1,9 @@
 #include <windows.h>
+#include <mmsystem.h> // We need the Windows MM header for the MCIERROR structure.
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 typedef struct {
     BOOL fPlaying;
@@ -32,25 +37,43 @@ typedef struct {
 	long lMIDIduration;
 } MCI_MIDI_STRUCT;
 
+#ifdef FREEZE_MCIDLL
+#	define MCIAPI 
+#else
+#	ifdef BUILD_MCIDLL
+#		define MCIAPI __declspec(dllexport)
+#	else
+#		define MCIAPI __declspec(dllimport)
+#		ifdef _DEBUG
+#			pragma comment(lib, "mcidll_d.lib")
+#		else
+#			pragma comment(lib, "mcidll.lib")
+#		endif
+#	endif
+#endif
 
-void MCIError(char *mes, MCIERROR rc);
+MCIAPI void MCIError(char *mes, MCIERROR rc);
 
-void positionMovie(MCI_AVI_STRUCT *mciAviInfo);
+MCIAPI void positionMovie(MCI_AVI_STRUCT *mciAviInfo);
 
-BOOL AviOpen(MCI_AVI_STRUCT *mciAviInfo);
-BOOL AviPlay(MCI_AVI_STRUCT *mciAviInfo);
-BOOL AviPause(MCI_AVI_STRUCT *mciAviInfo);
-BOOL AviStop(MCI_AVI_STRUCT *mciAviInfo);
-BOOL AviClose(MCI_AVI_STRUCT *mciAviInfo);
-BOOL AviSeek(MCI_AVI_STRUCT *mciAviInfo);
-long AviFrame(MCI_AVI_STRUCT *mciAviInfo);
-DWORD AviDuration(MCI_AVI_STRUCT *mciAviInfo);
-void AviSize(MCI_AVI_STRUCT *mciAviInfo, RECT* rec);
+MCIAPI BOOL AviOpen(MCI_AVI_STRUCT *mciAviInfo);
+MCIAPI BOOL AviPlay(MCI_AVI_STRUCT *mciAviInfo);
+MCIAPI BOOL AviPause(MCI_AVI_STRUCT *mciAviInfo);
+MCIAPI BOOL AviStop(MCI_AVI_STRUCT *mciAviInfo);
+MCIAPI BOOL AviClose(MCI_AVI_STRUCT *mciAviInfo);
+MCIAPI BOOL AviSeek(MCI_AVI_STRUCT *mciAviInfo);
+MCIAPI long AviFrame(MCI_AVI_STRUCT *mciAviInfo);
+MCIAPI DWORD AviDuration(MCI_AVI_STRUCT *mciAviInfo);
+MCIAPI void AviSize(MCI_AVI_STRUCT *mciAviInfo, RECT* rec);
+MCIAPI BOOL AviUpdate(MCI_AVI_STRUCT *mciAviInfo);
 
-BOOL MidiOpen(MCI_MIDI_STRUCT *mciMidiInfo);
-BOOL MidiPlay(MCI_MIDI_STRUCT *mciMidiInfo);
-BOOL MidiPause(MCI_MIDI_STRUCT *mciMidiInfo);
-BOOL MidiStop(MCI_MIDI_STRUCT *mciMidiInfo);
-BOOL MidiClose(MCI_MIDI_STRUCT *mciMidiInfo);
-BOOL MidiSeek(MCI_MIDI_STRUCT *mciMidiInfo);
+MCIAPI BOOL MidiOpen(MCI_MIDI_STRUCT *mciMidiInfo);
+MCIAPI BOOL MidiPlay(MCI_MIDI_STRUCT *mciMidiInfo);
+MCIAPI BOOL MidiPause(MCI_MIDI_STRUCT *mciMidiInfo);
+MCIAPI BOOL MidiStop(MCI_MIDI_STRUCT *mciMidiInfo);
+MCIAPI BOOL MidiClose(MCI_MIDI_STRUCT *mciMidiInfo);
+MCIAPI BOOL MidiSeek(MCI_MIDI_STRUCT *mciMidiInfo);
 
+#ifdef __cplusplus
+}
+#endif
