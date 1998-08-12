e1bfa311d6ceb4c2b2c95152ca53dd229099f24b
Mark Hammond <mhammond@users.noreply.github.com>
1998-08-12 15:04:08 +0000

    New sources from textex.

---
+++ mm/win32/src/CmifEx/winclass.h
@@ -0,0 +1,32 @@
+
+class myWin : public CWnd
+{
+	DECLARE_DYNAMIC(myWin);
+ public:
+    BOOL single_anchor;
+	int m_nPageSize;
+	int VHeight;
+	int m_cyChar;
+	int difer;
+	int m_nScrollPos,m_nOldScrollPos;
+	CString align;
+	//RECT rect;
+	char facename[50];
+    myWin ();
+	friend CString clearstring(CString str);
+	friend CString expandtabs(CString str,CString align);
+    void SetScroll(CString str);
+	void GetDim(CString s,char* fcname,int size);
+	friend int countlines (HDC dc,CString str,char* fcname,int sz,RECT rect);
+	afx_msg int OnCreate (LPCREATESTRUCT);
+    //afx_msg void OnPaint ();
+    //afx_msg void OnSize (UINT, int, int);
+    afx_msg void OnVScroll (UINT, UINT, CScrollBar*);
+
+    DECLARE_MESSAGE_MAP ()
+
+#ifdef _DEBUG
+	virtual void Dump( CDumpContext &dc ) const;
+	virtual void AssertValid() const;
+#endif
+};
