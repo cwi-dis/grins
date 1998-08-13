379fcb4ad1cc335d659daab81ae759d8ca745f37
Mark Hammond <mhammond@users.noreply.github.com>
1998-08-13 14:02:54 +0000

    Changes to boot script more suitable for freezing.

--- mm/exec_cmif.py
+++ mm/exec_cmif.py
@@ -1,46 +1,37 @@
 #
-# DOS  CMIF Player wrapper
+# Win32 CMIF Player wrapper
 #
-# d:\cmifdoc\ams\amsmpeg.cmif
-# d:\cmifdoc\ams\amsmpeg2.cmif
-# d:\cmifdoc\ams\amsmpeg4.cmif
-# d:\cmifdoc\anchor.cmif
-# d:\cmifdoc\anchort.cmif
-# d:\cmifdoc\mondrian\mon2.cmif
-# d:\cmifdoc\cartermill\meddir.cmif
+# Can be executed from "python.exe" or "wpython.exe"
+# Pythonwin should use "grins_app.py", which uses this.
 
 import os
 import sys
 import string
-import win32api, win32con
+import win32api
 from win32con import *
 
 print 'Running CMIF Multimedia presentation'
 if len(sys.argv)>1:
 	print sys.argv[1]
 	
-# For now:
-#progdir=os.path.split(sys.argv[0])[0]
-#CMIFDIR=os.path.split(progdir)[0]
-
 try:
-	CMIFDIR = win32api.RegQueryValue(win32con.HKEY_LOCAL_MACHINE, "Software\\Chameleon\\CmifPath")
+	CMIFDIR = win32api.RegQueryValue(HKEY_LOCAL_MACHINE, "Software\\Chameleon\\CmifPath")
 except win32api.error:
 	CMIFDIR = win32api.GetFullPathName(os.path.join(os.path.split(sys.argv[0])[0], "." ))
 
 # TEMP TEST FOLDER
-print "CMIFDIR = ", CMIFDIR
+print "Main GRiNS directory is", CMIFDIR
 
 CMIFPATH = [
 	os.path.join(CMIFDIR, 'grins\\win32'),
-	os.path.join(CMIFDIR, 'grins'),
 	os.path.join(CMIFDIR, 'common\\win32'),
-	os.path.join(CMIFDIR, 'common'),
 	os.path.join(CMIFDIR, 'lib\\win32'),
+	os.path.join(CMIFDIR, 'grins'),
+	os.path.join(CMIFDIR, 'common'),
 	os.path.join(CMIFDIR, 'lib'),
 	os.path.join(CMIFDIR, 'pylib'),
 	os.path.join(CMIFDIR, 'pylib\\audio'),
-	os.path.join(CMIFDIR, 'PyDLLs'),
+	os.path.join(CMIFDIR, 'win32\\src\\Build'),
 ]
 CMIF_USE_WIN32="ON"
 #CHANNELDEBUG="ON"
@@ -51,12 +42,25 @@ os.environ["CMIF"] = CMIFDIR
 #os.environ["CHANNELDEBUG"] = "ON"
 os.environ["CMIF_USE_WIN32"] = "ON"
 
+# Locate the GRiNSRes.dll file.  This is presumably in the same directory as
+# the extensionmodules, or if frozen, in the main directory
+# This call allows Pythonwin to automatically find resources in it.
+import win32ui
+try:
+	import cmifex
+	dllPath = os.path.split(cmifex.__file__)[0]
+except AttributeError:
+	# No __file__ attribute - must be frozen or built-in!
+	dllPath = ""
+
+try:
+	dll = win32ui.LoadLibrary(os.path.join(dllPath, "GRiNSRes.dll"))
+	dll.AttachToMFC()
+except win32ui.error:
+	win32ui.MessageBox("The application resource DLL 'GRiNSRes.dll' can not be located\r\n\r\nPlease correct this problem, and restart the application")
+	# For now just continue!?!?!
+
 # run the given cmif file
 #import main
-print "sys.argv-->", sys.argv
-import cmifex
-for i in range(1,len(sys.argv)):
-	sys.argv[i]=cmifex.ToLongName(sys.argv[i])
-print "long sys.argv-->", sys.argv
-
+import win32ui # Import this before any dependants try to import!
 import grins
