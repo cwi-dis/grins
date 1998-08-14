d1e9517c1098f48e9a983adb4d5c6404b0ca9c00
Mark Hammond <mhammond@users.noreply.github.com>
1998-08-14 11:01:40 +0000

    Update to avoid the registry etc.

--- mm/exec_cmif.py
+++ mm/exec_cmif.py
@@ -10,57 +10,63 @@ import string
 import win32api
 from win32con import *
 
-print 'Running CMIF Multimedia presentation'
-if len(sys.argv)>1:
-	print sys.argv[1]
+def Boot( bEditor = 0 ):
+	print 'Running CMIF Multimedia presentation'
+	if len(sys.argv)>1:
+		print sys.argv[1]
 	
-try:
-	CMIFDIR = win32api.RegQueryValue(HKEY_LOCAL_MACHINE, "Software\\Chameleon\\CmifPath")
-except win32api.error:
-	CMIFDIR = win32api.GetFullPathName(os.path.join(os.path.split(sys.argv[0])[0], "." ))
+	try:
+		CMIFDIR = win32api.RegQueryValue(HKEY_LOCAL_MACHINE, "Software\\Chameleon\\CmifPath")
+	except win32api.error:
+		CMIFDIR = win32api.GetFullPathName(os.path.join(os.path.split(sys.argv[0])[0], "." ))
 
-# TEMP TEST FOLDER
-print "Main GRiNS directory is", CMIFDIR
+	# TEMP TEST FOLDER
+	print "Main GRiNS directory is", CMIFDIR
+	if bEditor:
+		specificPath = "editor"
+	else:
+		specificPath = "grins"
 
-CMIFPATH = [
-	os.path.join(CMIFDIR, 'grins\\win32'),
-	os.path.join(CMIFDIR, 'common\\win32'),
-	os.path.join(CMIFDIR, 'lib\\win32'),
-	os.path.join(CMIFDIR, 'grins'),
-	os.path.join(CMIFDIR, 'common'),
-	os.path.join(CMIFDIR, 'lib'),
-	os.path.join(CMIFDIR, 'pylib'),
-	os.path.join(CMIFDIR, 'pylib\\audio'),
-	os.path.join(CMIFDIR, 'win32\\src\\Build'),
-]
-CMIF_USE_WIN32="ON"
-#CHANNELDEBUG="ON"
+	CMIFPATH = [
+		os.path.join(CMIFDIR, '%s\\win32' % specificPath),
+		os.path.join(CMIFDIR, 'common\\win32'),
+		os.path.join(CMIFDIR, 'lib\\win32'),
+		os.path.join(CMIFDIR, '%s' % specificPath),
+		os.path.join(CMIFDIR, 'common'),
+		os.path.join(CMIFDIR, 'lib'),
+		os.path.join(CMIFDIR, 'pylib'),
+		os.path.join(CMIFDIR, 'pylib\\audio'),
+		os.path.join(CMIFDIR, 'win32\\src\\Build'),
+	]
+	CMIF_USE_WIN32="ON"
+	#CHANNELDEBUG="ON"
 
-sys.path[0:0] = CMIFPATH
+	sys.path[0:0] = CMIFPATH
 
-os.environ["CMIF"] = CMIFDIR
-#os.environ["CHANNELDEBUG"] = "ON"
-os.environ["CMIF_USE_WIN32"] = "ON"
+	os.environ["CMIF"] = CMIFDIR
+	#os.environ["CHANNELDEBUG"] = "ON"
+	os.environ["CMIF_USE_WIN32"] = "ON"
 
-# Locate the GRiNSRes.dll file.  This is presumably in the same directory as
-# the extensionmodules, or if frozen, in the main directory
-# This call allows Pythonwin to automatically find resources in it.
-import win32ui
-try:
-	import cmifex
-	dllPath = os.path.split(cmifex.__file__)[0]
-except AttributeError:
-	# No __file__ attribute - must be frozen or built-in!
-	dllPath = ""
+	# Locate the GRiNSRes.dll file.  This is presumably in the same directory as
+	# the extensionmodules, or if frozen, in the main directory
+	# This call allows Pythonwin to automatically find resources in it.
+	import win32ui
+	try:
+		import cmifex
+		dllPath = os.path.split(cmifex.__file__)[0]
+	except AttributeError:
+		# No __file__ attribute - must be frozen or built-in!
+		dllPath = ""
 
-try:
-	dll = win32ui.LoadLibrary(os.path.join(dllPath, "GRiNSRes.dll"))
-	dll.AttachToMFC()
-except win32ui.error:
-	win32ui.MessageBox("The application resource DLL 'GRiNSRes.dll' can not be located\r\n\r\nPlease correct this problem, and restart the application")
-	# For now just continue!?!?!
+	try:
+		dll = win32ui.LoadLibrary(os.path.join(dllPath, "GRiNSRes.dll"))
+		dll.AttachToMFC()
+	except win32ui.error:
+		win32ui.MessageBox("The application resource DLL 'GRiNSRes.dll' can not be located\r\n\r\nPlease correct this problem, and restart the application")
+		# For now just continue!?!?!
 
-# run the given cmif file
-#import main
-import win32ui # Import this before any dependants try to import!
-import grins
+	# run the given cmif file
+	if bEditor:
+		import cmifed
+	else:
+		import grins
