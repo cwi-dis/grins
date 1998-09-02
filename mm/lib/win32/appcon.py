adddc0e4458731466ed6c192f4ed54be79edede9
anagnost <anagnost@localhost>
1998-09-02 08:45:41 +0000

    Split cmif/lib/win32 into components

---
+++ mm/lib/win32/appcon.py
@@ -0,0 +1,24 @@
+__version__ = "$Id$"
+
+[UNIT_MM, UNIT_SCREEN, UNIT_PXL] = range(3)
+[PAINT, SIZE, LBUTTONDOWN, MM_MCI_NOTIFY, SET_CURSOR, WIN_DESTROY, LBUTTONUP] = range(7)
+[OPAQUE, TRANSPARENT] = range(2)
+[HIDDEN, SHOWN] = range(2)
+[FALSE, TRUE] = range(2)
+[RESET_CANVAS, DOUBLE_HEIGHT, DOUBLE_WIDTH] = range(3)
+[SINGLE, HTM, TEXT, MPEG] = range(4)
+[SAVE_CMIF, OPEN_FILE] = range(2)
+[TOP, CENTER, BOTTOM] = range(3)
+[VERTICAL, HORIZONTAL] = range(2)
+
+# size of arrow head
+ARR_LENGTH = 18
+ARR_HALFWIDTH = 5
+ARR_SLANT = float(ARR_HALFWIDTH) / float(ARR_LENGTH)
+
+Continue = 'Continue'
+
+error = 'windowinterface.error'
+
+
+
