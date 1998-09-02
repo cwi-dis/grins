f705205d0b93b4844ae3f79831ab4173dff3f29e
anagnost <anagnost@localhost>
1998-09-02 14:51:03 +0000

    Cmif extension modules that will be hosted into win32ui

---
+++ mm/win32/src/win32ext/win32ext.h
@@ -0,0 +1,30 @@
+#ifndef INC_WIN32EXT
+#define INC_WIN32EXT
+
+#include "moddef.h"
+
+DECLARE_PYMODULECLASS(Cmifex);
+DECLARE_PYMODULECLASS(Cmifex2);
+DECLARE_PYMODULECLASS(Soundex);
+DECLARE_PYMODULECLASS(Gifex);
+DECLARE_PYMODULECLASS(Htmlex);
+DECLARE_PYMODULECLASS(Imageex);
+DECLARE_PYMODULECLASS(Midiex);
+DECLARE_PYMODULECLASS(Mpegex);
+DECLARE_PYMODULECLASS(Timerex);
+DECLARE_PYMODULECLASS(Timerex2);
+
+#define DEF_EXT_PY_METHODS \
+	{"GetCmifex",MCmifex::create,1},\
+	{"GetCmifex2",MCmifex2::create,1},\
+	{"GetSoundex",MSoundex::create,1},\
+	{"GetGifex",MGifex::create,1},\
+	{"GetHtmlex",MHtmlex::create,1},\
+	{"GetImageex",MImageex::create,1},\
+	{"GetMidiex",MMidiex::create,1},\
+	{"GetMpegex",MMpegex::create,1},\
+	{"GetTimerex",MTimerex::create,1},\
+	{"GetTimerex2",MTimerex2::create,1},\
+
+
+#endif
