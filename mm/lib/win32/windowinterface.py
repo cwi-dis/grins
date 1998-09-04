658282c43372cc39c29413d0a90b7d28310f31f0
anagnost <anagnost@localhost>
1998-09-04 10:26:23 +0000

    textwindow function transfered to windowinterface

--- mm/lib/win32/windowinterface.py
+++ mm/lib/win32/windowinterface.py
@@ -12,7 +12,7 @@ from AppForms import Window
 
 # dlgs
 from AppMessages import showmessage,showquestion,multchoice
-from Dialog import MainDialog
+from AppDialog import MainDialog
 from FileDialog import FileDialog
 from SelectionDialog import SelectionDialog
 from SelectionMenuDialog import SelectionMenuDialog
@@ -50,6 +50,16 @@ def GetVideoSize(file):
 	width, height = mpegex.SizeOfImage(w,file)
 	w.DestroyWindow()
 	return (width, height)
+
+
+def textwindow(text):
+	from win32modules import cmifex2
+	w = Window('Source', resizable = 1, deleteCallback = 'hide', havpar = 0)
+	t = w.TextEdit(text, None, editable = 0, top = 35, left = 0, right = 80*7, bottom = 300, rows = 30, columns = 80)
+	b = w.ButtonRow([('Close', (w.hide, ()))], top = 5, left = 5, right = 150, bottom = 30, vertical = 0)
+	cmifex2.ResizeWindow(w._hWnd, 80*7+20, 380)
+	w.show()
+	return w
 
 #########################################
 # Shortcuts
