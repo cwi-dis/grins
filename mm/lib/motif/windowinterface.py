011fb6c0df9c57df452eae6d2c0bb799cb1235d7
Sjoerd Mullender <sjoerdmullender@users.noreply.github.com>
1998-09-04 12:15:24 +0000

    Need to initialize parent correctly.

--- mm/lib/motif/windowinterface.py
+++ mm/lib/motif/windowinterface.py
@@ -2303,6 +2303,8 @@ class Window(_WindowHelpers, _MenuSupport):
 			attrs['dialogStyle'] = \
 					     Xmd.DIALOG_FULL_APPLICATION_MODAL
 			parent = options.get('parent', toplevel)
+			if parent is None:
+				parent = toplevel
 			while 1:
 				if hasattr(parent, '_shell'):
 					parent = parent._shell
