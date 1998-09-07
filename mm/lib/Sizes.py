e958f51db29586ced661178ad52ab0ebf730304c
anagnost <anagnost@localhost>
1998-09-07 12:17:18 +0000

    Use function exported by windowinterface in order to facilitate changes

--- mm/lib/Sizes.py
+++ mm/lib/Sizes.py
@@ -24,14 +24,8 @@ def GetVideoSize(file):
 	try:
 		import mv
 	except ImportError:
-		try:
-			import mpegex, cmifex
-		except ImportError:
-			return 0, 0
-		else:
-			h = cmifex.CreateWindow("",10,10,100,100,0)
-			width, height = mpegex.SizeOfImage(h,file)
-			h.DestroyWindow()
+		import windowinterface
+		width, height = windowinterface.GetVideoSize(h,file)
 	else:
 		movie = mv.OpenFile(file, mv.MV_MPEG1_PRESCAN_OFF)
 		track = movie.FindTrackByMedium(mv.DM_IMAGE)
