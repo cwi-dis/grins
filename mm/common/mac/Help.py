e3799f39157704395b28162dfa1c966f7c83f5a2
Jack Jansen <Jack.Jansen@cwi.nl>
1998-10-26 23:04:29 +0000

    Show html-based help. The interface is the same as for the Windows version, but both should eventually be redesigned to be a bit more object-oriented.

---
+++ mm/common/mac/Help.py
@@ -0,0 +1,59 @@
+__version__ = "$Id$"
+
+# Help window
+# XXXX This is far too stateful. The interface should be changed into
+# XXXX an object that is kept in the main object. We keep it like this
+# XXXX for now to stay compatible with the Windows version.
+
+# Interface:
+# (1) optionally call sethelpdir(dirname) with a directory name argument;
+# (2) if the user presses a Help button, call showhelpwindow();
+# (3) or call givehelp(topic) to show help on a particular subject.
+# When the help window is already open, it is popped up.
+
+
+import os
+import string
+import sys
+import MMurl
+
+def hashelp():
+	return 1
+
+helpbase = None				# directory where the help files live
+helpwindow = None
+
+#
+# This could be done better, by putting the version number in here.
+#
+DEFAULT_BASE_URL="http://www.cwi.nl/GRiNS/help/%s/index.html"%sys.platform
+
+def sethelpdir(dirname):
+	global helpbase
+	helpbase = MMurl.pathname2url(os.path.join(dirname, 'index.html'))
+
+def fixhelpdir():
+	global helpbase
+	if helpbase is None:
+		import cmif
+		helpdir = os.path.join(cmif.findfile('Help'), sys.platform)
+		if os.path.exists(helpdir):
+			basefile = os.path.join(helpdir, 'index.html')
+			helpbase = MMurl.pathname2url(basefile)
+		else:
+			helpbase = DEFAULT_BASE_URL
+		
+
+def givehelp(topic):
+	global helpwindow
+	import windowinterface
+	print 'GIVEHELP', topic
+	fixhelpdir()
+	helpfile = '%s.html'%topic
+	helpurl = MMurl.basejoin(helpbase, helpfile)
+	if not helpwindow is None and not helpwindow.is_closed():
+		helpwindow.goto_url(helpurl)
+	helpwindow = windowinterface.htmlwindow(helpurl)
+
+def showhelpwindow():
+	givehelp('index')
