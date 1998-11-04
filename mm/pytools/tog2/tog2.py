fa7d6075265e60282c2508131c2642e1f5d526e4
Jack Jansen <Jack.Jansen@cwi.nl>
1998-11-04 16:53:44 +0000

    Generally restructured with lots of classes. There are now two convert classes (either G2-only output or G2-GRiNS-switched-output), and there is a group of data-converter classes: a manual one (only prints messages telling you what to do), a common class for batchfile based conversion and a unix-specific shellscrip class based on that.
    None of these features are available on the command line (yet).

--- mm/pytools/tog2/tog2.py
+++ mm/pytools/tog2/tog2.py
@@ -11,55 +11,20 @@ import urllib
 import os
 import posixpath
 
-def userconvert(src, dst):
-	print "CONVERT:", src
-	print "     TO ", dst
-	print
-	return 1
-
-def usercheck(src):
-	print "  CHECK:", src
-	print
-	return 1
-
-MEDIA_NODES=('animation', 'audio', 'img', 'ref', 'text', 'textstream', 'video')
-OK_EXTENSIONS={
-	'.ra': None,
-	'.rm': None,
-	'.rt': None,
-	'.rv': None,
-	'.rp': None,
-	'.jpg': usercheck,
-}
-OK_SCHEMES=('', 'file', 'ftp', 'http', 'rtsp')
-CONVERTERS={
-	'.au': ('.ra', userconvert),
-	'.aif': ('.ra', userconvert),
-	'.aiff': ('.ra', userconvert),
-	'.wav': ('.ra', userconvert),
-	'.gif': ('.jpg', userconvert),
-	'.txt': ('.rt', userconvert),
-	'.htm': ('.rt', userconvert),
-	'.html': ('.rt', userconvert),
-	'.qt': ('.rv', userconvert),
-	'.mov': ('.rv', userconvert),
-	'.mpg': ('.rv', userconvert),
-	'.mpv': ('.rv', userconvert),
-}
-G2_TYPES={
-	'audio': '.ra',
-	'img': '.jpg',
-	'text': '.rt',
-	'textstream': '.rt',
-	'video': '.rv',
-}
 
 #
 # An extension of the transformer class. The base class can only
 # specify transformation of single nodes (with the do_* methods), this
 # one can do whole subtrees (with subtree_* methods).
 #
+# This class also contains the canhandle and canhandleurl methods, which
+# check nodes and urls for compatability. The actual data they use to
+# make this check are set by the subclasses.
+
 class MyTransformer(Transformer):
+	MEDIA_NODES=('animation', 'audio', 'img', 'ref',
+		     'text', 'textstream', 'video')
+
 	def _transform_node(self, node):
 		if node.NodeType == ELEMENT:
                         if hasattr(self, 'tree_' + node.tagName):
@@ -67,13 +32,150 @@ class MyTransformer(Transformer):
 				return func(node)
 		return Transformer._transform_node(self, node)
 
-class G2Transformer(MyTransformer):
-	within_switch = 0
+	def canhandle(self, node):
+		if node.NodeType == DOCUMENT:
+			return self.canhandle(node.documentElement)
+		ok = 1
+		if node.NodeType == ELEMENT:
+			if node.GI == 'switch':
+				for child in node.getChildren():
+					if self.canhandle(child):
+						return 1
+				return 0
+			elif node.GI in self.MEDIA_NODES:
+				if node.attributes.has_key('src'):
+					url = node.attributes['src']
+					if not self.canhandleurl(url):
+						print 'cannot handle', url
+						ok = 0
+			for child in node.getChildren():
+				if not self.canhandle(child):
+					ok = 0
+		return ok
+
+	def canhandleurl(self, url):
+		scheme, location, path, parameters, query, fragment = \
+			urlparse.urlparse(url)
+		if not scheme in self.OK_SCHEMES:
+			return 0
+		dummy, ext = posixpath.splitext(path)
+		if not self.OK_EXTENSIONS.has_key(ext):
+			return 0
+		#
+		# If the value is non-zero we should call the dataconverter
+		# check method to check actual validity.
+		if self.OK_EXTENSIONS[ext]:
+			return self.dataconverter.check(url)
+		return 1
 	
+
+class G2CommonTransformer(MyTransformer):
+	#
+	# Extensions that are OK for G2 playback. A non-zero value
+	# means we have to call the dataconverter check routine for
+	# additional checks.
+	#
+	OK_EXTENSIONS={
+		'.ra': 0,
+		'.rm': 0,
+		'.rt': 0,
+		'.rv': 0,
+		'.rp': 0,
+		'.jpg': 1,
+	}
+	#
+	# URL schemes that the G2 player understands.
+	#
+	OK_SCHEMES=('', 'file', 'ftp', 'http', 'rtsp')
+	
+	def __init__(self, dataconverter):
+		self.dataconverter = dataconverter
+		Transformer.__init__(self)
+
+
+	def do_region(self, node):
+		# G2 player needs width/height. Set to 100% if not specified
+		# Affects GRiNS too, but this is not to be helped.
+		#
+		if not node.attributes.has_key('width'):
+			node.attributes['width'] = '100%'
+		if not node.attributes.has_key('height'):
+			node.attributes['height'] = '100%'
+		return [node]
+
+	def other_g2_conversions(self, node):
+		# Handles various other things G2 doesn't do.
+		# These affect GRiNS too, but so be it...
+		if node.attributes.has_key('dur'):
+			dur = node.attributes['dur']
+			if dur == 'indefinite':
+				node.attributes['dur'] = '9999s'
+				print 'WARNING: indefinite duration set to 9999'
+				print
+
+	def convertnode(self, node):
+		"""Duplicate and convert a subtree, if possible"""
+		if not node.attributes.has_key('src'):
+			return None
+		url = node.attributes['src']
+		tag = node.GI
+		newurl = self.dataconverter.converturl(url, tag)
+		if not newurl:
+			return None
+		newnode = self.dupnode(node)
+		newnode.attributes['src'] = newurl
+		return newnode
+
+	def dupnode(self, node):
+		"""Duplicate a subtree"""
+		if node.NodeType == ELEMENT:
+			tag = node.GI
+			attrs = node.attributes.items()
+			newnode = self.dom_factory.createElement(tag, {})
+			attrs = self.dupattrs(node, attrs)
+			for attr, value in attrs:
+				newnode.attributes[attr] = value
+			for ch in node.getChildren():
+				newch = self.dupnode(ch)
+				newnode.insertBefore(newch, None)
+			return newnode
+		elif node.NodeType == TEXT:
+			return self.dom_factory.createTextNode(node.data)
+		elif node.NodeType == COMMENT:
+			return self.dom_factory.createComment(node.data)
+		else:
+			raise 'dupnode: unimplemented NodeType', node.NodeType
+
+
+	def dupattrs(self, node, attrs):
+		return attrs
+	
+class G2SwitchTransformer(G2CommonTransformer):
+	within_switch = 0
+
+	def tree_switch(self, node):
+		#
+		# Within a switch we don't have to add additional switches
+		#
+		if self.canhandle(node):
+			return [node]
+		self.within_switch = self.within_switch + 1
+		rv = Transformer._transform_node(self, node)
+		self.within_switch = self.within_switch - 1
+		return rv
+
+	def dupattrs(self, node, attrs):
+		newattrs = []
+		for attr, value in attrs:
+			if attr == 'id':
+				value = 'g2_' + value
+			newattrs.append(attr, value)
+		return newattrs
+
 	def do_ref(self, node):
 		if node.attributes.has_key('src'):
 			url = node.attributes['src']
-			if not canhandleurl(url):
+			if not self.canhandleurl(url):
 				node2 = self.convertnode(node)
 				if node2:
 					self.other_g2_conversions(node2)
@@ -99,6 +201,26 @@ class G2Transformer(MyTransformer):
 					return [SWITCH('\n', node, '\n')]
 		self.other_g2_conversions(node)
 		return [node]
+
+	do_animation = do_ref
+	do_audio = do_ref
+	do_img = do_ref
+	do_text = do_ref
+	do_textstream = do_ref
+	do_video = do_ref
+			
+	
+class G2OnlyTransformer(G2CommonTransformer):
+	
+	def do_ref(self, node):
+		if node.attributes.has_key('src'):
+			url = node.attributes['src']
+			if not self.canhandleurl(url):
+				node2 = self.convertnode(node)
+				if node2:
+					node = node2
+		self.other_g2_conversions(node)
+		return [node]
 				
 	do_animation = do_ref
 	do_audio = do_ref
@@ -107,90 +229,155 @@ class G2Transformer(MyTransformer):
 	do_textstream = do_ref
 	do_video = do_ref
 
-	def do_region(self, node):
-		# G2 player needs width/height. Set to 100% if not specified
-		if not node.attributes.has_key('width'):
-			node.attributes['width'] = '100%'
-		if not node.attributes.has_key('height'):
-			node.attributes['height'] = '100%'
-		return [node]
-
-	def tree_switch(self, node):
-		if canhandle(node):
-			return [node]
-		self.within_switch = self.within_switch + 1
-		rv = Transformer._transform_node(self, node)
-		self.within_switch = self.within_switch - 1
-		return rv
-
-	def convertnode(self, node):
-		if not node.attributes.has_key('src'):
+class DataConverter:
+	def converturl(self, url, type):
+		scheme, location, path, parameters, query, fragment = \
+			urlparse.urlparse(url)
+		if scheme == 'data':
+			return self.savedata(url, type)
+		if query or fragment:
 			return None
-		url = node.attributes['src']
-		tag = node.GI
-		newurl = converturl(url, tag)
-		if not newurl:
-			return None
-		newnode = self.dupnode(node)
-		newnode.attributes['src'] = newurl
-		return newnode
-
-	def dupnode(self, node):
-		if node.NodeType == ELEMENT:
-			tag = node.GI
-			attrs = node.attributes.items()
-			newnode = self.dom_factory.createElement(tag, {})
-			for attr, value in attrs:
-				if attr == 'id':
-					value = 'g2_' + value
-				newnode.attributes[attr] = value
-			for ch in node.getChildren():
-				newch = self.dupnode(ch)
-				newnode.insertBefore(newch, None)
-			return newnode
-		elif node.NodeType == TEXT:
-			return self.dom_factory.createTextNode(node.data)
-		elif node.NodeType == COMMENT:
-			return self.dom_factory.createComment(node.data)
+		dummy, filename = posixpath.split(path)
+		filebase, ext = posixpath.splitext(filename)
+		if  self.CONVERTERS.has_key(ext):
+			method, args = self.CONVERTERS[ext]
 		else:
-			raise 'dupnode: unimplemented NodeType', node.NodeType
-
-	def other_g2_conversions(self, node):
-		# Handles various other things G2 doesn't do
-		if node.attributes.has_key('dur'):
-			dur = node.attributes['dur']
-			if dur == 'indefinite':
-				node.attributes['dur'] = '9999s'
-				print 'WARNING: indefinite duration set to 9999'
-				print
-def converturl(url, type):
-	scheme, location, path, parameters, query, fragment = \
-		urlparse.urlparse(url)
-	if scheme == 'data':
-		return savedata(url, type)
-	if query or fragment:
-		return None
-	dummy, filename = posixpath.split(path)
-	filebase, ext = posixpath.splitext(filename)
-	if not CONVERTERS.has_key(ext):
-		print 'UNKNOWN:', url
-		print
-		return None
-	new = newfilename(filebase,CONVERTERS[ext][0])
-	if CONVERTERS[ext][1](url, new):
+			method = self.convert_unknown
+			args = ()
+		new = apply(method, (self, url, filebase) + args)
 		return new
-	return None
 
-def savedata(url, type):
-	if not G2_TYPES.has_key(type):
-		print 'UNKNOWN: immedeate data of type', type
+	def savedata(self, url, type):
+		if not self.DEFAULT_MEDIA_TYPES.has_key(type):
+			print 'UNKNOWN: immedeate data of type', type
+			print
+			return None
+		new = newfilename('immdata', self.DEFAULT_MEDIA_TYPES[type])
+		fp = createurlfile(new)
+		fp.write(urllib.urlopen(url).read())
+		fp.close()
+		return new
+
+	def convert_unknown(self, url):
+			print 'UNKNOWN:', url
+			print
+			return None
+
+class G2UserConverter(DataConverter):
+	def userconvert(self, url, filebase, ext):
+		new = newfilename(filebase, ext)
+		print "CONVERT:", url
+		print "     TO ", new
 		print
-		return None
-	new = newfilename('immdata', G2_TYPES[type])
-	fp = createurlfile(new)
-	fp.write(urllib.urlopen(url).read())
-	fp.close()
-	return new
+		return new
+		
+	def usercheck(self, url):
+		print "  CHECK:", url
+		print
+		return 1
+
+	CONVERTERS={
+		'.au': (userconvert, ('.ra',)),
+		'.aif': (userconvert, ('.ra',)),
+		'.aiff': (userconvert, ('.ra',)),
+		'.wav': (userconvert, ('.ra',)),
+		'.gif': (userconvert, ('.jpg',)),
+		'.txt': (userconvert, ('.rt',)),
+		'.htm': (userconvert, ('.rt',)),
+		'.html': (userconvert, ('.rt',)),
+		'.qt': (userconvert, ('.rv',)),
+		'.mov': (userconvert, ('.rv',)),
+		'.mpg': (userconvert, ('.rv',)),
+		'.mpv': (userconvert, ('.rv',)),
+	}
+	DEFAULT_MEDIA_TYPES={
+		'audio': '.ra',
+		'img': '.jpg',
+		'text': '.rt',
+		'textstream': '.rt',
+		'video': '.rv',
+	}
+
+class G2BatchConverter(DataConverter):
+	def __init__(self, fp):
+		self.fp = fp
+		
+	def filenames(self, url, filebase, ext):
+		new = newfilename(filebase, ext)
+		old_pathname = self.url2pathname(url)
+		if not old_pathname or not os.path.exists(old_pathname):
+			print "CONVERT:", url
+			print "     TO ", new
+			print
+			return None, None, new
+		new_pathname = self.url2pathname(new)
+		if os.path.exists(new_pathname):
+			oldtime = os.stat(old_pathname)[8]
+			newtime = os.stat(new_pathname)[8]
+			if newtime > oldtime:
+				print "UPTODATE:", new
+				print
+				return None, None, new
+		return old_pathname, new_pathname, new
+
+	def url2pathname(self, url):
+		scheme, location, path, parameters, query, fragment = \
+			urlparse.urlparse(url)
+		if scheme and scheme != 'file':
+			return None
+		if location or parameters or query or fragment:
+			return None
+		return urllib.url2pathname(path)
+	
+	def usercheck(self, url):
+		print "  CHECK:", url
+		print
+		return 1
+
+class G2UnixBatchConverter(G2BatchConverter):
+
+	def convert_image(self, url, filebase, ext):
+		old, new, newurl = self.filenames(url, filebase, ext)
+		if old and new:
+			self.fp.write('cjpeg -restart 1B "%s" > "%s"\n' %
+				        (old, new))
+		return newurl
+
+	def convert_av(self, url, filebase, ext):
+		old, new, newurl = self.filenames(url, filebase, ext)
+		if old and new:
+			self.fp.write('rmenc -I "%s" -O "%s"\n' %
+				        (old, new))
+		return newurl
+
+	def convert_text(self, url, filebase, ext):
+		new = newfilename(filebase, ext)
+		print "CONVERT:", url
+		print "     TO ", new
+		print
+		return new
+		
+	CONVERTERS={
+		'.au': (convert_av, ('.ra',)),
+		'.aif': (convert_av, ('.ra',)),
+		'.aiff': (convert_av, ('.ra',)),
+		'.wav': (convert_av, ('.ra',)),
+		'.gif': (convert_image, ('.jpg',)),
+		'.txt': (convert_text, ('.rt',)),
+		'.htm': (convert_text, ('.rt',)),
+		'.html': (convert_text, ('.rt',)),
+		'.qt': (convert_av, ('.rv',)),
+		'.mov': (convert_av, ('.rv',)),
+		'.mpg': (convert_av, ('.rv',)),
+		'.mpv': (convert_av, ('.rv',)),
+	}
+	DEFAULT_MEDIA_TYPES={
+		'audio': '.ra',
+		'img': '.jpg',
+		'text': '.rt',
+		'textstream': '.rt',
+		'video': '.rv',
+	}
 
 USED={}
 def newfilename(filebase, ext):
@@ -216,43 +403,8 @@ def createurlfile(url):
 	if dirname and not os.path.exists(dirname):
 		os.makedirs(dirname)
 	return open(filename, 'w')
+	converter = G2UnixBatchConverter(open('@g2conf.sh', 'w'))
 
-def canhandle(node):
-	if node.NodeType == DOCUMENT:
-		return canhandle(node.documentElement)
-	ok = 1
-	if node.NodeType == ELEMENT:
-		if node.GI == 'switch':
-			for child in node.getChildren():
-				if canhandle(child):
-					return 1
-			return 0
-		elif node.GI in MEDIA_NODES:
-			if node.attributes.has_key('src'):
-				url = node.attributes['src']
-				if not canhandleurl(url):
-					print 'cannot handle', url
-					ok = 0
-		for child in node.getChildren():
-			if not canhandle(child):
-				ok = 0
-	return ok
-
-def canhandleurl(url):
-	scheme, location, path, parameters, query, fragment = \
-		urlparse.urlparse(url)
-	if not scheme in OK_SCHEMES:
-		return 0
-	dummy, ext = posixpath.splitext(path)
-	if not OK_EXTENSIONS.has_key(ext):
-		return 0
-	if OK_EXTENSIONS[ext]:
-		return OK_EXTENSIONS[ext](url)
-	return 1
-	
-def transform(document):
-	transformer = G2Transformer()
-	return transformer.transform(document)
 
 def main():
 	if len(sys.argv) != 3:
@@ -263,7 +415,10 @@ def main():
 	parser = DcBuilder()
 	parser.feed(data)
 
-	document = transform(parser.document)
+	converter = G2UnixBatchConverter(open('@g2conf.sh', 'w'))
+	transformer = G2OnlyTransformer(converter)
+
+	document = transformer.transform(parser.document)
 
 	outfile = open(sys.argv[2], 'w')
 	writer = XmlWriter(outfile)
