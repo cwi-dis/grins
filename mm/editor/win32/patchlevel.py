944c214d417d8f425d077dd164f940dd1e2b74f8
anagnost <anagnost@localhost>
1998-11-16 13:51:08 +0000

    Editor patchlevel. Starting with 1

---
+++ mm/editor/win32/patchlevel.py
@@ -0,0 +1,14 @@
+__version__ = "$Id$"
+
+patchlevel = 'win32 patchlevel 1'
+
+
+patchinfo= """
+1. The application display now the proper version and patchlevel
+2. The anchors for the Video channel work now properly after stop/play sequence
+3. A select Charset dialog added for the benefit of multilingua machines 
+   (for example Greek/English)
+4. The ArcInfoDialog response to the Apply button now works
+5. Some improvements in repainting
+6. The 'Show log/debug window' is now enabled. Please use it in order to help us debug the application.
+"""
