c596ea60e64b19702e60116e9244b52e438fec43
Sjoerd Mullender <sjoerdmullender@users.noreply.github.com>
1998-11-17 15:38:10 +0000

    Converted audio modules into a package.

---
+++ mm/pylib/audio/file.py
@@ -0,0 +1,59 @@
+__version__ = "$Id$"
+
+import what, string
+from audio import Error
+
+# This dummy function is there to make freeze import the right modules
+def _dummy_freeze_func():
+	import aifc
+	import au
+	import hcom
+	import voc
+	import wav
+	import svx8
+	import sndt
+	import sndr
+	
+class _FileDict:
+	__fmtdict = {
+		'aiff': 'aifc',
+		'aifc': 'aifc',
+		'au': 'au',
+		'hcom': 'hcom',
+		'voc': 'voc',
+		'wav': 'wav',
+		'8svx': 'svx8',
+		'sndt': 'sndt',
+		'sndr': 'sndr',
+		}
+	has_key = __fmtdict.has_key
+	keys = __fmtdict.keys
+
+	def __init__(self):
+		self.__cache = {}
+
+	def __getitem__(self, key):
+		try:
+			return self.__cache[key]
+		except KeyError:
+			pass
+		module = self.__fmtdict[key]
+		try:
+			exec 'import %s' % module
+		except ImportError:
+			raise Error, 'No support for audio file type: '+key
+		module = eval(module)
+		self.__cache[key] = module
+		return module
+_filedict = _FileDict()
+
+def reader(filename):
+	file = open(filename, 'rb')
+	filetype = what.what(file)
+	if not filetype:
+		file.close()
+		raise Error, 'Unknown audio file type (bad magic number)'
+	if not _filedict.has_key(filetype):
+		file.close()
+		raise Error, 'No support for audio file type: '+filetype[0]
+	return _filedict[filetype].reader(file)
