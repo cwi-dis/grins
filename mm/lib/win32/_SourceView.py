3feb49d02a81a66479b1b00f7caf5d71c6a72c1a
anagnost <anagnost@localhost>
1998-11-30 11:29:05 +0000

    Documentation strings added plus minor changes

--- mm/lib/win32/_SourceView.py
+++ mm/lib/win32/_SourceView.py
@@ -1,3 +1,11 @@
+__version__ = "$Id$"
+
+""" @win32doc|_SourceView
+This module contains the ui implementation of the source
+viewer. It is an MFC EditView with the read only attribute set.
+It is exposed to Python through the win32ui pyd as PyCEditView
+"""
+
 import win32ui,win32con
 Sdk=win32ui.GetWin32Sdk()
 Afx=win32ui.GetAfx()
@@ -10,26 +18,31 @@ from pywin.mfc import window,object,docview
 import afxres,commctrl
 
 class _SourceView(docview.EditView):
+	# Class contructor. Calls the base EditView constructor
 	def __init__(self,doc):
 		docview.EditView.__init__(self,doc)
 		self._text=''
 		self._close_cmd_list=[]
 
+	# Create the OS window
 	def createWindow(self,parent):
 		self.CreateWindow(parent)
-
+	
+	# Called by the framework after the OS window has been created
 	def OnInitialUpdate(self):
 		edit=self.GetEditCtrl()
 		edit.SetWindowText(self._text)
 		edit.SetReadOnly(1)
 		self._mdiframe=(self.GetParent()).GetMDIFrame()
 
+	# Called by the framework before this window is closed
 	def OnClose(self):
 		if self._closecmdid>0:
 			self.GetParent().GetMDIFrame().PostMessage(win32con.WM_COMMAND,self._closecmdid)
 		else:
 			self.GetParent().DestroyWindow()
 
+	# Called by the framework when this view is activated or deactivated
 	def onActivate(self,f):
 		pass
 		#if f: self._mdiframe.set_commandlist(self._close_cmd_list)
@@ -37,9 +50,11 @@ class _SourceView(docview.EditView):
 
 
 	# cmif interface
+	# Set the text to be shown
 	def settext(self,text):
 		self._text=self.convert2ws(text)
 
+	# Convert the text from unix or mac to windows
 	def convert2ws(self,text):
 		import string
 		nl=string.split(text,'\n')
@@ -50,7 +65,8 @@ class _SourceView(docview.EditView):
 			return string.join(nl, '\r\n')
 		if len(nl)<len(rl):	# line_separator='\r'
 			return string.join(rl, '\r\n')
-			
+	
+	# Called by the framework to close the view		
 	def close(self):
 		# 1. clean self contends
 		del self._text
@@ -61,6 +77,7 @@ class _SourceView(docview.EditView):
 		if hasattr(self,'_obj_') and self._obj_:
 			self.GetParent().DestroyWindow()
 
+	# Returns true if this view is already closed
 	def is_closed(self):
 		if self._obj_==None: return 1
 		if self.GetSafeHwnd()==0: return 1
