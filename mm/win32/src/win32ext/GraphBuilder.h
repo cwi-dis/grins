cffddb30d722fff7879cc5e891acc4d0f6cd2a2a
anagnost <anagnost@localhost>
1998-11-30 11:48:32 +0000

    Documentation strings added

--- mm/win32/src/win32ext/GraphBuilder.h
+++ mm/win32/src/win32ext/GraphBuilder.h
@@ -9,6 +9,20 @@
 #include <uuids.h>
 #pragma comment (lib,"strmiids.lib")
 
+class GraphBuilder
+	{
+	public:
+	GraphBuilder(IGraphBuilder* p):m_pI(p),m_pIMP(NULL){}
+	virtual ~GraphBuilder(){Release();}
+	void Release()
+		{
+		if(m_pIMP)m_pIMP->Release();m_pIMP=NULL;
+		if(m_pI)m_pI->Release();m_pI=NULL;
+		}
+	IGraphBuilder* m_pI;
+	IMediaPosition* m_pIMP;
+	};
+
 class GraphBuilderCreator
 	{
 	public:
