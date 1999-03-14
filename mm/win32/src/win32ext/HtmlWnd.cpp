91b1113571a188842e8fb1c2c3f429f089d28bc4
mm <mm@localhost>
1999-03-14 16:45:37 +0000

    HtmlWnd supports both Internet Explorer control and Webster control. The control to use can be selected dynamically. Some more methods added. Webster control is initialized to have scrollbars when needed.

--- mm/win32/src/win32ext/HtmlWnd.cpp
+++ mm/win32/src/win32ext/HtmlWnd.cpp
@@ -1,8 +1,3 @@
-// CMIF_ADD
-//
-// kk@epsilon.com.gr
-//
-
 #include "stdafx.h"
 
 #include "win32win.h"
@@ -11,8 +6,11 @@
 #include <afxdisp.h>
 #include <exdisp.h>
 
-#define RELEASE(lpUnk) do \
-	{ if ((lpUnk) != NULL) { (lpUnk)->Release(); (lpUnk) = NULL; } } while (0)
+// IE_CONTROL (0)
+#include "webbrowser2.h"
+
+// WEBSTER_CONTROL (1)
+#include "websterpro.h"
 
 // the c++/mfc class 
 class CHtmlWnd: public CWnd
@@ -20,51 +18,48 @@ class CHtmlWnd: public CWnd
 	protected:
 	DECLARE_DYNCREATE(CHtmlWnd)
 	DECLARE_EVENTSINK_MAP()
+	DECLARE_MESSAGE_MAP()
 
-	// Overrides
 	public:
+	CHtmlWnd():m_selHtmlCtrl(0),m_bCtrlCreated(false),m_isclient(false){}
+	virtual ~CHtmlWnd(){}
 	virtual BOOL Create(LPCTSTR lpszClassName, LPCTSTR lpszWindowName,
 		DWORD dwStyle, const RECT& rect, CWnd* pParentWnd, UINT nID,
 		CCreateContext* pContext = NULL);
+	
+	// Browser Control Selection
+	void UseHtmlCtrl(int selControl){m_selHtmlCtrl=selControl;}
+	void UseIECtrl(){m_selHtmlCtrl=IE_CONTROL;}
+	void UseWebsterCtrl(){m_selHtmlCtrl=WEBSTER_CONTROL;}
+	int m_selHtmlCtrl;
+	enum {IE_CONTROL=0,WEBSTER_CONTROL=1};
+
+
+	// Control creation
 	BOOL CreateHtmlCtrl();
 	void DestroyHtmlCtrl();
-	void FitHtmlCtrl();
-
-	public:
-	// Events
-	virtual void OnBeforeNavigate2(LPCTSTR lpszURL, DWORD nFlags,
-		LPCTSTR lpszTargetFrameName, CByteArray& baPostedData,
-		LPCTSTR lpszHeaders, BOOL* pbCancel);
-	
-	// Event reflectors (not normally overridden)
-	protected:
-	virtual void BeforeNavigate2(LPDISPATCH pDisp, VARIANT* URL,
-		VARIANT* Flags, VARIANT* TargetFrameName, VARIANT* PostData,
-		VARIANT* Headers,   BOOL* Cancel);
-
 
 	// Operations
 	public:
-	void GoBack();
-	void GoForward();
-	void GoHome();
-	void GoSearch();
-	void Navigate(LPCTSTR URL, DWORD dwFlags = 0,
-		LPCTSTR lpszTargetFrameName = NULL,
-		LPCTSTR lpszHeaders = NULL, LPVOID lpvPostData = NULL,
-		DWORD dwPostDataLen = 0);
+	void Navigate(LPCTSTR lpszURL);
+	void SetImmHtml(LPCTSTR str);
+	void Refresh();
+	void SetBackColor(COLORREF clr);
 
-	public:
-	CHtmlWnd():m_pBrowser(NULL),m_isclient(false){}
-	virtual ~CHtmlWnd(){RELEASE(m_pBrowser);}
-	CWnd m_wndBrowser;
-	IWebBrowser* m_pBrowser;
-	bool m_isclient;
-
-	// custom support
-	CString m_foreignUrl;
-	LPCTSTR GetForeignUrl(){return m_foreignUrl;}
+	// Dim helpers
 	void SetClient(bool b){m_isclient=b;}
+	void FitHtmlCtrl();
+	// custom url support
+	LPCTSTR GetForeignUrl(){return m_foreignUrl;}
+	CString m_foreignUrl;
+
+
+	private:
+	void CheckForCustomURL(LPCTSTR pszURL,BOOL* bCancel);
+	CWebBrowser2 m_wndWebBrowser;
+	CWebsterPro  m_wndWebsterBrowser;
+	bool m_isclient;
+	bool m_bCtrlCreated;
 
 	// Generated message map functions
 	protected:
@@ -72,11 +67,15 @@ class CHtmlWnd: public CWnd
 	afx_msg void OnSize(UINT nType, int cx, int cy);
 	afx_msg void OnPaint();
 	afx_msg void OnDestroy();
+	// IE
+	afx_msg void OnBeforeNavigateIE(LPDISPATCH pDisp, VARIANT FAR* URL, VARIANT FAR* Flags, VARIANT FAR* TargetFrameName, VARIANT FAR* PostData, VARIANT FAR* Headers, BOOL FAR* Cancel);
+	// Webster
+	afx_msg void OnBeforeNavigateWebster(BSTR FAR* URL, long mFlags, long nHandle, BSTR FAR* TargetName, BSTR FAR* ExtraHeaders, BSTR FAR* TextToPost, BOOL FAR* Cancel);
 	//}}AFX_MSG
-	DECLARE_MESSAGE_MAP()
-
-
 	};
+
+///////////////////////////////////////////////
+//   IMPLEMENTATION
 
 IMPLEMENT_DYNCREATE(CHtmlWnd, CWnd)
 
@@ -90,13 +89,14 @@ END_MESSAGE_MAP()
 
 BEGIN_EVENTSINK_MAP(CHtmlWnd, CWnd)
     //{{AFX_EVENTSINK_MAP(CContainerWnd)
-	ON_EVENT(CHtmlWnd, AFX_IDW_PANE_FIRST, 250 /* BeforeNavigate2 */, BeforeNavigate2, VTS_DISPATCH VTS_PVARIANT VTS_PVARIANT VTS_PVARIANT VTS_PVARIANT VTS_PVARIANT VTS_PBOOL)
+	ON_EVENT(CHtmlWnd, AFX_IDW_PANE_FIRST, 250 /* BeforeNavigate2 */, OnBeforeNavigateIE, VTS_DISPATCH VTS_PVARIANT VTS_PVARIANT VTS_PVARIANT VTS_PVARIANT VTS_PVARIANT VTS_PBOOL)
+	ON_EVENT(CHtmlWnd, AFX_IDW_PANE_FIRST, 12 /* BeforeNavigate */, OnBeforeNavigateWebster, VTS_PBSTR VTS_I4 VTS_I4 VTS_PBSTR VTS_PBSTR VTS_PBSTR VTS_PBOOL)
 	//}}AFX_EVENTSINK_MAP
 END_EVENTSINK_MAP()
 
 void CHtmlWnd::OnDestroy()
 	{
-	RELEASE(m_pBrowser);
+	m_bCtrlCreated=false;
 	}
 
 void CHtmlWnd::OnSize(UINT nType, int cx, int cy)
@@ -110,74 +110,41 @@ void CHtmlWnd::OnPaint()
 	Default();
 	}
 
-
-void CHtmlWnd::BeforeNavigate2(LPDISPATCH /* pDisp */, VARIANT* URL,
-		VARIANT* Flags, VARIANT* TargetFrameName,
-		VARIANT* PostData, VARIANT* Headers, BOOL* Cancel)
+void CHtmlWnd::OnBeforeNavigateWebster(BSTR FAR* URL, long mFlags, long nHandle, 
+	BSTR FAR* TargetName, BSTR FAR* ExtraHeaders, BSTR FAR* TextToPost, BOOL FAR* Cancel)
 	{
-	ASSERT(V_VT(URL) == VT_BSTR);
-	ASSERT(V_VT(TargetFrameName) == VT_BSTR);
-	ASSERT(V_VT(PostData) == (VT_VARIANT | VT_BYREF));
-	ASSERT(V_VT(Headers) == VT_BSTR);
-	ASSERT(Cancel != NULL);
-
-	USES_CONVERSION;
-
-	VARIANT* vtPostedData = V_VARIANTREF(PostData);
-	CByteArray array;
-	if (V_VT(vtPostedData) & VT_ARRAY)
-		{
-		// must be a vector of bytes
-		ASSERT(vtPostedData->parray->cDims == 1 && vtPostedData->parray->cbElements == 1);
-
-		vtPostedData->vt |= VT_UI1;
-		COleSafeArray safe(vtPostedData);
-
-		DWORD dwSize = safe.GetOneDimSize();
-		LPVOID pVoid;
-		safe.AccessData(&pVoid);
-
-		array.SetSize(dwSize);
-		LPBYTE lpByte = array.GetData();
-
-		memcpy(lpByte, pVoid, dwSize);
-		safe.UnaccessData();
-		}
-	// make real parameters out of the notification
-
-	CString strTargetFrameName(V_BSTR(TargetFrameName));
-	CString strURL = V_BSTR(URL);
-	CString strHeaders = V_BSTR(Headers);
-	DWORD nFlags = V_I4(Flags);
-
-	// notify the user's class
-	OnBeforeNavigate2(strURL, nFlags, strTargetFrameName,
-		array, strHeaders, Cancel);
+	CString strURL(*URL);
+	CheckForCustomURL(strURL,Cancel);
 	}
 
-void CHtmlWnd::OnBeforeNavigate2(LPCTSTR lpszURL, DWORD nFlags,
-	LPCTSTR lpszTargetFrameName, CByteArray& baPostData,
-	LPCTSTR lpszHeaders, BOOL* bCancel)
+void CHtmlWnd::OnBeforeNavigateIE(
+	LPDISPATCH pDisp, 
+	VARIANT FAR* URL, 
+	VARIANT FAR* Flags, 
+	VARIANT FAR* TargetFrameName, 
+	VARIANT FAR* PostData, 
+	VARIANT FAR* Headers, 
+	BOOL FAR* Cancel) 
 	{
-	// default to continuing
+	CString strURL((BSTR)URL->bstrVal);
+	CheckForCustomURL(strURL,Cancel);
+	}
+
+void CHtmlWnd::CheckForCustomURL(LPCTSTR pszURL,BOOL* bCancel)
+	{
+	// default is to continue
 	*bCancel = FALSE;
 
 	// check for foreign format
-	CString url(lpszURL);
+	CString url(pszURL);
 	CString prefix= "cmif:";
 	if (url.Find(prefix)!=-1)
 		{
 		m_foreignUrl=url;
 		PostMessage(WM_USER); // notify user to get it
 		*bCancel=TRUE;
-		return;
 		}	
-	UNUSED_ALWAYS(nFlags);
-	UNUSED_ALWAYS(lpszTargetFrameName);
-	UNUSED_ALWAYS(baPostData);
-	UNUSED_ALWAYS(lpszHeaders);
 	}
-
 
 /////////////////////////////////////////////////////////////////////////////
 // CHtmlWnd operations
@@ -187,97 +154,158 @@ BOOL CHtmlWnd::Create(LPCTSTR lpszClassName, LPCTSTR lpszWindowName,
 						UINT nID, CCreateContext* pContext)
 	{
 	if (!CWnd::Create(lpszClassName, lpszWindowName,
-				dwStyle, rect, pParentWnd,  nID, pContext))
+		dwStyle, rect, pParentWnd,  nID, pContext))
 		return FALSE;
 	return TRUE;
 	}
 
+
 BOOL CHtmlWnd::CreateHtmlCtrl()
 	{
-	if(m_pBrowser) return TRUE;
+	if(m_bCtrlCreated) return TRUE;
 
 	AfxEnableControlContainer();
 
 	RECT rectClient;
 	GetClientRect(&rectClient);
 
-	// create the control window
-	// AFX_IDW_PANE_FIRST is a safe but arbitrary ID
-	if (!m_wndBrowser.CreateControl(CLSID_WebBrowser, "untitled",
-				WS_VISIBLE | WS_CHILD, rectClient, this, AFX_IDW_PANE_FIRST))
+	// create the control
+	if(m_selHtmlCtrl==WEBSTER_CONTROL)
 		{
-		DestroyWindow();
-		return FALSE;
+		if (!m_wndWebsterBrowser.Create(NULL, "untitled",
+				WS_CHILD, rectClient, this, AFX_IDW_PANE_FIRST))
+			{
+			DestroyWindow();
+			return FALSE;
+			}
+		m_bCtrlCreated=true;
+		m_wndWebsterBrowser.SetBorderStyle(0);
+		m_wndWebsterBrowser.SetScrollPosVertical(2);
+		m_wndWebsterBrowser.SetScrollbarStyleHorizontal(2);
+		m_wndWebsterBrowser.SetTitleWindowStyle(0);
+		m_wndWebsterBrowser.SetUrlWindowStyle(0);
+		m_wndWebsterBrowser.SetBevelStyleInner(0);
+		m_wndWebsterBrowser.SetBevelStyleOuter(0);
+		m_wndWebsterBrowser.SetBackColor((OLE_COLOR)RGB(255,255,255));
+		m_wndWebsterBrowser.SetBrowserName("GRiNS");
+		m_wndWebsterBrowser.Navigate("File:///Empty", 
+                           NAV_GET | NAV_NAVCREATEFROMTEXT, 
+                           0, 
+                           "",
+                           "<HTML><BODY></BODY></HTML> ",
+                           "");
+		m_wndWebsterBrowser.ShowWindow(SW_SHOW);	
 		}
-
-	LPUNKNOWN lpUnk = m_wndBrowser.GetControlUnknown();
-	HRESULT hr = lpUnk->QueryInterface(IID_IWebBrowser2, (void**) &m_pBrowser);
-	if (!SUCCEEDED(hr))
+	else
 		{
-		m_pBrowser = NULL;
-		m_wndBrowser.DestroyWindow();
-		DestroyWindow();
-		return FALSE;
+		if (!m_wndWebBrowser.Create(NULL, "untitled",
+				WS_CHILD, rectClient, this, AFX_IDW_PANE_FIRST))
+			{
+			DestroyWindow();
+			return FALSE;
+			}
+		m_wndWebBrowser.Navigate("about:",NULL,NULL,NULL,NULL);
+		m_wndWebBrowser.ShowWindow(SW_SHOW);
+		m_bCtrlCreated=true;
 		}
-
 	return TRUE;
 	}
 
 void CHtmlWnd::DestroyHtmlCtrl()
 	{
-	if(m_pBrowser)
-		{
-		m_pBrowser->Release();
-		m_pBrowser=NULL;
-		m_wndBrowser.DestroyWindow();
-		}
+	if(!m_bCtrlCreated) return;
+	if(m_selHtmlCtrl==WEBSTER_CONTROL)
+		m_wndWebsterBrowser.DestroyWindow();
+	else
+		m_wndWebBrowser.DestroyWindow();
 	}
 
 
 void CHtmlWnd::FitHtmlCtrl()
 	{
-	if (::IsWindow(m_wndBrowser.m_hWnd))
+	if (m_bCtrlCreated)
 		{
+		CWnd* pwndBrowser=NULL;
+		if(m_selHtmlCtrl==WEBSTER_CONTROL)
+			pwndBrowser=(CWnd*)&m_wndWebsterBrowser;
+		else
+			pwndBrowser=(CWnd*)&m_wndWebBrowser;
 		// need to push non-client borders out of the client area
 		CRect rect;
 		GetClientRect(rect);
 		::AdjustWindowRectEx(rect,
-			m_wndBrowser.GetStyle(), FALSE, WS_EX_CLIENTEDGE);
+			pwndBrowser->GetStyle(), FALSE, WS_EX_CLIENTEDGE);
 		if(m_isclient)
-			m_wndBrowser.SetWindowPos(NULL, rect.left, rect.top,
+			pwndBrowser->SetWindowPos(NULL, rect.left, rect.top,
 				rect.Width(), rect.Height(), SWP_NOACTIVATE | SWP_NOZORDER);
 		else
-			m_wndBrowser.SetWindowPos(NULL, rect.left+2, rect.top+2,
+			pwndBrowser->SetWindowPos(NULL, rect.left+2, rect.top+2,
 				rect.Width()-4, rect.Height()-4, SWP_NOACTIVATE | SWP_NOZORDER);
 		}
 	}
 
-void CHtmlWnd::Navigate(LPCTSTR lpszURL, DWORD dwFlags /* = 0 */,
-	LPCTSTR lpszTargetFrameName /* = NULL */ ,
-	LPCTSTR lpszHeaders /* = NULL */, LPVOID lpvPostData /* = NULL */,
-	DWORD dwPostDataLen /* = 0 */)
+// valid local file URL is
+// ///F|/SMIL/webnews/the.news/html/leader_title.html
+// or 
+// file:///F|/SMIL/webnews/the.news/html/leader_title.html
+void CHtmlWnd::Navigate(LPCTSTR lpszURL)
 	{
-	if(!m_pBrowser) return;
+	if(!m_bCtrlCreated){if(!CreateHtmlCtrl()) return;}
 
-	CString strURL(lpszURL);
-	BSTR bstrURL = strURL.AllocSysString();
-
-	COleSafeArray vPostData;
-	if (lpvPostData != NULL)
+	if(m_selHtmlCtrl==WEBSTER_CONTROL)
+		m_wndWebsterBrowser.Navigate(lpszURL,NULL, NULL, "", "", "");
+	else
 		{
-		if (dwPostDataLen == 0)
-			dwPostDataLen = lstrlen((LPCTSTR) lpvPostData);
-
-		vPostData.CreateOneDim(VT_UI1, dwPostDataLen, lpvPostData);
+		// if URL refers a local file convert it to IE convention
+		// i.e put the prefix 'file:'
+		CString str("file:");
+		if(lpszURL[0]=='/' && lpszURL[1]=='/' && lpszURL[2]=='/')
+			{
+			str+=lpszURL;
+			m_wndWebBrowser.Navigate(str,NULL,NULL,NULL,NULL);
+			}
+		else
+			m_wndWebBrowser.Navigate(lpszURL,NULL,NULL,NULL,NULL);
 		}
-
-	m_pBrowser->Navigate(bstrURL,
-		COleVariant((long) dwFlags, VT_I4),
-		COleVariant(lpszTargetFrameName, VT_BSTR),
-		vPostData,
-		COleVariant(lpszHeaders, VT_BSTR));
 	}
 
+void CHtmlWnd::SetImmHtml(LPCTSTR str)
+	{
+	if(!m_bCtrlCreated){if(!CreateHtmlCtrl()) return;}
+	if(m_selHtmlCtrl==WEBSTER_CONTROL)
+		{
+		m_wndWebsterBrowser.Navigate("File:///Imm", 
+                           NAV_GET | NAV_NAVCREATEFROMTEXT, 
+                           0, 
+                           "",
+                           str,
+                           "");
+		}
+	else
+		{
+		CString strp("about:");
+		m_wndWebBrowser.Navigate(strp+str,NULL,NULL,NULL,NULL);
+		}
+	}
+void CHtmlWnd::Refresh()
+	{
+	if(!m_bCtrlCreated)return;
+
+	if(m_selHtmlCtrl==WEBSTER_CONTROL)
+		m_wndWebsterBrowser.Refresh();
+	else
+		m_wndWebBrowser.Refresh();
+	}
+void CHtmlWnd::SetBackColor(COLORREF clr)
+	{
+	if(!m_bCtrlCreated){if(!CreateHtmlCtrl()) return;}
+
+	if(m_selHtmlCtrl==WEBSTER_CONTROL)
+		m_wndWebsterBrowser.SetBackColor(OLE_COLOR(clr));		
+
+	}
+
+/////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////
 // the respective Python class
 class PYW_EXPORT PyCHtmlWnd : public PyCWnd 
@@ -359,6 +387,24 @@ py_create_window(PyObject *self, PyObject *args)
 	RETURN_NONE;
 	}
 
+  // @pymethod |PyCHtmlWnd|UseControl|Set which control will be used (IE=0 or WEBSTER=1)
+static PyObject *
+py_use_html_ctrl(PyObject *self, PyObject *args)
+	{
+	int selCtrl;
+	if(!PyArg_ParseTuple(args, "i", &selCtrl))
+		return NULL;
+
+	CHtmlWnd *pHtmlWnd=PyCHtmlWnd::GetHtmlWndPtr(self);
+	if(pHtmlWnd==NULL) return NULL;
+
+	GUI_BGN_SAVE;
+	pHtmlWnd->UseHtmlCtrl(selCtrl);
+	GUI_END_SAVE;
+
+	RETURN_NONE;
+	}
+
 // @pymethod |PyCHtmlWnd|Navigate|Navigate to url
 static PyObject *
 py_navigate(PyObject *self, PyObject *args)
@@ -370,19 +416,30 @@ py_navigate(PyObject *self, PyObject *args)
 	CHtmlWnd *pHtmlWnd=PyCHtmlWnd::GetHtmlWndPtr(self);
 	if(pHtmlWnd==NULL) return NULL;
 
-
 	GUI_BGN_SAVE;
-	if(pHtmlWnd->m_pBrowser==NULL)
-		pHtmlWnd->CreateHtmlCtrl();
-	pHtmlWnd->Navigate(strUrl,NULL,NULL,NULL,NULL);
+	pHtmlWnd->Navigate(strUrl);
 	GUI_END_SAVE;
-
-	if(pHtmlWnd->m_pBrowser==NULL) 
-			RETURN_ERR("Failed to create Browser control. Check that IE4 is installed");
-	
 
 	RETURN_NONE;
 	}
+// @pymethod |PyCHtmlWnd|SetImmHtml|Set immediate html
+static PyObject *
+py_set_imm_html(PyObject *self, PyObject *args)
+	{
+	char* strHtml;
+	if(!PyArg_ParseTuple(args, "s", &strHtml))
+		return NULL;
+
+	CHtmlWnd *pHtmlWnd=PyCHtmlWnd::GetHtmlWndPtr(self);
+	if(pHtmlWnd==NULL) return NULL;
+
+	GUI_BGN_SAVE;
+	pHtmlWnd->SetImmHtml(strHtml);
+	GUI_END_SAVE;
+
+	RETURN_NONE;
+	}
+
 // @pymethod |PyCHtmlWnd|Refresh|Refresh 
 static PyObject *
 py_refresh(PyObject *self, PyObject *args)
@@ -390,11 +447,10 @@ py_refresh(PyObject *self, PyObject *args)
 	CHtmlWnd *pHtmlWnd=PyCHtmlWnd::GetHtmlWndPtr(self);
 	if(pHtmlWnd==NULL) return NULL;
 
-
 	GUI_BGN_SAVE;
-	if(pHtmlWnd->m_pBrowser!=NULL)
-		pHtmlWnd->m_pBrowser->Refresh();
+	pHtmlWnd->Refresh();
 	GUI_END_SAVE;
+
 	RETURN_NONE;
 	}
 
@@ -413,6 +469,25 @@ py_set_client(PyObject *self, PyObject *args)
 
 	GUI_BGN_SAVE;
 	pHtmlWnd->SetClient((isclient!=0));
+	GUI_END_SAVE;
+	RETURN_NONE;
+	}
+
+
+// @pymethod |PyCHtmlWnd|SetBackColor|Sets the background color
+static PyObject *
+py_set_back_color(PyObject *self, PyObject *args)
+	{
+	long color;
+	if(!PyArg_ParseTuple(args, "i",&color))
+		return NULL;
+
+	CHtmlWnd *pHtmlWnd=PyCHtmlWnd::GetHtmlWndPtr(self);
+	if(pHtmlWnd==NULL) return NULL;
+
+
+	GUI_BGN_SAVE;
+	pHtmlWnd->SetBackColor(color);
 	GUI_END_SAVE;
 	RETURN_NONE;
 	}
@@ -436,11 +511,11 @@ py_create_html_ctrl(PyObject *self, PyObject *args)
 	if(pHtmlWnd==NULL) return NULL;
 
 	GUI_BGN_SAVE;
-	pHtmlWnd->CreateHtmlCtrl();
+	BOOL bRes=pHtmlWnd->CreateHtmlCtrl();
 	GUI_END_SAVE;
 
-	if(pHtmlWnd->m_pBrowser==NULL) 
-			RETURN_ERR("Failed to create Browser control. Check that IE4 is installed");
+	if(!bRes) 
+		RETURN_ERR("Failed to create Browser control. Check that the browser control you have selected is installed");
 
 	RETURN_NONE;
 	}
@@ -475,15 +550,17 @@ py_fit_html_ctrl(PyObject *self, PyObject *args)
 	RETURN_NONE;
 	}
 
-//	{"CreateWindow",py_create_window,1}, // @pymeth CreateWindow|Create the underlying window object
 
 //////////////////////////////////////////////////////
 // @object PyCHtmlWnd|
 static struct PyMethodDef PyCHtmlWnd_methods[] = {
 	{"Navigate",py_navigate,1},
+	{"SetImmHtml",py_set_imm_html,1},
 	{"Refresh",py_refresh,1},
+	{"SetBackColor",py_set_back_color,1},
 	{"SetClient",py_set_client,1},
 	{"GetForeignUrl",py_get_foreign_url,1},
+	{"UseHtmlCtrl",py_use_html_ctrl,1},
 	{"CreateHtmlCtrl",py_create_html_ctrl,1},
 	{"DestroyHtmlCtrl",py_destroy_html_ctrl,1},
 	{"FitHtmlCtrl",py_fit_html_ctrl,1},
