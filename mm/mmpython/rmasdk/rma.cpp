89ee25fc512d9252c4f6f654abfdb0ba01faf47c
mm <mm@localhost>
1999-04-15 09:10:25 +0000

    The main rma module. For the moment it exports two types through the functions CreateEngine and CreatePlayer.

---
+++ mm/mmpython/rmasdk/rma.cpp
@@ -0,0 +1,28 @@
+#include "std.h"
+#include "PyCppApi.h"
+
+static char moduleName[] = "rma";
+static char errorName[] =  "rma";
+PyObject *module_error;
+
+// Objects served by this module
+extern PyObject* EngineObject_CreateInstance(PyObject *self, PyObject *args);
+extern PyObject* PlayerObject_CreateInstance(PyObject *self, PyObject *args);
+
+static struct PyMethodDef module_functions[] = {
+    {"CreateEngine",EngineObject_CreateInstance,1}, 
+    {"CreatePlayer",PlayerObject_CreateInstance,1}, 
+	{NULL,NULL}
+};
+
+PY_API
+void initrma()
+	{
+	PyObject *module = Py_InitModule(moduleName,module_functions);
+	PyObject *dict = PyModule_GetDict(module);
+	module_error = PyString_FromString(errorName);
+	PyDict_SetItemString(dict,"error",module_error);
+	PyObject *copyright = PyString_FromString("Copyright 1999 Oratrix");
+	PyDict_SetItemString(dict,"copyright",copyright);
+	Py_XDECREF(copyright);
+	}
