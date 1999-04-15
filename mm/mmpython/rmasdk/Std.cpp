f8bb8f2e3c666b11f9bff7170eb1cbca89ec7f84
mm <mm@localhost>
1999-04-15 08:56:13 +0000

    Std top level headers. Std.cpp is used with vc for precompiled headers.

---
+++ mm/mmpython/rmasdk/Std.cpp
@@ -0,0 +1,7 @@
+//  std.cpp : source file that includes just the standard includes
+//	std.obj will contain the pre-compiled type information
+
+#include "std.h"
+
+// TODO: reference any additional headers you need in STD.H
+// and not in this file
---
+++ mm/mmpython/rmasdk/Std.h
@@ -0,0 +1,23 @@
+#ifndef INC_STD
+#define INC_STD
+
+
+// Std headers for each platform
+// Plus types defs
+// for win32 for example should be 'windows.h' without any defs
+
+// borrow from rma, platform idependent:
+#include "pntypes.h" 
+
+
+// STD C++ LIB (STL)
+#ifdef _WIN32
+#pragma warning(disable: 4786) // Long names trunc
+#pragma warning(disable: 4251) // dll-interface
+#endif
+
+#include <string>
+#include <map>
+using namespace std;
+
+#endif
