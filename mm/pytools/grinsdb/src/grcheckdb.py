9a59f617b30edca775b2281dd84bfbc053973aee
Jack Jansen <Jack.Jansen@cwi.nl>
1999-04-15 14:49:08 +0000

    Also re-create information and editor mailinglists based on settings in the database records.

--- mm/pytools/grinsdb/src/grcheckdb.py
+++ mm/pytools/grinsdb/src/grcheckdb.py
@@ -9,28 +9,28 @@ import rfc822
 import grinsdb
 import grpasswd
 
-ADDPASSWDCMD="ssh trawler.cwi.nl /ufs/jack/bin/addgrinspasswd '%s' '%s' '%s'"
-DIR="/usr/local/www.cwi.nl/GRiNS/player"
 
 # Keys on which we want to index
 KEYS=['email']
 
-# PASSWD=os.path.join(DIR, ".htpasswd")
-PASSWD="/ufs/jack/test"
-
 Error="grcheckdb.Error"
 
 def main():
-	if len(sys.argv) > 2:
-		print "Usage checkdb [newpasswdfile]"
+	if len(sys.argv) > 1:
+		print "Usage checkdb"
 		sys.exit(1)
 	dbase = grinsdb.Database(indexed=0)
 	index = grinsdb.Index(keys=KEYS)
 	allids = dbase.search(None, None)
 	emaildict = {}
 	badids = []
+	print 'Checking email addresses and creating index',
+	count = 0
 	for id in allids:
-		print id
+		count = count + 1
+		if count % 100 == 0:
+			sys.stdout.write('.')
+			sys.stdout.flush()
 		obj = dbase.open(id)
 		index.update(id, obj)
 		if not obj.has_key('email'):
@@ -41,6 +41,7 @@ def main():
 			emaildict[email].append(id)
 		else:
 			emaildict[email] = [id]
+	sys.stdout.write('\n')
 	del index
 	if badids:
 		print 'Bad messages:',
@@ -55,13 +56,31 @@ def main():
 			for i in emaildict[email]:
 				print i,
 			print
-	if len(sys.argv) > 1:
-		fp = open(sys.argv[1], "w")
-		genpasswdfile(dbase, fp)
 
-def genpasswdfile(dbase, fp):
+	genfiles(dbase)
+
+def myopen(filename):
+	dirname = os.path.join(grinsdb.DATABASE, 'data')
+	return open(os.path.join(dirname, filename + '.NEW'), 'w')
+
+def myinstall(filename):
+	dirname = os.path.join(grinsdb.DATABASE, 'data')
+	name =  os.path.join(dirname, filename)
+	os.rename(name + '.NEW', name)
+	
+def genfiles(dbase):
+	passwd_fp = myopen('htpasswd')
+	news_fp = myopen('grins-news')
+	edit_fp = myopen('grins-edit-news')
+	
 	allids = dbase.search(None, None)
+	print 'Generating password and mailinglist files',
+	count = 0
 	for id in allids:
+		count = count + 1
+		if count % 100 == 0:
+			sys.stdout.write('.')
+			sys.stdout.flush()		
 		obj = dbase.open(id)
 		if not obj.has_key('email'):
 			print 'Missing email:', id
@@ -72,7 +91,20 @@ def genpasswdfile(dbase, fp):
 		user = obj['email']
 		clear = obj['password']
 		crypted = crypt_passwd(clear)
-		add_passwd(fp, user, crypted)
+		add_passwd(passwd_fp, user, crypted)
+		if obj.has_key('want-maillist') and \
+		   obj['want-maillist'] == 'yes':
+			news_fp.write(user+'\n')
+		if obj.has_key('want-editor') and \
+		   obj['want-editor'] == 'yes':
+			edit_fp.write(user+'\n')
+	sys.stdout.write('\n')
+	passwd_fp.close()
+	news_fp.close()
+	edit_fp.close()
+	myinstall('htpasswd')
+	myinstall('grins-news')
+	myinstall('grins-edit-news')
 
 def crypt_passwd(clear):
 	salt = grpasswd.invent_passwd(2)
