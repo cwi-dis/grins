f854b3bed6cf4e155363372b4afd6c82101b558c
mm <mm@localhost>
1999-04-15 07:02:49 +0000

    Std Win32 include.

---
+++ mm/mmpython/rmasdk/win32/os.h
@@ -0,0 +1,26 @@
+/****************************************************************************
+ * 
+ *  $Id$
+ *
+ *  Copyright (C) 1995,1996,1997 RealNetworks, Inc.
+ *  All rights reserved.
+ *
+ *  http://www.real.com/devzone
+ *
+ *  This program contains proprietary information of RealNetworks, Inc.,
+ *  and is licensed subject to restrictions on use and distribution.
+ *
+ *  Operating System Dependant defines, functions, and includes
+ *
+ */
+
+#ifndef _OS_
+#define _OS_
+
+#include <stdio.h>
+
+#if defined( _WIN32 ) || defined( _WINDOWS )
+#include <windows.h>
+#endif
+
+#endif
