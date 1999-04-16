38863555983a32d7453c01b77b365e57af8c1bd6
mm <mm@localhost>
1999-04-16 11:20:25 +0000

    Added some missing app headers.

--- mm/mmpython/rmasdk/StdApp.h
+++ mm/mmpython/rmasdk/StdApp.h
@@ -1,7 +1,10 @@
 
 #include "fivemmap.h"
+#include "fivemlist.h"
 #include "exadvsnk.h"
 #include "exerror.h"
 #include "exsitsup.h"
 #include "exaumgr.h"
 #include "excontxt.h"
+#include "exnwsite.h"
+#include "exvsurf.h"
