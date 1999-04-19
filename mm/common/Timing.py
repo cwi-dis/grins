1fa599a0f663a0b488381d97f8e396e7b669324b
Jack Jansen <Jack.Jansen@cwi.nl>
1999-04-19 15:31:27 +0000

    Fixed a crash in Timing, where node.t1 didn't exist. However, I'm not 100% sure whether the fix is correct, so we should watch out for strange behaviour in the timeline view and/or prearming of nodes.

--- mm/common/Timing.py
+++ mm/common/Timing.py
@@ -194,7 +194,9 @@ def prep1(node):
 			adddep(node, HD, 0, c, HD)
 			adddep(c, TL, 0, node, TL)
 		# Make sure there is *some* path from head to tail
-		adddep(node, HD, MMAttrdefs.getattr(node, 'duration'), node, TL)
+## XXXX (Jack): this code appears incorrect: the corresponding event is
+## never fired...
+##		adddep(node, HD, MMAttrdefs.getattr(node, 'duration'), node, TL)
 	elif type in bagtypes:
 		adddep(node, HD, 0, node, TL)
 	else:
