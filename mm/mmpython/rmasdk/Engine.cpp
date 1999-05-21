94d6df883024cefa31023796f7c28cd5ed8cf67f
mm <mm@localhost>
1999-05-21 14:17:40 +0000

    Changed rma module so that you always need to do an explicit CreateEngine and use the CreatePlayer method of the object it returns to create a player object.

--- mm/mmpython/rmasdk/Engine.cpp
+++ mm/mmpython/rmasdk/Engine.cpp
@@ -1,11 +1,8 @@
 #include "std.h"
 #include "PyCppApi.h"
 #include "StdRma.h"
-
 #include "Engine.h"
-
 #include "os.h"
-
 #include "StdApp.h"
 
 #ifdef _MACINTOSH
@@ -14,9 +11,8 @@
 #include "macglue.h"
 #endif
 
-class EngineObject : public Object
-	{
-	public:
+class EngineObject : public Object {
+public:
 	MAKE_PY_CTOR(EngineObject)
 	static TypeObject type;
 	static PyObject *CreateInstance(PyObject *self, PyObject *args);
@@ -26,66 +22,210 @@ class EngineObject : public Object
 	static PyObject *CreatePlayer(PyObject *self, PyObject *args);
 	static PyObject *EventOccurred(PyObject *self, PyObject *args);
 
+	static IRMAClientEngine *pEngine;
+	static EngineObject *inst;
 
-	static IRMAClientEngine* pEngine;
-	static EngineObject* inst;
+	static FPRMCREATEENGINE fpCreateEngine;
+	static FPRMCLOSEENGINE fpCloseEngine;
+	static HINSTANCE hDll;
 
-	protected:
+protected:
 	EngineObject();
 	~EngineObject();
 	virtual string repr();
-	};
+};
 
-PyObject *EngineObject_CreateInstance(PyObject *self, PyObject *args)
-	{return EngineObject::CreateInstance(self,args);}
-void EngineObject_AddRef()
-	{if(EngineObject::inst)Py_INCREF(EngineObject::inst);}
-void EngineObject_Release()
-	{if(EngineObject::inst)Py_DECREF(EngineObject::inst);}
+IRMAClientEngine *EngineObject::pEngine = NULL;
+EngineObject *EngineObject::inst = NULL;
+FPRMCREATEENGINE EngineObject::fpCreateEngine = NULL;
+FPRMCLOSEENGINE EngineObject::fpCloseEngine = NULL;
+HINSTANCE EngineObject::hDll = NULL;
 
+void
+CloseEngine()
+{   
+	if (EngineObject::pEngine) {
+		EngineObject::fpCloseEngine(EngineObject::pEngine);
+		EngineObject::pEngine = NULL;
+	}
+	if (EngineObject::hDll) {
+		FreeLibrary(EngineObject::hDll);
+		EngineObject::hDll = NULL;
+	}
 
-IRMAClientEngine* EngineObject::pEngine=NULL;
-EngineObject* EngineObject::inst=NULL;
+	// set static var to NULL again
+	EngineObject::inst = NULL;
+}
 
+inline bool
+cleanup()
+{
+	CloseEngine();
+	return false;
+}
+
+static bool
+CreateEngine()
+{
+	if (EngineObject::pEngine)
+		return true;
+	// initialize the globals
+	EngineObject::fpCreateEngine = NULL;
+	EngineObject::fpCloseEngine = NULL;
+
+	// prepare/load the RMACore module
+	char szDllName[_MAX_PATH];
+    
+#if defined(_UNIX)
+	strcpy(szDllName, "rmacore.dll");
+
+#elif defined(_MACINTOSH)
+	// ASSUME rmacore is in System Folder:Extensions:Real:Common
+	// SHOULD obtain value from preference file
+	OSErr macErr;
+
+	// set szDllName to empty string
+	szDllName[0] = '\0'; 
+
+	// find the Extensions folder
+	short vRefNum;
+	long dirID;
+#if 0
+	/* Older G2 beta's had the engine in the extensions folder */
+	macErr = FindFolder (kOnSystemDisk, kExtensionFolderType, kDontCreateFolder, &vRefNum, &dirID);
+#else
+	macErr = FindFolder (kOnSystemDisk, kApplicationSupportFolderType, kDontCreateFolder, &vRefNum, &dirID);
+#endif
+
+	// find the path of the Extensions folder
+	DirInfo block;
+	Str63 directoryName;
+	char tmpName[_MAX_PATH];
+
+	block.ioDrParID = dirID;
+	block.ioNamePtr = directoryName;
+	do {
+		block.ioVRefNum = vRefNum;
+		block.ioFDirIndex = -1;
+		block.ioDrDirID = block.ioDrParID;
+
+		macErr = PBGetCatInfoSync ((CInfoPBPtr)&block);
+		if (macErr == noErr) {
+			strcpy (tmpName, szDllName);
+			szDllName[0] = '\0';
+			strncat (szDllName, (char*)&directoryName[1], directoryName[0]);
+			if (strlen(tmpName)) {
+				strcat (szDllName, ":");
+				strcat (szDllName, tmpName);
+			}
+		}
+	} while ((macErr == noErr) && (block.ioDrDirID != 2));// 2 means root directory of a volume
+	strcat (szDllName, ":Real:Common:rmacore60.dll");
+	//strcpy(szDllName, "rmacore60.dll");
+
+#elif defined(_WIN32) 
+	// get location of rmacore from windows registry 
+	DWORD bufSize;
+	HKEY hKey; 
+	PN_RESULT hRes;
+    
+	szDllName[0] = '\0'; 
+	bufSize = sizeof(szDllName) - 1;
+
+	if(ERROR_SUCCESS == RegOpenKey(HKEY_CLASSES_ROOT,
+				       "Software\\RealNetworks\\Preferences\\DT_Common", &hKey)) { 
+		// get the path to pnen 
+		hRes = RegQueryValue(hKey, "", szDllName, (long *)&bufSize); 
+		RegCloseKey(hKey); 
+	}
+
+	strcat(szDllName, "pnen3260.dll");
+
+#elif defined(_WIN16) 
+	strcpy(szDllName, "pnen1660.dll");
+#else
+#error Unknown platform
+#endif
+    
+	if ((EngineObject::hDll = LoadLibrary(szDllName)) == NULL) {
+#if defined(_UNIX)
+		fprintf(stdout, "Failed to load the 'rmacore.so.6.0' shared library\n");
+		fprintf(stdout, dlerror());
+#else
+		fprintf(stdout, "Failed to load the '%s' library.\n", szDllName);
+#endif
+		return cleanup();
+	}
+
+	fprintf(stdout, "Loaded rmacore\n");
+
+	// retrieve the proc addresses from the module
+	EngineObject::fpCreateEngine = (FPRMCREATEENGINE) GetProcAddress(EngineObject::hDll, "CreateEngine");
+	EngineObject::fpCloseEngine  = (FPRMCLOSEENGINE)  GetProcAddress(EngineObject::hDll, "CloseEngine");
+ 
+	if (EngineObject::fpCreateEngine == NULL ||
+	    EngineObject::fpCloseEngine == NULL) {
+		return cleanup();
+	}
+
+	// create client engine 
+	if (PNR_OK != EngineObject::fpCreateEngine((IRMAClientEngine**) & EngineObject::pEngine)) {
+		return cleanup();
+	}
+	return true;
+}
 
 EngineObject::EngineObject()
-	{
-	}
+{
+}
 
 EngineObject::~EngineObject()
-	{
+{
 	CloseEngine();
-	}
+}
 
-PyObject *EngineObject::CreateInstance(PyObject *self, PyObject *args)
-	{
+PyObject *
+EngineObject::CreateInstance(PyObject *self, PyObject *args)
+{
 	CHECK_NO_ARGS(args);
-	if(EngineObject::inst)
-		{
+	if (EngineObject::inst) {
 		Py_INCREF(EngineObject::inst);
 		return EngineObject::inst;
-		}
-	EngineObject::inst=(EngineObject*)Object::make(EngineObject::type);
+	}
+	EngineObject::inst= (EngineObject*) Object::make(EngineObject::type);
+	if (!CreateEngine()) {
+		PyErr_SetString(PyExc_IOError, "CreateEngine failed");
+		Py_DECREF(EngineObject::inst);
+		EngineObject::inst = NULL;
+		return NULL;
+	}
 	return EngineObject::inst;
-	}
+}
 
-string EngineObject::repr()
-	{
+PyObject *
+EngineObject_CreateInstance(PyObject *self, PyObject *args)
+{
+	return EngineObject::CreateInstance(self, args);
+}
+
+string
+EngineObject::repr()
+{
 	char buf[256];
-	sprintf (buf," instance 0x%X",this);
+	sprintf (buf, " instance 0x%X", this);
 	return Object::repr() + buf;
-	}
+}
 
-/////////////////////////////////////////////
-// PyMethods
-PyObject* EngineObject::CreatePlayer(PyObject *self, PyObject *args)
-	{
+PyObject *
+EngineObject::CreatePlayer(PyObject *self, PyObject *args)
+{
 	extern PyObject *PlayerObject_CreateInstance(PyObject *self, PyObject *args);
 	return PlayerObject_CreateInstance(self,args);
-	}
+}
 	
-PyObject* EngineObject::EventOccurred(PyObject *self, PyObject *args)
-	{
+PyObject *
+EngineObject::EventOccurred(PyObject *self, PyObject *args)
+{
 	PNxEvent pn_event;
 	PN_RESULT res;
 #ifdef _MACINTOSH
@@ -94,7 +234,7 @@ PyObject* EngineObject::EventOccurred(PyObject *self, PyObject *args)
 	
 	/* Event = PyArg_ParseTuple(blabla) */
 #ifdef _MACINTOSH
-	if( !PyArg_ParseTuple(args, "O&", PyMac_GetEventRecord, &ev) )
+	if (!PyArg_ParseTuple(args, "O&", PyMac_GetEventRecord, &ev))
 		return NULL;
 	pn_event.event = ev.what;
 	pn_event.param1 = &ev;
@@ -104,177 +244,27 @@ PyObject* EngineObject::EventOccurred(PyObject *self, PyObject *args)
 	** On Windows this magically seems to be unneeded at all.
 	*/
 	PyErr_SetString(PyExc_SystemError, 
-		"rma PNxEvent mapping not implemented on this platform yet");
+			"rma PNxEvent mapping not implemented on this platform yet");
 	return NULL;
 #endif
 	res = pEngine->EventOccurred(&pn_event);
 	return Py_BuildValue("i", res);
-	}
+}
 
-static struct PyMethodDef PyRMEngine_methods[] =
-	{
+static struct PyMethodDef PyRMEngine_methods[] = {
 	{"CreatePlayer",EngineObject::CreatePlayer,1},
 	{"EventOccurred", EngineObject::EventOccurred,1},
-	{NULL, 	NULL}
-	};
+	{NULL, 	NULL},
+};
 
-TypeObject EngineObject::type("PyRMEngine", 
-						  GetBaseType(), 
-						  sizeof(EngineObject), 
-						  PyRMEngine_methods, 
-						  GET_PY_CTOR(EngineObject));
+TypeObject EngineObject::type("PyRMEngine",
+			      GetBaseType(),
+			      sizeof(EngineObject),
+			      PyRMEngine_methods,
+			      GET_PY_CTOR(EngineObject));
 
-
-
-/////////////////////////////////////////////////////////////
-static FPRMCREATEENGINE m_fpCreateEngine;
-static FPRMCLOSEENGINE m_fpCloseEngine;
-static HINSTANCE hDll;
-static PN_RESULT theErr = PNR_OK;
-
-inline bool cleanup(){CloseEngine();return false;}
-
-static bool CreateEngine()
-	{
-	if( EngineObject::pEngine) return true;
-    // initialize the globals
-    m_fpCreateEngine = NULL;
-    m_fpCloseEngine	= NULL;
-    
-    // prepare/load the RMACore module
-    char   szDllName[_MAX_PATH];
-    
-#if defined(_UNIX)
-    strcpy(szDllName, "rmacore.dll");
-
-#elif defined(_MACINTOSH)
-    // ASSUME rmacore is in System Folder:Extensions:Real:Common
-    // SHOULD obtain value from preference file
-    OSErr	macErr;
-
-    // set szDllName to empty string
-    szDllName[0] = '\0'; 
-
-    // find the Extensions folder
-    short	vRefNum;
-    long	dirID;
-#if 0
-    /* Older G2 beta's had the engine in the extensions folder */
-    macErr = FindFolder (kOnSystemDisk, kExtensionFolderType, kDontCreateFolder, &vRefNum, &dirID);
-#else
-    macErr = FindFolder (kOnSystemDisk, kApplicationSupportFolderType, kDontCreateFolder, &vRefNum, &dirID);
-#endif
-
-    // find the path of the Extensions folder
-    DirInfo	block;
-    Str63	directoryName;
-    char	tmpName[_MAX_PATH];
-
-    block.ioDrParID = dirID;
-    block.ioNamePtr = directoryName;
-    do
-		{
-		block.ioVRefNum = vRefNum;
-		block.ioFDirIndex = -1;
-		block.ioDrDirID = block.ioDrParID;
-
-		macErr = PBGetCatInfoSync ((CInfoPBPtr)&block);
-		if (macErr == noErr)
-			{
-			strcpy (tmpName, szDllName);
-			szDllName[0] = '\0';
-			strncat (szDllName, (char*)&directoryName[1], directoryName[0]);
-			if (strlen(tmpName))
-				{
-				strcat (szDllName, ":");
-				strcat (szDllName, tmpName);
-				}
-			}
-		}
-    while ((macErr == noErr) && (block.ioDrDirID != 2)); // 2 means root directory of a volume
-    strcat (szDllName, ":Real:Common:rmacore60.dll");
-	//strcpy(szDllName, "rmacore60.dll");
-
-#elif defined(_WIN32) 
-    // get location of rmacore from windows registry 
-    DWORD bufSize;
-    HKEY hKey; 
-    PN_RESULT hRes;
-    
-    szDllName[0] = '\0'; 
-    bufSize = sizeof(szDllName) - 1;
-
-    if(ERROR_SUCCESS == RegOpenKey(HKEY_CLASSES_ROOT,
-		"Software\\RealNetworks\\Preferences\\DT_Common", &hKey)) 
-    { 
-	// get the path to pnen 
-	hRes = RegQueryValue(hKey, "", szDllName, (long *)&bufSize); 
-	RegCloseKey(hKey); 
-    }
-
-    strcat(szDllName, "pnen3260.dll");
-
-#elif defined(_WIN16) 
-    strcpy(szDllName, "pnen1660.dll");
-#else
-#error Unknown platform
-#endif
-    
-    if (!(hDll = LoadLibrary(szDllName))) 
-    {
-	theErr = PNR_FAILED;
-#if defined(_UNIX)
- 	fprintf(stdout, "Failed to load the 'rmacore.so.6.0' shared library\n");
-	fprintf(stdout, dlerror());
-#else
- 	fprintf(stdout, "Failed to load the '%s' library.\n", szDllName);
-#endif
-	return cleanup();
-    }
-
-    fprintf(stdout, "Loaded rmacore\n");
-
-    // retrieve the proc addresses from the module
-    m_fpCreateEngine = (FPRMCREATEENGINE) GetProcAddress(hDll, "CreateEngine");
-    m_fpCloseEngine  = (FPRMCLOSEENGINE)  GetProcAddress(hDll, "CloseEngine");
- 
-    if (m_fpCreateEngine == NULL ||
- 		m_fpCloseEngine == NULL)
-		{
-		theErr = PNR_FAILED;
-		return cleanup();
-		}
-
-    // create client engine 
-    if (PNR_OK != m_fpCreateEngine((IRMAClientEngine**) & EngineObject::pEngine))
-		{
-		theErr = PNR_FAILED;
-		return cleanup();
-		}
-	return true;
-	}
-
-IRMAClientEngine* GetEngine()
-	{
-	if(! EngineObject::pEngine)CreateEngine();
-	return  EngineObject::pEngine;
-	}
-
-void CloseEngine()
-	{   
-    if ( EngineObject::pEngine)
-		{
-		m_fpCloseEngine( EngineObject::pEngine);
-		 EngineObject::pEngine = NULL;
-		}
-    if (hDll)
-		{
-		FreeLibrary(hDll);
-		hDll = NULL;
-		}
-
-	// set static var to NULL again
-	 EngineObject::inst=NULL;
-	}
-
-
+IRMAClientEngine *
+GetEngine(PyObject *engine)
+{
+	return ((EngineObject *) engine)->pEngine;
+}
