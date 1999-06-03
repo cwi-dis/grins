b8872f03e0511fef96a3f118852988d4de3e0d98
Sjoerd Mullender <sjoerdmullender@users.noreply.github.com>
1999-06-03 10:40:39 +0000

    Horizontal and vertical were switched.

--- mm/editor/motif/ChannelViewDialog.py
+++ mm/editor/motif/ChannelViewDialog.py
@@ -54,8 +54,8 @@ class ChannelViewDialog(ViewDialog):
 				]),
 			('Navigate', [
 				('Level of detail', [
-					('More horizontal detail', CANVAS_HEIGHT),
-					('More vertical detail', CANVAS_WIDTH),
+					('More horizontal detail', CANVAS_WIDTH),
+					('More vertical detail', CANVAS_HEIGHT),
 					('Fit in window', CANVAS_RESET),
 					]),
 				None,
