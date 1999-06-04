8ef2459ca6142e64904a3aee0d1f7034b10305ad
Jack Jansen <Jack.Jansen@cwi.nl>
1999-06-04 13:18:18 +0000

    RealMedia channels added (exact copies of windows versions)

---
+++ mm/common/mac/RealChannel.py
@@ -0,0 +1,75 @@
+__version__ = "$Id$"
+
+import windowinterface
+try:
+	import rma
+except ImportError:
+	# no Real support available
+	rma = None
+import MMurl
+
+class RealChannel:
+	__engine = None
+	__has_rma_support = rma is not None
+
+	def __init__(self):
+		self.__rmaplayer = None
+		if self.__engine is None and self.__has_rma_support:
+			try:
+				RealChannel.__engine = rma.CreateEngine()
+			except:
+				self.errormsg(None, 'No playback support for RealMedia on this system')
+				RealChannel.__has_rma_support = 0
+		# release any resources on exit
+		windowinterface.addclosecallback(self.release_player,())
+
+	def release_player(self):
+		self.__rmaplayer = None
+
+	def prepare_player(self, node = None):
+		if not self.__has_rma_support:
+			return 0
+		if not self.__rmaplayer:
+			try:
+				self.__rmaplayer = self.__engine.CreatePlayer()
+			except:
+				self.errormsg(node, 'No playback support for RealMedia on this system')
+				return 0
+		return 1
+
+	def playit(self, node, window = None):
+		if not self.__rmaplayer:
+			return 0
+		self.__loop = self.getloop(node)
+		url = MMurl.canonURL(self.getfileurl(node))
+		self.__url = url
+		self.__window = window
+		self.__rmaplayer.SetStatusListener(self)
+		if window is not None:
+			self.__rmaplayer.SetOsWindow(window)
+		self.__rmaplayer.OpenURL(url)
+		self.__rmaplayer.Begin()
+		return 1
+
+	def OnStop(self):
+		if self.__loop:
+			self.__loop = self.__loop - 1
+			if self.__loop == 0:
+				self.playdone(0)
+				return
+		print 'looping'
+		windowinterface.settimer(0.1,(self.__rmaplayer.Begin,()))
+#		self.__rmaplayer.Stop()
+#		self.__rmaplayer.Begin()
+
+	def pauseit(self, paused):
+		if self.__rmaplayer:
+			if paused:
+				self.__rmaplayer.Pause()
+			else:
+				self.__rmaplayer.Begin()
+
+	def stopit(self):
+		if self.__rmaplayer:
+			self.__rmaplayer.Stop()
+		self.__rmaplayer = None
---
+++ mm/common/mac/RealWindowChannel.py
@@ -0,0 +1,44 @@
+__version__ = "$Id$"
+
+#
+# WIN32 RealWindowChannel.
+#
+
+""" @win32doc|RealWindowChannel
+
+"""
+import Channel, RealChannel
+
+class RealWindowChannel(Channel.ChannelWindowAsync, RealChannel.RealChannel):
+	def __init__(self, name, attrdict, scheduler, ui):
+		RealChannel.RealChannel.__init__(self)
+		Channel.ChannelWindowAsync.__init__(self, name, attrdict, scheduler, ui)
+
+	def do_arm(self, node, same = 0):
+		if not self.prepare_player(node):
+			import MMAttrdefs
+			name = MMAttrdefs.getattr(node, 'name')
+			if not name:
+				name = '<unnamed node>'
+			chtype = self.__class__.__name__[:-7] # minus "Channel"
+			msg = 'Warning:\nNo playback support for %s in this version\n' \
+			      'node %s on channel %s' % (chtype, name, self._name)
+			parms = self.armed_display.fitfont('Times-Roman', msg)
+			w, h = self.armed_display.strsize(msg)
+			self.armed_display.setpos((1.0 - w) / 2, (1.0 - h) / 2)
+			self.armed_display.fgcolor((255, 0, 0))		# red
+			box = self.armed_display.writestr(msg)
+		return 1
+
+	def do_play(self, node):
+		if not self.playit(node, self.window.GetSafeHwnd()):
+			self.playdone(0)
+
+	# toggles between pause and run
+	def setpaused(self, paused):
+		Channel.ChannelWindowAsync.setpaused(self, paused)
+		self.pauseit(paused)
+
+	def stopplay(self, node):
+		self.stopit()
+		Channel.ChannelWindowAsync.stopplay(self, node)
