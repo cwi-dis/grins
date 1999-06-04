99df2f2188b35444e2217d3d1f9bcfb0d5d20bd8
Jack Jansen <Jack.Jansen@cwi.nl>
1999-06-04 16:05:35 +0000

    Added a RealEngine object, a wrapper around the C object, which keeps usage counts. This is needed because on the Mac we have to call the engine EventHandler in our event loop to keep it moving along.

--- mm/common/mac/RealChannel.py
+++ mm/common/mac/RealChannel.py
@@ -7,6 +7,52 @@ except ImportError:
 	# no Real support available
 	rma = None
 import MMurl
+import os
+if os.name == 'mac':
+	import Evt
+	NEEDTICKER = 1
+else:
+	NEEDTICKER = 0
+
+class RealEngine:
+	# This class holds the RMA engine and a useage counter. This counter is
+	# needed because on the mac (and maybe on unix) whenever any player is active
+	# we should periodically pass events to the engine to keep it ticking.
+	# XXXX Eventually we should probably also pass redraw events and such.
+	def __init__(self):
+		self.engine = rma.CreateEngine()
+		self.usagecount = 0
+		
+	def __del__(self):
+		if self.usagecount:
+			windowinterface.cancelidleproc(self._tick)
+		
+	def CreatePlayer(self):
+		return self.engine.CreatePlayer()
+		
+	def startusing(self):
+		if NEEDTICKER and self.usagecount == 0:
+			self._startticker()
+		self.usagecount = self.usagecount + 1
+		
+	def stopusing(self):
+		if self.usagecount <= 0:
+			raise 'RealEngine usage count <= 0'
+		self.usagecount = self.usagecount - 1
+		if NEEDTICKER and self.usagecount == 0:
+			self._stopticker()
+			
+	def _startticker(self):
+		print 'DBG startticker'
+		windowinterface.setidleproc(self._tick)
+		
+	def _stopticker(self):
+		print 'DBG stopticker'
+		windowinterface.cancelidleproc(self._tick)
+		
+	def _tick(self):
+		# XXXX Mac-specific
+		self.engine.EventOccurred((0, 0, Evt.TickCount(), (0, 0), 0))
 
 class RealChannel:
 	__engine = None
@@ -16,7 +62,7 @@ class RealChannel:
 		self.__rmaplayer = None
 		if self.__engine is None and self.__has_rma_support:
 			try:
-				RealChannel.__engine = rma.CreateEngine()
+				RealChannel.__engine = RealEngine()
 			except:
 				self.errormsg(None, 'No playback support for RealMedia on this system')
 				RealChannel.__has_rma_support = 0
@@ -49,6 +95,7 @@ class RealChannel:
 			self.__rmaplayer.SetOsWindow(window)
 		self.__rmaplayer.OpenURL(url)
 		self.__rmaplayer.Begin()
+		self.__engine.startusing()
 		return 1
 
 	def OnStop(self):
@@ -56,6 +103,7 @@ class RealChannel:
 			self.__loop = self.__loop - 1
 			if self.__loop == 0:
 				self.playdone(0)
+				##self.__engine.stopusing()
 				return
 		print 'looping'
 		windowinterface.settimer(0.1,(self.__rmaplayer.Begin,()))
@@ -72,4 +120,5 @@ class RealChannel:
 	def stopit(self):
 		if self.__rmaplayer:
 			self.__rmaplayer.Stop()
+			self.__engine.stopusing()
 		self.__rmaplayer = None
