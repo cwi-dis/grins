fd8ab280aa7d6ae70552143cb75c5e1a027cb04f
mm <mm@localhost>
1999-06-07 14:33:18 +0000

    Minor changes to adjust for changes in the base class MediaChannel. -kk

--- mm/common/win32/SoundChannel.py
+++ mm/common/win32/SoundChannel.py
@@ -18,6 +18,7 @@ debug=0
 class SoundChannel(Channel.ChannelAsync,MediaChannel.MediaChannel):
 	node_attrs = Channel.ChannelAsync.node_attrs + ['duration',
 						'clipbegin', 'clipend']
+
 	def __init__(self, name, attrdict, scheduler, ui):
 		MediaChannel.MediaChannel.__init__(self)
 		Channel.ChannelAsync.__init__(self, name, attrdict, scheduler, ui)
@@ -26,17 +27,15 @@ class SoundChannel(Channel.ChannelAsync,MediaChannel.MediaChannel):
 		return '<SoundChannel instance, name=' + `self._name` + '>'
 
 	def do_hide(self):
-		self.release_player()
+		self.release_res()
 		Channel.ChannelAsync.do_hide(self)
 
 	def destroy(self):
-		self.release_player()
+		self.release_res()
 		self.unregister_for_timeslices()
 		Channel.ChannelAsync.destroy(self)
 
-
 	def do_arm(self, node, same=0):
-		if debug:print 'SoundChannel.do_arm('+`self`+','+`node`+'same'+')'
 		if node.type != 'ext':
 			self.errormsg(node, 'Node must be external')
 			return 1
@@ -44,15 +43,15 @@ class SoundChannel(Channel.ChannelAsync,MediaChannel.MediaChannel):
 			self.showwarning(node,'System missing infrastructure to playback')
 		return 1
 
-
 	def do_play(self, node):
-		if debug: print 'SoundChannel.do_play('+`self`+','+`node`+')'
-		self.playit(node)
+		if not self.playit(node):
+			self.showwarning(node,'Can not play')
+			self.playdone(0)
 
 	# part of stop sequence
 	def stopplay(self, node):
-		self.stopit()
 		Channel.ChannelAsync.stopplay(self, node)
+		self.stopit()
 
 	# toggles between pause and run
 	def setpaused(self, paused):
@@ -69,6 +68,7 @@ class SoundChannel(Channel.ChannelAsync,MediaChannel.MediaChannel):
 		if not name:
 			name = '<unnamed node>'
 		chtype = self.__class__.__name__[:-7] # minus "Channel.ChannelAsync"
+		import windowinterface
 		windowinterface.showmessage('%s\n'
 						    '%s node %s on Channel.ChannelAsync %s' % (inmsg, chtype, name, self._name), mtype = 'warning')
 
