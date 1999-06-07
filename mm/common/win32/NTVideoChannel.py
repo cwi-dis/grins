acb911a9bdc418a1fac649330252925a1cb3d5d2
mm <mm@localhost>
1999-06-07 14:28:11 +0000

    Many changes to the base class of audio and video channels for better performance and improve behavior for streaming media (asf). -kk

--- mm/common/win32/NTVideoChannel.py
+++ mm/common/win32/NTVideoChannel.py
@@ -1,11 +1,11 @@
 __version__ = "$Id$"
 
 #
-# WIN32 Video 
+# WIN32 Video channel
 #
 
 # the core
-from Channel import *
+import Channel
 
 # common component
 import MediaChannel
@@ -13,83 +13,75 @@ import MediaChannel
 # node attributes
 import MMAttrdefs
 
-# channel types
-[SINGLE, HTM, TEXT, MPEG] = range(4)
+# channel types and message
+import windowinterface
 
 debug=0
 
-class VideoChannel(ChannelWindowAsync,MediaChannel.MediaChannel):
-	node_attrs = ChannelWindowAsync.node_attrs + \
+class VideoChannel(Channel.ChannelWindowAsync,MediaChannel.MediaChannel):
+	node_attrs = Channel.ChannelWindowAsync.node_attrs + \
 		     ['bucolor', 'hicolor', 'scale', 'center',
 		      'clipbegin', 'clipend']
-	_window_type = MPEG
+	_window_type = windowinterface.MPEG
 
 	def __init__(self, name, attrdict, scheduler, ui):
 		MediaChannel.MediaChannel.__init__(self)
-		ChannelWindowAsync.__init__(self, name, attrdict, scheduler, ui)
+		Channel.ChannelWindowAsync.__init__(self, name, attrdict, scheduler, ui)
 
 	def __repr__(self):
 		return '<VideoChannel instance, name=' + `self._name` + '>'
 
 	def do_show(self, pchan):
-		if not ChannelWindowAsync.do_show(self, pchan):
+		if not Channel.ChannelWindowAsync.do_show(self, pchan):
 			return 0
 		self.showit(self.window)
 		return 1
 
 	def do_hide(self):
-		self.release_player()
-		ChannelWindowAsync.do_hide(self)
+		self.release_res()
+		Channel.ChannelWindowAsync.do_hide(self)
 
 	def destroy(self):
 		self.unregister_for_timeslices()
-		self.release_player()
-		ChannelWindowAsync.destroy(self)
+		self.release_res()
+		Channel.ChannelWindowAsync.destroy(self)
 		
 	def do_arm(self, node, same=0):
-		if debug:print 'Videodo_arm('+`self`+','+`node`+'same'+')'
 		if same and self.armed_display:
 			return 1
 		if node.type != 'ext':
 			self.errormsg(node, 'Node must be external')
 			return 1
-		self.arm_display(node)
-		if not self.prepare_player(node):
+		self.prepare_armed_display(node)
+		res=self.prepare_player(node)
+		if res==0:
 			self.showwarning(node,'System missing infrastructure to playback')
+		elif res==-1:
+			self.showwarning(node,'Failed to render file')
 		return 1
 
 	def do_play(self, node):
-		if debug:print 'Videodo_play('+`self`+','+`node`+')'
-		self.playit(node,self.window)
-
-	# Part of stop sequence. Stop and remove last frame 
-	def stopplay(self, node):
-		self.stopit()
-		#if hasattr(self,'_bmp') and self._bmp:self._bmp=None
-		ChannelWindowAsync.stopplay(self, node)
-
-	def playstop(self):
-		ChannelWindowAsync.playstop(self)
-		if self.window:
-			self.window.RedrawWindow()
+		if not self.playit(node,self.window):
+			windowinterface.settimer(5,(self.playdone,(0,)))
 
 	# toggles between pause and run
 	def setpaused(self, paused):
+		Channel.ChannelWindowAsync.setpaused(self, paused)
 		self.pauseit(paused)
 
+	# Part of stop sequence. Stop and remove last frame 
+	def stopplay(self, node):
+		Channel.ChannelWindowAsync.stopplay(self, node)
+		self.stopit()
+		if self.window:self.window.RedrawWindow()
+
+	# interface for anchor creation
 	def defanchor(self, node, anchor, cb):
-		import windowinterface
 		windowinterface.showmessage('The whole window will be hot.')
 		cb((anchor[0], anchor[1], [0,0,1,1]))
 
-
-	def arm_display(self,node):
-		if debug: print 'NTVideoChannel arm_display'
-
-		# force backgroud color (there must be a better way)
+	def prepare_armed_display(self,node):
 		self.armed_display._bgcolor=self.getbgcolor(node)
-		self.armed_display.clear(self.window.getgeometry())
-
 		drawbox = MMAttrdefs.getattr(node, 'drawbox')
 		if drawbox:
 			self.armed_display.fgcolor(self.getbucolor(node))
@@ -97,22 +89,20 @@ class VideoChannel(ChannelWindowAsync,MediaChannel.MediaChannel):
 			self.armed_display.fgcolor(self.getbgcolor(node))
 		hicolor = self.gethicolor(node)
 		for a in node.GetRawAttrDef('anchorlist', []):
-			atype = a[A_TYPE]
-			if atype not in SourceAnchors or atype == ATYPE_AUTO:
+			atype = a[Channel.A_TYPE]
+			if atype not in Channel.SourceAnchors or atype == Channel.ATYPE_AUTO:
 				continue
 			b = self.armed_display.newbutton((0,0,1,1))
 			b.hiwidth(3)
 			if drawbox:
 				b.hicolor(hicolor)
-			self.setanchor(a[A_ID], a[A_TYPE], b)
-		#self.armed_display.drawvideo(self.update)
+			self.setanchor(a[Channel.A_ID], a[Channel.A_TYPE], b)
+		self.armed_display.drawvideo(self.update)
 
 
-############################ 
-# showwarning if the infrastucture is missing.
-# The user should install Windows Media Player
-# since then this infrastructure is installed
-
+	# showwarning if the infrastucture is missing.
+	# The user should install Windows Media Player
+	# since then this infrastructure is installed
 	def showwarning(self,node,inmsg):
 		name = MMAttrdefs.getattr(node, 'name')
 		if not name:
@@ -128,22 +118,3 @@ class VideoChannel(ChannelWindowAsync,MediaChannel.MediaChannel):
 
 
 
-############################# unused stuff but keep it for now
-	# Make a copy of frame and keep it until stopplay is called
-	def _freeze(self):
-		return # bmp not used
-		import win32mu,win32api
-		if not hasattr(self,'_bmp'):self._bmp=None
-		if self.window and self.window.IsWindow():
-			if self._bmp: 
-				self._bmp.DeleteObject()
-				del self._bmp
-			win32api.Sleep(0)
-			self._bmp=win32mu.WndToBmp(self.window)
-
-	def update(self,dc):
-		import win32mu
-		if self._playBuilder and (self.__playdone or self._paused):
-			self.window.RedrawWindow()
-			if hasattr(self,'_bmp') and self._bmp: 
-				win32mu.BitBltBmp(dc,self._bmp,self.window.GetClientRect())
