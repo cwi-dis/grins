a14d99adbc0648a900e6eaaefc4921b914441410
Jack Jansen <Jack.Jansen@cwi.nl>
1999-06-11 11:37:58 +0000

    Build options file for rma

---
+++ mm/mmpython/rmasdk/mwerks_rma_config.h
@@ -0,0 +1,10 @@
+/*
+** Config file for dynamically-loaded ppc/cfm68k plugin modules.
+*/
+
+#define USE_GUSI		/* Stdio implemented with GUSI */
+#define USE_MSL			/* Use MSL libraries */
+#ifdef USE_MSL
+#define MSL_USE_PRECOMPILED_HEADERS 1	/* Use precomp headers */
+#include <ansi_prefix.mac.h>
+#endif
