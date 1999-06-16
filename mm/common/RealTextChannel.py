977dd3f1e275e9e128332f4128d287e51ce63bf2
Jack Jansen <Jack.Jansen@cwi.nl>
1999-06-16 11:19:04 +0000

    Real channels unified and put in common directory.

--- mm/common/RealTextChannel.py
+++ mm/common/RealTextChannel.py
@@ -1,18 +1,6 @@
 __version__ = "$Id$"
 
-from Channel import ChannelWindow
+from RealWindowChannel import RealWindowChannel
 
-class RealTextChannel(ChannelWindow):
-	def do_arm(self, node, same = 0):
-		import MMAttrdefs
-		name = MMAttrdefs.getattr(node, 'name')
-		if not name:
-			name = '<unnamed node>'
-		msg = 'Warning:\nNo playback support for RealText in this version\n' \
-		      'node %s on channel %s' % (name, self._name)
-		parms = self.armed_display.fitfont('Times-Roman', msg)
-		w, h = self.armed_display.strsize(msg)
-		self.armed_display.setpos((1.0 - w) / 2, (1.0 - h) / 2)
-		self.armed_display.fgcolor((255, 0, 0))		# red
-		box = self.armed_display.writestr(msg)
-		return 1
+class RealTextChannel(RealWindowChannel):
+	pass
