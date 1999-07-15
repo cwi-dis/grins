732bec27dd0ebf93ebb6e1f2239c995d8f335172
mm <mm@localhost>
1999-07-15 07:13:03 +0000

    The DropTarget mixin class can accept any type of object. The default is to accept files. You add an entry into the drop map if you want to accept any other type of object. The OnDragXXX callbacks now get a PyDataObject as their first argument and which can be queried for the type of object and its data. File objects is just one case. -kk

--- mm/lib/win32/DropTarget.py
+++ mm/lib/win32/DropTarget.py
@@ -4,6 +4,7 @@ import WMEVENTS, win32con
 class DropTarget:
 	def __init__(self):
 		self._isregistered=0
+		self._dropmap={'FileName':(self.dragfile,self.dropfile)}
 
 	def registerDropTarget(self):
 		if not self._isregistered:
@@ -17,17 +18,35 @@ class DropTarget:
 				self.RevokeDropTarget()
 			self._isregistered=0
 
-	def OnDragEnter(self,filename,kbdstate,x,y):
-		#print 'OnDragEnter',filename,kbdstate,x,y
-		return self.OnDragOver(filename,kbdstate,x,y)
+	def OnDragEnter(self,dataobj,kbdstate,x,y):
+		return self.OnDragOver(dataobj,kbdstate,x,y)
 
-	def OnDragOver(self,filename,kbdstate,x,y):
-		x,y=self._DPtoLP((x,y))
-		x,y = self._inverse_coordinates((x, y),self._canvas)
-		return self.onEventEx(WMEVENTS.DragFile,(x, y, filename))
+	def OnDragOver(self,dataobj,kbdstate,x,y):
+		for fmt in self._dropmap.keys():
+			cb=	self._dropmap[fmt][0]
+			res = cb(dataobj,kbdstate,x,y)
+			if res: return res
+				
+	def OnDrop(self,dataobj,effect,x,y):
+		for fmt in self._dropmap.keys():
+			cb=	self._dropmap[fmt][1]
+			res = cb(dataobj,effect,x,y)
+			if res: return res
+		return 0
 
-	def OnDrop(self,filename,effect,x,y):
-		#print 'OnDrop',filename,effect,x,y
+	def OnDragLeave(self):
+		pass
+
+	def dragfile(self,dataobj,kbdstate,x,y):
+		filename=dataobj.GetGlobalData('FileName')
+		if filename:
+			x,y=self._DPtoLP((x,y))
+			x,y = self._inverse_coordinates((x, y),self._canvas)
+			return self.onEventEx(WMEVENTS.DragFile,(x, y, filename))
+		return 0
+
+	def dropfile(self,dataobj,effect,x,y):
+		filename=dataobj.GetGlobalData('FileName')
 		if filename:
 			import longpath
 			filename=longpath.short2longpath(filename)
@@ -36,7 +55,3 @@ class DropTarget:
 			self.onEvent(WMEVENTS.DropFile,(x, y, filename))
 			return 1
 		return 0
-
-	def OnDragLeave(self):
-		#print 'OnDragLeave'
-		pass
