cd2e5811fe2a958f4b63454560cb83847134d455
mm <mm@localhost>
1999-07-15 06:59:50 +0000

    Define the win3ui method CreatOleDataObject that creates a PyDataObject.-kk

--- mm/win32/src/win32ext/newfiles.h
+++ mm/win32/src/win32ext/newfiles.h
@@ -12,12 +12,15 @@ DECLARE_PYMODULECLASS(Afx);
 
 //extern PyObject *ui_create_dc_from_handle (PyObject *self, PyObject *args);
 
+extern PyObject *create_ole_data_object( PyObject *self, PyObject *args );
+
 // @pymeth GetWin32Sdk|Creates a Win32 SDK module wrapper object.
 // @pymeth GetAfx|Creates an Afx module wrapper object.
 
 #define DEF_NEW_PY_METHODS\
 	{"GetWin32Sdk",MWin32Sdk::create,1},\
-	{"GetAfx",MAfx::create,1},
+	{"GetAfx",MAfx::create,1},\
+	{"CreateOleDataObject",create_ole_data_object,1},
 //	{"CreateFrame",ui_create_frame,1},\
 //	{"CreateToolTipCtrl",PyCToolTipCtrl_create,1},
 //	{"CreateRgn",PyCRgn::create,1},\
