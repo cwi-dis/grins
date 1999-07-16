8c30575d34dd7d8764798268158ccf12d1aeea77
Jack Jansen <Jack.Jansen@cwi.nl>
1999-07-16 11:45:11 +0000

    Dummy (but functional) LicenseDialog.

---
+++ mm/editor/win32/LicenseDialog.py
@@ -0,0 +1,16 @@
+# License dialog
+import windowinterface
+
+
+class LicenseDialog:
+	def __init__(self):
+		pass
+
+	def show(self):
+		pass
+			
+	def setdialoginfo(self):
+		if self.can_try:
+			self.cb_try()
+		else:
+			self.cb_enterkey()
