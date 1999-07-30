e74dd841b9172c5d4c237176d7c991426dd6ad11
Jack Jansen <Jack.Jansen@cwi.nl>
1999-07-30 12:45:43 +0000

    New splash screen code, using normal Dlg dialogs. Added methods so we can change the message in the splash screen, and show user/organization/license.

--- mm/lib/mac/splash.py
+++ mm/lib/mac/splash.py
@@ -1,25 +1,47 @@
-import MacOS
+import Dlg
 
 firsttime = 1
+_dialog = None
 
-RESOURCE_ID={
-	'loadprog': 513,
-	'loaddoc': 514,
-	'initdoc': 515
+RESOURCE_ID=513
+
+ITEM_MSG=2
+ITEM_USER=3
+ITEM_ORG=4
+ITEM_LICENSE=5
+MESSAGE={
+	'loadprog': "Loading program...",
+	'loaddoc': "Loading document...",
+	'initdoc': "Initialize document...",
 }
 
 def splash(arg=0, version=None):
-	global firsttime
+	global firsttime, _dialog, _starttime
 	if not firsttime:
 		return
 	if version and not arg:
 		return
 	if not arg:
-		MacOS.splash()
+		_dialog = None
 		firsttime = 0
 	else:
-		MacOS.splash(RESOURCE_ID[arg])
+		if not _dialog:
+			d = Dlg.GetNewDialog(RESOURCE_ID, -1)
+			d.ShowWindow()
+			d.DrawDialog()
+			_dialog = d
+		setitem(ITEM_MSG, MESSAGE[arg])
 		
+def setuserinfo(user, org, license):
+	if not _dialog:
+		return
+	setitem(ITEM_USER, user)
+	setitem(ITEM_ORG, org)
+	setitem(ITEM_LICENSE, license)
+	
+def setitem(item, value):
+	tp, h, rect = _dialog.GetDialogItem(item)
+	Dlg.SetDialogItemText(h, value)
 
 def unsplash():
 	pass
