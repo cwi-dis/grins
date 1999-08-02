b8e94edfef4a4dd390b2585d9f0b97642deafd1d
mm <mm@localhost>
1999-08-02 16:28:03 +0000

    Auto generated include files for the resources. -kk

---
+++ mm/Build/win32/GRiNS/resource.h
@@ -0,0 +1,18 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by Player.rc
+//
+#define IDI_PLAYER                       102
+#define IDI_CMIFDOCP                     103
+#define IDI_SMILDOCP                     104
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        105
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
