337ea00e4c5581b405c9a2ca65f297188e798dd8
Sjoerd Mullender <sjoerdmullender@users.noreply.github.com>
1999-08-25 10:25:59 +0000

    Delay writing of temporary RealPix file to when it is needed (when arming the node, and when saving the SMIL file), and warn about a duration which is too short to accomodate all transitions.

--- mm/common/RealPixChannel.py
+++ mm/common/RealPixChannel.py
@@ -8,6 +8,7 @@ class RealPixChannel(RealWindowChannel):
 
 	def getfileurl(self, node):
 		if hasattr(node, 'tmpfile'):
-			import MMurl
+			import MMurl, realsupport
+			realsupport.writeRP(node.tmpfile, node.slideshow.rp, node)
 			return MMurl.pathname2url(node.tmpfile)
 		return RealWindowChannel.getfileurl(self, node)
