31aa62cc1538e2d05795740931e1ae7b5776fca2
anadelt <anadelt@localhost>
1999-11-01 20:01:12 +0000

    A better video to real media script.

--- mm/mmpython/wmsdk/vid2rm.py
+++ mm/mmpython/wmsdk/vid2rm.py
@@ -4,16 +4,26 @@ import dshow
 inputfile='d:\\ufs\\mm\\cmif\\bin\\win32\\fox.mpg'
 outputfile='d:\\ufs\\mm\\cmif\\bin\\win32\\fox.rm'
 
+# no checks
 def vid2rm(infile,outfile):
 	b = dshow.CreateGraphBuilder()
-	source=b.AddSourceFilter(infile)
+	b.RenderFile(infile)
+	renderer=b.FindFilterByName('Video Renderer')
+	enumpins=renderer.EnumPins()
+	pin=enumpins.Next()
+	lastpin=pin.ConnectedTo()
+	b.RemoveFilter(renderer)
+
 	f = dshow.CreateFilter('Video Real Media Converter');
-	b.AddFilter(f,'RMC')
+	b.AddFilter(f,'VRMC')
 	sink = f.QueryIFileSinkFilter()
 	sink.SetFileName(outfile)
-	pin=source.FindPin("Output")
-	b.Render(pin)
+	b.Render(lastpin)
+
 	mc = b.QueryIMediaControl()
 	mc.Run()
 	b.WaitForCompletion()
+	mc.Stop()
+
+# vid2rm(inputfile,outputfile)
 
