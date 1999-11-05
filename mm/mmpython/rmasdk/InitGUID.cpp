f7ed1a3e1b4633eaece5639f56ef51f3cce240c7
Jack Jansen <Jack.Jansen@cwi.nl>
1999-11-05 16:10:06 +0000

    Allow producer and rma to live in one binary, in which case rma InitGUID.cpp takes care of the GUID initializations.

--- mm/mmpython/rmasdk/InitGUID.cpp
+++ mm/mmpython/rmasdk/InitGUID.cpp
@@ -1,3 +1,13 @@
 // Init Interface ID's.
 #define INITGUID     
 #include "StdRma.h"
+#ifdef WITH_REALPRODUCER_SUPPORT
+#include "rmaenum.h"
+#include "engtypes.h"
+#include "engtargs.h"
+#include "engcodec.h"
+#include "rmbldeng.h"
+#include "rmmetain.h"
+#include "rmapckts.h"
+#include "progsink.h"
+#endif
