1ed607cccc5794db881ab6b22a37520fdced7a13
anadelt <anadelt@localhost>
1999-11-10 07:49:59 +0000

    The script now converts testdoc/testdatampg.mpg to a real media video using the new conversion process.

--- mm/mmpython/wmsdk/vid2rm.py
+++ mm/mmpython/wmsdk/vid2rm.py
@@ -1,29 +1,102 @@
 
 import dshow
+import os
+engine=None
 
-inputfile='d:\\ufs\\mm\\cmif\\bin\\win32\\fox.mpg'
-outputfile='d:\\ufs\\mm\\cmif\\bin\\win32\\fox.rm'
+def convertvideofile(u, dstdir, file, node):
+	import producer
+	global engine
+	if os.environ.has_key('REAL_PRODUCER'):
+		producer.SetDllCategoryPaths(os.environ['REAL_PRODUCER'])
+	# ignore suggested extension and make our own
+	file = os.path.splitext(file)[0] + '.rm'
+	fullpath = os.path.join(dstdir, file)
+	if engine is None:
+		engine = producer.CreateRMBuildEngine()
+	for pin in engine.GetPins():
+		if pin.GetOutputMimeType() == producer.MIME_REALVIDEO:
+			videopin = pin
+	engine.SetDoOutputMimeType(producer.MIME_REALAUDIO, 0)
+	engine.SetDoOutputMimeType(producer.MIME_REALVIDEO, 1)
+	engine.SetDoOutputMimeType(producer.MIME_REALEVENT, 0)
+	engine.SetDoOutputMimeType(producer.MIME_REALIMAGEMAP, 0)
+	engine.SetDoOutputMimeType(producer.MIME_REALPIX, 0)
+	engine.SetRealTimeEncoding(0)
+	engine.SetDoMultiRateEncoding(1)
+	cp = engine.GetClipProperties()
+	ts = engine.GetTargetSettings()
+	if node is not None:
+		cp.SetTitle(MMAttrdefs.getattr(node, 'title'))
+		cp.SetAuthor(MMAttrdefs.getattr(node, 'author'))
+		cp.SetCopyright(MMAttrdefs.getattr(node, 'copyright'))
+		ts.SetVideoQuality(MMAttrdefs.getattr(node, 'project_videotype'))
+		ts.RemoveAllTargetAudiences()
+		target = MMAttrdefs.getattr(node, 'project_targets')
+		for i in range(5):
+			if (1 << i) & target:
+				ts.AddTargetAudience(i)
+		if not target:
+			ts.AddTargetAudience(producer.ENC_TARGET_28_MODEM)
+	else:
+		# we don't know nothin' about the node so use some defaults
+		cp.SetTitle('')
+		cp.SetAuthor('')
+		cp.SetCopyright('')
+		ts.AddTargetAudience(producer.ENC_TARGET_28_MODEM)
+		ts.SetVideoQuality(producer.ENC_VIDEO_QUALITY_NORMAL)
+	cp.SetPerfectPlay(1)
+	cp.SetMobilePlay(0)
+	cp.SetSelectiveRecord(0)
+	cp.SetDoOutputServer(0)
+	cp.SetDoOutputFile(1)
+	cp.SetOutputFilename(fullpath)
 
-# no checks
-def vid2rm(infile,outfile):
 	b = dshow.CreateGraphBuilder()
-	b.RenderFile(infile)
+	b.RenderFile(u)
 	renderer=b.FindFilterByName('Video Renderer')
 	enumpins=renderer.EnumPins()
 	pin=enumpins.Next()
 	lastpin=pin.ConnectedTo()
 	b.RemoveFilter(renderer)
-
-	f = dshow.CreateFilter('Video Real Media Converter');
+	try:
+		f = dshow.CreateFilter('Video Real Media Converter')
+	except:
+		print 'Video real media converter filter is not installed'
+		return
 	b.AddFilter(f,'VRMC')
+	# sink not used yet
 	sink = f.QueryIFileSinkFilter()
-	sink.SetFileName(outfile)
+	sink.SetFileName(fullpath)
 	b.Render(lastpin)
+	try:
+		rconv=f.QueryIRealConverter()
+	except:
+		print 'Filter does not support interface IRealConverter'
+		return
+	try:
+		uk=engine.QueryInterfaceUnknown()
+	except:
+		print 'RMABuildEngine QueryInterfaceUnknown failed'
+		return
+	rconv.SetInterface(uk,'IRMABuildEngine')
+
+	try:
+		uk=videopin.QueryInterfaceUnknown()
+	except:
+		print 'RMAInputPin QueryInterfaceUnknown failed'
+		return
+	rconv.SetInterface(uk,'IRMAInputPin')
+
+	# PinProperties,MediaSample,PrepareToEncode,Encode, DoneEncoding
+	# are all managed by our dshow filter
 
 	mc = b.QueryIMediaControl()
 	mc.Run()
 	b.WaitForCompletion()
 	mc.Stop()
 
-# vid2rm(inputfile,outputfile)
+inputfile='D:\\ufs\\mm\\cmif\\Build\\common\\testdoc\\testdatampg.mpg'
+outputdir='d:\\ufs\\mm\\cmif\\win32\\DXMedia\\bin'
+os.environ['REAL_PRODUCER']='c:\\Program Files\\RealSDK\\Producer\\BIN\\'
 
+convertvideofile(inputfile,outputdir,'xxx.rm',None)
