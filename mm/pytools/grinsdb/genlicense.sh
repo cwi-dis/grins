3609f7811fff361ad90c07ddd45ab858c7690064
Jack Jansen <Jack.Jansen@cwi.nl>
1999-11-29 16:35:00 +0000

    grlicense had a bug when you tried to generate non-standard features.

--- mm/pytools/grinsdb/genlicense.sh
+++ mm/pytools/grinsdb/genlicense.sh
@@ -2,4 +2,4 @@
 PYTHON=/ufs/jack/bin/sgi/python
 SCRIPTDIR=/ufs/jack/src/mm/pytools/grinsdb/src
 DATABASE=/ufs/mm/clients
-$PYTHON $SCRIPTDIR/grlicense.py -E -d +31 -o $DATABASE/.evallicense
+$PYTHON $SCRIPTDIR/grlicense.py -E -d +25 -o $DATABASE/.evallicense
