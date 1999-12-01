e430a9cd6f8e2a89059331286574689988a04230
Sjoerd Mullender <sjoerdmullender@users.noreply.github.com>
1999-12-01 13:14:08 +0000

    Version 1.5 beta, build 1.

--- mm/editor/version.py
+++ mm/editor/version.py
@@ -1,4 +1,8 @@
 __version__ = "$Id$"
 
 from patchlevel import patchlevel
-version = 'editor 1.0.1alpha ' + patchlevel
+import settings
+if settings.get('lightweight'):
+	version = 'Lite for RealSystem G2, v1.5 beta ' + patchlevel
+else:
+	version = 'Editor 1.5 beta ' + patchlevel
