db6722593c0aa967e8405156b4c4ec768391e495
Sjoerd Mullender <sjoerdmullender@users.noreply.github.com>
1999-12-09 13:54:23 +0000

    Changed a few MIME types: .ram and .rmm are now audio/x-pn-realaudio .rm is now application/vnd.rn-realmedia These types are equal to what the G2 player puts in the registry.

--- mm/lib/grins_mimetypes.py
+++ mm/lib/grins_mimetypes.py
@@ -8,14 +8,14 @@ mimetypes = {
 	'.grins': 'application/x-grins-project',
 
 	'.ra': 'audio/vnd.rn-realaudio',
-	'.ram': 'audio/vnd.rn-realaudio',
-	'.rmm': 'audio/vnd.rn-realaudio',
+	'.ram': 'audio/x-pn-realaudio',
+	'.rmm': 'audio/x-pn-realaudio',
 	'.rp': 'image/vnd.rn-realpix',
 	'.rt': 'text/vnd.rn-realtext',
 	'.rv': 'video/vnd.rn-realvideo',
 	'.rf': 'image/vnd.rn-realflash',
 	'.swf': 'application/x-shockwave-flash',
-	'.rm': 'video/vnd.rn-realvideo',
+	'.rm': 'application/vnd.rn-realmedia',
 
 	'.mp3': 'audio/mpeg',
 	}
