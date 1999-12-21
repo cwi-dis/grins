ba52cacd1f1f53f96f8c3c1f8c7852aba109218c
anadelt <anadelt@localhost>
1999-12-21 14:21:48 +0000

    Make transitions conditional if a condition is suplied.

--- mm/lib/win32/fsm.py
+++ mm/lib/win32/fsm.py
@@ -15,20 +15,26 @@ class FSM:
 		# create graph of states from triplelist
 		graph={}
 		for t in triplelist:
-			s1,s2,input=t
+			if len(t)==3:
+				s1,s2,input=t
+				cond=None
+			elif len(t)==4:
+				s1,s2,input,cond=t
+			else:
+				continue
 			if graph.has_key(s1):
 				if graph.has_key(s2):
-					graph[s1]._transition[input]=graph[s2]
+					graph[s1]._transition[input]=(graph[s2],cond)
 				else:
 					graph[s2]=State(s2)
-					graph[s1]._transition[input]=graph[s2]	
+					graph[s1]._transition[input]=(graph[s2],cond)	
 			else:
 				graph[s1]=State(s1)
 				if graph.has_key(s2):
-					graph[s1]._transition[input]=graph[s2]
+					graph[s1]._transition[input]=(graph[s2],cond)
 				else:
 					graph[s2]=State(s2)
-					graph[s1]._transition[input]=graph[s2]	
+					graph[s1]._transition[input]=(graph[s2],cond)	
 		self._graph=graph
 		self._current=None
 		if inistate:
@@ -44,15 +50,21 @@ class FSM:
 	# input is the string that causes the transitions and usualy describes a set
 	# ainput is the actual input
 	def advance(self,input,ainput=None):
+		if not ainput: ainput=input
 		if self._current:
 			if self._current._transition.has_key(input):
-				self._current = self._current._transition[input]
-				if ainput:
-					self._input=self._input+ainput	
+				state,cond = self._current._transition[input]
+				if cond:
+					if apply(cond,(self._input+ainput,)):
+						self._current=state
+						self._input=self._input+ainput
+					else:
+						self._current=None
 				else:
-					self._input=self._input+input
+					self._current=state
+					self._input=self._input+ainput	
 			else:
-				self._current=None	
+				self._current=None
 		return self._current
 
 	def getstate(self):
