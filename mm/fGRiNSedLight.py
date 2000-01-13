4a036ca0320a2a1a82a26253e39c3bd0719f7797
Sjoerd Mullender <sjoerdmullender@users.noreply.github.com>
2000-01-13 15:44:03 +0000

    Startup file for frozen lite version.

---
+++ mm/fGRiNSedLight.py
@@ -0,0 +1,13 @@
+import settings
+settings.default_settings['lightweight'] = 1
+
+import grins_app_core
+resdll = None
+
+class EditorApp(grins_app_core.GrinsApp):
+	def BootGrins(self):
+		import exec_cmif
+		exec_cmif.Boot(1)
+
+grins_app_core.fix_argv()
+grins_app_core.BootApplication(EditorApp)
