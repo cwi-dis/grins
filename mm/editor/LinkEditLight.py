8acae59b522e1d136b14754ea20a6b76743bf546
Sjoerd Mullender <sjoerdmullender@users.noreply.github.com>
2000-01-19 18:32:13 +0000

    Various improvements to link editing.

--- mm/editor/LinkEditLight.py
+++ mm/editor/LinkEditLight.py
@@ -8,10 +8,10 @@ from AnchorDefs import *
 
 class LinkEditLight:
 	def __init__(self, toplevel):
-		self.interesting = []
 		self.toplevel = toplevel
 		self.context = toplevel.root.GetContext()
 		self.editmgr = toplevel.editmgr
+		self.make_interesting()
 
 	def hide(self):
 		pass
@@ -37,6 +37,22 @@ class LinkEditLight:
 		self.context = None
 		self.editmgr = None
 
+	def make_interesting(self):
+		self.interesting = []
+		self.__make_interesting(self.toplevel.root)
+
+	def __make_interesting(self, node):
+		uid = node.GetUID()
+		hlinks = self.context.hyperlinks
+		for aid, atype, aargs in node.GetAttrDef('anchorlist', []):
+			if atype not in SourceAnchors:
+				continue
+			anchor = uid, aid
+			if not hlinks.findsrclinks(anchor):
+				self.interesting.append(anchor)
+		for c in node.GetChildren():
+			self.__make_interesting(c)
+
 	def set_interesting(self, anchor):
 		self.interesting.append(anchor)
 
@@ -46,16 +62,21 @@ class LinkEditLight:
 	# Make sure all anchors in 'interesting' actually exist
 	def fixinteresting(self):
 		dlist = []
+		hlinks = self.context.hyperlinks
 		for nid, aid in self.interesting:
 			node = self.context.mapuid(nid)
 			alist = MMAttrdefs.getattr(node, 'anchorlist')
 			for a in alist:
 				if a[A_ID] == aid:
+					if a[A_TYPE] not in SourceAnchors or \
+					   hlinks.findsrclinks((nid, aid)):
+						# exists, but wrong type or has link
+						dlist.append((nide, aid))
 					break
 			else:
-				dlist.append(nid,aid)
+				dlist.append((nid,aid))
 		for a in dlist:
-			print 'lost anchor:', a
+##			print 'lost anchor:', a
 			self.interesting.remove(a)
 
 	# Method to return a whole-node anchor for a node, or optionally
@@ -121,5 +142,21 @@ class LinkEditLight:
 			return aid + ' in ' + uid
 		node = self.context.mapuid(uid)
 		nodename = node.GetRawAttrDef('name', '#' + uid)
+		if self.findanchor(anchor)[A_TYPE] == ATYPE_DEST:
+			return nodename
 		if type(aid) is not type(''): aid = `aid`
 		return nodename + '.' + aid
+
+	def findanchor(self, anchor):
+		if anchor is not None:
+			if type(anchor) is not type(()):
+				return (anchor, ATYPE_DEST, ())
+			uid, aid = anchor
+			if '/' in uid:
+				# external anchor
+				return (aid, ATYPE_DEST, ())
+			node = self.context.mapuid(uid)
+			for a in MMAttrdefs.getattr(node, 'anchorlist'):
+				if a[A_ID] == aid:
+					return a
+		return None
