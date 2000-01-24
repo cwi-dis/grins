b19d2abb45d8a85259952c49e6a33d3ba726b627
Sjoerd Mullender <sjoerdmullender@users.noreply.github.com>
2000-01-24 20:15:20 +0000

    Implemented attributes for MobilePlay and PerfectPlay flags in Real conversion.

--- mm/common/SGIVideoChannel.py
+++ mm/common/SGIVideoChannel.py
@@ -24,8 +24,10 @@ mv.SetSelectEvents(mv.MV_EVENT_MASK_STOP)
 
 class VideoChannel(Channel.ChannelWindowAsync):
 	_our_attrs = ['bucolor', 'hicolor', 'scale', 'center']
-	node_attrs = Channel.ChannelWindowAsync.node_attrs + \
-		      ['clipbegin', 'clipend', 'project_audiotype', 'project_videotype', 'project_targets']
+	node_attrs = Channel.ChannelWindowAsync.node_attrs + [
+		'clipbegin', 'clipend',
+		'project_audiotype', 'project_videotype', 'project_targets',
+		'project_perfect', 'project_mobile']
 	if Channel.CMIF_MODE:
 		node_attrs = node_attrs + _our_attrs
 	else:
