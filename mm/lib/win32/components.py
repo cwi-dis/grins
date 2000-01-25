a7f513569045642e080b74b9854e8cd95ed3a5a9
Jack Jansen <Jack.Jansen@cwi.nl>
2000-01-25 16:31:23 +0000

    Removed target setting in template dialog.

--- mm/lib/win32/components.py
+++ mm/lib/win32/components.py
@@ -772,9 +772,9 @@ class TemplateDialog(ResDialog):
 		self._select=ComboBox(self,grinsRC.IDC_TEMPLATECOMBO)
 		self._explanation = Static(self, grinsRC.IDC_EXPLANATION)
 		self._picture = WndCtrl(self, grinsRC.IDC_PICTURE)
-		self._html_templates = (
-			RadioButton(self, grinsRC.IDC_TARGET_EXTERN),
-			RadioButton(self, grinsRC.IDC_TARGET_EMBEDDED))
+##		self._html_templates = (
+##			RadioButton(self, grinsRC.IDC_TARGET_EXTERN),
+##			RadioButton(self, grinsRC.IDC_TARGET_EMBEDDED))
 		self._bmp = None
 
 		self.show()
@@ -784,8 +784,8 @@ class TemplateDialog(ResDialog):
 		self.init_subwindows()
 		for i in range(len(self._names)):
 			self._select.insertstring(i, self._names[i])
-		self._html_templates[0].setcheck(1)
-		self._html_templates[1].setcheck(0)
+##		self._html_templates[0].setcheck(1)
+##		self._html_templates[1].setcheck(0)
 		self._select.setcursel(0)
 		self._select.hookcommand(self, self.OnChangeTemplate)
 		self._setdialoginfo()
@@ -837,10 +837,10 @@ class TemplateDialog(ResDialog):
 		which = self._select.getcursel()
 		if 0 <= which <= len(self._descriptions):
 			rv = self._descriptions[which]
-			if self._html_templates[0].getcheck():
-				rv = rv + ('external_player.html',)
-			else:
-				rv = rv + ('embedded_player.html',)
+##			if self._html_templates[0].getcheck():
+##				rv = rv + ('external_player.html',)
+##			else:
+##				rv = rv + ('embedded_player.html',)
 			self._cb(rv)
 		self.close()
 
