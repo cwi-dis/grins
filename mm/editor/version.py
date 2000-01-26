34573d411c4bd8df908c9b09a007cf0ed1739f01
Sjoerd Mullender <sjoerdmullender@users.noreply.github.com>
2000-01-26 15:24:26 +0000

    Changed version string.

--- mm/editor/version.py
+++ mm/editor/version.py
@@ -3,9 +3,9 @@ import sys
 from patchlevel import patchlevel
 import settings
 if settings.get('lightweight'):
-	version = 'Lite for RealSystem G2, v1.5 beta ' + patchlevel
+	version = 'Lite for RealSystem G2, v1.5 ' + patchlevel
 	shortversion = 'grinslite-%s-1.5beta'%sys.platform
 else:
-	version = 'Editor 1.5 alpha ' + patchlevel
+	version = 'Pro for RealSystem G2, v1.5 ' + patchlevel
 	shortversion = 'grinspro-%s-1.5alpha'%sys.platform
 	
