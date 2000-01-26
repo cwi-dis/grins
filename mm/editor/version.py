b37c3dd2795743b6fb354363d5a4c90e2fec6ed2
Jack Jansen <Jack.Jansen@cwi.nl>
2000-01-26 14:42:29 +0000

    Also export shortversion, which is usable to create URLs.

--- mm/editor/version.py
+++ mm/editor/version.py
@@ -1,8 +1,11 @@
 __version__ = "$Id$"
-
+import sys
 from patchlevel import patchlevel
 import settings
 if settings.get('lightweight'):
 	version = 'Lite for RealSystem G2, v1.5 beta ' + patchlevel
+	shortversion = 'grinslite-%s-1.5beta'%sys.platform
 else:
 	version = 'Editor 1.5 alpha ' + patchlevel
+	shortversion = 'grinspro-%s-1.5alpha'%sys.platform
+	
