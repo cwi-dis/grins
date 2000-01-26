6681fd1e1ca8b4a196973877b1c6b99349e48d02
Sjoerd Mullender <sjoerdmullender@users.noreply.github.com>
2000-01-26 08:43:14 +0000

    Added additional optional argument to specify the product the licenses are for.

--- mm/pytools/grinsdb/src/genrealstorelicenses.py
+++ mm/pytools/grinsdb/src/genrealstorelicenses.py
@@ -5,16 +5,20 @@ import posix
 import string
 
 def main():
-	if len(sys.argv) != 3:
-		print "Usage %s count outputfile"%sys.argv[0]
+	if not (3 <= len(sys.argv) <= 4):
+		print "Usage %s count outputfile [ product ]"%sys.argv[0]
 		print "  Generates count licenses and stores them in the"
 		print "  (dos-linefeeds) outputfile"
 		sys.exit(1)
 	count = string.atoi(sys.argv[1])
 	fp = open(sys.argv[2], "w")
 	posix.chmod(sys.argv[2], 0444) # So we don't override it
+	if len(sys.argv) > 3:
+		product = sys.argv[3]
+	else:
+		product = None
 	for i in range(count):
-		license = grlicense.gencommerciallicense()
+		license = grlicense.gencommerciallicense(product)
 		fp.write(license+'\r\n')
 	fp.close()
 
