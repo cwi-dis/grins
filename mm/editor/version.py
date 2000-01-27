55ad1720cbf5c3da05ebb74cbdb6e88040d8eeaa
Jack Jansen <Jack.Jansen@cwi.nl>
2000-01-27 14:46:16 +0000

    For the mac return beta shortversions, for windows return final.

--- mm/editor/version.py
+++ mm/editor/version.py
@@ -4,8 +4,14 @@ from patchlevel import patchlevel
 import settings
 if settings.get('lightweight'):
 	version = 'Lite for RealSystem G2, v1.5 ' + patchlevel
-	shortversion = 'grinslite-%s-1.5beta'%sys.platform
+	if sys.platform == 'mac':
+		shortversion = 'grinslite-%s-1.5beta'%sys.platform
+	else:
+		shortversion = 'grinslite-%s-1.5'%sys.platform
 else:
 	version = 'Pro for RealSystem G2, v1.5 ' + patchlevel
-	shortversion = 'grinspro-%s-1.5alpha'%sys.platform
+	if sys.platform == 'mac':
+		shortversion = 'grinspro-%s-1.5beta'%sys.platform
+	else:
+		shortversion = 'grinspro-%s-1.5'%sys.platform
 	
