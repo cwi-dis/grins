01842a7bea1b14185139c76dbf6f7a438cd31de2
anadelt <anadelt@localhost>
2000-02-11 11:51:20 +0000

    Design change: Restore win32ui.pyd to its original dll dependencies. i.e remove dependency on Accusoft's image gear dll (gear32sd.dll). Support for gear32sd.dll is now given by a separate module that exports to python this dll's interface: gear32sd.pyd. (Not active yet in CVS only on my machine. So no visible change yet. And support not removed from win32ui yet.)

---
+++ mm/win32/ACCUSOFT/src/igif.h
@@ -0,0 +1,30 @@
+#ifndef INC_IGIF
+#define INC_IGIF
+
+#ifndef INC_MFCSTRING
+#include "mfcstring.h"
+#endif
+
+class GifConverter
+	{
+	public:
+	GifConverter();
+	~GifConverter();
+
+	bool LoadGif(LPCTSTR fileNameLoad);
+
+	int m_transparent;
+	int m_tc[3];
+	BYTE *m_pBmpBuffer;
+	DWORD m_dwSize;
+
+	void SaveGif();
+	String m_tempFile;
+
+	private:
+	BYTE *m_buf;
+	UINT m_width;
+	UINT m_height;
+	};
+
+#endif
