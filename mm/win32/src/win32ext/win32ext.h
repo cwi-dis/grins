96c080070749b209f627816c9b6ce7d624330ca1
anadelt <anadelt@localhost>
2000-02-24 09:39:49 +0000

    Remove from exports 'DS' module.

--- mm/win32/src/win32ext/win32ext.h
+++ mm/win32/src/win32ext/win32ext.h
@@ -9,12 +9,10 @@ PyObject* py_create_html_wnd(PyObject *self, PyObject *args);
 PyObject* ui_create_miniframe(PyObject *self, PyObject *args);
 
 DECLARE_PYMODULECLASS(ig);
-DECLARE_PYMODULECLASS(DS);
 DECLARE_PYMODULECLASS(Util);
 
 #define DEF_EXT_PY_METHODS \
 	{"CreateHtmlWnd",py_create_html_wnd,1},\
-	{"GetDS",MDS::create,1},\
 	{"CreateAsyncMonikerFile",PyAsyncMonikerFile::Create,1},\
 	{"GetUtil",MUtil::create,1},\
 	{"CreateMiniFrame",ui_create_miniframe,1},
