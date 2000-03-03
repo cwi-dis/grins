ac278830c30662449b1aabf8b4adba9e1aef2797
anadelt <anadelt@localhost>
2000-03-03 12:54:37 +0000

    Now the windows media reader is quite ready to be used for something usefull. To convert windows media to real media for example (we have all the info to doit).

--- mm/win32/wmsdk/wmfsdk/src/wmreader.py
+++ mm/win32/wmsdk/wmfsdk/src/wmreader.py
@@ -3,31 +3,48 @@ import wmfapi
 # init COM libs
 wmfapi.CoInitialize()
 
-# temp debug: find the name of a guid
-def findguid(guid):
-    for s in dir(wmfapi):
-		if s.find("WMMEDIA")>=0 or s.find("WMFORMAT")>=0:
-			if guid==getattr(wmfapi,s):
-				return s
 
-class PyReaderCallback:
+class ReaderAgent:
 	def __init__(self):
-		pass
+		print 'ReaderAgent'
+	def __del__(self):
+		print '~ReaderAgent'
 	def OnStatus(self,status):
-		print status
-	def OnSample(self,*arg):
-		print arg
+		if status==1:
+			print 'WMT_OPENED'
+		elif status == 11:
+			print 'WMT_STARTED'
+		elif status == 6:
+			print 'WMT_END_OF_STREAMING'
+		elif status == 4:
+			print 'WMT_EOF'
+		else:
+			print 'status=',status
+	def OnSample(self,msSampleTime):
+		print msSampleTime
 
-reader = wmfapi.CreateReader(0)
-readercbobj = wmfapi.CreatePyReaderCallback(PyReaderCallback())
+
+rights = 0 # we don't use a rights manager
+reader = wmfapi.CreateReader(rights)
+
+readercbobj = wmfapi.CreatePyReaderCallback(ReaderAgent())
+
 reader.Open(r'D:\ufs\mm\cmif\win32\wmsdk\wmfsdk\src\test.wma',readercbobj)
 readercbobj.WaitOpen()
 noutputs = reader.GetOutputCount()
 print 'media file has',noutputs,'stream(s)'
 
+reader.Start(0,0,1.0)
+
+# wait efficiently for completion
+readercbobj.WaitForCompletion()
+
+# no more python callbacks please
+readercbobj.SetListener()
+
 reader.Close()
+
 del reader
-del readercbobj
 
 # on exit: release COM libs
 wmfapi.CoUninitialize()
