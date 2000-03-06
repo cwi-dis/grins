eee87c78471c9940054a2cae4c98a805d1ab817c
Jack Jansen <Jack.Jansen@cwi.nl>
2000-03-06 16:35:38 +0000

    Added code to get track durations.

--- mm/lib/mac/videoreader.py
+++ mm/lib/mac/videoreader.py
@@ -2,6 +2,9 @@
 import sys
 import Qt
 import QuickTime
+import Qd
+import Qdoffs
+import QDOffscreen
 import Res
 import MediaDescr
 import imgformat
@@ -37,24 +40,35 @@ class _Reader:
 		fsspec = macfs.FSSpec(path)
 		fd = Qt.OpenMovieFile(fsspec, 0)
 		self.movie, d1, d2 = Qt.NewMovieFromFile(fd, 0, 0)
-		
-		self.audiotrack = self.movie.GetMovieIndTrackType(1,
+		try:
+			self.audiotrack = self.movie.GetMovieIndTrackType(1,
 				QuickTime.SoundMediaType, QuickTime.movieTrackMediaType)
-		self.audiomedia = self.audiotrack.GetTrackMedia()
-		handle = Res.Resource('')
-		self.audiomedia.GetMediaSampleDescription(1, handle)
-		print len(handle.data)
-		self.audiodescr = MediaDescr.SoundDescription.decode(handle.data)
-		del handle
-		
-		self.videotrack = self.movie.GetMovieIndTrackType(1,
-				QuickTime.VideoMediaType, QuickTime.movieTrackMediaType)
-		self.videomedia = self.videotrack.GetTrackMedia()
-		handle = Res.Resource('')
-		self.videomedia.GetMediaSampleDescription(1, handle)
-		print 'video', len(handle.data)
-		self.videodescr = MediaDescr.ImageDescription.decode(handle.data)
-		del handle
+			self.audiomedia = self.audiotrack.GetTrackMedia()
+		except Qt.Error:
+			self.audiotrack = self.audiomedia = None
+			self.audiodescr = {}
+		else:
+			handle = Res.Resource('')
+			self.audiomedia.GetMediaSampleDescription(1, handle)
+			print len(handle.data)
+			self.audiodescr = MediaDescr.SoundDescription.decode(handle.data)
+			del handle
+			print 'AUDIO DUR', self._gettrackduration_ms(self.audiotrack)
+	
+		try:	
+			self.videotrack = self.movie.GetMovieIndTrackType(1,
+					QuickTime.VideoMediaType, QuickTime.movieTrackMediaType)
+			self.videomedia = self.videotrack.GetTrackMedia()
+		except Qt.Error:
+			self.videotrack = self.videomedia = self.videotimescale = None
+		else:
+			handle = Res.Resource('')
+			self.videomedia.GetMediaSampleDescription(1, handle)
+			print 'video', len(handle.data)
+			self.videodescr = MediaDescr.ImageDescription.decode(handle.data)
+			del handle
+			self._initgworld()
+			print 'VIDEO DUR', self._gettrackduration_ms(self.videotrack)
 		
 	def __del__(self):
 		self.audiomedia = None
@@ -63,11 +77,38 @@ class _Reader:
 		self.videotrack = None
 		self.movie = None
 		
+	def _initgworld(self):
+		old_port, old_dev = Qdoffs.GetGWorld()
+		try:
+			movie_w = self.videodescr['width']
+			movie_h = self.videodescr['height']
+			movie_rect = (0, 0, movie_w, movie_h)
+			self.gworld = Qdoffs.NewGWorld(32,  movie_rect, None, None, QDOffscreen.keepLocal)
+			self.pixmap = self.gworld.GetGWorldPixMap()
+			Qdoffs.LockPixels(self.pixmap)
+			Qdoffs.SetGWorld(self.gworld.as_GrafPtr(), None)
+			Qd.EraseRect(movie_rect)
+			self.movie.SetMovieGWorld(self.gworld.as_GrafPtr(), None)
+			self.movie.SetMovieBox(movie_rect)
+			self.movie.SetMovieActive(1)
+			self.movie.MoviesTask(0)
+			self.movie.SetMoviePlayHints(QuickTime.hintsHighQuality, QuickTime.hintsHighQuality)
+			# XXXX framerate
+			self.videocurtime = 0
+		finally:
+			Qdoffs.SetGWorld(old_port, old_dev)
+		
+	def _gettrackduration_ms(self, track):
+		movietimescale = self.movie.GetMovieTimeScale()
+		tracktime = track.GetTrackDuration()
+		value, d1, d2 = Qt.ConvertTimeScale((tracktime, movietimescale, None), 1000)
+		return value
+		
 	def HasAudio(self):
-		return 1
+		return not self.audiotrack is None
 		
 	def HasVideo(self):
-		return 1
+		return not self.videotrack is None
 		
 	def GetAudioFormat(self):
 		return audio.format.AudioFormatLinear('dummy_format', 'Dummy Audio Format', 
@@ -85,16 +126,34 @@ class _Reader:
 		return 25
 		
 	def ReadAudio(self, nframes):
-		if self._did_audio:
-			return ''
-		self._did_audio = 1
-		return '\0' * nframes * 2
+		return ''
 		
-	def ReadVideo(self):
-		if self._did_video:
-			return ''
-		self._did_video = 1
-		return '\020\040\060\077' * 320 * 240
+	def ReadVideo(self, time=None):
+		if not time is None:
+			self.videocurtime = time
+		tv, dur = self.videomedia.GetMediaNextInterestingTime(
+				QuickTime.nextTimeMediaSample|QuickTime.nextTimeEdgeOK,
+				self.videocurtime, 1.0)
+		if tv < 0:
+			return self.videocurtime, None
+		print 'DBG: old', self.videocurtime, 'tv, dur', tv, dur
+		self.videocurtime = tv+dur
+		self.movie.SetMovieTimeValue(self.videocurtime)
+		self.movie.MoviesTask(0)
+		return tv, self._getpixmapcontent()
+		
+	def _getpixmapcontent(self):
+		"""Shuffle the offscreen PixMap data, because it may have funny stride values"""
+		rowbytes = Qdoffs.GetPixRowBytes(self.pixmap)
+		width = self.videodescr['width']
+		height = self.videodescr['height']
+		start = 0
+		rv = ''
+		for i in range(height):
+			nextline = Qdoffs.GetPixMapBytes(self.pixmap, start, width*4)
+			start = start + rowbytes
+			rv = rv + nextline
+		return rv
 
 def reader(url):
 ##	print 'No video conversion yet'
@@ -106,6 +165,8 @@ def reader(url):
 	if not rdr.HasVideo():
 		print "DBG: No video in", url
 		return None
+	if not rdr.HasAudio():
+		print "DBG: warning: no audio"
 	return rdr
 
 def _test():
@@ -127,19 +188,19 @@ def _test():
 	videofmt = rdr.GetVideoFormat()
 	imgfmt = videofmt.getformat()
 	imgw, imgh = videofmt.getsize()
-	data = rdr.ReadVideo()
+	timestamp, data = rdr.ReadVideo()
 	while data:
 		fname = 'frame%04.4d.jpg'%num
 		num = num+1
 		pname = os.path.join(dstdir, fname)
-		print 'Writing', fname
-		wrt = img.writer(imgfmt, pname)
-		wrt.width = imgw
-		wrt.height = imgh
-		wrt.write(data)
-		data = rdr.ReadVideo()
-		del rdr
-		MacOS.SetCreatorAndType(pname, 'ogle', 'JPEG')
+		print 'Writing', fname, imgw, imgh, len(data)
+##		wrt = img.writer(imgfmt, pname)
+##		wrt.width = imgw
+##		wrt.height = imgh
+##		wrt.write(data)
+		timestamp, data = rdr.ReadVideo()
+##		MacOS.SetCreatorAndType(pname, 'ogle', 'JPEG')
+		if num > 20: break
 	print 'Total frames:', num
 		
 if __name__ == '__main__':
