9bd555b69b95f3201a8d469d690d17be6bab03c5
anadelt <anadelt@localhost>
2000-03-06 09:48:56 +0000

    A more complete windows media reader. Also add a windows media audio player agent.

--- mm/win32/wmsdk/wmfsdk/src/wmreader.py
+++ mm/win32/wmsdk/wmfsdk/src/wmreader.py
@@ -1,43 +1,99 @@
 import wmfapi
+large_int = wmfapi.large_int
 
 # init COM libs
 wmfapi.CoInitialize()
 
 
-class ReaderAgent:
+class DebugReaderAgent:
 	def __init__(self):
 		print 'ReaderAgent'
 	def __del__(self):
 		print '~ReaderAgent'
 	def OnStatus(self,status):
-		if status==1:
-			print 'WMT_OPENED'
-		elif status == 11:
-			print 'WMT_STARTED'
-		elif status == 6:
-			print 'WMT_END_OF_STREAMING'
-		elif status == 4:
-			print 'WMT_EOF'
-		else:
-			print 'status=',status
-	def OnSample(self,msSampleTime):
+		print wmfapi.wmt_status_str[status]
+	def OnSample(self,msSampleTime,sampleBuf):
 		print msSampleTime
 
+class DebugReaderAgentAdvanced(DebugReaderAgent):
+	def OnTime(self,msCurrTime):
+		print 'OnTime',msCurrTime
 
+
+# a very simple windows media audio player
+class WMAPlayerAgent:
+	def __init__(self):
+		self._aout = None
+		
+	def OnStatus(self,status):
+		pass
+
+	def OnSample(self,msSampleTime,sampleBuf):		
+		if self._aout:
+			header = self._aout.PrepareHeader(sampleBuf)
+			self._aout.Write(header)
+
+	def WaveOutOpen(self,wfx):
+		import winmmapi
+		self._aout = winmmapi.WaveOutOpen(wfx)
+
+def printWMInfo(reader):
+	nstreams = reader.GetOutputCount()
+	print 'media has',nstreams,"output stream(s)"
+	wmheader = reader.QueryIWMHeaderInfo()
+	for i in range(nstreams):
+		print 'stream',i,'info:'
+		props = reader.GetOutputProps(i)
+		mt = props.GetMediaType()
+		major, subtype = mt.GetType()
+		if major == wmfapi.WMMEDIATYPE_Audio:
+			print 'WMMEDIATYPE_Audio'
+		elif major == wmfapi.WMMEDIATYPE_Video:
+			print 'WMMEDIATYPE_Video'
+		elif major == wmfapi.WMMEDIATYPE_Script:
+			print 'WMMEDIATYPE_Script'				
+		nattrs  = wmheader.GetAttributeCount(i)
+		for j in range(nattrs):
+			attr = wmheader.GetAttributeByIndex(j,i)
+			print wmfapi.wmt_attr_datatype_str[attr[0]],attr[1:]
+	
+		
 rights = 0 # we don't use a rights manager
 reader = wmfapi.CreateReader(rights)
 
-readercbobj = wmfapi.CreatePyReaderCallback(ReaderAgent())
+agent = WMAPlayerAgent()
+#agent = DebugReaderAgent()
+#agent = DebugReaderAgentAdvanced()
+
+readercbobj = wmfapi.CreatePyReaderCallback(agent)
+#readercbobj = wmfapi.CreatePyReaderCallbackAdvanced(agent)
 
 reader.Open(r'D:\ufs\mm\cmif\win32\wmsdk\wmfsdk\src\test.wma',readercbobj)
 readercbobj.WaitOpen()
-noutputs = reader.GetOutputCount()
-print 'media file has',noutputs,'stream(s)'
 
-reader.Start(0,0,1.0)
+# print some info for windows media file
+printWMInfo(reader)
+
+
+# get output props of pin 0
+nout = reader.GetOutputCount()
+props = reader.GetOutputProps(0)
+
+if nout==1 and agent.__class__ == WMAPlayerAgent:
+	mt = props.GetMediaType()
+	major, subtype = mt.GetType()
+	if major==wmfapi.WMMEDIATYPE_Audio:
+		wfx = mt.GetAsWaveFormatEx()
+		agent.WaveOutOpen(wfx)
+
+# start and duration are in cns == 100 ns
+start = large_int(0)
+dur = large_int(0)
+reader.Start(start,dur,1.0)
 
 # wait efficiently for completion
 readercbobj.WaitForCompletion()
+reader.Stop()
 
 # no more python callbacks please
 readercbobj.SetListener()
