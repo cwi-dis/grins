9e60c150016dc40be8bb771ee0a832dd8f16281a
Sjoerd Mullender <sjoerdmullender@users.noreply.github.com>
2000-03-09 08:57:19 +0000

    Moved smil-boston branch.

---
+++ mm/lib/MMTypes.py
@@ -0,0 +1,7 @@
+__version__ = "$Id$"
+
+leaftypes = ['imm', 'ext']
+bagtypes = ['bag']
+interiortypes = ['seq', 'par', 'alt', 'excl'] + bagtypes
+playabletypes = leaftypes + bagtypes
+alltypes = leaftypes + interiortypes
