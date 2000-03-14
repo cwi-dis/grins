72ba27794b793450bdeb417fb6b039236efae048
anadelt <anadelt@localhost>
2000-03-14 12:11:06 +0000

    GetVideoSize and GetMediaDuration are naturally part of this module.

--- mm/lib/win32/win32dxm.py
+++ mm/lib/win32/win32dxm.py
@@ -18,6 +18,7 @@ class GraphBuilder:
 		except dshow.error, arg:
 			print arg
 			self._builder = None
+		self._rendered = 0
 
 	def __repr__(self):
 		s = '<%s instance' % self.__class__.__name__
@@ -42,44 +43,49 @@ class GraphBuilder:
 			self._builder.RenderFile(url)
 		except dshow.error, arg:
 			print arg
-			return 0
-		return 1
+			self._rendered = 0
+		else:
+			self._rendered = 1
+		return self._rendered
 
 	def Run(self):
-		if self._builder:
+		if self._builder and self._rendered:
 			mc = self._builder.QueryIMediaControl()
 			mc.Run()
 	def Stop(self):
-		if self._builder:
+		if self._builder and self._rendered:
 			mc = self._builder.QueryIMediaControl()
 			mc.Stop()
 	def Pause(self):
-		if self._builder:
+		if self._builder and self._rendered:
 			mc = self._builder.QueryIMediaControl()
 			mc.Pause()
 
 	def GetDuration(self):
-		if self._builder:
-			mp = self._builder.QueryIMediaPosition()
-			return mp.GetDuration()
-		return 0
+		if self._builder and self._rendered:
+			try:
+				mp = self._builder.QueryIMediaPosition()
+				return mp.GetDuration()
+			except:
+				return 100 # sometimes should be infinite
+		return 1.0
 
 	def SetPosition(self,pos):
-		if self._builder:
+		if self._builder and self._rendered:
 			mp = self._builder.QueryIMediaPosition()
 			mp.SetCurrentPosition(pos)
 	def GetPosition(self):
-		if self._builder:
+		if self._builder and self._rendered:
 			mp = self._builder.QueryIMediaPosition()
 			return mp.GetCurrentPosition()
 		return 0
 
 	def SetStopTime(self,pos):
-		if self._builder:
+		if self._builder and self._rendered:
 			mp = self._builder.QueryIMediaPosition()
 			mp.SetStopTime(pos)
 	def GetStopTime(self):
-		if self._builder:
+		if self._builder and self._rendered:
 			mp = self._builder.QueryIMediaPosition()
 			return mp.GetStopTime()
 		return 0
@@ -102,8 +108,9 @@ class GraphBuilder:
 			mex.SetNotifyWindow(wnd.GetSafeHwnd(),msgid)
 
 	def SetNotifyWindow(self,wnd,msgid=WM_GRPAPHNOTIFY):
-		mex = self._builder.QueryIMediaEventEx()
-		mex.SetNotifyWindow(wnd.GetSafeHwnd(),msgid)
+		if self._builder and self._rendered:
+			mex = self._builder.QueryIMediaEventEx()
+			mex.SetNotifyWindow(wnd.GetSafeHwnd(),msgid)
 
 	def GetWindowPosition(self):
 		vw = self.GetVideoWindow()
@@ -118,7 +125,7 @@ class GraphBuilder:
 	# get all filter names of GraphBuilder graph
 	# note: The file reader filter gets its name from the file
 	def GetFiltersNames(self):
-		if not self._builder: return
+		if not self._builder or not self._rentered: return []
 		enumobj = self._builder.EnumFilters()
 		f = enumobj.Next()
 		filters = []
@@ -129,7 +136,7 @@ class GraphBuilder:
 		return filters
 
 	def HasVideo(self):
-		if not self._builder: return
+		if not self._builder or not self._rentered: return None
 		try:
 			return self._builder.FindFilterByName('Video Renderer')
 		except:
@@ -137,11 +144,56 @@ class GraphBuilder:
 		
 # a shortcut usefull when we want to know
 # the type of an asf stream (video or audio)
-def hasvideo(self,url):
-	b = GraphBuilder()
-	b.RenderFile(url)
-	return b.HasVideo()!=None
+def HasVideo(url):
+	try:
+		builder = dshow.CreateGraphBuilder()
+	except:
+		print 'Missing DirectShow infrasrucrure'
+		return None
+	try:
+		vrenderer = builder.FindFilterByName('Video Renderer')
+	except:
+		vrenderer = None
+	return vrenderer
+
+# Returns the size of a video	
+def GetVideoSize(url):
+	try:
+		builder = dshow.CreateGraphBuilder()
+	except:
+		print 'Missing DirectShow infrasrucrure'
+		return (0, 0)
+	try:
+		builder.RenderFile(url)
+	except:
+		print 'failed to render',url
+		return(0, 0)
+	vw = builder.QueryIVideoWindow()
+	try:
+		width, height = vw.GetWindowPosition()[2:]
+	except:
+		print 'failed to get size',url
+		width, height = 0, 0
+	return (width, height)
 
 
+# Returns the duration of the media file in secs	
+def GetMediaDuration(url):
+	try:
+		builder = dshow.CreateGraphBuilder()
+	except:
+		print 'Missing DirectShow infrasrucrure'
+		return 0
+	try:
+		builder.RenderFile(url)
+	except:
+		print 'failed to render',url
+		return 0
+	mp = builder.QueryIMediaPosition()
+	try:
+		duration = mp.GetDuration()
+	except:
+		duration = 1 # sometimes should be infinite
+	return duration
 
 
