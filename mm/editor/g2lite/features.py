ada26090c12cfc5bcaaf32f78610edcf3d76c493
Jack Jansen <Jack.Jansen@cwi.nl>
2000-03-14 15:27:03 +0000

    Moved lightweight and compatibility setting to their own module. Various other settings should probably migrate here too. The correct features module is picked by setting up the correct sys.path. There is no features module for grins-for-smil yet.

---
+++ mm/editor/g2lite/features.py
@@ -0,0 +1,11 @@
+# Features for GRiNS for G2, lite version
+
+# compatibility values
+CMIF = 'cmif'
+SMIL10 = 'SMIL 1.0'
+G2 = 'g2'
+
+#
+compatibility = G2
+cmif = 0
+lightweight = 1
---
+++ mm/editor/g2pro/features.py
@@ -0,0 +1,10 @@
+# Features for GRiNS for G2, pro version
+
+# compatibility values
+CMIF = 'cmif'
+SMIL10 = 'SMIL 1.0'
+G2 = 'g2'
+
+#
+compatibility = G2
+lightweight = 0
