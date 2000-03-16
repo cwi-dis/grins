83754bc8fbef8ec5aef48e29a7157610a26c6100
Jack Jansen <Jack.Jansen@cwi.nl>
2000-03-16 14:13:58 +0000

    Finished mac creator/type support. Unfortunately it doesn't work because everyone passes relative URLs to guess_type. Needs a bit more thinking.

--- mm/lib/MMmimetypes.py
+++ mm/lib/MMmimetypes.py
@@ -11,6 +11,9 @@ mimetypes.types_map.update(grins_mimetypes.mimetypes)
 # the extension in the registry if we don't know of it.
 import sys
 if sys.platform == 'mac':
+	import MMurl
+	import urlparse
+	import MacOS
 	try:
 		import ic
 		_ic_instance = ic.IC()
@@ -26,14 +29,28 @@ if sys.platform == 'mac':
 		# belongs to files with a different mimetype.
 		type = mimetypes.guess_type(url)
 		if type or not _ic_instance: return type
+		#
+		# Next step is to see whether the extension is known to Internet Config
+		#
 		try:
 			descr = _ic_instance.mapfile(url)
 		except ic.error:
 			descr = None
 		else:
 			return descr[8]
-		pass # XXXX If file is local get creator/type
-		pass # descr = _ic_instance.maptypecreator(type, creator, url)
+		#
+		# Final step, for urls pointing to existing local files, we use
+		# the creator/type code and give a warning
+		#
+		utype, host, path, params, query, fragment = urlparse.urlparse(url)
+		if (utype and utype != 'file') or (host and host != 'localhost'):
+			return None
+		filename = MMurl.url2pathname(path)
+		try:
+			creator, type = MacOS.GetCreatorType(filename)
+		except MacOS.Error:
+			return None
+		descr = _ic_instance.maptypecreator(type, creator, url)
 		if not descr:
 			return None
 		import windowinterface
