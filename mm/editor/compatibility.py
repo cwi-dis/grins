e67487c3bf08aead545b263d0331b9e582c00d82
Sjoerd Mullender <sjoerdmullender@users.noreply.github.com>
2000-03-28 12:56:11 +0000

    Put compatibility values in separate (shared) module.

---
+++ mm/editor/compatibility.py
@@ -0,0 +1,6 @@
+__version__ = "$Id$"
+
+# compatibility values
+CMIF = 'cmif'
+SMIL10 = 'SMIL 1.0'
+G2 = 'g2'
--- mm/editor/g2pro/features.py
+++ mm/editor/g2pro/features.py
@@ -1,9 +1,8 @@
+__version__ = "$Id$"
+
 # Features for GRiNS for G2, pro version
 
-# compatibility values
-CMIF = 'cmif'
-SMIL10 = 'SMIL 1.0'
-G2 = 'g2'
+from compatibility import *
 
 #
 compatibility = G2
