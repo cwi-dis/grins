21a309bb351aa6f05b6b55015140b23d25cffc59
Jack Jansen <Jack.Jansen@cwi.nl>
2000-04-05 14:32:24 +0000

    Removed Mac special-case code.

--- mm/editor/version.py
+++ mm/editor/version.py
@@ -3,17 +3,9 @@ import sys
 from patchlevel import patchlevel
 import features
 if features.lightweight:
-	if sys.platform == 'mac':
-		shortversion = 'grinslite-%s-1.5beta'%sys.platform
-		version = 'Lite for RealSystem G2, v1.5beta ' + patchlevel
-	else:
-		shortversion = 'grinslite-%s-1.5'%sys.platform
-		version = 'Lite for RealSystem G2, v1.5 ' + patchlevel
+	shortversion = 'grinslite-%s-1.5'%sys.platform
+	version = 'Lite for RealSystem G2, v1.5 ' + patchlevel
 else:
-	if sys.platform == 'mac':
-		shortversion = 'grinspro-%s-1.5beta'%sys.platform
-		version = 'Pro for RealSystem G2, v1.5beta ' + patchlevel
-	else:
-		shortversion = 'grinspro-%s-1.5'%sys.platform
-		version = 'Pro for RealSystem G2, v1.5 ' + patchlevel
-	
+	shortversion = 'grinspro-%s-1.5'%sys.platform
+	version = 'Pro for RealSystem G2, v1.5 ' + patchlevel
+
