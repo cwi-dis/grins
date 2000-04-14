920afb4d774cfafe07464a78222e3d12a161371c
anadelt <anadelt@localhost>
2000-04-14 12:09:07 +0000

    Update dev test sample.

--- mm/mmpython/rmaexp/test.py
+++ mm/mmpython/rmaexp/test.py
@@ -3,12 +3,26 @@ import rma
 
 engine = rma.CreateEngine()
 
-player =  engine.CreatePlayer()
-
 context = rma.CreateClientContext()
+print context
+cobj = rma.CreateClientAdviseSink()
+print cobj
+errorSink = rma.CreateErrorSink()
+print errorSink
+cobj = rma.CreateSiteSupplier()
+print cobj
+cobj = rma.CreateAuthenticationManager()
+print cobj
+
+
+player =  engine.CreatePlayer()
 
 player.SetClientContext(context)
 
+errorctrl = player.QueryIRMAErrorSinkControl()
+print errorctrl
+
+errorctrl.AddErrorSink(errorSink)
 
 # real audio
 url1="file://D|/ufs/mm/cmif/mmpython/rmasdk/testdata/thanks3.ra"
