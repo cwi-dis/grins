5e2864cd06b02c6a3ffbb9c68142dbaf06118ed9
anadelt <anadelt@localhost>
2000-04-17 08:23:24 +0000

    A collective header for rma includes.

---
+++ mm/mmpython/rmaexp/rma.h
@@ -0,0 +1,13 @@
+
+#include "pntypes.h" 
+#include "pncom.h"
+#include "pnresult.h"
+#include "pnwintyp.h"
+
+#include "rmacore.h"
+#include "rmawin.h"
+#include "rmaerror.h"
+#include "rmaclsnk.h"
+#include "rmaauth.h"
+#include "rmacomm.h"
+#include "rmapckts.h"
