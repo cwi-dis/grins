6f71e568d5130efae964749cd6baf7dad1d3e166
Sjoerd Mullender <sjoerdmullender@users.noreply.github.com>
2000-05-02 16:27:05 +0000

    Converted flags to new scheme (this has to do with the flags in the window adornments). Added the Add external button to the Link View.

--- mm/lib/motif/XAdornment.py
+++ mm/lib/motif/XAdornment.py
@@ -51,7 +51,7 @@ class _AdornmentSupport(_CommandSupport, _ButtonSupport):
 			menu.packing = Xmd.PACK_COLUMN
 		for entry in list:
 			flag = entry[0]
-			if (flag & flags) != flag:
+			if (flag & flags) == 0:
 				continue
 			label = entry[1]
 			if label is None:
@@ -85,7 +85,7 @@ class _AdornmentSupport(_CommandSupport, _ButtonSupport):
 	def _create_toolbar(self, tb, list, vertical, flags):
 		for entry in list:
 			flag = entry[0]
-			if (flag & flags) != flag:
+			if (flag & flags) == 0:
 				continue
 			if entry[1] is None:
 				if vertical:
