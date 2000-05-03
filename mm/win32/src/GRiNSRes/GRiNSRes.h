b1569b0d20f1e6fde64386922a52b1b1a007b754
Sjoerd Mullender <sjoerdmullender@users.noreply.github.com>
2000-05-03 09:13:12 +0000

    Enable horizontal scrollbar display for left list in links view.

--- mm/win32/src/GRiNSRes/GRiNSRes.h
+++ mm/win32/src/GRiNSRes/GRiNSRes.h
@@ -415,7 +415,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        290
 #define _APS_NEXT_COMMAND_VALUE         32860
-#define _APS_NEXT_CONTROL_VALUE         1241
+#define _APS_NEXT_CONTROL_VALUE         1242
 #define _APS_NEXT_SYMED_VALUE           104
 #endif
 #endif
