4072a5461a07ef5530be9fb5aea01010f41cb81a
Jack Jansen <Jack.Jansen@cwi.nl>
2000-05-04 13:15:12 +0000

    Obtain the macintosh preference file name from the version module

--- mm/editor/version.py
+++ mm/editor/version.py
@@ -5,6 +5,8 @@ import features
 if features.lightweight:
 	shortversion = 'grinslite-%s-1.5.1'%sys.platform
 	version = 'Lite for %s, v1.5.1 %s' % (features.compatibility, patchlevel)
+	macpreffilename = 'GRiNS-lite-%s-1.5 Prefs' % features.compatibility_short
 else:
 	shortversion = 'grinspro-%s-1.5.1'%sys.platform
 	version = 'Pro for %s, v1.5.1 %s' % (features.compatibility, patchlevel)
+	macpreffilename = 'GRiNS-pro-%s-1.5 Prefs' % features.compatibility_short
