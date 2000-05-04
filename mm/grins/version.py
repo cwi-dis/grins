9b57c5d9400a9aaece11045ea3c7c732d3fce8f2
Jack Jansen <Jack.Jansen@cwi.nl>
2000-05-04 13:07:25 +0000

    Obtain the macintosh preferrence file name from the version module

--- mm/grins/version.py
+++ mm/grins/version.py
@@ -1,4 +1,6 @@
 __version__ = "$Id$"
-
+import sys
 from patchlevel import patchlevel
-version = 'player 1.0.1alpha ' + patchlevel
+version = 'Player 1.5 ' + patchlevel
+shortversion = 'grinsplay-%s-1.5.1' % sys.platform
+macpreffilename = 'GRiNS-play-1.5 Prefs'
