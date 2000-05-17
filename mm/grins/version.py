d3d7c9ce123ad8dd01275b4dbf01545b19672d57
Sjoerd Mullender <sjoerdmullender@users.noreply.github.com>
2000-05-17 10:21:33 +0000

    Updated version number.

--- mm/grins/version.py
+++ mm/grins/version.py
@@ -1,6 +1,6 @@
 __version__ = "$Id$"
 import sys
 from patchlevel import patchlevel
-version = 'Player 1.5 ' + patchlevel
+version = 'Player v1.5.1 ' + patchlevel
 shortversion = 'grinsplay-%s-1.5.1' % sys.platform
 macpreffilename = 'GRiNS-play-1.5 Prefs'
--- mm/grins/win32/patchlevel.py
+++ mm/grins/win32/patchlevel.py
@@ -5,4 +5,5 @@ Contains the subversion of the application in the
 format <platform> patchlevel <number>
 """
 
-patchlevel = 'win32 patchlevel 31'
+##patchlevel = 'win32 patchlevel 31'
+patchlevel = ''
