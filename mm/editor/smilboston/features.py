649121bb6e05c5b3795516ac7f23adf07fa4556b
Sjoerd Mullender <sjoerdmullender@users.noreply.github.com>
2000-05-17 14:24:11 +0000

    Missing file.

---
+++ mm/editor/smilboston/features.py
@@ -0,0 +1,12 @@
+__version__ = "$Id$"
+
+# Features for GRiNS for G2, pro version
+
+from compatibility import *
+
+#
+compatibility = Boston
+compatibility_short = 'SMIL2'
+cmif = 0
+lightweight = 0
+editor = 1
