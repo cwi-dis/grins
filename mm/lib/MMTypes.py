36558ce00581654b20298b80ab3cb8ffbc5d1b06
Sjoerd Mullender <sjoerdmullender@users.noreply.github.com>
2000-08-02 15:51:01 +0000

    We can now read in priorityClass elements.  They are saved as nodes in the tree.  We can also play documents with PriorityClass elements, but I haven't implemented the semantics yet.

--- mm/lib/MMTypes.py
+++ mm/lib/MMTypes.py
@@ -2,6 +2,6 @@ __version__ = "$Id$"
 
 leaftypes = ['imm', 'ext']
 bagtypes = ['bag']
-interiortypes = ['seq', 'par', 'alt', 'excl'] + bagtypes
+interiortypes = ['seq', 'par', 'alt', 'excl', 'prio'] + bagtypes
 playabletypes = leaftypes + bagtypes
 alltypes = leaftypes + interiortypes
