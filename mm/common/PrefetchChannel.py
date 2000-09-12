8bcee8808e792fa60bf6ff816131a98ad1339e1d
anadelt <anadelt@localhost>
2000-09-12 11:31:12 +0000

    Complete skeleton of prefetch channel. At least initially we are going to use timeslices instead of threads (~os timeslices) for the prefetch engine.

--- mm/common/PrefetchChannel.py
+++ mm/common/PrefetchChannel.py
@@ -1,51 +1,152 @@
 __version__ = "$Id$"
 
-
 # the core
 import Channel
 
-# canonURL
+# urlopen
 import MMurl
 
+# for timing support
+import windowinterface
+import time
+
+debug = 1
+
+USE_IDLE_PROC=hasattr(windowinterface, 'setidleproc')
 
 class PrefetchChannel(Channel.ChannelAsync):
 	def __init__(self, name, attrdict, scheduler, ui):
 		Channel.ChannelAsync.__init__(self, name, attrdict, scheduler, ui)
+		self.__duration = None
+		self.__fetching = None
 
 	def __repr__(self):
 		return '<PrefetchChannel instance, name=' + `self._name` + '>'
 
+	#
+	# Channel overrides
+	#
+
 	def do_hide(self):
 		Channel.ChannelAsync.do_hide(self)
 	
-		
-	def destroy(self):
-		Channel.ChannelAsync.destroy(self)
-
-	def do_arm(self, node, same=0):
-		if same: return
-		url = self.getfileurl(node)
-		if not url:
-			self.errormsg(node, 'No URL set on node')
-			return 1
-		url = MMurl.canonURL(url)
-		return 0
-
 	def do_play(self, node):
-		self.playdone(0)
+		Channel.ChannelAsync.do_play(self, node)
+
+		self.__initEngine(node)
+
+		if not self.__ready():
+			self.playdone(0)
+			return
+		
+		self.__fetching = node
+
+		# get timing
+		self.play_loop = self.getloop(node)
+		self.__duration = node.GetAttrDef('duration', None)
+		
+		self.__startFetch()
+		
+	def setpaused(self, paused):
+		Channel.ChannelAsync.setpaused(self, paused)
+		self.__pauseFetch(paused)
 
 	def stopplay(self, node):
+		if self.__fetching:
+			self.__stopFetch()
+			self.__fetching = None
 		Channel.ChannelAsync.stopplay(self, node)
 
-	def setpaused(self, paused):
-		pass
+	#
+	# Fetch engine
+	#
 
-	def OnProgress(self,progress,progressMax,status,statusText):
-		if status==14:
-			self._tmpfile=statusText
-			print 'TEMPFILE:',self._tmpfile
-		print progress,'/',progressMax,'status:',status,statusText
+	def __initEngine(self, node):
+		self.__fiber_id = 0
+		self.__start = None
+		self.__reader = None
+		self.__pausedt = 0
 
-	def OnDownloadComplete(self):
-		self.arm_1()
+		url = self.getfileurl(node)
+		if not url:
+			print 'No URL set on node'
+			return
 
+		url = MMurl.canonURL(url)
+		try:
+			u = MMurl.urlopen(url)
+		except:
+			print 'Warning: cannot open url %s', url
+		else:
+			#print u.headers
+			#print dir(u)
+			self.__reader = u
+			
+	def __ready(self):
+		return self.__reader!=None
+			
+	def __startFetch(self, repeat=0):
+		self.__start = time.time()
+		self.__fetch()
+		self.__register_for_timeslices()
+
+	def __stopFetch(self):
+		if self.__fetching:
+			self.__unregister_for_timeslices()
+			self.__reader.close()
+			self.__reader = None
+
+	def __pauseFetch(self, paused):
+		if self.__fetching:
+			if paused:
+				self.__pausedt = time.time() - self.__start
+				self.__unregister_for_timeslices()
+			else:
+				self.__start = time.time() - self.__pausedt
+				self.__register_for_timeslices()
+
+	def __fetch(self):
+		dt = time.time() - self.__start
+		if self.__reader:
+			self.__reader.read()
+
+	def __onFetchDur(self):
+		if not self.__fetching:
+			return
+		if self.play_loop:
+			self.play_loop = self.play_loop - 1
+			if self.play_loop: # more loops ?
+				self.__startFetch(repeat=1)
+				return
+			self.playdone(0)
+			return
+		# self.play_loop is 0 so repeat
+		self.__startFetch(repeat=1)
+
+	def onIdle(self):
+		if not USE_IDLE_PROC:
+			self.__fiber_id = 0
+		if self.__fetching:
+			t_sec = time.time() - self.__start
+			if t_sec>=self.__duration:
+				self.__onFetchDur()
+				self.__unregister_for_timeslices()
+			else:
+				self.__fetch()
+				self.__register_for_timeslices()
+			
+	def __register_for_timeslices(self):
+		if not self.__fiber_id:
+			if USE_IDLE_PROC:
+				windowinterface.setidleproc(self.onIdle)
+				self.__fiber_id = 1
+			else:
+				self.__fiber_id = windowinterface.settimer(0.05, (self.onIdle,()))
+
+	def __unregister_for_timeslices(self):
+		if self.__fiber_id:
+			if USE_IDLE_PROC:
+				windowinterface.cancelidleproc(self.onIdle)
+			else:
+				windowinterface.canceltimer(self.__fiber_id)
+			self.__fiber_id = 0
