d1e804c41ace85ad20cbde583c1dece0530a681c
Jack Jansen <Jack.Jansen@cwi.nl>
2000-09-14 13:19:21 +0000

    Updated for Python 2.0, generated new frozen player (build 4).

--- mm/Build/mac/build.player-smil2/player_rma_config.h
+++ mm/Build/mac/build.player-smil2/player_rma_config.h
@@ -6,6 +6,9 @@
 */
 
 #define USE_GUSI2		/* Stdio implemented with GUSI */
+#if defined(USE_GUSI1) || defined(USE_GUSI2)
+#define USE_GUSI
+#endif
 #define USE_MSL			/* Use Mw Standard Library (as opposed to Plaugher C libraries) */
 #define USE_TOOLBOX		/* Include toolbox modules in core Python */
 #define USE_QT			/* Include quicktime modules in core Python */
@@ -23,8 +26,12 @@
 /* #define USE_MALLOC_DEBUG			/* Enable range checking and other malloc debugging */
 /* #define USE_GDBM		/* Include the gdbm module */
 /* #define USE_ZLIB		/* Include the zlib module */
+#define USE_MSL_MALLOC	/* Disable private malloc. Also disables next two defines */
+#ifndef USE_MSL_MALLOC
+/* #define USE_MALLOC_DEBUG			/* Enable range checking and other malloc debugging */
 #ifdef __powerc
 #define USE_CACHE_ALIGNED 8		/* Align on 32-byte boundaries for 604 */
+#endif
 #endif
 #ifdef USE_MSL
 #define MSL_USE_PRECOMPILED_HEADERS 0	/* Don't use precomp headers: we include our own */
