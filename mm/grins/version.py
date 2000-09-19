eeef09b84cd729388bd1fcf9b733c5963423fda0
Jack Jansen <Jack.Jansen@cwi.nl>
2000-09-19 11:52:25 +0000

    Version is now 2.0b1

--- mm/grins/version.py
+++ mm/grins/version.py
@@ -1,6 +1,6 @@
 __version__ = "$Id$"
 import sys
 from patchlevel import patchlevel
-version = 'Player 2.0 ' + patchlevel
-shortversion = 'grinsplay-%s-2.0' % sys.platform
-macpreffilename = 'GRiNS-play-2.0 Prefs'
+version = 'Player 2.0b1 ' + patchlevel
+shortversion = 'grinsplay-%s-2.0b1' % sys.platform
+macpreffilename = 'GRiNS-play-2.0b1 Prefs'
