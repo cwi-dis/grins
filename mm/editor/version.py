7ddf8d6fdd345cbce58af59d58b55136c9582b87
mikevdg <mikevdg@localhost>
2000-10-05 10:49:59 +0000

    A couple of small changes; namely added GRiNS Snap! for development.

--- mm/editor/version.py
+++ mm/editor/version.py
@@ -3,7 +3,11 @@ import sys
 from patchlevel import patchlevel
 import features
 
-if features.lightweight:
+if features.version == 'Snap!':
+	shortversion = 'grinssnap-%s-2.0'%sys.platform
+	version = 'Snap! for %s, v2.0 %s' % (features.compatibility, patchlevel)
+	macpreffilename = 'GRiNS-Snap!-%s-2.0 Prefs' % features.compatibility_short	
+elif features.lightweight:		# TODO: change this to..? mjvdg.
 	shortversion = 'grinslite-%s-2.0'%sys.platform
 	version = 'Lite for %s, v2.0 %s' % (features.compatibility, patchlevel)
 	macpreffilename = 'GRiNS-lite-%s-2.0 Prefs' % features.compatibility_short
