31d8082a01c08e9e9bbe1a8637b18fa9c4abc41b
anadelt <anadelt@localhost>
2000-10-08 17:47:53 +0000

    Now GRiNS structure views have their own module.

---
+++ mm/lib/win32/_StructView.py
@@ -0,0 +1,173 @@
+
+# win32 libs
+import win32ui, win32con
+
+# app constants
+from types import *
+from WMEVENTS import *
+from appcon import *
+
+# win32 structures helpers
+import win32mu
+
+# usercmd
+import usercmd, usercmdui, wndusercmd
+
+import math
+
+# drag & drop constants
+import DropTarget
+
+# base class
+from win32dlview import DisplayListView
+
+#################################################
+
+class _StructView(DisplayListView):
+	# Class contructor. initializes base classes
+	def __init__(self, doc, bgcolor=None):
+		DisplayListView.__init__(self,doc)
+
+		# shortcut for GRiNS private clipboard format
+		self.CF_NODE = self.getClipboardFormat('Node')
+
+		# enable or dissable node drag and drop
+		self._enableNodeDragDrop = 1
+			
+		if self._enableNodeDragDrop:
+			self._dropmap['Node']=(self.dragnode, self.dropnode)
+			self._dragging = None
+
+	def OnCreate(self,params):
+		DisplayListView.OnCreate(self,params)
+		# enable mechanism to accept paste files
+		# when the event PasteFile is registered
+		id=usercmdui.class2ui[wndusercmd.PASTE_FILE].id
+		frame=self.GetParent().GetMDIFrame()
+		frame.HookCommand(self.OnPasteFile,id)
+		frame.HookCommandUpdate(self.OnUpdateEditPaste,id)
+
+	def PaintOn(self,dc):
+		# only paint the rect that needs repainting
+		rect=win32mu.Rect(dc.GetClipBox())
+
+		# draw to offscreen bitmap for fast looking repaints
+		dcc=dc.CreateCompatibleDC()
+
+		bmp=win32ui.CreateBitmap()
+		bmp.CreateCompatibleBitmap(dc,rect.width(),rect.height())
+		
+		# called by win32ui
+		#self.OnPrepareDC(dcc)
+		
+		# offset origin more because bitmap is just piece of the whole drawing
+		dcc.OffsetViewportOrg((-rect.left, -rect.top))
+		oldBitmap = dcc.SelectObject(bmp)
+		dcc.SetBrushOrg((rect.left % 8, rect.top % 8))
+		dcc.IntersectClipRect(rect.tuple())
+
+		# background decoration on dcc
+		if self._active_displist:color=self._active_displist._bgcolor
+		else: color=self._bgcolor
+		dcc.FillSolidRect(rect.tuple(),win32mu.RGB(color))
+
+		# draw objects on dcc
+		if self._active_displist:
+			self._active_displist._render(dcc,rect.tuple())
+
+		# copy bitmap
+		dcc.SetViewportOrg((0, 0))
+		dcc.SetWindowOrg((0,0))
+		dcc.SetMapMode(win32con.MM_TEXT)
+		dc.BitBlt(rect.pos(),rect.size(),dcc,(0, 0), win32con.SRCCOPY)
+
+		# clean up
+		dcc.SelectObject(oldBitmap)
+		dcc.DeleteDC()
+		del bmp
+
+	def OnEraseBkgnd(self,dc):
+		return 1
+
+	# Response to left button double click
+	def onLButtonDblClk(self, params):
+		import usercmd, usercmdui
+		#self._parent.PostMessage(win32con.WM_COMMAND,usercmdui.class2ui[usercmd.INFO].id)
+		callAttr=0
+		for cmd in self._commandlist:
+			if cmd.__class__==usercmd.INFO:
+				self._parent.PostMessage(win32con.WM_COMMAND,usercmdui.class2ui[usercmd.INFO].id)
+				return
+			if cmd.__class__==usercmd.ATTRIBUTES:
+				callAttr=1
+		if callAttr:
+			self._parent.PostMessage(win32con.WM_COMMAND,usercmdui.class2ui[usercmd.ATTRIBUTES].id)
+
+	# Response to left button down
+	def onLButtonDown(self, params):
+		DisplayListView.onLButtonDown(self, params)
+		if self._enableNodeDragDrop:
+			msgpos=win32mu.Win32Msg(params).pos()
+			self._dragging = msgpos
+
+	def onLButtonUp(self, params):
+		DisplayListView.onLButtonUp(self, params)
+		if self._enableNodeDragDrop:
+			if self._dragging: 
+				self._dragging = None
+
+	def onMouseMove(self, params):
+		DisplayListView.onMouseMove(self, params)
+		if self._enableNodeDragDrop and self._dragging:
+			xp, yp = self._dragging
+			x, y =win32mu.Win32Msg(params).pos()
+			if math.fabs(xp-x)>4 or math.fabs(yp-y)>4:
+				str='%d %d' % (xp, yp)
+				self.DoDragDrop(self.CF_NODE, str)
+
+	def OnDragLeave(self):
+		self._dragging = None
+
+	# we must return DROPEFFECT_NONE
+	# when paste at x, y is not allowed
+	def dragnode(self, dataobj, kbdstate, x, y):
+		node=dataobj.GetGlobalData(self.CF_NODE)
+		if node:
+			x, y = self._DPtoLP((x,y))
+			x, y = self._pxl2rel((x, y),self._canvas)
+			if self.isShiftPressed(kbdstate):
+				cmd = 'move'
+			else:
+				cmd = 'copy'
+			return self.onEventEx(DragNode,(x, y, cmd))
+		return DropTarget.DROPEFFECT_NONE
+
+	def dropnode(self, dataobj, effect, x, y):
+		DROP_FAILED, DROP_SUCCEEDED = 0, 1
+		node = dataobj.GetGlobalData(self.CF_NODE)
+		if node and self._dragging:
+			
+			# the drag and drop cmd is:
+			# copy or cut (according to arg effect)
+			# the node at point self._dragging 
+			# to position x, y
+			x, y = self._DPtoLP((x,y))
+			x, y = self._pxl2rel((x, y),self._canvas)
+
+			xf, yf = self._dragging
+			xf, yf = self._DPtoLP((xf,yf))
+			xf, yf = self._pxl2rel((xf, yf),self._canvas)
+			self._dragging = None
+
+			# if the operation can not be executed return DROP_FAILED
+			# else return DROP_SUCCEEDED
+
+			if effect == DropTarget.DROPEFFECT_MOVE:
+				cmd = 'move'
+			else:
+				cmd = 'copy'
+
+			return self.onEventEx(DropNode,(x, y, cmd, xf, yf))
+
+		return DROP_FAILED
+							
