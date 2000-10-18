cf64be827a4fa3faafa38f3167f16b59c2a2cd2c
Sjoerd Mullender <sjoerdmullender@users.noreply.github.com>
2000-10-18 14:30:25 +0000

    Make all export commands available in PRO version.

--- mm/editor/win32/MenuTemplate.py
+++ mm/editor/win32/MenuTemplate.py
@@ -49,15 +49,15 @@ MENUBAR=(
 		(FLAG_ALL, SEP,),
 		(FLAG_QT, ENTRY, 'Publish for &QuickTime...', None, EXPORT_QT),
 		(FLAG_QT, ENTRY, 'Pu&blish for QuickTime and upload...', None, UPLOAD_QT),
-		(FLAG_G2, ENTRY, 'Publish for &G2...', None, EXPORT_G2),
+		(FLAG_G2|FLAG_PRO, ENTRY, 'Publish for &G2...', None, EXPORT_G2),
 		(FLAG_G2, ENTRY, 'Pu&blish for G2 and upload...', None, UPLOAD_G2),
                 # TODO: These should not appear on all versions of GRiNS!
-                (FLAG_ALL, ENTRY, 'Publish for &Windows Media...', None, EXPORT_WMP), # mjvdg 11-oct-2000
-                (FLAG_ALL, ENTRY, 'Publish for Windows Media and upload...', None, UPLOAD_WMP),
+                (FLAG_PRO, ENTRY, 'Publish for &Windows Media...', None, EXPORT_WMP), # mjvdg 11-oct-2000
+                (FLAG_PRO, ENTRY, 'Publish for Windows Media and upload...', None, UPLOAD_WMP),
                 
-		(FLAG_SMIL_1_0, ENTRY, '&Publish for SMIL 2.0...', None, EXPORT_SMIL),
-		(FLAG_SMIL_1_0, ENTRY, 'Pu&blish for SMIL 2.0 and upload...', None, UPLOAD_SMIL),
-		(FLAG_SMIL_1_0 | FLAG_QT | FLAG_G2, SEP,),
+		(FLAG_SMIL_1_0|FLAG_PRO, ENTRY, '&Publish for SMIL 2.0...', None, EXPORT_SMIL),
+		(FLAG_SMIL_1_0|FLAG_PRO, ENTRY, 'Pu&blish for SMIL 2.0 and upload...', None, UPLOAD_SMIL),
+		(FLAG_SMIL_1_0 | FLAG_QT | FLAG_G2 | FLAG_PRO, SEP,),
 		(FLAG_ALL, ENTRY, '&Document Properties...', None, PROPERTIES),
 		(FLAG_DBG, SEP,),
 		(FLAG_DBG, CASCADE, 'D&ebug', (
