e766cb0a0e60e2521b7173c41544bc45ce74d34c
anadelt <anadelt@localhost>
2000-10-22 19:58:27 +0000

    A copy of WMWriter class. It is used by the samples.

--- mm/win32/wmsdk/wmfsdk/src/wmwriter.py
+++ mm/win32/wmsdk/wmfsdk/src/wmwriter.py
@@ -1,90 +1,78 @@
 __version__ = "$Id$"
 
 
-import wmfapi
-large_int = wmfapi.large_int
+"""
+System configuration profiles
+0 Dial-up Modems - ISDN Multiple Bit Rate Video
+1 Intranet - High Speed LAN Multiple Bit Rate Video
+2 28.8, 56, and 100 Multiple Bit Rate Video
+3 6.5 voice audio
+4 16 AM Radio
+5 28.8 FM Radio Mono
+6 28.8 FM Radio Stereo
+7 56 Dial-up High Quality Stereo
+8 64 Near CD Quality Audio
+9 96 CD Quality Audio
+10 128 CD Quality Audio
+11 28.8 Video - Voice
+12 28.8 Video - Audio Emphasis
+13 28.8 Video for Web Server
+14 56 Dial-up Modem Video
+15 56 Dial-up Video for Web Server
+16 100 Video
+17 250 Video
+18 512 Video
+19 1Mb Video
+20 3Mb Video
+"""
 
+try:
+	import wmfapi
+except ImportError:
+	wmfapi = None
+else:
+	wmfapi.CoInitialize()
+		
+class WMWriter:
+	def __init__(self, dds, profile=20):
+		self._dds = dds
+		self._writer = None
+		self._filename = None
+		self._sample = None
+		self._lasttm = 0
+		if not wmfapi:
+			return
+		profman = wmfapi.CreateProfileManager()
+		prof = profman.LoadSystemProfile(profile) 
+		writer = wmfapi.CreateDDWriter(prof)
+		wmtype = wmfapi.CreateDDVideoWMType(self._dds)
+		writer.SetVideoFormat(wmtype)
+		self._writer = writer
 
-# init COM libs
-wmfapi.CoInitialize()
+	def setOutputFilename(self, filename):
+		self._filename = filename
+		if self._writer:
+			self._writer.SetOutputFilename(filename)
+		
+	def beginWriting(self):
+		if self._writer:
+			self._writer.BeginWriting()
+			self._sample = self._writer.AllocateDDSample(self._dds)
+			self._lasttm = 0
 
-# temp debug: find the name of a guid
-def findguid(guid):
-    for s in dir(wmfapi):
-		if s.find("WMMEDIA")>=0 or s.find("WMFORMAT")>=0:
-			if guid==getattr(wmfapi,s):
-				return s
+	def endWriting(self):
+		if self._writer:
+			self._writer.Flush()
+			self._writer.EndWriting()
 
-profman = wmfapi.CreateProfileManager()
-nprofiles = profman.GetSystemProfileCount()
-print nprofiles,'system profiles:'
-for ix in range(nprofiles):
-	prof = profman.LoadSystemProfile(ix)
-	print ix, prof.GetName()
+	def update(self, now):
+		now = int(1000.0*now+0.5)
+		now = 100*(now/100)
+		if not self._writer: return
+		t = self._lasttm
+		while t <= now:
+			self._writer.WriteVideoSample(t, self._sample)
+			t = t + 100
+		self._sample = self._writer.AllocateDDSample(self._dds)
+		self._lasttm = now
 
-# find audio pin
-prof = profman.LoadSystemProfile(10)
-writer = wmfapi.CreateWriter()
-writer.SetProfile(prof)
-npins = writer.GetInputCount()
-audiopinix = -1
-audiopinmt = None
-audiopinprops = None
-print 'profile pins:'
-for i in range(npins):
-	pinprop = writer.GetInputProps(i)
-	pintype = pinprop.GetType()
-	print i,'\t', findguid(pintype)
-	if pintype == wmfapi.WMMEDIATYPE_Audio:
-		audiopinix = i
-		audiopinprops = pinprop
-		audiopinmt = pinprop.GetMediaType()
-if audiopinix>=0:
-	print 'audiopin is pin ',audiopinix
-
-writer.SetOutputFilename(r'D:\ufs\mm\cmif\win32\wmsdk\wmfsdk\src\testdata\test.wma');
-
-# read header and set audiopinmt
-import avi
-avi.AVIFileInit()
-avifile = avi.AVIFileOpen(r'D:\ufs\mm\cmif\win32\DXMedia\bin\yyy.wav')
-avistream = avifile.GetStream()
-avifsize = avistream.FormatSize()
-wfx = avistream.ReadFormat(avifsize)
-fmt = wfx.GetMembers()
-bps = fmt[3]
-aviinfo = avistream.ReadInfo()
-
-audiopinmt.SetType(wmfapi.WMMEDIATYPE_Audio,wmfapi.WMMEDIASUBTYPE_PCM)
-audiopinmt.SetSampleSize(aviinfo.GetSampleSize())
-audiopinmt.SetFormat(wmfapi.WMFORMAT_WaveFormatEx,wfx.GetBuffer())
-audiopinprops.SetMediaType(audiopinmt)
-
-writer.SetInputProps(audiopinix,audiopinprops)
-
-nSamples = avistream.End()
-print nSamples,'samples in avi'
-curSample = avistream.Start()
-tmsec = 0
-ms2cns = large_int(10000)
-
-writer.BeginWriting()
-samplesToRead = 1024
-while curSample < nSamples:
-	data, bytes, nsamples = avistream.Read(curSample,samplesToRead)
-	sample = writer.AllocateSample(bytes)
-	sample.SetBuffer(data)
-	tmsec = tmsec + bytes*1000/bps
-	tcnsec = large_int(tmsec)*ms2cns  
-	curSample = curSample + nsamples
-	writer.WriteSample(audiopinix,tcnsec,0,sample)
-
-writer.Flush()
-writer.EndWriting()
-
-del avistream
-del avifile
-avi.AVIFileExit()
-
-# on exit: release COM libs
-wmfapi.CoUninitialize()
