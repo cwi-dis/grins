a35bccd572edaf2d5b521a946d89b7497cf4e037
anadelt <anadelt@localhost>
2000-10-23 16:24:38 +0000

    Assert that the latest wmfsdk is checked in.

--- mm/win32/wmsdk/wmfsdk/include/wmsdkidl.h
+++ mm/win32/wmsdk/wmfsdk/include/wmsdkidl.h
@@ -2,7 +2,7 @@
 
 
 /* File created by MIDL compiler version 5.01.0164 */
-/* at Mon Jun 26 06:49:05 2000
+/* at Tue Aug 08 11:36:40 2000
  */
 /* Compiler settings for .\wmsdkidl.idl:
     Oicf (OptLev=i2), W1, Zp8, env=Win32, ms_ext, c_ext
