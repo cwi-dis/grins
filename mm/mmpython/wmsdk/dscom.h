1f9d7a505c9477c77ca184ab72b5f0443809acdc
anadelt <anadelt@localhost>
2000-10-23 19:48:57 +0000

    Update IWMConverter.

--- mm/mmpython/wmsdk/dscom.h
+++ mm/mmpython/wmsdk/dscom.h
@@ -15,9 +15,9 @@ DEFINE_GUID(IID_IRendererAdviceSink,
 interface IRendererAdviceSink : public IUnknown
 	{
 	virtual HRESULT __stdcall OnSetMediaType(const CMediaType *pmt) = 0;
-	virtual HRESULT __stdcall OnActive()=0;
-	virtual HRESULT __stdcall OnInactive()=0;
-	virtual HRESULT __stdcall OnRenderSample(IMediaSample *pMediaSample) = 0;
+	virtual HRESULT __stdcall OnActive() = 0;
+	virtual HRESULT __stdcall OnInactive() = 0;
+	virtual HRESULT __stdcall OnRenderSample(IMediaSample *pMediaSample) = 0; 
 	};
 
 // {BDBC884C-0FCE-414f-9941-035F900E43B6}
@@ -25,7 +25,6 @@ DEFINE_GUID(IID_IWMConverter,
 0xbdbc884c, 0xfce, 0x414f, 0x99, 0x41, 0x3, 0x5f, 0x90, 0xe, 0x43, 0xb6);
 interface IWMConverter : public IUnknown
 	{
-	virtual HRESULT __stdcall SetWMWriter(IUnknown *pI) = 0;
 	virtual HRESULT __stdcall SetRendererAdviceSink(IRendererAdviceSink *pI) = 0;
 	};
 
