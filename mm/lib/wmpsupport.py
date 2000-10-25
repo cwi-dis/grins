ec8ad44d150da932abac0f724b0a4f362a435048
anadelt <anadelt@localhost>
2000-10-25 18:27:54 +0000

    Show a progress dialog while exporting albeit the progress is not set yet. Also remove some unused stuff.

--- mm/lib/wmpsupport.py
+++ mm/lib/wmpsupport.py
@@ -9,22 +9,24 @@ class Exporter:
 		self.player = player
 		self.writer = None
 		self.profile = profile
-		self.aborted = 0
-		self.completed = 0
 		self.topwindow = None
 		windowinterface.settimevirtual(1)
 		self.starttime = windowinterface.getcurtime()
 		print 'starttime=', self.starttime
+		
+		self._progress = windowinterface.ProgressDialog("Exporting", self.cancel_callback)
+		self._progress.set('Exporting document to WMP...')
+		
+		# XXX: temp until set progress is enabled
+		self._progress._dialog.OnCancel = self.cancel_callback 
+		
 		self.player.exportplay(self)
 		
-	def _cleanup(self):
-		self.player = None
-		self.topwindow = None
+	def __del__(self):
+		del self.writer
+		del self.player
+		del self.topwindow
 		
-	# we have to locate audio nodes and find their audio format
-	def setcontext(self, context):
-		pass 
-
 	def getWriter(self):
 		return self.writer
 
@@ -33,34 +35,27 @@ class Exporter:
 		if self.topwindow:
 			if self.topwindow != window:
 				print "Cannot export multiple topwindows"
-				# self.cancel_callback() # XXX Or schedule with timer? We're in a callback...
 				return
 			else:
 				self.writer.update(timestamp-self.starttime)
-		elif not self.completed:
+		else:
 			self.topwindow = window
 			print 'Begin export', self.filename, 'using profile', self.profile
 			self.writer = wmwriter.WMWriter(self, window.getDrawBuffer(), self.profile)
 			self.writer.setOutputFilename(self.filename)
 			self.writer.beginWriting()
 
-	def audiofragment(self):
-		"""XXXX This needs work"""
-		pass
-		
-	def finished(self, aborted):
+	def finished(self):
 		if self.writer:
 			self.writer.endWriting()
 			print 'End export', self.writer._filename
 			self.writer = None
 			self.topwindow = None
-		self.completed = 1
-		if aborted:
-			self.aborted = 1
+		if self._progress:
+			del self._progress
 		stoptime = windowinterface.getcurtime()
 		windowinterface.settimevirtual(0)
 		
 	def cancel_callback(self):
-		self.aborted = 1
 		self.player.stop()
-		self._cleanup()
+		windowinterface.showmessage('Export interrupted.')
