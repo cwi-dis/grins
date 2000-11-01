aefdb367a47958683d5dc5ced56f011b4eb5d49a
mikevdg <mikevdg@localhost>
2000-11-01 11:41:34 +0000

    build 5 of snap.

--- mm/editor/win32/patchlevel.py
+++ mm/editor/win32/patchlevel.py
@@ -6,4 +6,5 @@ format <platform> patchlevel <number>
 """
 
 ##patchlevel = 'win32 build 7'
-patchlevel = 'alpha win32 build 4'
+# Grins Snap! - maybe this should be in the snap/win32 directory??
+patchlevel = 'alpha win32 build 5'
