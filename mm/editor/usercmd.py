6dd0eefc4f72347379fa1b1c8a76f95935299ae1
Jack Jansen <Jack.Jansen@cwi.nl>
2000-11-08 15:35:20 +0000

    Undid Michaels "gratuitous change" (according to the log message) of PASTE, PASTE_BEFORE and PASTE_AFTER. This has just cost me an hour of debugging:-( MICHAEL, DON'T DO THIS!!!!

--- mm/editor/usercmd.py
+++ mm/editor/usercmd.py
@@ -150,10 +150,9 @@ class PASTE_BEFORE(_CommandBase):
 	help = 'Paste copied/cut node before selected node'
 class PASTE_AFTER(_CommandBase):
 	help = 'Paste copied/cut node after selected node'
-#PASTE_AFTER=PASTE
+PASTE=PASTE_AFTER
 class PASTE_UNDER(_CommandBase):
 	help = 'Paste copied/cut node under selected node'
-PASTE = PASTE_UNDER			# mjvdg 27-oct-2000
 class NEW_BEFORE(_CommandBase):
 	help = 'Create new node before selected node'
 class NEW_BEFORE_IMAGE(_CommandBase):
