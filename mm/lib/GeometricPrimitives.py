7aa4d073a9a1811c39bef01d68557d1068438566
mikevdg <mikevdg@localhost>
2000-11-16 15:41:35 +0000

    More work done on this new display list.

--- mm/lib/GeometricPrimitives.py
+++ mm/lib/GeometricPrimitives.py
@@ -5,7 +5,7 @@ __version__ = "$Id$"
 # primitives.
 
 import windowinterface, WMEVENTS
-
+from AppDefaults import *
 
 from Widgets import *
 
@@ -18,12 +18,14 @@ class GeoWidget(Widget):
 	def __init__(self, root):
 		# Root is the root window.
 		Widget.__init__(self, root)
+		self.canvassize = 800, 200
 		self.widgets = []
 
 	def AddWidget(self, widget):
 		assert isinstance(widget, Widget)
 		self.widgets.append(widget)
 		widget.parent = self
+		return widget
 
 	def DelWidget(self, widget):
 		assert isinstance(widget, Widget)
@@ -39,57 +41,88 @@ class GeoWidget(Widget):
 
 	def redraw(self):
 		# Handle display lists and so forth.
-		self.displist = self.root.newdisplaylist((50,50,50), windowinterface.UNIT_PXL)
+		# I may reuse the display list for small changes.
+		x,y = self.canvassize
+		self.root.window.setcanvassize((windowinterface.UNIT_PXL, x,y))
+		self.displist = self.root.window.newdisplaylist((227,223,145), windowinterface.UNIT_PXL)
 		for i in self.widgets:
 			i.displist = self.displist
 			i.redraw()
 		self.displist.render()
 
+	def notify_moveto(self, coords):
+		# This is the upcall from widgets when they are moved.
+		# set the canvas size.
+		x,y = self.canvassize
+		l,t,r,b = coords
+		if r > x:
+			x = r
+		if b > y:
+			y = b
+		self.canvassize = x,y
 
+class GeoClientWidget(Widget):
+	# Common routines for all widgets.
+	def moveto(self, coords):
+		Widget.moveto(self, coords)
+		self.parent.notify_moveto(coords)
 
-class Image(Widget):
+class Image(GeoClientWidget):
 	def redraw(self):
 		print "TODO"
 
-class Line(Widget):
+class Line(GeoClientWidget):
 	def redraw(self):
 		self.displist.drawline(self.get_box())
 
-class HLine3D(Widget):
+class HLine3D(GeoClientWidget):
 	pass;
 
-class Box(Widget):
+class Box(GeoClientWidget):
+	color = (0,0,0)
 	def redraw(self):
+		self.displist.fgcolor(self.color)
 		self.displist.drawbox(self.get_box())
 
-#class FBox(Widget):
+	def set_color(self, color):
+		self.color = color
+
+
+#class FBox(GeoClientWidget):
 #	pass;
 FBox=Box # TODO
 
-class Marker(Widget):
+class Marker(GeoClientWidget):
 	pass;
 
-#class Text(Widget):
-#	pass;
-Text=Box
+class Text(GeoClientWidget):
+	text = "No text set."
+	def set_text(self, text):
+		self.text = text
+	# fonts??
+	def redraw(self):
+		l,t,w,h = self.get_box()
+		self.displist.usefont(f_channel)
+		self.displist.fgcolor((0,0,0))
+		self.displist.centerstring(l,t,l+w,t+h,self.text)
 
-class FPolygon(Widget):
+class FPolygon(GeoClientWidget):
 	pass;
 
-#class Box3D(Widget):
+#class Box3D(GeoClientWidget):
 #	pass;
 Box3D=Box
 
-class Diamond(Widget):
+class Diamond(GeoClientWidget):
 	pass;
 
-class FDiamond(Widget):
+class FDiamond(GeoClientWidget):
 	pass;
 
-class Diamond3D(Widget):
+class Diamond3D(GeoClientWidget):
 	pass;
 
-class Arrow(Widget):
+class Arrow(GeoClientWidget):
 	pass;
 
 
