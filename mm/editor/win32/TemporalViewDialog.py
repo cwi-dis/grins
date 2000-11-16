64ff2284da1bcd5b20918f225d8240522619dd47
mikevdg <mikevdg@localhost>
2000-11-16 17:09:56 +0000

    more work. Events handled.

--- mm/editor/win32/TemporalViewDialog.py
+++ mm/editor/win32/TemporalViewDialog.py
@@ -27,8 +27,18 @@ class TemporalViewDialog(ViewDialog):
 		print "TODO: hide."
 
 	def _reg_events(self):
-		# Register events for this window.
-		pass;
+		self.window.register(WMEVENTS.Mouse0Press, self.ev_mouse0press, None)
+		self.window.register(WMEVENTS.Mouse0Release, self.ev_mouse0release, None)
+		self.window.register(WMEVENTS.Mouse2Press, self.ev_mouse2press, None)
+		self.window.register(WMEVENTS.Mouse2Release, self.ev_mouse2release, None)
+
+		self.window.register(WMEVENTS.WindowExit, self.ev_exit, None)
+		
+		self.window.register(WMEVENTS.PasteFile, self.ev_pastefile, None)
+		self.window.register(WMEVENTS.DragFile, self.ev_dragfile, None)
+		self.window.register(WMEVENTS.DropFile, self.ev_dropfile, None)
+		self.window.register(WMEVENTS.DragNode, self.ev_dragnode, None)
+		self.window.register(WMEVENTS.DropNode, self.ev_dropnode, None)
 
 	def setcommands(self, commandlist, title):
 		self.window.set_commandlist(commandlist)
