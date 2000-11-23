5d8dc99bf5f8f15cf0903fc7a9f24917dac605b0
anadelt <anadelt@localhost>
2000-11-23 14:15:34 +0000

    Do not simulate only an mfc startup but also execution and exit.

---
+++ mm/grins_app_core.py
@@ -0,0 +1,115 @@
+# grins_app_core.py  - The Pythonwin Application class for the GRINS Player and Editor.
+#
+import sys, os
+import win32ui, win32con, win32api
+from pywin.framework import app, intpyapp
+import traceback
+import longpath
+
+def fix_argv():
+	# Turn pathnames into their full NT version
+	for i in range(1, len(sys.argv)):
+		if os.path.exists(sys.argv[i]):
+			sys.argv[i] = longpath.short2longpath(sys.argv[i])
+
+try:
+	os.environ['GRINS_REMOTE_TRACE']
+	bRemoteTracing = 1
+except KeyError:
+	bRemoteTracing = 0
+
+if bRemoteTracing:
+	import win32traceutil
+
+def SafeCallbackCaller(fn, args):
+	try:
+		return apply(fn, args)
+	except SystemExit, rc:
+		# We trap a system exit, and translate it to the "official" way to bring down a GUI.
+		try:
+			rc = int(rc[0])
+		except (ValueError, TypeError):
+			rc = 0
+		win32api.PostQuitMessage(rc)
+	except:
+		# We trap all other errors, ensure the main window is shown, then
+		# print the traceback.
+		try:
+			win32ui.GetMainFrame().ShowWindow(win32con.SW_SHOW)
+		except win32ui.error:
+			print "Cant show the main frame!"
+		traceback.print_exc()
+		return
+
+
+win32ui.InstallCallbackCaller(SafeCallbackCaller)
+
+# AppParent = app.CApp 
+class GrinsApp(app.CApp):
+	def HookCommands(self):
+		app.CApp.HookCommands(self)
+		
+	def CreateMainFrame(self):
+		return app.MainFrame()
+
+	def LoadMainFrame(self):
+		" Create the main applications frame "
+		self.frame = self.CreateMainFrame()
+		self.SetMainFrame(self.frame)
+		self.frame.LoadFrame(win32ui.IDR_MAINFRAME, win32con.WS_OVERLAPPEDWINDOW)
+		self.frame.DragAcceptFiles()	# we can accept these.
+
+#		self.frame.ShowWindow(win32ui.GetInitialStateRequest());
+#		self.frame.UpdateWindow();
+
+		self.frame.SetWindowText("GRiNS Debugging Terminal")
+		self.HookCommands()
+
+		
+	def InitInstance(self):
+		#win32ui.MessageBox('InitInstance')
+		afx=win32ui.GetAfx()
+		afx.OleInit()
+		afx.EnableControlContainer()
+		win32ui.SetAppName("GRiNS")
+		self.LoadMainFrame()
+		if not bRemoteTracing:
+			from pywin.framework import interact
+			interact.CreateInteractiveWindow()
+			# Maximize the interactive window.
+			interact.edit.currentView.GetParent().ShowWindow(win32con.SW_MAXIMIZE)
+
+	def Run(self):
+		self.BootGrins()
+		return self.ExitInstance()
+
+	def ExitInstance(self):
+		#win32ui.MessageBox('Exit Instance')
+		if self.frame and hasattr(self.frame,'DestroyWindow'):
+			self.frame.DestroyWindow()
+		self.frame = None
+		if hasattr(self,'SetMainFrame'):
+			self.SetMainFrame(None)
+		import __main__
+		if hasattr(__main__,'resdll'):
+			del __main__.resdll
+		return app.CApp.ExitInstance(self)
+
+	def BootGrins(self):
+		raise RuntimeError, "You must subclass this object"
+
+def BootApplication(appClass):
+
+	# Create the application itself.
+	gapp = appClass()
+
+	# If we are not hosted by Pythonwin.exe, we need to simulate an MFC startup.
+	if win32ui.GetApp().IsInproc():
+		print "Not hosted by pythonwin - simulating MFC startup..."
+		#win32ui.MessageBox('Not hosted by pythonwin - simulating MFC startup...')
+		rc = gapp.InitInstance()
+		print "InitInstance returned", rc
+		if not rc: # None or 0 mean "this is OK"
+			gapp.Run()
+
+
