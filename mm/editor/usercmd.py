0f73e0439c2f3afe993f63c7350c76db1bd257d7
anadelt <anadelt@localhost>
2000-11-27 09:41:20 +0000

    Experimental: Define user cmd EXPORT_HTML_TIME.

--- mm/editor/usercmd.py
+++ mm/editor/usercmd.py
@@ -72,6 +72,8 @@ class EXPORT_WMP(_CommandBase):		# mjvdg 11-oct-2000
 	help = 'Save document in a new file as a Windows Media document.'
 class UPLOAD_WMP(_CommandBase):		# mjvdg 11-oct-2000
 	help = 'Upload document as Windows Media document to FTP server';
+class EXPORT_HTML_TIME(_CommandBase):	
+	help = 'Save document in a new file as an Internet Explorer HTML+TIME document.'
 class EXPORT_HTML(_CommandBase):
 	help = 'Create a template webpage linking to your presentation'
 class RESTORE(_CommandBase):
