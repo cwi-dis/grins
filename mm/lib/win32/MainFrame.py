2dc148a031b0a2bb097e8c3e30416e579f9e8cdf
Sjoerd Mullender <sjoerdmullender@users.noreply.github.com>
2000-12-07 15:27:31 +0000

    Made temporal view closable (now Michael just has to implement the hide method).

--- mm/lib/win32/MainFrame.py
+++ mm/lib/win32/MainFrame.py
@@ -109,6 +109,7 @@ if IsPlayer:
 	usercmd.HIDE_TRANSITIONVIEW=None
 	usercmd.HIDE_SOURCE=usercmd.SOURCE
 	usercmd.HIDE_LAYOUTVIEW2=None
+	usercmd.HIDE_TEMPORALVIEW=None
 
 appview={
 	0:{'cmd':usercmd.HIDE_PLAYERVIEW,'title':'Player','id':'pview_','class':_PlayerView,},
@@ -120,6 +121,7 @@ appview={
 	6:{'cmd':usercmd.HIDE_TRANSITIONVIEW,'title':'Transitions','id':'trview_','class':_TransitionView,'freezesize':1},
 	7:{'cmd':usercmd.HIDE_SOURCE,'title':'Source','id':'sview_','class':_SourceView,'hosted':0},
 	8:{'cmd':usercmd.HIDE_LAYOUTVIEW2,'title':'Source','id':'lview2_','class':_LayoutView2,'hosted':0},
+	9:{'cmd':usercmd.HIDE_TEMPORALVIEW,'title':'Temporal view','id':'tview_','class':_ChannelView,},
 }
 
 
