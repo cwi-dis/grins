6374f195d0ca53726cc6b1b8d425b549213e60a9
Sjoerd Mullender <sjoerdmullender@users.noreply.github.com>
2000-12-08 10:40:45 +0000

    Check for existance of Help/index.html instead of just Help.

---
+++ mm/common/Help.py
@@ -0,0 +1,58 @@
+__version__ = "$Id$"
+
+# Help window
+# XXXX This is far too stateful. The interface should be changed into
+# XXXX an object that is kept in the main object. We keep it like this
+# XXXX for now to stay compatible with the Windows version.
+
+# Interface:
+# (1) optionally call sethelpdir(dirname) with a directory name argument;
+# (2) if the user presses a Help button, call showhelpwindow();
+# (3) or call givehelp(topic) to show help on a particular subject.
+# When the help window is already open, it is popped up.
+
+
+import os
+import string
+import sys
+import MMurl
+import version
+
+def hashelp():
+	import windowinterface
+	return hasattr(windowinterface, 'htmlwindow')
+
+helpbase = None				# directory where the help files live
+helpwindow = None
+
+def sethelpprogram(program):
+	pass
+	
+def sethelpdir(dirname):
+	global helpbase
+	helpbase = MMurl.pathname2url(os.path.join(dirname, 'index.html'))
+
+def fixhelpdir():
+	global helpbase
+	if helpbase is None:
+		import cmif
+		helpdir = cmif.findfile('Help')
+		basefile = os.path.join(helpdir, 'index.html')
+		if os.path.exists(basefile):
+			helpbase = MMurl.pathname2url(basefile)
+		else:
+			helpbase = 'http://www.oratrix.com/indir/%s/help/index.html'%version.shortversion
+
+def givehelp(topic):
+	global helpwindow
+	import windowinterface
+	fixhelpdir()
+	helpfile = '%s.html'%topic
+	helpurl = MMurl.basejoin(helpbase, helpfile)
+	if helpwindow is not None and not helpwindow.is_closed():
+		helpwindow.goto_url(helpurl)
+	else:
+		helpwindow = windowinterface.htmlwindow(helpurl)
+
+def showhelpwindow():
+	givehelp('index')
