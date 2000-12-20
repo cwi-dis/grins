0c8434e4dd852efed5fdd29b2ee525cd18a1ac9c
auginet <auginet@localhost>
2000-12-20 11:16:00 +0000

    add custom regions into features list for the 'full smil editor' version

--- mm/editor/smilboston/features.py
+++ mm/editor/smilboston/features.py
@@ -7,12 +7,12 @@ from FeatureSet import *
 import sys
 
 feature_set = [EXPORT_CMIF, EXPORT_SMIL2, EXPORT_QT, EXPORT_WMP,
-               PLAYER_VIEW, STRUCTURE_VIEW, TIMELINE_VIEW, LAYOUT_VIEW, HYPERLINKS_VIEW, CHANNEL_VIEW,
-               USER_GROUPS, TRANSITION_VIEW,
-	       UNIFIED_FOCUS,
-#               H_VBANDWIDTH,
-               H_MODIFY_STRUCTURE
-               ];
+				PLAYER_VIEW, STRUCTURE_VIEW, TIMELINE_VIEW, LAYOUT_VIEW, HYPERLINKS_VIEW, CHANNEL_VIEW,
+				USER_GROUPS, TRANSITION_VIEW,
+				UNIFIED_FOCUS,
+#				H_VBANDWIDTH,
+				H_MODIFY_STRUCTURE, CUSTOM_REGIONS
+				];
 #
 version = 'pro'                         # I'm not sure here.. mjvdg
 compatibility = Boston
