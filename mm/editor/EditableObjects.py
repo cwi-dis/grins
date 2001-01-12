db5e276bb6b4ac711c46f625adfb004065efbb41
mikevdg <mikevdg@localhost>
2001-01-12 15:53:16 +0000

    New file. Architectural change. Again.
    This file will contain base classes used by the StructureView, TimelineView
    and TemporalView. These will be objects for nodes and regions which provide
    pop-up menus, editing functionality, menu bindings, command bindings, node
    traversals and stuff like that. They will _not_ do any drawing code - this is
    meant to be done by widgets. These will probably also be interfaces for the
    edit manager.
    
    In the M-V-C OO pattern, these are controllers. Widgets are the Views and
    MMNode/MMChannel are the models. This is a very rough, hacked design, but
    it will make things a bit easier.

---
+++ mm/editor/EditableObjects.py
@@ -0,0 +1,23 @@
+# This file will contain the base objects for the various views.
+
+# Editing MMNodes.
+
+class EditableNode:
+	# TODO
+	pass
+
+class EditableLeafNode(EditableNode):
+	# TODO. Maybe this class will not be needed.
+	pass
+
+class EditableInternalNode(EditableNode):
+	# TODO. Maybe this class will not be needed.
+	pass
+
+# Editing regions.
+
+class EditableRegion:
+	pass
+
+class EditableViewport(EditableRegion):
+	pass
