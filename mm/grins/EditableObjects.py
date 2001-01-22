31e0bc39bd3707c89748ecdb6c1c3f4fd2b72add
mikevdg <mikevdg@localhost>
2001-01-22 09:22:28 +0000

    Added this file.

---
+++ mm/grins/EditableObjects.py
@@ -0,0 +1,6 @@
+__version__="$Id: "
+# This file is just a placeholder. The player also has editable objects - except
+# that they are not editable :-).
+
+import MMNode
+EditableMMNode = MMNode.MMNode
