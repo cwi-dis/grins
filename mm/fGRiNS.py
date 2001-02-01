c285f7ba14f2dcdf3c8d43e9eb3d0a995b43053b
anadelt <anadelt@localhost>
2001-02-01 08:48:59 +0000

    Complete support for register, unregister and embedding.

--- mm/fGRiNS.py
+++ mm/fGRiNS.py
@@ -1,5 +1,6 @@
 import grins_app_core
 resdll = None
+embedded = 0
 
 class PlayerApp(grins_app_core.GrinsApp):
 	def BootGrins(self):
@@ -21,18 +22,22 @@ if grinspapi:
 		if arg[:1]=='/' or arg[:1]=='-':
 			val = arg[1:]
 			if val == 'UnregServer':
-				grinpapi.UnregisterServer()
+				grinspapi.UnregisterServer()
 				runApp = 0
 			elif val == 'RegServer':
-				grinpapi.RegisterServer()
+				grinspapi.RegisterServer()
 				runApp = 0
 			elif val == 'Embedding':
 				sys.argv = sys.argv[:1]
+				global embedded
+				embedded = 1
 				runApp = 1
-		
+
 if not runApp:
 	grinspapi.OleUninitialize()
 else:
+	if not embedded and grinspapi:
+		grinspapi.commodule.Lock()
 	grins_app_core.fix_argv()
 	grins_app_core.BootApplication(PlayerApp)
 
