1f6eea0cce2b2b9a0cb1a48745ed457bb25acc86
anadelt <anadelt@localhost>
2001-02-06 11:44:57 +0000

    Add two classes of java exceptions: GRiNSException: encapsulates what we display in GRiNS by an error message box. GRiNSInterfaceException: reports GRiNS interface errors that have nothing to do with GRiNS and the document

---
+++ mm/win32/COM/javaclient/GRiNSException.java
@@ -0,0 +1,20 @@
+
+
+public
+class GRiNSException extends Exception {
+    /**
+     * Constructs an <code>GRiNSException</code> with no specified detail message. 
+     */
+    public GRiNSException() {
+	super();
+    }
+
+    /**
+     * Constructs an <code>GRiNSException</code> with the specified detail message. 
+     *
+     * @param   s   the detail message.
+     */
+    public GRiNSException(String s) {
+	super(s);
+    }
+}
---
+++ mm/win32/COM/javaclient/GRiNSInterfaceException.java
@@ -0,0 +1,19 @@
+
+public
+class GRiNSInterfaceException extends Exception {
+    /**
+     * Constructs an <code>GRiNSException</code> with no specified detail message. 
+     */
+    public GRiNSInterfaceException() {
+	super();
+    }
+
+    /**
+     * Constructs an <code>GRiNSException</code> with the specified detail message. 
+     *
+     * @param   s   the detail message.
+     */
+    public GRiNSInterfaceException(String s) {
+	super(s);
+    }
+}
