18fda8eac6e615c217c3567da80fc0b3437a62bc
Sjoerd Mullender <sjoerdmullender@users.noreply.github.com>
2001-02-19 09:23:03 +0000

    Upped.

--- mm/editor/win32/patchlevel.py
+++ mm/editor/win32/patchlevel.py
@@ -7,4 +7,4 @@ format <platform> patchlevel <number>
 
 ##patchlevel = 'win32 build 7'
 # Grins Snap! - maybe this should be in the snap/win32 directory??
-patchlevel = 'alpha win32 build 27'
+patchlevel = 'alpha win32 build 28'
