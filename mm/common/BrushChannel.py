69a07c37857fdb9c8190ceab98114f9f18725ed0
Sjoerd Mullender <sjoerdmullender@users.noreply.github.com>
2001-03-08 13:21:51 +0000

    Allow animation of brush color.

--- mm/common/BrushChannel.py
+++ mm/common/BrushChannel.py
@@ -12,3 +12,12 @@ class BrushChannel(ChannelWindow):
 		color = MMAttrdefs.getattr(node, 'fgcolor')
 		self.armed_display.drawfbox(color, (0,0,1,1), units = UNIT_SCREEN)
 		return 1
+
+	def do_updateattr(self, node, name, value):
+		if name != 'fgcolor':
+			return
+		d = self.played_display.clone()
+		d.drawfbox(value, (0,0,1,1), units = UNIT_SCREEN)
+		d.render()
+		self.played_display.close()
+		self.played_display = d
