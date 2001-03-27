52b2d976450eb0054f392684f45ffadc048c6792
anadelt <anadelt@localhost>
2001-03-27 14:30:37 +0000

    Fix duplicate IDs again. Set NEXT_XXX values to point to the next available position (otherwise the mess will continue)

--- mm/win32/src/GRiNSRes/GRiNSRes.h
+++ mm/win32/src/GRiNSRes/GRiNSRes.h
@@ -187,9 +187,10 @@
 #define IDD_SOURCEEDIT1                 391
 #define IDD_EDITATTR_QTPREF             392
 #define IDD_LAYOUT_T2                   393
-#define IDI_ZOOMIN                      393
 #define IDI_ZOOMOUT                     394
-#define IDD_EDITATTR_EMPTY              394
+#define IDD_EDITATTR_EMPTY              395
+#define IDI_ZOOMIN                      396
+
 #define IDC_SPLASH                      1001
 #define IDC_EXPLANATION                 1002
 #define IDC_LAYOUT_CHANNELS             1003
@@ -466,6 +467,7 @@
 #define IDC_Z                           1325
 #define IDC_ZOOMIN                      1326
 #define IDC_ZOOMOUT                     1327
+
 #define ID_FILE_NEW_GRINS               32771
 #define ID_FILE_OPEN_GRINS              32772
 #define ID_FILE_SAVE_GRINS              32773
@@ -552,7 +554,7 @@
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        395
+#define _APS_NEXT_RESOURCE_VALUE        397
 #define _APS_NEXT_COMMAND_VALUE         32877
 #define _APS_NEXT_CONTROL_VALUE         1328
 #define _APS_NEXT_SYMED_VALUE           104
