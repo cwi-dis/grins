8a926301a30d5b2e182bac342f665fee624180ba
auginet <auginet@localhost>
2001-04-04 15:06:20 +0000

    add module to support system colors defined in CSS2 for now: leave the tables empty on Mac and Unix, because the values are system dependant

---
+++ mm/lib/mac/SystemColors.py
@@ -0,0 +1,15 @@
+# these colors depend of the configuration of your system, and so
+# these tables are built dynamicly from init_colors method
+# not to have a list of the supported system-colors, look at the CSS specification
+
+colors = {
+}
+
+rcolors = {
+}
+	
+def init_colors():
+	# to do: fill the table. The values are system dependant
+	pass
+
+init_colors()
