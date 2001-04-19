d06a004fbaf3c97a025656d93081d44f9babf3fb
anadelt <anadelt@localhost>
2001-04-19 11:25:08 +0000

    Fix typo in matrix transormation.

--- mm/lib/svgtypes.py
+++ mm/lib/svgtypes.py
@@ -708,8 +708,9 @@ class TM:
 	
 	# svg transforms
 	def matrix(self, et):
+		assert len(et)==6, 'invalid matrix transformation'
 		a1, b1, c1, d1, e1, f1 = self.elements
-		a2, b2, c2, d2, e2, f2 = tm.elements
+		a2, b2, c2, d2, e2, f2 = et
 		self.elements = a1*a2+c1*b2, b1*a2+d1*b2, a1*c2+c1*d2, b1*c2+d1*d2, a1*e2+c1*f2+e1, b1*e2+d1*f2+f1
 
 	def translate(self, tt):
