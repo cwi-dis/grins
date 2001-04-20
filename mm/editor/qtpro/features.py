922792c63f60ad551758f9712db6ee0c032765af
Sjoerd Mullender <sjoerdmullender@users.noreply.github.com>
2001-04-20 15:06:26 +0000

    Moved the smil-boston branch to the main branch.

--- mm/editor/qtpro/features.py
+++ mm/editor/qtpro/features.py
@@ -3,11 +3,14 @@ __version__ = "$Id$"
 # Features for GRiNS for QT, pro version
 
 from compatibility import *
+from FeatureSet import *
 import sys
 
 #
+version = 'pro'
 compatibility = QT
 compatibility_short = 'QT'
+cmif = 0
 lightweight = 0
 editor = 1
 level = 'pro'
