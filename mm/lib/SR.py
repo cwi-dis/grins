daa47683120a777f533ecee7d16892a61858dd81
Sjoerd Mullender <sjoerdmullender@users.noreply.github.com>
2001-04-20 15:08:45 +0000

    Moved the smil-boston branch to the main branch.

--- mm/lib/SR.py
+++ mm/lib/SR.py
@@ -11,38 +11,29 @@ __version__ = "$Id$"
 # All *DONE events are 'upcalls'.
 # The *STOP events have no corresponding upcalls.
 #
-[NO_EVENT, SCHED, SCHED_DONE, PLAY, PLAY_DONE, SCHED_STOP, PLAY_STOP,\
-	  SYNC, SYNC_DONE, PLAY_ARM, ARM_DONE, SCHED_FINISH, BAG_START, \
-	  BAG_STOP, BAG_DONE, TERMINATE, LOOPSTART, LOOPSTART_DONE, \
-	  LOOPEND, LOOPEND_DONE, LOOPRESTART, PLAY_OPTIONAL_ARM] = range(22)
+[NO_EVENT, SCHED, SCHED_DONE, PLAY, PLAY_DONE, SCHED_STOP, PLAY_STOP,
+	PLAY_ARM, ARM_DONE, SCHED_FINISH,
+	LOOPSTART, LOOPSTART_DONE,
+	LOOPEND, LOOPEND_DONE, LOOPRESTART, PLAY_OPTIONAL_ARM,
+	SCHED_STOPPING, SCHED_START] = range(18)
 
 ## side_effects = (PLAY, PLAY_STOP, PLAY_ARM)
 
-op_names = [ 'NO_EVENT', 'SCHED', 'SCHED_DONE', 'PLAY', 'PLAY_DONE', \
-	  'SCHED_STOP', 'PLAY_STOP', 'SYNC', 'SYNC_DONE', \
-	  'PLAY_ARM', 'ARM_DONE', 'SCHED_FINISH', 'BAG_START', \
-	  'BAG_STOP', 'BAG_DONE', 'TERMINATE', 'LOOPSTART', 'LOOPSTART_DONE', \
-	  'LOOPEND', 'LOOPEND_DONE', 'LOOPRESTART', 'PLAY_OPTIONAL_ARM']
+op_names = [ 'NO_EVENT', 'SCHED', 'SCHED_DONE', 'PLAY', 'PLAY_DONE',
+	     'SCHED_STOP', 'PLAY_STOP',
+	     'PLAY_ARM', 'ARM_DONE', 'SCHED_FINISH', 'LOOPSTART',
+	     'LOOPSTART_DONE', 'LOOPEND', 'LOOPEND_DONE', 'LOOPRESTART',
+	     'PLAY_OPTIONAL_ARM', 'SCHED_STOPPING', 'SCHED_START' ]
 
 def ev2string(ev):
 	try:
 		opcode, node = ev
-		if opcode == SYNC:
-			delay, node = node
-			if type(node) == type(0):
-				return 'SYNC(%e, %d)' % (delay, node)
-			return 'SYNC(%e, %s)'%(delay, _getname(node))
-		if opcode == SYNC_DONE and type(node) == type(0):
-			return 'SYNC_DONE(%d)' % node
-		name = _getname(node)
-		return op_names[opcode] + '(' + name + ')'
+		return '%s(%s)' % (op_names[opcode], _getname(node))
 	except:
 		return 'ILL-FORMED-EVENT:'+`ev`
 		
 def _getname(node):
-		uid = node.GetUID()
-		name = node.GetAttrDef('name', '') + '#' + uid
-		return name
+	return '%s#%s' % (node.GetAttrDef('name', ''), node.GetUID())
 
 def evlist2string(evlist):
 	first = 1
