f4ffe66388e184dcc0a7cba8e620daef3a37fc0c
Sjoerd Mullender <sjoerdmullender@users.noreply.github.com>
2001-04-20 09:51:31 +0000

    EventEditorDialog is not used.

--- mm/editor/mac/EventEditorDialog.py
+++
@@ -1,4 +0,0 @@
-# TODO: Jack: You'll need to do this, I'm afraid. -mjvdg.
-
-class EventEditorDialog:
-	pass
--- mm/editor/motif/EventEditorDialog.py
+++
@@ -1,6 +0,0 @@
-# Sjoerd:
-
-import modal dialog base class.
-
-class EventEditorDialog(base class):
-	pass
