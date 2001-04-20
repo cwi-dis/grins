7a7fcb5bb10f7b14ee3862a6e289f16cc7440a9a
Sjoerd Mullender <sjoerdmullender@users.noreply.github.com>
2001-04-20 15:08:50 +0000

    Moved the smil-boston branch to the main branch.

--- mm/lib/mksplash.py
+++ mm/lib/mksplash.py
@@ -6,4 +6,4 @@ if len(sys.argv) != 2:
 	print 'Usage:',sys.argv[0] or 'python mksplash.py','img-file'
 	sys.exit(1)
 
-mkimg.mkimg(sys.argv[1], imgformat.rgb)
+mkimg.mkimg(sys.argv[1], imgformat.bmprgbbe_noalign)
