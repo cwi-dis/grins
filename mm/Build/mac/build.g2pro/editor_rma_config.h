c228288524e25ca2a997d5dc4b35e2e7e9c57e6d
Sjoerd Mullender <sjoerdmullender@users.noreply.github.com>
2001-04-20 14:57:58 +0000

    Moved the smil-boston branch to the main branch.

--- mm/Build/mac/build.g2pro/editor_rma_config.h
+++ mm/Build/mac/build.g2pro/editor_rma_config.h
@@ -10,13 +10,13 @@
 #define USE_TOOLBOX		/* Include toolbox modules in core Python */
 #define USE_QT			/* Include quicktime modules in core Python */
 #define USE_WASTE		/* Include waste module in core Python */
-/* #define USE_MACSPEECH		/* Include macspeech module in core Python */
+#define USE_MACSPEECH		/* Include macspeech module in core Python */
 #define USE_IMG	       		/* Include img modules in core Python */
-/* #define USE_MACCTB		/* Include ctb module in core Python */
+#define USE_MACCTB		/* Include ctb module in core Python */
 /* #define USE_STDWIN		/* Include stdwin module in core Python */
 /* #define USE_MACTCP		/* Include mactcp (*not* socket) modules in core */
-/* #define USE_TK			/* Include _tkinter module in core Python */
-/* #define MAC_TCL			/* This *must* be on if USE_TK is on */
+#define USE_TK			/* Include _tkinter module in core Python */
+#define MAC_TCL			/* This *must* be on if USE_TK is on */
 /* #define USE_MAC_SHARED_LIBRARY	/* Enable code to add shared-library resources */
 #define USE_MAC_APPLET_SUPPORT	/* Enable code to run a PYC resource */
 /* #define USE_MAC_DYNAMIC_LOADING		/* Enable dynamically loaded modules */
