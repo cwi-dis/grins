3c61a967970fbea50047fa0dfc7241a6d1c495fe
Sjoerd Mullender <sjoerdmullender@users.noreply.github.com>
2001-05-22 06:40:52 +0000

    Upped.

--- mm/editor/win32/patchlevel.py
+++ mm/editor/win32/patchlevel.py
@@ -4,4 +4,4 @@ __version__ = "$Id$"
 Contains the subversion of the application in the
 format <platform> patchlevel <number>
 """
-patchlevel = 'alpha win32 build 52'
+patchlevel = 'alpha win32 build 53'
