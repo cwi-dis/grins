389be1ee66328babd7547e4ddbc476df86b709d8
auginet <auginet@localhost>
2001-06-19 14:48:58 +0000

    oops: showhidden is not a command

--- mm/editor/usercmd.py
+++ mm/editor/usercmd.py
@@ -184,8 +184,6 @@ class MAGIC_PLAY(_CommandBase):
 	help = 'Continue when paused, pause when playing, play when stopped'
 class USERGROUPS(_DynamicCascade):
 	help = 'Custom tests'
-class SHOWHIDDEN(_CommandBase):
-	help = 'Show hidden custom tests'
 class CHANNELS(_DynamicCascade):
 	help = 'Toggle channels on/off'
 class CRASH(_CommandBase):
