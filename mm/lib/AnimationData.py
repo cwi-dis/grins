8dc5d437fc044faa06ef9dda4aeb5388eda36f1d
anadelt <anadelt@localhost>
2001-08-03 10:56:49 +0000

    Rough implementation of read animate nodes and set self data No error checking and zero flexibility but its just the base to start.

--- mm/lib/AnimationData.py
+++ mm/lib/AnimationData.py
@@ -2,10 +2,12 @@ __version__ = "$Id$"
 
 
 # AnimationData pattern:
-# <animateMotion targetElement="xxx" values="x1, y1; x2, y2; x3, y3" keyTimes="0, 0.3, 1.0" dur="use_attr_edit"/>
-# <animate targetElement="xxx" attributeName="width" values="w1, w2, w3" keyTimes="0, 0.3, 1.0" dur="use_attr_edit"/>
-# <animate targetElement="xxx"  attributeName="height" values="h1, h2, h3" keyTimes="0, 0.3, 1.0" dur="use_attr_edit"/>
-# <animateColor targetElement="xxx" attributeName="backgroundColor" values="rgb1, rgb2, rgb3" keyTimes="0, 0.3, 1.0" dur="use_attr_edit"/>
+# <ref ...>
+#   <animateMotion targetElement="xxx" values="x1, y1; x2, y2; x3, y3" keyTimes="0, 0.3, 1.0" dur="use_attr_edit"/>
+#   <animate targetElement="xxx" attributeName="width" values="w1, w2, w3" keyTimes="0, 0.3, 1.0" dur="use_attr_edit"/>
+#   <animate targetElement="xxx"  attributeName="height" values="h1, h2, h3" keyTimes="0, 0.3, 1.0" dur="use_attr_edit"/>
+#   <animateColor targetElement="xxx" attributeName="backgroundColor" values="rgb1, rgb2, rgb3" keyTimes="0, 0.3, 1.0" dur="use_attr_edit"/>
+# </ref>
 
 class AnimationData:
 	def __init__(self, node):
@@ -44,8 +46,43 @@ class AnimationData:
 
 	# read animate nodes and set self data
 	def readData(self):
-		pass
+		children = self._getAnimateChildren()
+		if not children:
+			return
 
+		str = MMAttrdefs.getattr(children[0], 'keyTimes')
+		self.times = self._strToFloatList(str)
+
+		animateMotionValues = [] 
+		animateWidthValues = []
+		animateHeightValues = [] 
+		animateColorValues = []
+		for anim in children:
+			tag = anim.attrdict.get('atag')
+			if tag == 'animateMotion':
+				str = MMAttrdefs.getattr(anim, 'values')
+				animateMotionValues = self._strToPosList(str)
+			elif tag == 'animate':
+				attributeName = MMAttrdefs.getattr(anim, 'attributeName')
+				if attributeName == 'width':
+					animateWidthValues = self._strToIntList(str)
+				elif attributeName == 'height':
+					animateHeightValues = self._strToIntList(str)
+			elif tag == 'animateColor':
+				animateColorValues = self._strToColorList(str)
+		n = len(self.times)
+		assert len(animateMotionValues) == n, ''
+		assert len(animateWidthValues) == n, ''
+		assert len(animateHeightValues) == n, ''
+		assert len(animateColorValues) == n, ''
+		for i in range(n):
+			x, y = animateMotionValues[i]
+			w = animateWidthValues[i]
+			h = animateHeightValues[i]
+			rect = x, y, w, h
+			color = animateColorValues[i]
+			self.data.append((rect, color))
+	
 	# create animate nodes from self data
 	def applyData(self, editmgr):
 		pass
@@ -55,6 +92,13 @@ class AnimationData:
 	#
 	def _initDomValues(self):
 		pass
+
+	def _getAnimateChildren(self):
+		children = []
+		for c in self.node.children:
+			if c.GetType() == 'animate':
+				children.append(c)
+		return children
 
 	def _dataToValuesAttr(self):
 		animateMotionValues = [] 
@@ -91,13 +135,13 @@ class AnimationData:
 		return []
 
 	def _strToFloatList(self, str):
-		return ''
+		return []
 		
 	def _strToPosList(self, str):
-		return ''
+		return []
 
 	def _strToColorList(self, str):
-		return ''
+		return []
 
 
 
