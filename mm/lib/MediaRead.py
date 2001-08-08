c42860ac5e1b070477f53d31d3ee0dd47324c7f4
anadelt <anadelt@localhost>
2001-08-08 10:03:52 +0000

    A helper module to toplevel for when we open a media file. Its purpose its to adjust the duration (needed by WGBH) and its in a separate module to avoid TopLevel pollution and be easily removable.

---
+++ mm/lib/MediaRead.py
@@ -0,0 +1,28 @@
+__version__ = "$Id$"
+
+import sys
+
+mediaWrapper = """\
+<smil>
+  <body>
+    <ref%s src="%s"/>
+  </body>
+</smil>"""
+
+def MediaRead(filename, mtype, printfunc):
+	import SMILTreeRead
+	print 'media item', mtype
+	if mtype is None or \
+		(mtype[:6] != 'audio/' and
+		mtype[:6] != 'video/'):
+		dur = ' dur="indefinite"'
+	else:
+		dur = ''
+	root = SMILTreeRead.ReadString(mediaWrapper % (dur, filename), filename, printfunc)
+	if sys.platform == 'win32':
+		mediaNode = root.GetChildren()[0]
+		import Duration
+		dur = Duration.getintrinsicduration(mediaNode, 0)
+		if dur:
+			mediaNode.attrdict['duration'] = dur
+	return root
