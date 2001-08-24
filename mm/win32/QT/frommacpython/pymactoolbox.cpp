a661ad243b7d80f1a4868a6023ce53067ca5a24b
Jack Jansen <Jack.Jansen@cwi.nl>
2001-08-24 14:40:41 +0000

    After various global fixes this compiles and links again. It experiences a SystemError when trying to import it.
    Checking it in so I can get at it easily from the Mac, where I can add a few
    bits of missing functionality.

--- mm/win32/QT/frommacpython/pymactoolbox.cpp
+++ mm/win32/QT/frommacpython/pymactoolbox.cpp
@@ -1,9 +1,15 @@
-#include "Python.h"
+#include "pywintoolbox.h"
 
-#include "macglue.h"
-#include "pymactoolbox.h"
+static PyObject *PyMac_OSErrException;
 
-PyObject *PyMac_GetOSErrException(void){return 0;}	/* Initialize & return it */
+PyObject *
+PyMac_GetOSErrException(void){
+	/* Call it Qt.Error on windows */
+	if ( PyMac_OSErrException == NULL )
+		PyMac_OSErrException = PyString_FromString("Qt.Error");
+	return PyMac_OSErrException;
+}
+
 PyObject *PyErr_Mac(PyObject *, int){return 0;}		/* Exception with a mac error */
 PyObject *PyMac_Error(OSErr){return 0;}				/* Uses PyMac_GetOSErrException */
 
@@ -14,11 +20,11 @@ PyObject *PyMac_Error(OSErr){return 0;}				/* Uses PyMac_GetOSErrException */
 int PyMac_GetOSType(PyObject *, OSType *) {return 0;}	/* argument parser for OSType */
 PyObject *PyMac_BuildOSType(OSType) {return 0;}		/* Convert OSType to PyObject */
 
-PyObject *PyMac_BuildNumVersion(NumVersion) {return 0;}/* Convert NumVersion to PyObject */
+//PyObject *PyMac_BuildNumVersion(NumVersion) {return 0;}/* Convert NumVersion to PyObject */
 
 int PyMac_GetStr255(PyObject *, Str255) {return 0;}	/* argument parser for Str255 */
-PyObject *PyMac_BuildStr255(Str255) {return 0;}		/* Convert Str255 to PyObject */
-PyObject *PyMac_BuildOptStr255(Str255) {return 0;}		/* Convert Str255 to PyObject, NULL to None */
+//PyObject *PyMac_BuildStr255(Str255) {return 0;}		/* Convert Str255 to PyObject */
+//PyObject *PyMac_BuildOptStr255(Str255) {return 0;}		/* Convert Str255 to PyObject, NULL to None */
 
 int PyMac_GetRect(PyObject *_args, Rect *pr) 
 	{
@@ -31,13 +37,21 @@ int PyMac_GetRect(PyObject *_args, Rect *pr)
 	pr->bottom = short(rc.bottom);
 	return 1;
 	}
+
 PyObject *PyMac_BuildRect(Rect *pr) 
 	{return Py_BuildValue("iiii", pr->left, pr->top, pr->right, pr->bottom);}			
 
 int PyMac_GetPoint(PyObject *, Point *) {return 0;}	/* argument parser for Point */
 PyObject *PyMac_BuildPoint(Point) {return 0;}			/* Convert Point to PyObject */
-
-int PyMac_GetEventRecord(PyObject *, EventRecord *) {return 0;} /* argument parser for EventRecord */
+//
+int PyMac_GetEventRecord(PyObject *obj, EventRecord  *pe)
+	{
+	MSG msg;
+	if (!PyArg_ParseTuple(obj, "iiiii(ii)", &msg.hwnd, &msg.message, &msg.wParam, &msg.lParam, &msg.time, &msg.pt.x, &msg.pt.y))
+		return 0;
+	WinEventToMacEvent(&msg, pe);
+	return 1;
+}
 PyObject *PyMac_BuildEventRecord(EventRecord *) {return 0;} /* Convert EventRecord to PyObject */
 
 int PyMac_GetFixed(PyObject *, Fixed *) {return 0;}	/* argument parser for Fixed */
@@ -45,83 +59,49 @@ PyObject *PyMac_BuildFixed(Fixed) {return 0;}			/* Convert Fixed to PyObject */
 int PyMac_Getwide(PyObject *, wide *) {return 0;}		/* argument parser for wide */
 PyObject *PyMac_Buildwide(wide *) {return 0;}			/* Convert wide to PyObject */
 
-/*
-** The rest of the routines are implemented by extension modules. If they are
-** dynamically loaded mactoolboxglue will contain a stub implementation of the
-** routine, which imports the module, whereupon the module's init routine will
-** communicate the routine pointer back to the stub.
-** If USE_TOOLBOX_OBJECT_GLUE is not defined there is no glue code, and the
-** extension modules simply declare the routine. This is the case for static
-** builds (and could be the case for MacPython CFM builds, because CFM extension
-** modules can reference each other without problems).
-*/
-
-#ifdef USE_TOOLBOX_OBJECT_GLUE
-/*
-** These macros are used in the module init code. If we use toolbox object glue
-** it sets the function pointer to point to the real function.
-*/
-#define PyMac_INIT_TOOLBOX_OBJECT_NEW(object, rtn) { \
-	extern PyObject *(*PyMacGluePtr_##rtn)(object); \
-	PyMacGluePtr_##rtn = _##rtn; \
-}
-#define PyMac_INIT_TOOLBOX_OBJECT_CONVERT(object, rtn) { \
-	extern int (*PyMacGluePtr_##rtn)(PyObject *, object *); \
-	PyMacGluePtr_##rtn = _##rtn; \
-}
-#else
-/*
-** If we don't use toolbox object glue the init macros are empty. Moreover, we define
-** _xxx_New to be the same as xxx_New, and the code in mactoolboxglue isn't included.
-*/
-#define PyMac_INIT_TOOLBOX_OBJECT_NEW(object, rtn)
-#define PyMac_INIT_TOOLBOX_OBJECT_CONVERT(object, rtn)
-#endif /* USE_TOOLBOX_OBJECT_GLUE */
-
-/* macfs exports */
-extern int PyMac_GetFSSpec(PyObject *, FSSpec *) {return 0;}
+int PyMac_GetFSSpec(PyObject *, FSSpec *) {return 0;}
 
 /* AE exports */
-extern PyObject *AEDesc_New(AppleEvent *) {return 0;} /* XXXX Why passed by address?? */
-extern int AEDesc_Convert(PyObject *, AppleEvent *) {return 0;}
+//extern PyObject *AEDesc_New(AppleEvent *) {return 0;} /* XXXX Why passed by address?? */
+//extern int AEDesc_Convert(PyObject *, AppleEvent *) {return 0;}
 
 /* Cm exports */
-extern PyObject *CmpObj_New(Component) {return 0;}
-extern int CmpObj_Convert(PyObject *, Component *) {return 0;}
-extern PyObject *CmpInstObj_New(ComponentInstance) {return 0;}
-extern int CmpInstObj_Convert(PyObject *, ComponentInstance *) {return 0;}
+PyObject *CmpObj_New(Component) {return 0;}
+int CmpObj_Convert(PyObject *, Component *) {return 0;}
+PyObject *CmpInstObj_New(ComponentInstance) {return 0;}
+int CmpInstObj_Convert(PyObject *, ComponentInstance *) {return 0;}
 
 /* Ctl exports */
-extern PyObject *CtlObj_New(ControlHandle) {return 0;}
-extern int CtlObj_Convert(PyObject *, ControlHandle *) {return 0;}
+//extern PyObject *CtlObj_New(ControlHandle) {return 0;}
+//extern int CtlObj_Convert(PyObject *, ControlHandle *) {return 0;}
 
 /* Dlg exports */
-extern PyObject *DlgObj_New(DialogPtr) {return 0;}
-extern int DlgObj_Convert(PyObject *, DialogPtr *) {return 0;}
-extern PyObject *DlgObj_WhichDialog(DialogPtr) {return 0;}
+//extern PyObject *DlgObj_New(DialogPtr) {return 0;}
+//extern int DlgObj_Convert(PyObject *, DialogPtr *) {return 0;}
+//extern PyObject *DlgObj_WhichDialog(DialogPtr) {return 0;}
 
 /* Drag exports */
-extern PyObject *DragObj_New(DragReference) {return 0;}
-extern int DragObj_Convert(PyObject *, DragReference *) {return 0;}
+//extern PyObject *DragObj_New(DragReference) {return 0;}
+//extern int DragObj_Convert(PyObject *, DragReference *) {return 0;}
 
 /* List exports */
-extern PyObject *ListObj_New(ListHandle) {return 0;}
-extern int ListObj_Convert(PyObject *, ListHandle *) {return 0;}
+//extern PyObject *ListObj_New(ListHandle) {return 0;}
+//extern int ListObj_Convert(PyObject *, ListHandle *) {return 0;}
 
 /* Menu exports */
-extern PyObject *MenuObj_New(MenuHandle) {return 0;}
+//extern PyObject *MenuObj_New(MenuHandle) {return 0;}
 extern int MenuObj_Convert(PyObject *, MenuHandle *) {return 0;}
 
 /* Qd exports */
 extern PyObject *GrafObj_New(GrafPtr) {return 0;}
 extern int GrafObj_Convert(PyObject *, GrafPtr *) {return 0;}
-extern PyObject *BMObj_New(BitMapPtr) {return 0;}
-extern int BMObj_Convert(PyObject *, BitMapPtr *) {return 0;}
+//extern PyObject *BMObj_New(BitMapPtr) {return 0;}
+//extern int BMObj_Convert(PyObject *, BitMapPtr *) {return 0;}
 extern PyObject *QdRGB_New(RGBColor *) {return 0;}
 extern int QdRGB_Convert(PyObject *, RGBColor *) {return 0;}
 
 /* Qdoffs exports */
-extern PyObject *GWorldObj_New(GWorldPtr) {return 0;}
+//extern PyObject *GWorldObj_New(GWorldPtr) {return 0;}
 extern int GWorldObj_Convert(PyObject *, GWorldPtr *) {return 0;}
 
 
@@ -132,10 +112,10 @@ extern PyObject *OptResObj_New(Handle) {return 0;}
 extern int OptResObj_Convert(PyObject *, Handle *) {return 0;}
 
 /* TE exports */
-extern PyObject *TEObj_New(TEHandle) {return 0;}
-extern int TEObj_Convert(PyObject *, TEHandle *) {return 0;}
+//extern PyObject *TEObj_New(TEHandle) {return 0;}
+//extern int TEObj_Convert(PyObject *, TEHandle *) {return 0;}
 
 /* Win exports */
-extern PyObject *WinObj_New(WindowPtr) {return 0;}
+//extern PyObject *WinObj_New(WindowPtr) {return 0;}
 extern int WinObj_Convert(PyObject *, WindowPtr *) {return 0;}
-extern PyObject *WinObj_WhichWindow(WindowPtr) {return 0;}
+//extern PyObject *WinObj_WhichWindow(WindowPtr) {return 0;}
