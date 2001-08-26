bbfb1237471d80b5504aa25793bb7e0f40f70571
anadelt <anadelt@localhost>
2001-08-26 14:32:55 +0000

    Do not write pythonwin toolbars.

--- mm/editor/win32/ToolbarState.py
+++ mm/editor/win32/ToolbarState.py
@@ -1,6 +1,8 @@
 # machine generated bar state by regedit tool
 
 DefaultState = """
+[HKEY_CURRENT_USER\Software\Oratrix GRiNS]
+
 [HKEY_CURRENT_USER\Software\Oratrix GRiNS\Editor 2.0]
 
 [HKEY_CURRENT_USER\Software\Oratrix GRiNS\Editor 2.0\GRiNSToolBars-Bar0]
@@ -128,32 +130,4 @@ DefaultState = """
 "ScreenCX"=dword:00000500
 "ScreenCY"=dword:00000400
 
-[HKEY_CURRENT_USER\Software\Oratrix GRiNS\Editor 2.0\ToolbarDefault-Bar0]
-"BarID"=dword:0000e801
-
-[HKEY_CURRENT_USER\Software\Oratrix GRiNS\Editor 2.0\ToolbarDefault-Bar1]
-"BarID"=dword:0000e800
-"XPos"=dword:fffffffe
-"YPos"=dword:fffffffe
-"Docking"=dword:00000001
-"MRUDockID"=dword:00000000
-"MRUDockLeftPos"=dword:fffffffe
-"MRUDockTopPos"=dword:fffffffe
-"MRUDockRightPos"=dword:000001b9
-"MRUDockBottomPos"=dword:0000001e
-"MRUFloatStyle"=dword:00002004
-"MRUFloatXPos"=dword:80000000
-"MRUFloatYPos"=dword:000003e0
-
-[HKEY_CURRENT_USER\Software\Oratrix GRiNS\Editor 2.0\ToolbarDefault-Bar2]
-"BarID"=dword:0000e81b
-"Bars"=dword:00000003
-"Bar#0"=dword:00000000
-"Bar#1"=dword:0000e800
-"Bar#2"=dword:00000000
-
-[HKEY_CURRENT_USER\Software\Oratrix GRiNS\Editor 2.0\ToolbarDefault-Summary]
-"Bars"=dword:00000003
-"ScreenCX"=dword:00000500
-"ScreenCY"=dword:00000400
 """
