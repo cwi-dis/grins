da86340b122e9bd79a0c257b6930224fbd9e3d43
Sjoerd Mullender <sjoerdmullender@users.noreply.github.com>
2001-08-30 14:30:59 +0000

    Added RCS Id (__version__ variable).

--- mm/lib/win32/SystemColors.py
+++ mm/lib/win32/SystemColors.py
@@ -1,3 +1,5 @@
+__version__ = "$Id$"
+
 # these colors depend of the configuration of your system, and so
 # these tables are built dynamicly from init_colors method
 # not to have a list of the supported system-colors, look at the CSS specification
