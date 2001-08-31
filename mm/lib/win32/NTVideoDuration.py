128f2ecde3dd8505bbad24654b4e983ba16700a2
anadelt <anadelt@localhost>
2001-08-31 11:20:34 +0000

    If video mimetype is video/quicktime then use Qt support to get the duration.

--- mm/lib/win32/NTVideoDuration.py
+++ mm/lib/win32/NTVideoDuration.py
@@ -8,10 +8,25 @@ when you want the duration of a media file (audio and video).
 """
 
 import MMurl
+import MMmimetypes
 import urllib
 
+import win32dxm
+
 def get(url):
+	mtype = MMmimetypes.guess_type(url)[0]
+	if mtype and mtype.find('quicktime') >= 0:
+		import winqt
+		if winqt.HasQtSupport():
+			try:
+				fn = MMurl.urlretrieve(url)[0]
+			except IOError, arg:
+				print arg
+				return 0
+			player = winqt.QtPlayer()
+			player.open(fn)
+			return player.getDuration()
 	url = MMurl.canonURL(url)
 	url = urllib.unquote(url)
-	import win32dxm
 	return win32dxm.GetMediaDuration(url)
+
