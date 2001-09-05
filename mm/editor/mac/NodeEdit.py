c67488590a60c332b7e2c67b9e6a231fc263f89b
Jack Jansen <Jack.Jansen@cwi.nl>
2001-09-05 15:42:20 +0000

    Mass checkin: cleanup of all texts in message and question dialogs.

--- mm/editor/mac/NodeEdit.py
+++ mm/editor/mac/NodeEdit.py
@@ -124,7 +124,7 @@ def showeditor(node, url=None):
 			return
 		if node.GetType() <> 'ext':
 			windowinterface.showmessage(
-				'Only extern nodes can be edited',
+				'Only external nodes can be edited.',
 				mtype = 'error')
 			return
 		import MMAttrdefs
@@ -134,7 +134,7 @@ def showeditor(node, url=None):
 	utype, host, path, params, query, fragment = urlparse.urlparse(url)
 	if (utype and utype != 'file') or (host and host != 'localhost'):
 		windowinterface.showmessage(
-			'Can only edit local files',
+			'Can only edit local files.',
 			mtype = 'warning')
 		return
 	filename = MMurl.url2pathname(path)
