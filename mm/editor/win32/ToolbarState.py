117f8fd843a31c403df0e1c8e354885ce7f48f0f
anadelt <anadelt@localhost>
2001-09-13 08:43:44 +0000

    Sync with toolbars template.

--- mm/editor/win32/ToolbarState.py
+++ mm/editor/win32/ToolbarState.py
@@ -9,32 +9,17 @@ DefaultState = """
 
 [HKEY_CURRENT_USER\Software\Oratrix GRiNS\Editor 2.0\GRiNSToolBars-Bar0]
 "BarID"=dword:0000e81b
-"Bars"=dword:00000007
+"Bars"=dword:00000008
 "Bar#0"=dword:00000000
-"Bar#1"=dword:0001e802
-"Bar#2"=dword:0001e804
-"Bar#3"=dword:0000e800
-"Bar#4"=dword:0000e805
-"Bar#5"=dword:0001e803
-"Bar#6"=dword:00000000
+"Bar#1"=dword:0000e800
+"Bar#2"=dword:0000e805
+"Bar#3"=dword:00000000
+"Bar#4"=dword:0001e804
+"Bar#5"=dword:00000000
+"Bar#6"=dword:0001e803
+"Bar#7"=dword:00000000
 
 [HKEY_CURRENT_USER\Software\Oratrix GRiNS\Editor 2.0\GRiNSToolBars-Bar1]
-"BarID"=dword:0000e802
-"Visible"=dword:00000000
-"XPos"=dword:fffffffe
-"YPos"=dword:fffffffe
-"MRUWidth"=dword:000000c4
-"Docking"=dword:00000001
-"MRUDockID"=dword:00000000
-"MRUDockLeftPos"=dword:fffffffe
-"MRUDockTopPos"=dword:fffffffe
-"MRUDockRightPos"=dword:000000d9
-"MRUDockBottomPos"=dword:0000001e
-"MRUFloatStyle"=dword:00002000
-"MRUFloatXPos"=dword:000000dc
-"MRUFloatYPos"=dword:00000121
-
-[HKEY_CURRENT_USER\Software\Oratrix GRiNS\Editor 2.0\GRiNSToolBars-Bar2]
 "BarID"=dword:0000e803
 "Visible"=dword:00000000
 "XPos"=dword:fffffffe
@@ -42,78 +27,89 @@ DefaultState = """
 "MRUWidth"=dword:00000079
 "Docking"=dword:00000001
 "MRUDockID"=dword:00000000
-"MRUDockLeftPos"=dword:000002b2
-"MRUDockTopPos"=dword:fffffffe
-"MRUDockRightPos"=dword:00000342
-"MRUDockBottomPos"=dword:0000001e
+"MRUDockLeftPos"=dword:fffffffe
+"MRUDockTopPos"=dword:0000001c
+"MRUDockRightPos"=dword:0000008e
+"MRUDockBottomPos"=dword:0000003c
 "MRUFloatStyle"=dword:00002000
-"MRUFloatXPos"=dword:00000237
-"MRUFloatYPos"=dword:00000112
+"MRUFloatXPos"=dword:000000dc
+"MRUFloatYPos"=dword:0000013d
 
-[HKEY_CURRENT_USER\Software\Oratrix GRiNS\Editor 2.0\GRiNSToolBars-Bar3]
+[HKEY_CURRENT_USER\Software\Oratrix GRiNS\Editor 2.0\GRiNSToolBars-Bar2]
 "BarID"=dword:0000e804
 "Visible"=dword:00000000
 "XPos"=dword:fffffffe
 "YPos"=dword:fffffffe
-"MRUWidth"=dword:000000f2
+"MRUWidth"=dword:000000db
 "Docking"=dword:00000001
 "MRUDockID"=dword:00000000
-"MRUDockLeftPos"=dword:000000d7
-"MRUDockTopPos"=dword:fffffffe
-"MRUDockRightPos"=dword:000001e0
-"MRUDockBottomPos"=dword:0000001e
+"MRUDockLeftPos"=dword:fffffffe
+"MRUDockTopPos"=dword:0000001c
+"MRUDockRightPos"=dword:000000f0
+"MRUDockBottomPos"=dword:0000003c
 "MRUFloatStyle"=dword:00002000
-"MRUFloatXPos"=dword:00000180
-"MRUFloatYPos"=dword:00000123
+"MRUFloatXPos"=dword:00000106
+"MRUFloatYPos"=dword:00000148
 
-[HKEY_CURRENT_USER\Software\Oratrix GRiNS\Editor 2.0\GRiNSToolBars-Bar4]
+[HKEY_CURRENT_USER\Software\Oratrix GRiNS\Editor 2.0\GRiNSToolBars-Bar3]
 "BarID"=dword:0000e800
 "XPos"=dword:fffffffe
 "YPos"=dword:fffffffe
 "Docking"=dword:00000001
-"MRUDockID"=dword:0000e81b
+"MRUDockID"=dword:00000000
 "MRUDockLeftPos"=dword:fffffffe
 "MRUDockTopPos"=dword:fffffffe
-"MRUDockRightPos"=dword:000000d4
+"MRUDockRightPos"=dword:000000fc
 "MRUDockBottomPos"=dword:0000001e
 "MRUFloatStyle"=dword:00002004
 "MRUFloatXPos"=dword:80000000
 "MRUFloatYPos"=dword:00000000
 
-[HKEY_CURRENT_USER\Software\Oratrix GRiNS\Editor 2.0\GRiNSToolBars-Bar5]
+[HKEY_CURRENT_USER\Software\Oratrix GRiNS\Editor 2.0\GRiNSToolBars-Bar4]
 "BarID"=dword:0000e805
-"XPos"=dword:000000d6
+"XPos"=dword:000000fa
 "YPos"=dword:fffffffe
 "Docking"=dword:00000001
 "MRUDockID"=dword:0000e81b
-"MRUDockLeftPos"=dword:000000d6
+"MRUDockLeftPos"=dword:000000fa
 "MRUDockTopPos"=dword:fffffffe
-"MRUDockRightPos"=dword:00000136
+"MRUDockRightPos"=dword:0000015a
 "MRUDockBottomPos"=dword:0000001e
 "MRUFloatStyle"=dword:00002000
-"MRUFloatXPos"=dword:000002e3
-"MRUFloatYPos"=dword:000000d0
+"MRUFloatXPos"=dword:000002d3
+"MRUFloatYPos"=dword:000000df
+
+[HKEY_CURRENT_USER\Software\Oratrix GRiNS\Editor 2.0\GRiNSToolBars-Bar5]
+"BarID"=dword:0000e81f
+"Horz"=dword:00000001
+"Floating"=dword:00000001
+"XPos"=dword:0000010a
+"YPos"=dword:0000015c
+"Bars"=dword:00000003
+"Bar#0"=dword:00000000
+"Bar#1"=dword:0000e804
+"Bar#2"=dword:00000000
 
 [HKEY_CURRENT_USER\Software\Oratrix GRiNS\Editor 2.0\GRiNSToolBars-Bar6]
 "BarID"=dword:0000e81f
 "Horz"=dword:00000001
 "Floating"=dword:00000001
 "XPos"=dword:000000e0
-"YPos"=dword:00000135
+"YPos"=dword:00000151
 "Bars"=dword:00000003
 "Bar#0"=dword:00000000
-"Bar#1"=dword:0000e802
+"Bar#1"=dword:0000e803
 "Bar#2"=dword:00000000
 
 [HKEY_CURRENT_USER\Software\Oratrix GRiNS\Editor 2.0\GRiNSToolBars-Bar7]
 "BarID"=dword:0000e81f
 "Horz"=dword:00000001
 "Floating"=dword:00000001
-"XPos"=dword:00000184
-"YPos"=dword:00000137
+"XPos"=dword:000002d7
+"YPos"=dword:000000f3
 "Bars"=dword:00000003
 "Bar#0"=dword:00000000
-"Bar#1"=dword:0000e804
+"Bar#1"=dword:0000e805
 "Bar#2"=dword:00000000
 
 [HKEY_CURRENT_USER\Software\Oratrix GRiNS\Editor 2.0\GRiNSToolBars-Bar8]
@@ -128,7 +124,36 @@ DefaultState = """
 "Bar#2"=dword:00000000
 
 [HKEY_CURRENT_USER\Software\Oratrix GRiNS\Editor 2.0\GRiNSToolBars-Summary]
-"Bars"=dword:00000009
+"Bars"=dword:00000007
+"ScreenCX"=dword:00000500
+"ScreenCY"=dword:00000400
+
+[HKEY_CURRENT_USER\Software\Oratrix GRiNS\Editor 2.0\ToolbarDefault-Bar0]
+"BarID"=dword:0000e801
+
+[HKEY_CURRENT_USER\Software\Oratrix GRiNS\Editor 2.0\ToolbarDefault-Bar1]
+"BarID"=dword:0000e800
+"XPos"=dword:fffffffe
+"YPos"=dword:fffffffe
+"Docking"=dword:00000001
+"MRUDockID"=dword:00000000
+"MRUDockLeftPos"=dword:fffffffe
+"MRUDockTopPos"=dword:fffffffe
+"MRUDockRightPos"=dword:000001b9
+"MRUDockBottomPos"=dword:0000001e
+"MRUFloatStyle"=dword:00002004
+"MRUFloatXPos"=dword:80000000
+"MRUFloatYPos"=dword:000003e0
+
+[HKEY_CURRENT_USER\Software\Oratrix GRiNS\Editor 2.0\ToolbarDefault-Bar2]
+"BarID"=dword:0000e81b
+"Bars"=dword:00000003
+"Bar#0"=dword:00000000
+"Bar#1"=dword:0000e800
+"Bar#2"=dword:00000000
+
+[HKEY_CURRENT_USER\Software\Oratrix GRiNS\Editor 2.0\ToolbarDefault-Summary]
+"Bars"=dword:00000003
 "ScreenCX"=dword:00000500
 "ScreenCY"=dword:00000400
 
