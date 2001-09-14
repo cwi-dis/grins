1cca24b8634c21d6cccaaac2c24752847d20ffb1
anadelt <anadelt@localhost>
2001-09-14 10:53:20 +0000

    Update buildit and docit bat files. Normally they should have been working and before these changes.

--- mm/win32/COM/javaclient/buildit.bat
+++ mm/win32/COM/javaclient/buildit.bat
@@ -1,5 +1,6 @@
-cd grins
-javac *.java 
-cd ..
+set path=C:\jdk1.3.1\bin;%path%
+javac   grins/*.java grins/demo/*.java grins/player/*.java
+jar cvf grinsp.jar grins/*.class grins/demo/*.class grins/player/*.class
+move grinsp.jar bin
 javah grins.GRiNSPlayer
 javah grins.GRiNSPlayerMonitor
--- mm/win32/COM/javaclient/docit.bat
+++ mm/win32/COM/javaclient/docit.bat
@@ -1,1 +1,2 @@
+set path=C:\jdk1.3.1\bin;%path%
 javadoc grins -nohelp -d api
