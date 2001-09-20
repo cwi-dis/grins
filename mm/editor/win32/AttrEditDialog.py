fdf3b4a34682b6cd000f0315d5ef858f19df6a8c
Sjoerd Mullender <sjoerdmullender@users.noreply.github.com>
2001-09-20 11:47:02 +0000

    Changed doc strings into comments.

--- mm/editor/win32/AttrEditDialog.py
+++ mm/editor/win32/AttrEditDialog.py
@@ -1,50 +1,46 @@
-#"""Dialog for the Attribute Editor.
+# Dialog for the Attribute Editor.
 #
-#The Attribute editor dialog consists of four buttons that are always
-#present (`Cancel', `Restore', `Apply', and `OK'), and a row of
-#attribute editor fields.  The number and type of these fields are
-#determined at run time, but there are only a limited number of
-#different types.  The fields are represented by instances of
-#sub-classes of the AttrEditorDialogField.  A list of these instances
-#is passed to the constructor of the AttrEditorDialog class.
+# The Attribute editor dialog consists of four buttons that are always
+# present (`Cancel', `Restore', `Apply', and `OK'), and a row of
+# attribute editor fields.  The number and type of these fields are
+# determined at run time, but there are only a limited number of
+# different types.  The fields are represented by instances of
+# sub-classes of the AttrEditorDialogField.  A list of these instances
+# is passed to the constructor of the AttrEditorDialog class.
 
-#Each of the attribute editor fields has a label with the name of the
-#attribute, an interface to edit the value (the interface depends on
-#the type of the value), and a `Reset' button.  There are two callbacks
-#associated with an attribute editor field, one to get help, and one
-#that is called when the `Reset' button is pressed.  There are also
-#some methods to get and set the current value of the attribute.
+# Each of the attribute editor fields has a label with the name of the
+# attribute, an interface to edit the value (the interface depends on
+# the type of the value), and a `Reset' button.  There are two callbacks
+# associated with an attribute editor field, one to get help, and one
+# that is called when the `Reset' button is pressed.  There are also
+# some methods to get and set the current value of the attribute.
 
-#The base class of the AttrEditorDialogField class must provide some
-#methods also.
-#
-#"""
+# The base class of the AttrEditorDialogField class must provide some
+# methods also.
 
 __version__ = "$Id$"
 
 
-#""" @win32doc|AttrEditorDialog
-#This class represents the interface between the AttrEditor platform independent
-#class and its implementation AttrEditorForm in lib/win32/AttrEditorForm.py which
-#implements the actual dialog.
-#
-#"""
+# @win32doc|AttrEditorDialog
+# This class represents the interface between the AttrEditor platform independent
+# class and its implementation AttrEditorForm in lib/win32/AttrEditorForm.py which
+# implements the actual dialog.
+
 import windowinterface
 import usercmd
 
 class AttrEditorDialog:
 	def __init__(self, title, attriblist, toplevel=None, initattr = None):
-##		"""Create the AttrEditor dialog.
+		# Create the AttrEditor dialog.
 
-##		Create the dialog window (non-modal, so does not grab
-##		the cursor) and pop it up (i.e. display it on the
-##		screen).
+		# Create the dialog window (non-modal, so does not grab
+		# the cursor) and pop it up (i.e. display it on the
+		# screen).
 
-##		Arguments (no defaults):
-##		title -- string to be displayed as the window title
-##		attriblist -- list of instances of subclasses of
-##			AttrEditorDialogField
-##		"""
+		# Arguments (no defaults):
+		# title -- string to be displayed as the window title
+		# attriblist -- list of instances of subclasses of
+		# 	AttrEditorDialogField
 		if hasattr(self, 'willreopen') and self.willreopen:
 			# This is a reopen call.
 			w = self.__window
@@ -104,7 +100,7 @@ class AttrEditorDialog:
 		self.wrapper.context.color_list = color_list
 
 	def close(self, willreopen=0):
-##		"""Close the dialog and free resources."""
+		# Close the dialog and free resources.
 		# XXXX To be done: if willreopen=1 we should not close the window but
 		# reset it to the initial state. Also, in the init routine we should not
 		# create __window if it already exists.
@@ -131,16 +127,15 @@ class AttrEditorDialog:
 			self.is_changed())
 
 	def pop(self):
-##		"""Pop the dialog window to the foreground."""
+		# Pop the dialog window to the foreground.
 		if self.__window:
 			self.__window.pop()
 
 	def settitle(self, title):
-##		"""Set (change) the title of the window.
+		# Set (change) the title of the window.
 
-##		Arguments (no defaults):
-##		title -- string to be displayed as new window title
-##		"""
+		# Arguments (no defaults):
+		# title -- string to be displayed as new window title
 		if self.__window:
 			self.__window.settitle(title)
 
@@ -171,38 +166,35 @@ class AttrEditorDialog:
 
 class AttrEditorDialogField:
 	def attach_ui(self, form):
-##		"""Set context for this attribute.  (internal method)
+		# Set context for this attribute.  (internal method)
 
-##		Arguments (no defaults):
-##		form -- instance of AttrEditorForm
-##		"""
+		# Arguments (no defaults):
+		# form -- instance of AttrEditorForm
 		self.__form=form
 
 	def close(self):
-##		"""Close the instance and free all resources."""
+		# Close the instance and free all resources.
 		# nothing to free
 		pass
 
 	def getvalue(self):
-##		"""Return the current value of the attribute.
+		# Return the current value of the attribute.
 ##
-##		The return value is a string giving the current value.
-##		"""
+		# The return value is a string giving the current value.
 		try:
 			return self.__form.getvalue(self)
 		except AttributeError:
 			return self.getcurrent()
 
 	def setvalue(self, value):
-##		"""Set the current value of the attribute.
+		# Set the current value of the attribute.
 
-##		Arguments (no defaults):
-##		value -- string giving the new value
-##		"""
+		# Arguments (no defaults):
+		# value -- string giving the new value
 		self.__form.setvalue(self,value)
 
 	def recalcoptions(self):
-##		"""Recalculate the list of options and set the value."""
+		# Recalculate the list of options and set the value.
 		self.__form.setoptions(self,self.getoptions(), self.getcurrent())
 
 	def askchannelname(self, default):
@@ -211,41 +203,40 @@ class AttrEditorDialogField:
 
 	# Methods to be overridden by the sub class.
 	def gettype(self):
-##		"""Return the type of the attribute as a string.
+		# Return the type of the attribute as a string.
 
-##		Valid types are:
-##		option -- attribute value is one of a fixed set of
-##			values
-##		file -- attribute is a file name (an interface to a
-##			file dialog is a good idea)
-##		string -- attribute is a string
-##		int -- attribute is a string representing an integer
-##		float -- attribute is a string representing a float
+		# Valid types are:
+		# option -- attribute value is one of a fixed set of
+		# 	values
+		# file -- attribute is a file name (an interface to a
+		# 	file dialog is a good idea)
+		# string -- attribute is a string
+		# int -- attribute is a string representing an integer
+		# float -- attribute is a string representing a float
 
-##		`option' and `file' must be handled differently from
-##		the others, the others can all be handled as a string.
-##		An attribute field of type `option' must have a
-##		getoptions method that returns a list of strings
-##		giving all the possible values.
-##		An attribute field of type `file' may invoke a
-##		callback browser_callback that pops up a file browser.
-##		"""
+		# `option' and `file' must be handled differently from
+		# the others, the others can all be handled as a string.
+		# An attribute field of type `option' must have a
+		# getoptions method that returns a list of strings
+		# giving all the possible values.
+		# An attribute field of type `file' may invoke a
+		# callback browser_callback that pops up a file browser.
 		return 'type'
 
 	def getlabel(self):
-#		"""Return the label for the attribute field."""
+		# Return the label for the attribute field.
 		return 'Button Label'
 
 	def getcurrent(self):
-#		"""Return the current value of the attribute as a string."""
+		# Return the current value of the attribute as a string.
 		return 'current value'
 
 	def reset_callback(self):
-#		"""Callback called when the `Reset' button is pressed."""
+		# Callback called when the `Reset' button is pressed.
 		pass
 
 	def help_callback(self):
-#		"""Callback called when help is requested."""
+		# Callback called when help is requested.
 		pass
 
 
