8b66c8f56f91eddcb73c173f338d2cc3f66318e5
Sjoerd Mullender <sjoerdmullender@users.noreply.github.com>
2001-11-08 14:42:26 +0000

    Made shortversion base grins2pro. Removed stuff for Snap! and lite.

--- mm/editor/version.py
+++ mm/editor/version.py
@@ -3,18 +3,9 @@ import sys
 from patchlevel import patchlevel
 import features
 
-if features.version == 'Snap!':
-	shortversion = 'grinssnap-%s-2.0'%sys.platform
-	version = 'Snap! for %s, v2.0 %s' % (features.compatibility, patchlevel)
-	macpreffilename = 'GRiNS-Snap!-%s-2.0 Prefs' % features.compatibility_short	
-elif features.lightweight:		# TODO: change this to..? mjvdg.
-	shortversion = 'grinslite-%s-2.0'%sys.platform
-	version = 'Lite for %s, v2.0 %s' % (features.compatibility, patchlevel)
-	macpreffilename = 'GRiNS-lite-%s-2.0 Prefs' % features.compatibility_short
-else:
-	shortversion = 'grinspro-%s-2.0'%sys.platform
-	version = 'Pro for %s, v2.0 %s' % (features.compatibility, patchlevel)
-	macpreffilename = 'GRiNS-pro-%s-2.0 Prefs' % features.compatibility_short
+shortversion = 'grins2pro-%s-2.0'%sys.platform
+version = 'Pro for %s, v2.0 %s' % (features.compatibility, patchlevel)
+macpreffilename = 'GRiNS-pro-%s-2.0 Prefs' % features.compatibility_short
 
 title = 'GRiNS Editor'
 registrykey = 'Oratrix GRiNS'
