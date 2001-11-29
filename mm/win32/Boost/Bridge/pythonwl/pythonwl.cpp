20e7fe3313eb9b60721d48321dc2f3291c1494b2
anadelt <anadelt@localhost>
2001-11-29 22:10:10 +0000

    Some imporatnt improvements: uses ini with pythonhome and path as we have done for pythonwin + runs arg script in __main__

--- mm/win32/Boost/Bridge/pythonwl/pythonwl.cpp
+++ mm/win32/Boost/Bridge/pythonwl/pythonwl.cpp
@@ -4,35 +4,31 @@
 
 #include "util.h"
 	
-
-
 int main(int argc, char **argv)
 	{
-	CoInitialize(NULL);
-	
-	char *defaulthome = "d:\\ufs\\mm\\python";
-	char *pythonhome = getenv("PYTHONHOME");
-
-	if(!python::initialize(argc, argv, pythonhome?pythonhome:defaulthome))
-		{
-		std::cout << "initialize_python failed" << std::endl;
-		return 1;
-		}
-	if(argc > 1)
-		{
-		python::run_file(argv[1]);
-
-		MSG msg;
-		while(GetMessage(&msg, NULL, 0, 0)) 
-			{
-			TranslateMessage(&msg);
-			DispatchMessage(&msg);
-			}
-		}
-	else
+	if(argc == 1)
 		{
 		char *p = strrchr(argv[0], '\\');
 		std::cout << "usage: "<< (p?(p+1):argv[0]) << " filename.py" << std::endl;
+		return 0;
+		}
+
+	CoInitialize(NULL);
+	
+	if(!python::initialize(argc-1, argv))
+		{
+		std::cout << "initialize_python failed" << std::endl;
+		CoUninitialize();
+		return 1;
+		}
+
+	python::run_file(argv[1]);
+
+	MSG msg;
+	while(GetMessage(&msg, NULL, 0, 0)) 
+		{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
 		}
 
 	python::finalize();
