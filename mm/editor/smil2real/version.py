04330cb9392ca25fcdb74f3e8710ff60fa533aa6
Sjoerd Mullender <sjoerdmullender@users.noreply.github.com>
2001-12-03 13:59:37 +0000

    Version 2.1.

--- mm/editor/smil2real/version.py
+++ mm/editor/smil2real/version.py
@@ -4,8 +4,8 @@ import sys
 from patchlevel import patchlevel
 import features
 
-shortversion = 'grins2real-%s-2.0'%sys.platform
-version = ('Editor for RealONE %s' % patchlevel).strip()
+shortversion = 'grins2real-%s-2.1'%sys.platform
+version = ('Editor for RealONE, Version 2.1 %s' % patchlevel).strip()
 macpreffilename = 'GRiNS-%s-2.0 Prefs' % features.compatibility_short
 title = 'GRiNS Editor for RealONE'
 registrykey = 'Oratrix GRiNS'
