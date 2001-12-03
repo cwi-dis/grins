993515eba23eb69bca8849aa5aeba812599b9cf3
anadelt <anadelt@localhost>
2001-12-03 13:49:11 +0000

    More or less final form of wingdi.

--- mm/win32/winsdk/wingdi/wingdi_main.h
+++ mm/win32/winsdk/wingdi/wingdi_main.h
@@ -5,8 +5,16 @@
 #include "Python.h"
 #endif
 
+PyObject* Wingdi_DeleteObject(PyObject *self, PyObject *args);
+PyObject* Wingdi_GetStockObject(PyObject *self, PyObject *args);
+PyObject* Wingdi_ExtCreatePen(PyObject *self, PyObject *args);
+PyObject* Wingdi_CreateSolidBrush(PyObject *self, PyObject *args);
+PyObject* Wingdi_CreateBrushIndirect(PyObject *self, PyObject *args);
+PyObject* Wingdi_CreateFontIndirect(PyObject *self, PyObject *args);
+
 PyObject* Wingdi_IntersectRect(PyObject *self, PyObject *args);
 PyObject* Wingdi_UnionRect(PyObject *self, PyObject *args);
+
 PyObject* Wingdi_GetRGBValues(PyObject *self, PyObject *args);
 
 #endif
