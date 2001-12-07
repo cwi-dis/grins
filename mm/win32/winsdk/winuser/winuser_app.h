b3e181058e59341e8bd35a27acbf3ddb5242ff6d
anadelt <anadelt@localhost>
2001-12-07 15:44:08 +0000

    C/C++ part of GRiNS Player for CE. It consist of two parts: One is the top level UI which is for now auto generated using EVC Wizard. The other part is an interface framework to python for running grins. GRiNS can have access/control over the auto generated UI since it can gets python objects representing it. The autogenerated UI lives in the project folder and the framework in ./pfc and thus can be easily regenerated.

---
+++ mm/win32/winsdk/winuser/winuser_app.h
@@ -0,0 +1,11 @@
+#ifndef INC_WINUSER_APP
+#define INC_WINUSER_APP
+
+#ifndef Py_PYTHON_H
+#include "Python.h"
+#endif
+
+PyObject* Winuser_GetApplication(PyObject *self, PyObject *args);
+
+#endif
+
