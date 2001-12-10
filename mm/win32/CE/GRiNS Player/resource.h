44f3048c271472e9f06d28d1590391544ee0c890
anadelt <anadelt@localhost>
2001-12-10 18:13:17 +0000

    Add an open (file) command.

--- mm/win32/CE/GRiNS Player/resource.h
+++ mm/win32/CE/GRiNS Player/resource.h
@@ -19,13 +19,17 @@
 #define IDM_PLAY                        40009
 #define IDM_PAUSE                       40010
 #define IDM_STOP                        40011
+#define IDM_MAIN_COMMAND3               40012
+#define IDS_CAP_HELP                    40014
+#define IDM_EXIT                        40015
+#define IDM_OPEN                        40016
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        106
-#define _APS_NEXT_COMMAND_VALUE         40012
+#define _APS_NEXT_COMMAND_VALUE         40017
 #define _APS_NEXT_CONTROL_VALUE         1002
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
