1ad2e21f6e38777ce83c04f2d0654deb14c6f753
anadelt <anadelt@localhost>
2001-12-28 11:25:15 +0000

    GRiNS Player for CE status 1. What does work:   a. we can now play documents with bmp, gif, and jpg images (support for png will be added soon)   b. animations do work but need optimization  (try for example commute.grins) 2. To have enough samples for testing we do allow documents with any viewport size.    That means scaling and thus quality/speed are not the best possible. 3. Transitions don't work yet but the infrustructure to enable them is in place.    We do have a framework similar to direct draw and thus a lot of code can be reused. 4. Transparency does not work and will be attacked with (3)

---
+++ mm/win32/winsdk/common/strutil.h
@@ -0,0 +1,50 @@
+#ifndef INC_STRUTIL
+#define INC_STRUTIL
+
+#pragma warning(disable: 4786) // long names trunc (debug)
+#pragma warning(disable: 4018) // signed/unsigned mismatch
+#include <string>
+
+#ifndef _WINDOWS_
+#include <windows.h>
+#endif
+
+#include <vector>
+class StrRec : public std::vector<TCHAR*>
+	{
+	public:
+    StrRec(const TCHAR* pString,const TCHAR* pDelims)
+	:	buf(new TCHAR[lstrlen(pString)+1])
+		{
+		lstrcpy(buf, pString);
+		TCHAR *pLook = text_strtok(buf, pDelims);
+		while(pLook)
+			{
+			push_back(pLook);
+			pLook = text_strtok(NULL, pDelims);
+			}
+		}
+    ~StrRec() {delete[] buf;}
+	private:
+	TCHAR* buf;
+	};
+
+
+inline std::string fixendl(const char *psz)
+	{
+	std::string str;
+	const char *p = psz;
+	while(*p)
+		{
+		if(*p == '\r' && *(p+1) != '\n')
+			str += "\r\n";
+		else if(*p == '\n' && (p==psz || *(p-1) != '\r'))
+			str += "\r\n";
+		else
+			str += *p;
+		p++;
+		}
+	return str;
+	}
+
+#endif // INC_STRUTIL
