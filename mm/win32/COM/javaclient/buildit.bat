ec8dea36ebcd0a10ff1052f97ee86de0aa7d7ccc
anadelt <anadelt@localhost>
2002-01-07 12:14:39 +0000

    You can use buildit.bat to build grinsp.jar and headers needed for grinsp.dll.

--- mm/win32/COM/javaclient/buildit.bat
+++ mm/win32/COM/javaclient/buildit.bat
@@ -1,4 +1,4 @@
-set path=C:\jdk1.3.1\bin;%path%
+set path=C:\jdk1.4\bin;%path%
 javac   grins/*.java grins/demo/*.java grins/player/*.java
 jar cvf grinsp.jar grins/*.class grins/demo/*.class grins/player/*.class
 move grinsp.jar bin
