f965c18991b8c263eff1508e9ec90e4ac3b7710c
anadelt <anadelt@localhost>
2002-01-12 16:13:54 +0000

    Some minor changes.

--- mm/win32/COM/javaclient/buildit.bat
+++ mm/win32/COM/javaclient/buildit.bat
@@ -1,6 +1,8 @@
 set path=C:\jdk1.4\bin;%path%
+
 javac   grins/*.java grins/demo/*.java grins/player/*.java
 jar cvf grinsp.jar grins/*.class grins/demo/*.class grins/player/*.class
 move grinsp.jar bin
-javah grins.GRiNSPlayer
-javah grins.GRiNSPlayerMonitor
+
+javah -classpath .;D:\ufs\mm\cmif\win32\COM\javaclient\grins grins.GRiNSPlayer
+javah -classpath .;D:\ufs\mm\cmif\win32\COM\javaclient\grins grins.GRiNSPlayerMonitor
