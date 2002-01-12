5eedc58a16fc64b43c9640dae064316f38c9cf53
anadelt <anadelt@localhost>
2002-01-12 16:28:25 +0000

    Remove package 'grins' from cmd line arguments

--- mm/win32/COM/javaclient/docit.bat
+++ mm/win32/COM/javaclient/docit.bat
@@ -1,4 +1,4 @@
 set path=C:\jdk1.4\bin;%path%
 
-javadoc -d api -classpath .;D:\ufs\mm\cmif\win32\COM\javaclient\grins grins ./grins/*.java
+javadoc -d api -classpath .;D:\ufs\mm\cmif\win32\COM\javaclient\grins ./grins/*.java
 
