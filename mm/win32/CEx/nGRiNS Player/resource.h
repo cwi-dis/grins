965dd84cfcb65016e76afd6fbd307450a75a2530
anadelt <anadelt@localhost>
2002-01-25 11:17:51 +0000

    Start the nova (or native if you like) branch of the GRiNS mobile Player project.
    The design task/context:
    
    The project will
    * be developed using std c/c++ only.
    * be portable to any platform with a std c/c++ compiler except ui and media display modules
    * target initially win CE but most of the code will work on any win platform.
    * be acceptable fast under win CE
    * use, at least for its first version, the open source projects (regex, expat, jpeg, etc)
    * reuse the generic and ui c/c++ modules we have developed for python Player for CE
    * use python smil def modules to auto-generate c++ modules when possible
    * use existing GRiNS architecture when seems appropriate and possible.
    * be component based (loosly coupled), including originally most components at source level (for RAD)
    
    The highest risk task is: "be acceptable fast under win CE".
    I will try to build something quick and dirty which, I hope, will allow us to have some first estimates on this early.

---
+++ mm/win32/CEx/nGRiNS Player/resource.h
@@ -0,0 +1,38 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by nGRiNS Player.rc
+//
+#define IDS_APP_TITLE                   1
+#define IDS_HELLO                       2
+#define IDC_NGRINSPLAYER                3
+#define IDI_NGRINSPLAYER                101
+#define IDM_MENU                        102
+#define IDD_ABOUTBOX                    103
+#define IDS_HELP                        104
+#define IDD_PYSTDOUT                    105
+#define IDD_STDOUT                      105
+#define IDS_COMMAND1                    301
+#define IDC_STDOUT                      1001
+#define IDM_MAIN_COMMAND1               40001
+#define IDM_HELP_ABOUT                  40003
+#define IDM_MAIN_COMMAND2               40007
+#define IDS_MAIN_COMMAND2               40008
+#define IDM_PLAY                        40009
+#define IDM_PAUSE                       40010
+#define IDM_STOP                        40011
+#define IDM_MAIN_COMMAND3               40012
+#define IDS_CAP_HELP                    40014
+#define IDM_EXIT                        40015
+#define IDM_OPEN                        40016
+#define IDM_CLOSE                       40017
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        106
+#define _APS_NEXT_COMMAND_VALUE         40018
+#define _APS_NEXT_CONTROL_VALUE         1002
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
