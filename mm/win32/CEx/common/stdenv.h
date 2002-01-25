39f2354cc770a37289fbf8904a180f424f55630a
anadelt <anadelt@localhost>
2002-01-25 15:12:04 +0000

    strsream is not available under CE. Replace with something sensible and useful.

--- mm/win32/CEx/common/stdenv.h
+++ mm/win32/CEx/common/stdenv.h
@@ -23,5 +23,16 @@
 #include <math.h>
 // end stdc
 
+// strsteam replacement
+template <class T> std::string& operator<<(std::string& s, T c) 
+	{ s+=c; return s;}
+
+inline std::string& operator<<(std::string& s, int c) 
+	{char sz[16];sprintf(sz,"%d",c); s+=sz; return s;}	
+
+inline std::string& operator<<(std::string& s, double c) 
+	{char sz[16];sprintf(sz,"%f",c); s+=sz; return s;}
+// end of strsteam replacement
+
 #endif // INC_STDENV
 
