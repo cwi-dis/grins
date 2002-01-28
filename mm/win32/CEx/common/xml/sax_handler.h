31503333ca3a5f177f59137429a6b2e05a71b5c4
anadelt <anadelt@localhost>
2002-01-28 18:00:37 +0000

    Use python GRiNS Player top level architecture.

---
+++ mm/win32/CEx/common/xml/sax_handler.h
@@ -0,0 +1,28 @@
+/************************************************************************
+Copyright 1991-2002 by Oratrix Development BV, Amsterdam, The Netherlands.
+
+                        All Rights Reserved
+
+*************************************************************************/
+
+#ifndef INC_XML_SAX_HANDLER
+#define INC_XML_SAX_HANDLER
+
+namespace xml {
+
+class sax_handler
+	{
+	public:
+	virtual ~sax_handler(){}
+	virtual void start_element(const char *name, const char **atts) = 0;
+	virtual void end_element() = 0;
+	virtual void handle_data(const char *s, int len) = 0;
+	// ...
+	virtual void show_error(std::string& report) = 0;
+	};
+
+
+} // namespace xml
+
+
+#endif  // INC_XML_SAX_HANDLER
