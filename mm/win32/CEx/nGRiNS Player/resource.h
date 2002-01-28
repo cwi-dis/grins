13f2b0b0c9406a27f23e046f2ab1c44473cdfa3d
anadelt <anadelt@localhost>
2002-01-28 17:54:28 +0000

    Use MFC for this version of top level ui.

--- mm/win32/CEx/nGRiNS Player/resource.h
+++ mm/win32/CEx/nGRiNS Player/resource.h
@@ -1,38 +1,25 @@
 //{{NO_DEPENDENCIES}}
 // Microsoft Developer Studio generated include file.
-// Used by nGRiNS Player.rc
+// Used by GRiNS Player.rc
 //
-#define IDS_APP_TITLE                   1
-#define IDS_HELLO                       2
-#define IDC_NGRINSPLAYER                3
-#define IDI_NGRINSPLAYER                101
-#define IDM_MENU                        102
-#define IDD_ABOUTBOX                    103
-#define IDS_HELP                        104
-#define IDD_PYSTDOUT                    105
-#define IDD_STDOUT                      105
-#define IDS_COMMAND1                    301
-#define IDC_STDOUT                      1001
-#define IDM_MAIN_COMMAND1               40001
-#define IDM_HELP_ABOUT                  40003
-#define IDM_MAIN_COMMAND2               40007
-#define IDS_MAIN_COMMAND2               40008
-#define IDM_PLAY                        40009
-#define IDM_PAUSE                       40010
-#define IDM_STOP                        40011
-#define IDM_MAIN_COMMAND3               40012
-#define IDS_CAP_HELP                    40014
-#define IDM_EXIT                        40015
-#define IDM_OPEN                        40016
-#define IDM_CLOSE                       40017
+#define IDD_ABOUTBOX                    100
+#define IDB_SPLASH                      106
+#define IDR_MAINFRAME                   128
+#define ID_FILE_EXIT                    40001
+#define ID_CMD_EXIT                     40001
+#define ID_CMD_PLAY                     40002
+#define ID_CMD_PAUSE                    40003
+#define ID_CMD_STOP                     40004
+#define ID_CMD_OPEN                     40005
+#define ID_CMD_CLOSE                    40006
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        106
-#define _APS_NEXT_COMMAND_VALUE         40018
-#define _APS_NEXT_CONTROL_VALUE         1002
+#define _APS_NEXT_RESOURCE_VALUE        107
+#define _APS_NEXT_COMMAND_VALUE         40007
+#define _APS_NEXT_CONTROL_VALUE         1000
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
