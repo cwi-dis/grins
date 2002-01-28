648d50f5b061daa6ac81dfc31db118558feb9d58
anadelt <anadelt@localhost>
2002-01-28 17:55:16 +0000

    Use MFC for this version of top level ui.

---
+++ mm/win32/CEx/nGRiNS Player/AboutDlg.h
@@ -0,0 +1,29 @@
+#ifndef INC_ABOUTDLG
+#define INC_ABOUTDLG
+
+class CAboutDlg : public CDialog
+	{
+	public:
+	CAboutDlg();
+
+	// Dialog Data
+	//{{AFX_DATA(CAboutDlg)
+	enum { IDD = IDD_ABOUTBOX};
+	//}}AFX_DATA
+
+	// ClassWizard generated virtual function overrides
+	//{{AFX_VIRTUAL(CAboutDlg)
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
+	//}}AFX_VIRTUAL
+
+	// Implementation
+	protected:
+	//{{AFX_MSG(CAboutDlg)
+	virtual BOOL OnInitDialog();		// Added for WCE apps
+	//}}AFX_MSG
+	DECLARE_MESSAGE_MAP()
+	};
+
+#endif // INC_ABOUTDLG
+
