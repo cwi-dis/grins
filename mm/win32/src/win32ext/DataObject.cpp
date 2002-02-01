6864565b017f0a60387a40d39fc697d933d7d485
anadelt <anadelt@localhost>
2002-02-01 17:23:44 +0000

    Fix an important source of crashes. INCLUDING THE LATEST ONE REPORTED BY DICK. Data objects strangely enough interfered with cmd ui updates. (No so strange if you think that win32ui keeps a global map of all c to python objects)

--- mm/win32/src/win32ext/DataObject.cpp
+++ mm/win32/src/win32ext/DataObject.cpp
@@ -2,97 +2,106 @@
 
 #include "DataObject.h"
 
-
-// this returns a pointer that should not be stored.
-COleDataObject *ui_data_object::GetDataObject(PyObject *self)
+struct PyDataObject
 	{
-	return (COleDataObject *)ui_assoc_object::GetGoodCppObject(self, &type);
-	}
+	PyObject_HEAD
+	COleDataObject* m_pOleDataObject;
 
-void ui_data_object::DoKillAssoc( BOOL bDestructing /*= FALSE*/ )
-	{
-	if (m_deleteDO) 
+	static PyTypeObject type;
+	static PyMethodDef methods[];
+
+	static PyDataObject *createInstance(COleDataObject* pOleDataObject = NULL)
 		{
-		COleDataObject *pDO = GetDataObject(this);
-		if (pDO)delete pDO;
+		PyDataObject *instance = PyObject_NEW(PyDataObject, &type);
+		if (instance == NULL) return NULL;
+		instance->m_pOleDataObject = pOleDataObject;
+		return instance;
 		}
-	ui_assoc_object::DoKillAssoc(bDestructing);
-	}
 
-ui_data_object::~ui_data_object()
+	static void dealloc(PyDataObject *instance) 
+		{ 
+		PyMem_DEL(instance);
+		}
+
+	static PyObject *getattr(PyDataObject *instance, char *name)
+		{ 
+		return Py_FindMethod(methods, (PyObject*)instance, name);
+		}
+	};
+
+//////////////////////
+// Create PyDataObject (wrapper of COleDataObject)
+
+PyObject* CreatePyDataObject(COleDataObject *pOleDataObject)
 	{
-	DoKillAssoc(TRUE);
+	return (PyObject*)PyDataObject::createInstance(pOleDataObject);
 	}
 
-void *ui_data_object::GetGoodCppObject(ui_type *ui_type_check) const
-	{
-	COleDataObject* pDO= (COleDataObject*)ui_assoc_object::GetGoodCppObject(ui_type_check);
-	if (!pDO)RETURN_ERR("There is no OleDataObject associated with the object");
-	return pDO;
-	}
+//////////////////////
+// PyDataObject methos
 
-// @pymethod |win32ui|CreateOleDataObject|Creates an OleDataObject.
-PyObject *create_ole_data_object( PyObject *self, PyObject *args )
-	{
-	CHECK_NO_ARGS2(args, CreateOleDataObject);
-	COleDataObject* pDO= new COleDataObject;
-    ui_data_object *pPyDO =
-      (ui_data_object *) ui_assoc_object::make (ui_data_object::type, pDO)->GetGoodRet();
-    pPyDO->m_deleteDO=TRUE;
-	return pPyDO;
-	}
-
-// @pymethod int|PyDataObject|IsDataAvailable|Returns TRUE if there are data in the specified format. FALSE if not.
 static PyObject *
-ui_data_object_is_data_available(PyObject *self, PyObject *args)
+PyDataObject_IsDataAvailable(PyDataObject *self, PyObject *args)
 	{
 	int cf; 
 	if (!PyArg_ParseTuple(args,"i:IsDataAvailable",&cf))
 		return NULL;
-
-	COleDataObject* pDO = ui_data_object::GetDataObject(self);
-	if (!pDO)return NULL;
-
 	CLIPFORMAT cfPrivate=(CLIPFORMAT)cf;
-	BOOL bRes = pDO->IsDataAvailable(cfPrivate);
-
+	BOOL bRes = self->m_pOleDataObject->IsDataAvailable(cfPrivate);
 	return Py_BuildValue("i",bRes);
 	}
 
-// @pymethod int|PyDataObject|GetGlobalData|Returns the global data in the specified format as a string. None if not available.
 static PyObject *
-ui_data_object_get_global_data(PyObject *self, PyObject *args)
+PyDataObject_GetGlobalData(PyDataObject *self, PyObject *args)
 	{
 	int cf; 
 	if (!PyArg_ParseTuple(args,"i:GetGlobalData",&cf))
 		return NULL;
-
-	COleDataObject* pDO = ui_data_object::GetDataObject(self);
-	if (!pDO)return NULL;
-
 	CLIPFORMAT cfPrivate=(CLIPFORMAT)cf;
-	HGLOBAL hObjDesc = pDO->GetGlobalData(cfPrivate);
-	if(!hObjDesc) RETURN_NONE;
-;
+	HGLOBAL hObjDesc = self->m_pOleDataObject->GetGlobalData(cfPrivate);
+	if(!hObjDesc) 
+		{
+		Py_INCREF(Py_None);
+		return Py_None;
+		}
 	LPSTR lpClipMem=(LPSTR)GlobalLock(hObjDesc);
 	CString str(lpClipMem);
 	::GlobalUnlock(lpClipMem);
-
 	return Py_BuildValue("s",str);
 	}
 
 
+PyMethodDef PyDataObject::methods[] = {
+	{"IsDataAvailable", (PyCFunction)PyDataObject_IsDataAvailable, 1},
+	{"GetGlobalData", (PyCFunction)PyDataObject_GetGlobalData, 1},
+	{NULL, (PyCFunction)NULL, 0}		// sentinel
+};
 
-static struct PyMethodDef ui_data_object_methods[] = 
-	{
-	{"IsDataAvailable",ui_data_object_is_data_available,1},
-	{"GetGlobalData",ui_data_object_get_global_data,1},
-	{NULL,			NULL},
+PyTypeObject PyDataObject::type = {
+	PyObject_HEAD_INIT(&PyType_Type)
+	0,					// ob_size
+	"PyDataObject",			// tp_name
+	sizeof(PyDataObject),		// tp_basicsize
+	0,					// tp_itemsize
+	
+	// methods
+	(destructor)PyDataObject::dealloc,	// tp_dealloc
+	(printfunc)0,				// tp_print
+	(getattrfunc)PyDataObject::getattr,// tp_getattr
+	(setattrfunc)0,	// tp_setattr
+	(cmpfunc)0,		// tp_compare
+	(reprfunc)0,	// tp_repr
+	0,				// tp_as_number
+	0,				// tp_as_sequence
+	0,				// tp_as_mapping
+	(hashfunc)0,	// tp_hash
+	(ternaryfunc)0,	// tp_call
+	(reprfunc)0,	// tp_str
+
+	// Space for future expansion
+	0L,0L,0L,0L,
+
+	"PyDataObject Type" // Documentation string
 	};
 
 
-ui_type ui_data_object::type("PyDataObject", 
-						   &ui_assoc_object::type, 
-						   sizeof(ui_data_object), 
-						   ui_data_object_methods, 
-						   GET_PY_CTOR(ui_data_object));
