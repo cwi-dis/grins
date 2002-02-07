d18736f204b6933c6b430fb9897ab96c1fcf5ea5
Sjoerd Mullender <sjoerdmullender@users.noreply.github.com>
2002-02-07 10:03:40 +0000

    Detail.

--- mm/lib/wince/mediainterface.py
+++ mm/lib/wince/mediainterface.py
@@ -14,15 +14,11 @@ class Image:
 		return 24
 
 # this method should return an object with at least the above Image interface
-def get_image(filename, params = None):
+def get_image(filename, params = 0):
 	if is_dummy: return Image()
-	if params: dc = params
-	else: dc = 0
 	import wingdi
 	try:
-		img = wingdi.CreateDIBSurfaceFromFile(dc, filename)
+		return wingdi.CreateDIBSurfaceFromFile(params, filename)
 	except wingdi.error, arg:
 		print arg
 		return Image()
-	else:
-		return img
