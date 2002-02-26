3e10f56010fde11d40989da1629d272183cb74bb
Jack Jansen <Jack.Jansen@cwi.nl>
2002-02-26 15:26:53 +0000

    The Mouse0Release event didn't get the modifier keys as parameter. Now it does.

--- mm/lib/win32/win32dlview.py
+++ mm/lib/win32/win32dlview.py
@@ -611,7 +611,15 @@ class DisplayListView(docview.ScrollView, win32window.Window, DropTarget.DropTar
 		if self._tooltip:
 			self._tooltip.onLButtonUp(params)
 		msg=win32mu.Win32Msg(params)
-		self.onMouseEvent(msg.pos(),Mouse0Release)
+		# Find modifier keys. Code gleaned from the way Michael did this in the
+		# HierarchyView, I'm pretty sure there must be a better way to do this.
+		keystatus = params[2] # is a bunch of boolean flags for each key.
+		ctrlstatus = keystatus & win32con.MK_CONTROL # i.e. 0x8 or 0x0
+		if ctrlstatus:
+			params = 'add'
+		else:
+			params = None
+		self.onMouseEvent(msg.pos(),Mouse0Release, params)
 
 	# Response to right button down
 	def onRButtonDown(self, params):
