dccf9132ca51cbd0f82b4030888c58e1959fe810
Sjoerd Mullender <sjoerdmullender@users.noreply.github.com>
2002-02-26 16:43:14 +0000

    Version 2.2.

--- mm/editor/version.py
+++ mm/editor/version.py
@@ -3,8 +3,8 @@ import sys
 from patchlevel import patchlevel
 import features
 
-shortversion = 'grins2pro-%s-2.1'%sys.platform
-version = 'Pro for %s, v2.1 %s' % (features.compatibility, patchlevel)
+shortversion = 'grins2pro-%s-2.2'%sys.platform
+version = 'Pro for %s, v2.2 %s' % (features.compatibility, patchlevel)
 macpreffilename = 'GRiNS-pro-%s-2.0 Prefs' % features.compatibility_short
 
 title = 'GRiNS Editor'
