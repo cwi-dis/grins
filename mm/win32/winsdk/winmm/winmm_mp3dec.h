25cad2442d1d348053e152f5e3b2a838b5b5e127
anadelt <anadelt@localhost>
2002-03-07 17:58:39 +0000

    Add mp3 decoder. Experimental yet.

---
+++ mm/win32/winsdk/winmm/winmm_mp3dec.h
@@ -0,0 +1,11 @@
+#ifndef INC_WINMM_MP3DEC
+#define INC_WINMM_MP3DEC
+
+#ifndef Py_PYTHON_H
+#include "Python.h"
+#endif
+
+PyObject* Winmm_CreateMp3Decoder(PyObject *self, PyObject *args);
+
+#endif  // INC_WINMM_MP3DEC
+
