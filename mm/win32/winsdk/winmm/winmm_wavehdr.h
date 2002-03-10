ba53216680c4de5847bda5fe558f55e93a98200c
anadelt <anadelt@localhost>
2002-03-10 19:25:09 +0000

    Support streaming audio (and large files) playback. This support has nothing to do with audio type.

---
+++ mm/win32/winsdk/winmm/winmm_wavehdr.h
@@ -0,0 +1,11 @@
+#ifndef INC_WINMM_WAVEHDR
+#define INC_WINMM_WAVEHDR
+
+#ifndef Py_PYTHON_H
+#include "Python.h"
+#endif
+
+PyObject* Winmm_CreateWaveHdr(PyObject *self, PyObject *args);
+
+#endif  // INC_WINMM_WAVEHDR
+
