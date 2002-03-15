11e306785dbd60424cd8f9791f1a7b9a85205003
anadelt <anadelt@localhost>
2002-03-15 14:13:26 +0000

    Make this module type safe.
    Passing Py_None instead of type object would break this module
    (with conclusions:  "winmm does strange things with objects that are passed to it").

--- mm/win32/winsdk/winmm/winmm_wavehdr.cpp
+++ mm/win32/winsdk/winmm/winmm_wavehdr.cpp
@@ -13,44 +13,9 @@ Copyright 1991-2002 by Oratrix Development BV, Amsterdam, The Netherlands.
 
 #include "utils.h"
 
-struct PyWaveHdr
-	{
-	PyObject_HEAD
-	WAVEHDR *m_pWaveHdr;
-	PyObject *m_strObj;
+#include "winmm_wavehdr_impl.h"
+#include "winmm_waveout_impl.h"
 
-	static PyTypeObject type;
-	static PyMethodDef methods[];
-
-	static PyWaveHdr *createInstance(PyObject *strObj = NULL)
-		{
-		PyWaveHdr *instance = PyObject_NEW(PyWaveHdr, &type);
-		if (instance == NULL) return NULL;
-		Py_XINCREF(strObj);
-		instance->m_strObj = strObj;
-		instance->m_pWaveHdr = new WAVEHDR;
-		memset(instance->m_pWaveHdr, 0, sizeof(WAVEHDR));
-		instance->m_pWaveHdr->dwUser = DWORD(instance);
-		if(strObj != NULL)
-			{
-			instance->m_pWaveHdr->lpData = PyString_AS_STRING(strObj);
-			instance->m_pWaveHdr->dwBufferLength = PyString_GET_SIZE(strObj);
-			}
-		return instance;
-		}
-	static void dealloc(PyWaveHdr *p) 
-		{ 
-		if(p->m_pWaveHdr != 0) delete p->m_pWaveHdr;
-		Py_XDECREF(p->m_strObj);
-		PyMem_DEL(p);
-		}
-
-	static PyObject *getattr(PyWaveHdr *instance, char *name)
-		{ 
-		return Py_FindMethod(methods, (PyObject*)instance, name);
-		}
-
-	};
 
 PyObject* Winmm_CreateWaveHdr(PyObject *self, PyObject *args)
 	{
@@ -65,11 +30,10 @@ PyObject* Winmm_CreateWaveHdr(PyObject *self, PyObject *args)
 
 static PyObject* PyWaveHdr_PrepareHeader(PyWaveHdr *self, PyObject *args)
 {
-	PyObject *wavOutObj;
-	if (!PyArg_ParseTuple(args, "O",&wavOutObj))
+	PyWaveOut *wavOutObj;
+	if (!PyArg_ParseTuple(args, "O!", &PyWaveOut::type, &wavOutObj))
 		return NULL;
-	HWAVEOUT hWaveOut = (HWAVEOUT)GetObjHandle(wavOutObj);
-	MMRESULT mmres = waveOutPrepareHeader(hWaveOut, self->m_pWaveHdr, sizeof(WAVEHDR));
+	MMRESULT mmres = waveOutPrepareHeader(wavOutObj->m_hWaveOut, self->m_pWaveHdr, sizeof(WAVEHDR));
 	if(mmres != MMSYSERR_NOERROR)
 		{
 		seterror("waveOutPrepareHeader", mmres);
@@ -80,21 +44,13 @@ static PyObject* PyWaveHdr_PrepareHeader(PyWaveHdr *self, PyObject *args)
 
 static PyObject* PyWaveHdr_UnprepareHeader(PyWaveHdr *self, PyObject *args)
 {
-	PyObject *wavOutObj;
-	if (!PyArg_ParseTuple(args, "O",&wavOutObj))
+	PyWaveOut *wavOutObj;
+	if (!PyArg_ParseTuple(args, "O", &PyWaveOut::type, &wavOutObj))
 		return NULL;
-	HWAVEOUT hWaveOut = (HWAVEOUT)GetObjHandle(wavOutObj);
-	waveOutUnprepareHeader(hWaveOut, self->m_pWaveHdr, sizeof(WAVEHDR));
+	waveOutUnprepareHeader(wavOutObj->m_hWaveOut, self->m_pWaveHdr, sizeof(WAVEHDR));
 	return none(); 
 }
 
-static PyObject* PyWaveHdr_HasWaveHeader(PyWaveHdr *self, PyObject *args)
-{
-	DWORD p;
-	if (!PyArg_ParseTuple(args, "p", &p))
-		return NULL;
-	return Py_BuildValue("i", (p == DWORD(self->m_pWaveHdr)?1:0));
-}
 
 PyMethodDef PyWaveHdr::methods[] = {
 	{"PrepareHeader", (PyCFunction)PyWaveHdr_PrepareHeader, METH_VARARGS, ""},
