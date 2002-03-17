0facd1a42c4196c20b3d30df77b35e8bc6e72ed4
anadelt <anadelt@localhost>
2002-03-17 13:49:25 +0000

    Some more generic common classes that will be used by video playback modules.

---
+++ mm/win32/winsdk/common/video.h
@@ -0,0 +1,23 @@
+#ifndef INC_VIDEO
+#define INC_VIDEO
+
+#ifndef INC_SURFACE
+#include "surface.h"
+#endif
+
+class VideoPlayer
+	{
+	public:
+	virtual ~VideoPlayer() {}
+	virtual bool can_decode(int handle) = 0;
+	virtual void close() = 0;
+	virtual int get_width() const = 0;
+	virtual int get_height() const = 0;
+	virtual double get_duration() const = 0;
+	virtual void prepare_playback(surface<color_repr_t> *psurf) = 0;
+	virtual void suspend_playback() = 0;
+	virtual void resume_playback() = 0;
+	virtual bool finished_playback() = 0;
+	};
+
+#endif // INC_VIDEO
