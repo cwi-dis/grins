1f6de4b0729c9035cba7f7f4d7ad3446e0fa1480
anadelt <anadelt@localhost>
2002-03-18 21:26:06 +0000

    Implement get video duration for mpegs.

--- mm/lib/wince/VideoDuration.py
+++ mm/lib/wince/VideoDuration.py
@@ -1,4 +1,27 @@
 __version__ = "$Id$"
 
+import MMurl
+import winmm
+
 def get(url):
-	return 10
+	import urlcache
+	cache = urlcache.urlcache[url]
+	dur = cache.get('duration')
+	if dur is None:
+		mtype = urlcache.mimetype(url)
+		if mtype and mtype.find('mpeg') >= 0:
+			try:
+				fn = MMurl.urlretrieve(url)[0]
+			except IOError, arg:
+				print arg
+				dur = 10
+			else:	
+				try:
+					dur = winmm.GetVideoDuration(fn)
+				except winmm.error, msg:
+					print msg
+					dur = 10
+		if dur is None:
+			dur = 10
+	cache['duration'] = dur
+	return dur
