d45f46260c62bde8420660a3f28a9cb6bb39d677
Sjoerd Mullender <sjoerdmullender@users.noreply.github.com>
2002-03-20 15:00:02 +0000

    Implemented components chooser (not enabled in desktop player).

--- mm/win32/CE/GRiNS Player/resource.h
+++ mm/win32/CE/GRiNS Player/resource.h
@@ -30,13 +30,14 @@
 #define IDM_OPEN                        40016
 #define IDM_CLOSE                       40017
 #define IDM_SKIN                        40018
+#define IDM_COMPONENTS                  40019
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        107
-#define _APS_NEXT_COMMAND_VALUE         40019
+#define _APS_NEXT_COMMAND_VALUE         40020
 #define _APS_NEXT_CONTROL_VALUE         1007
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
