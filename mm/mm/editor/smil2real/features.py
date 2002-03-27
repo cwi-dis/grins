824c1c73aa308b067bc9dc61168cced6b0603353
Sjoerd Mullender <sjoerdmullender@users.noreply.github.com>
2002-03-27 10:17:53 +0000

    Added Digital River buy URL.

---
+++ mm/mm/editor/smil2real/features.py
@@ -0,0 +1,40 @@
+__version__ = "$Id$"
+
+# Features for GRiNS for G2, pro version
+
+from compatibility import *
+from FeatureSet import *
+import sys
+
+feature_set = [
+	ADVANCED_PROPERTIES,
+	ASSETS_VIEW,
+	CUSTOM_REGIONS,
+	EXPORT_REAL,
+	HYPERLINKS_VIEW,
+	H_MODIFY_STRUCTURE,
+	H_ASSIGN_NAME,
+	LAYOUT_VIEW,
+	PLAYER_VIEW,
+	SOURCE_VIEW,
+	STRUCTURE_VIEW,
+	TRANSITION_VIEW,
+	UNIFIED_FOCUS,
+	AUTO_EVALUATE,
+	SHOW_MEDIA_CHILDREN,
+	UNSUPPORTED_ERROR,
+	]
+
+version = 'Real'
+compatibility = Boston
+compatibility_short = 'SMIL2Real'
+cmif = 0
+lightweight = 0
+editor = 1
+license_features_needed = ('smil2real', sys.platform)
+
+auto_evaluate_period = 7	# 7 days auto-evaluate
+
+# URL to be used for buying the product
+buyurl = 'http://www.realnetworks.com/special/partners/upsell.html?act=orx' # via Real
+##buyurl = 'http://www.digitalriver.com/dr/v2/ec_main.entry17c?cid=0&sid=39552&sp=10007&pn=21&pid=885783' # via DigitalRiver
---
+++ mm/mm/grins/smil20/features.py
@@ -0,0 +1,25 @@
+__version__ = "$Id$"
+
+# Features for GRiNS for G2, pro version
+
+from compatibility import *
+import sys
+
+#
+compatibility = Boston
+compatibility_short = 'SMIL2'
+cmif = 0
+lightweight = 0
+editor = 0
+# XXX do we require sys.platform?
+license_features_needed = ('smil2player', sys.platform)
+# Expiry date not relevant anymore since the Player requires a license.
+# On Windows in the frozen version, we get a crash when the player
+# tries to exit because it expired.
+#expiry_date = (2001, 1, 15)
+
+# RTIPA start
+RTIPA = 0
+# RTIPA end
+
+buyurl = 'http://www.digitalriver.com/dr/v2/ec_main.entry17c?cid=0&sid=39552&sp=10007&pn=21&pid=885784'
