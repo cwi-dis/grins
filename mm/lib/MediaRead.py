acdaf3003144616d70c46d37d2e4b6d0848d12ff
Sjoerd Mullender <sjoerdmullender@users.noreply.github.com>
2002-04-03 20:46:48 +0000

    All Real media has intrinsic duration, so don't say dur="indefinite". nameencode() already provides quotes, so don't add extra.

--- mm/lib/MediaRead.py
+++ mm/lib/MediaRead.py
@@ -7,14 +7,15 @@ from Duration import getintrinsicduration
 mediaWrapper = """\
 <smil>
   <body>
-    <ref%s src="%s"/>
+    <ref%s src=%s/>
   </body>
 </smil>"""
 
 def MediaRead(filename, mtype, printfunc):
 	if mtype is None or \
 		(mtype[:6] != 'audio/' and
-		mtype[:6] != 'video/'):
+		 mtype[:6] != 'video/' and
+		 mtype.find('real') < 0):
 		dur = ' dur="indefinite"'
 	else:
 		dur = ''
