99e9545cb688e38a64586bc5bff4d154204ce7bb
Jack Jansen <Jack.Jansen@cwi.nl>
2003-03-25 14:27:09 +0000

    Various bits of mac code are also valid for darwin.

---
+++ mm/lib/VideoDuration.py
@@ -0,0 +1,17 @@
+__version__ = "$Id$"
+
+from sys import platform
+
+if platform == 'irix6':
+	try:
+		from SGIVideoDuration import *
+	except ImportError:
+		from MPEGVideoDuration import *
+elif platform == 'sunos5' or platform == 'irix5' or platform[:5] == 'linux':
+	from MPEGVideoDuration import *
+elif platform in ('mac', 'darwin'):
+	from MACVideoDuration import *
+elif platform == 'nt' or platform == 'win32':
+	from NTVideoDuration import *
+else:
+	raise ImportError('No appropriate VideoDuration for this platform')
