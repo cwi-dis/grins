18f14a19af3bc412acbe57347a9da58848300e2c
Sjoerd Mullender <sjoerdmullender@users.noreply.github.com>
2008-02-08 14:59:54 +0000

    Use more modern (and consistent) indentation.

--- mm/pytools/grinsdb/src/grcheckdb.py
+++ mm/pytools/grinsdb/src/grcheckdb.py
@@ -16,107 +16,107 @@ KEYS=['email']
 Error="grcheckdb.Error"
 
 def main():
-	if len(sys.argv) > 1:
-		print "Usage checkdb"
-		sys.exit(1)
-	dbase = grinsdb.Database(indexed=0)
-	index = grinsdb.Index(keys=KEYS)
-	try:
-		allids = dbase.search(None, None)
-		emaildict = {}
-		badids = []
-		print 'Checking email addresses and creating index',
-		count = 0
-		for id in allids:
-			count = count + 1
-			if count % 100 == 0:
-				sys.stdout.write('.')
-				sys.stdout.flush()
-			obj = dbase.open(id)
-			index.update(id, obj)
-			if not obj.has_key('email'):
-				badids.append(id)
-				continue
-			email = obj['email']
-			if emaildict.has_key(email):
-				emaildict[email].append(id)
-			else:
-				emaildict[email] = [id]
-		sys.stdout.write('\n')
-		index.close()
-		del index
-		if badids:
-			print 'Bad messages:',
-			for id in badids:
-				print id,
-				obj = dbase.open(id, 'w')
-				dbase.remove(obj)
-			print
-		for email in emaildict.keys():
-			if len(emaildict[email]) > 1:
-				print 'Multiple:', email,
-				for i in emaildict[email]:
-					print i,
-				print
+    if len(sys.argv) > 1:
+        print "Usage checkdb"
+        sys.exit(1)
+    dbase = grinsdb.Database(indexed=0)
+    index = grinsdb.Index(keys=KEYS)
+    try:
+        allids = dbase.search(None, None)
+        emaildict = {}
+        badids = []
+        print 'Checking email addresses and creating index',
+        count = 0
+        for id in allids:
+            count = count + 1
+            if count % 100 == 0:
+                sys.stdout.write('.')
+                sys.stdout.flush()
+            obj = dbase.open(id)
+            index.update(id, obj)
+            if not obj.has_key('email'):
+                badids.append(id)
+                continue
+            email = obj['email']
+            if emaildict.has_key(email):
+                emaildict[email].append(id)
+            else:
+                emaildict[email] = [id]
+        sys.stdout.write('\n')
+        index.close()
+        del index
+        if badids:
+            print 'Bad messages:',
+            for id in badids:
+                print id,
+                obj = dbase.open(id, 'w')
+                dbase.remove(obj)
+            print
+        for email in emaildict.keys():
+            if len(emaildict[email]) > 1:
+                print 'Multiple:', email,
+                for i in emaildict[email]:
+                    print i,
+                print
 
-		genfiles(dbase)
-	finally:
-		dbase.close()
+        genfiles(dbase)
+    finally:
+        dbase.close()
 
 def myopen(filename):
-	dirname = os.path.join(grinsdb.DATABASE, 'data')
-	return open(os.path.join(dirname, filename + '.NEW'), 'w')
+    dirname = os.path.join(grinsdb.DATABASE, 'data')
+    return open(os.path.join(dirname, filename + '.NEW'), 'w')
 
 def myinstall(filename):
-	dirname = os.path.join(grinsdb.DATABASE, 'data')
-	name =  os.path.join(dirname, filename)
-	os.rename(name + '.NEW', name)
-	
+    dirname = os.path.join(grinsdb.DATABASE, 'data')
+    name =  os.path.join(dirname, filename)
+    os.rename(name + '.NEW', name)
+
 def genfiles(dbase):
-	passwd_fp = myopen('htpasswd')
-	news_fp = myopen('grins-news')
-	edit_fp = myopen('grins-edit-news')
-	
-	allids = dbase.search(None, None)
-	print 'Generating password and mailinglist files',
-	count = 0
-	for id in allids:
-		count = count + 1
-		if count % 100 == 0:
-			sys.stdout.write('.')
-			sys.stdout.flush()		
-		obj = dbase.open(id)
-		if not obj.has_key('email'):
-			print 'Missing email:', id
-			continue
-		if not obj.has_key('password'):
-			print 'Missing password:', id
-			continue
-		user = obj['email']
-		clear = obj['password']
-		crypted = crypt_passwd(clear)
-		add_passwd(passwd_fp, user, crypted)
-		if obj.has_key('want-maillist') and \
-		   obj['want-maillist'] == 'yes':
-			news_fp.write(user+'\n')
-		if obj.has_key('want-editor') and \
-		   obj['want-editor'] == 'yes':
-			edit_fp.write(user+'\n')
-	sys.stdout.write('\n')
-	passwd_fp.close()
-	news_fp.close()
-	edit_fp.close()
-	myinstall('htpasswd')
-	myinstall('grins-news')
-	myinstall('grins-edit-news')
+    passwd_fp = myopen('htpasswd')
+    news_fp = myopen('grins-news')
+    edit_fp = myopen('grins-edit-news')
+
+    allids = dbase.search(None, None)
+    print 'Generating password and mailinglist files',
+    count = 0
+    for id in allids:
+        count = count + 1
+        if count % 100 == 0:
+            sys.stdout.write('.')
+            sys.stdout.flush()
+        obj = dbase.open(id)
+        if not obj.has_key('email'):
+            print 'Missing email:', id
+            continue
+        if not obj.has_key('password'):
+            print 'Missing password:', id
+            continue
+        user = obj['email']
+        clear = obj['password']
+        crypted = crypt_passwd(clear)
+        add_passwd(passwd_fp, user, crypted)
+        if obj.has_key('want-maillist') and \
+           obj['want-maillist'] == 'yes':
+            news_fp.write(user+'\n')
+        if obj.has_key('want-editor') and \
+           obj['want-editor'] == 'yes':
+            edit_fp.write(user+'\n')
+    sys.stdout.write('\n')
+    passwd_fp.close()
+    news_fp.close()
+    edit_fp.close()
+    myinstall('htpasswd')
+    myinstall('grins-news')
+    myinstall('grins-edit-news')
 
 def crypt_passwd(clear):
-	salt = grpasswd.invent_passwd(2)
-	return crypt.crypt(clear, salt)
+    salt = grpasswd.invent_passwd(2)
+    return crypt.crypt(clear, salt)
 
 def add_passwd(fp, user, passwd):
-	if ':' in user or '\n' in user or ':' in passwd or '\n' in passwd:
-		raise Error, "Illegal username/password"
-	fp.write('%s:%s\n'%(user, passwd))
-	
+    if ':' in user or '\n' in user or ':' in passwd or '\n' in passwd:
+        raise Error, "Illegal username/password"
+    fp.write('%s:%s\n'%(user, passwd))
+
 main()
--- mm/pytools/grinsdb/src/grevalstat.py
+++ mm/pytools/grinsdb/src/grevalstat.py
@@ -10,40 +10,40 @@ import grinsdb
 import grpasswd
 
 MONTHS={'jan':1, 'feb':2, 'mar':3, 'apr':4, 'may':5, 'jun':6, 'jul':7,
-	'aug':8, 'sep':9, 'oct':10, 'nov':11, 'dec':12}
+        'aug':8, 'sep':9, 'oct':10, 'nov':11, 'dec':12}
 
 Error="grcheckdb.Error"
 
 def main():
-	if len(sys.argv) > 1:
-		print "Usage grevalstat"
-		sys.exit(1)
-	try:
-		dbase = grinsdb.Database()
-		allids = dbase.search(None, None)
-		ddict = {}
-		for id in allids:
-			obj = dbase.open(id)
-			if not obj.has_key('eval-license-req'):
-				continue
-			dates = obj['eval-license-req']
-			date = string.strip(string.split(dates, ',')[-1])
-			pdate = parsedate(date)
-			if ddict.has_key(pdate):
-				ddict[pdate] = ddict[pdate] + 1
-			else:
-				ddict[pdate] = 1
-		pdates = ddict.keys()
-		pdates.sort()
-		pdates.reverse()
-		for pdate in pdates:
-			print pdate[3], ddict[pdate]
-	finally:
-		dbase.close()
+    if len(sys.argv) > 1:
+        print "Usage grevalstat"
+        sys.exit(1)
+    try:
+        dbase = grinsdb.Database()
+        allids = dbase.search(None, None)
+        ddict = {}
+        for id in allids:
+            obj = dbase.open(id)
+            if not obj.has_key('eval-license-req'):
+                continue
+            dates = obj['eval-license-req']
+            date = string.strip(string.split(dates, ',')[-1])
+            pdate = parsedate(date)
+            if ddict.has_key(pdate):
+                ddict[pdate] = ddict[pdate] + 1
+            else:
+                ddict[pdate] = 1
+        pdates = ddict.keys()
+        pdates.sort()
+        pdates.reverse()
+        for pdate in pdates:
+            print pdate[3], ddict[pdate]
+    finally:
+        dbase.close()
 
 def parsedate(date):
-	[dd, mmstr, yy] = string.split(date, '-')
-	mm = MONTHS[string.lower(mmstr)]
-	return (yy, mm, dd, date)
-	
+    [dd, mmstr, yy] = string.split(date, '-')
+    mm = MONTHS[string.lower(mmstr)]
+    return (yy, mm, dd, date)
+
 main()
