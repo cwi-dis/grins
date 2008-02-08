6355f7588ba3f320a805ac31ee5967428eba8f82
Sjoerd Mullender <sjoerdmullender@users.noreply.github.com>
2008-02-08 14:59:56 +0000

    Use more modern (and consistent) indentation.

--- mm/win32/CE/GRiNS Player/freeze/startup.py
+++ mm/win32/CE/GRiNS Player/freeze/startup.py
@@ -5,12 +5,11 @@ sys.path[:] = [r'\Program Files\GRiNS\bin\wince']
 
 # Global Application Objects
 if __name__ == '__main__':
-	toplevel = None
-	resdll = None
+    toplevel = None
+    resdll = None
 else:
-	import __main__
-	__main__.toplevel = None
-	__main__.resdll = None
-	
-import grinsNL_CE	
+    import __main__
+    __main__.toplevel = None
+    __main__.resdll = None
 
+import grinsNL_CE
