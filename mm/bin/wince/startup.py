e8c3e0646236bcf2cc2b9f102d1325b504dd0ca2
Sjoerd Mullender <sjoerdmullender@users.noreply.github.com>
2008-02-08 14:59:30 +0000

    Use more modern (and consistent) indentation.

--- mm/bin/wince/startup.py
+++ mm/bin/wince/startup.py
@@ -1,14 +1,14 @@
 
 # Global Application Objects
 if __name__ == '__main__':
-	toplevel = None
-	resdll = None
+    toplevel = None
+    resdll = None
 else:
-	import __main__
-	__main__.toplevel = None
-	__main__.resdll = None
-	
-##################### Main Script
+    import __main__
+    __main__.toplevel = None
+    __main__.resdll = None
+
+## ################### Main Script
 import os
 import sys
 
@@ -18,23 +18,22 @@ GRINSDIR = r'\Program Files\GRiNS'
 specificPath = "grins"
 
 GRINSPATH = [
-	os.path.join(GRINSDIR, r'lib\wince'), # override folder
-#	os.path.join(GRINSDIR, r'bin\wince'), # bin (set by exe)
+        os.path.join(GRINSDIR, r'lib\wince'), # override folder
+#       os.path.join(GRINSDIR, r'bin\wince'), # bin (set by exe)
 
-	os.path.join(GRINSDIR, r'%s\smil20\win32' % specificPath),
-	os.path.join(GRINSDIR, r'%s\smil20' % specificPath),
-	os.path.join(GRINSDIR, r'%s\wince' % specificPath),
-	os.path.join(GRINSDIR, r'%s\win32' % specificPath),
-	os.path.join(GRINSDIR, r'%s' % specificPath),
+        os.path.join(GRINSDIR, r'%s\smil20\win32' % specificPath),
+        os.path.join(GRINSDIR, r'%s\smil20' % specificPath),
+        os.path.join(GRINSDIR, r'%s\wince' % specificPath),
+        os.path.join(GRINSDIR, r'%s\win32' % specificPath),
+        os.path.join(GRINSDIR, r'%s' % specificPath),
 
-	os.path.join(GRINSDIR, r'common\wince'),
+        os.path.join(GRINSDIR, r'common\wince'),
 
-	os.path.join(GRINSDIR, r'common'),
-	os.path.join(GRINSDIR, r'lib'),
-	os.path.join(GRINSDIR, r'pylib'),
+        os.path.join(GRINSDIR, r'common'),
+        os.path.join(GRINSDIR, r'lib'),
+        os.path.join(GRINSDIR, r'pylib'),
 ]
 
 sys.path[0:0] = GRINSPATH
 
-import grinsNL_CE	
-
+import grinsNL_CE
