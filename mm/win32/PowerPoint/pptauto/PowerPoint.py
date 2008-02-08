f5e3e385ee99f615a3031f634d001af6c74233b5
Sjoerd Mullender <sjoerdmullender@users.noreply.github.com>
2008-02-08 14:59:56 +0000

    Use more modern (and consistent) indentation.

--- mm/win32/PowerPoint/pptauto/PowerPoint.py
+++ mm/win32/PowerPoint/pptauto/PowerPoint.py
@@ -1,21 +1,20 @@
 
 def ExportPowerPoint(filename, outdir = None):
-	import pptauto
-	try:
-		if outdir:
-			nslides = pptauto.Export(filename, outdir)
-		else:
-			nslides = pptauto.Export(filename)
-	except pptauto.error, arg:
-		print arg
-		return -1
-	else:
-		return nslides
-		
+    import pptauto
+    try:
+        if outdir:
+            nslides = pptauto.Export(filename, outdir)
+        else:
+            nslides = pptauto.Export(filename)
+    except pptauto.error, arg:
+        print arg
+        return -1
+    else:
+        return nslides
+
 
 if __name__ == '__main__':
-	filename = r'D:\ufs\mm\cmif\win32\PowerPoint\testdata\test1.ppt'
-	nslides = ExportPowerPoint(filename)
-	if nslides >= 0:
-		print nslides, 'slides exported'
-		
+    filename = r'D:\ufs\mm\cmif\win32\PowerPoint\testdata\test1.ppt'
+    nslides = ExportPowerPoint(filename)
+    if nslides >= 0:
+        print nslides, 'slides exported'
