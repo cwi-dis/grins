40dfa9666a1b981dc9fc594a6ff2a47db6035103
Sjoerd Mullender <sjoerdmullender@users.noreply.github.com>
2008-02-08 14:59:41 +0000

    Use more modern (and consistent) indentation.

--- mm/editor/win32/ToolbarTemplate.py
+++ mm/editor/win32/ToolbarTemplate.py
@@ -43,129 +43,129 @@ IDW_TOOLBAR_CONTAINERS=0xe804
 IDW_TOOLBAR_PLAYER_PANEL = 0xe805
 
 class Button:
-	def __init__(self, cmdid, iconindex):
-		self.type = 'button'
-		self.cmdid = cmdid
-		self.arg = iconindex
+    def __init__(self, cmdid, iconindex):
+        self.type = 'button'
+        self.cmdid = cmdid
+        self.arg = iconindex
 
 class Separator:
-	def __init__(self, width):
-		self.type = 'separator'
-		self.cmdid = None
-		self.width = width
+    def __init__(self, width):
+        self.type = 'separator'
+        self.cmdid = None
+        self.width = width
 
 class Pulldown:
-	def __init__(self, name, width=None):
-		self.type = 'pulldown'
-		self.name = name
-		self.width = width
+    def __init__(self, name, width=None):
+        self.type = 'pulldown'
+        self.name = name
+        self.width = width
 
 GENERAL_TEMPLATE = (
-	('General', 'toolbar', 'docked', wndusercmd.TOOLBAR_GENERAL, IDW_TOOLBAR_GENERAL, grinsRC.IDR_TB_COMMON, 0, (
-		Button(usercmd.NEW_DOCUMENT, TBICON_NEW),
-		Separator(6),
-		Button(usercmd.OPENFILE, TBICON_OPENFILE),
-		Button(usercmd.SAVE, TBICON_SAVE),
-		Separator(6),
-		Button(usercmd.RESTORE, TBICON_RESTORE),
-		Button(usercmd.CLOSE, TBICON_CLOSE),
-		Button(wndusercmd.CLOSE_ACTIVE_WINDOW, TBICON_CLOSE_WINDOW),
-		Separator(6),
-		Button(usercmd.ZOOMIN, TBICON_ZOOMIN),
-		Button(usercmd.ZOOMOUT, TBICON_ZOOMOUT),
-		Button(usercmd.ZOOMRESET, TBICON_ZOOMRESET),
-		Separator(6),
-		Button(usercmd.HELP, TBICON_HELP),
-		)
-	)
+        ('General', 'toolbar', 'docked', wndusercmd.TOOLBAR_GENERAL, IDW_TOOLBAR_GENERAL, grinsRC.IDR_TB_COMMON, 0, (
+                Button(usercmd.NEW_DOCUMENT, TBICON_NEW),
+                Separator(6),
+                Button(usercmd.OPENFILE, TBICON_OPENFILE),
+                Button(usercmd.SAVE, TBICON_SAVE),
+                Separator(6),
+                Button(usercmd.RESTORE, TBICON_RESTORE),
+                Button(usercmd.CLOSE, TBICON_CLOSE),
+                Button(wndusercmd.CLOSE_ACTIVE_WINDOW, TBICON_CLOSE_WINDOW),
+                Separator(6),
+                Button(usercmd.ZOOMIN, TBICON_ZOOMIN),
+                Button(usercmd.ZOOMOUT, TBICON_ZOOMOUT),
+                Button(usercmd.ZOOMRESET, TBICON_ZOOMRESET),
+                Separator(6),
+                Button(usercmd.HELP, TBICON_HELP),
+                )
+        )
 )
 
 FRAME_TEMPLATE = (
-	('General', 'toolbar', 'docked', wndusercmd.TOOLBAR_GENERAL, IDW_TOOLBAR_GENERAL, grinsRC.IDR_TB_COMMON, 0, (
-		Button(usercmd.NEW_DOCUMENT, TBICON_NEW),
-		Separator(6),
-		Button(usercmd.OPENFILE, TBICON_OPENFILE),
-		Button(usercmd.SAVE, TBICON_SAVE),
-		)
-	)
+        ('General', 'toolbar', 'docked', wndusercmd.TOOLBAR_GENERAL, IDW_TOOLBAR_GENERAL, grinsRC.IDR_TB_COMMON, 0, (
+                Button(usercmd.NEW_DOCUMENT, TBICON_NEW),
+                Separator(6),
+                Button(usercmd.OPENFILE, TBICON_OPENFILE),
+                Button(usercmd.SAVE, TBICON_SAVE),
+                )
+        )
 )
-##PLAYER_TEMPLATE = (
-##	('Player Controls', 'toolbar', 'docked', wndusercmd.TOOLBAR_PLAYER, IDW_TOOLBAR_PLAYER, grinsRC.IDR_TB_COMMON, 0, (
-##		Button(usercmd.PLAY, TBICON_PLAY),
-##		Button(usercmd.PAUSE, TBICON_PAUSE),
-##		Button(usercmd.STOP, TBICON_STOP),
-##		)
-##	)
-##)
+## PLAYER_TEMPLATE = (
+##     ('Player Controls', 'toolbar', 'docked', wndusercmd.TOOLBAR_PLAYER, IDW_TOOLBAR_PLAYER, grinsRC.IDR_TB_COMMON, 0, (
+##         Button(usercmd.PLAY, TBICON_PLAY),
+##         Button(usercmd.PAUSE, TBICON_PAUSE),
+##         Button(usercmd.STOP, TBICON_STOP),
+##         )
+##     )
+## )
 
 buttonlist = [
-	Button(usercmd.CREATEANCHOREXTENDED, TBICON_CREATE_ANCHOREXTENDED),
-	Separator(6),
-	Button(usercmd.CREATE_EVENT_SOURCE, TBICON_EVENT_SOURCE),
-	Button(usercmd.CREATE_BEGIN_EVENT, TBICON_BEGIN_EVENT),
-	Button(usercmd.CREATE_END_EVENT, TBICON_END_EVENT),
-	]
+        Button(usercmd.CREATEANCHOREXTENDED, TBICON_CREATE_ANCHOREXTENDED),
+        Separator(6),
+        Button(usercmd.CREATE_EVENT_SOURCE, TBICON_EVENT_SOURCE),
+        Button(usercmd.CREATE_BEGIN_EVENT, TBICON_BEGIN_EVENT),
+        Button(usercmd.CREATE_END_EVENT, TBICON_END_EVENT),
+        ]
 if features.EXPORT_REAL in features.feature_set:
-	# add animate button between media and brush
-	buttonlist.insert(1, Button(usercmd.CREATEANCHOR_CONTEXT, TBICON_CREATE_ANCHOR_CONTEXT))
-	buttonlist.insert(2, Button(usercmd.CREATEANCHOR_BROWSER, TBICON_CREATE_ANCHOR_BROWSER))
+    # add animate button between media and brush
+    buttonlist.insert(1, Button(usercmd.CREATEANCHOR_CONTEXT, TBICON_CREATE_ANCHOR_CONTEXT))
+    buttonlist.insert(2, Button(usercmd.CREATEANCHOR_BROWSER, TBICON_CREATE_ANCHOR_BROWSER))
 if features.INTERNAL_LINKS in features.feature_set:
-	buttonlist.insert(0, Button(usercmd.CREATEANCHOR, TBICON_CREATE_ANCHOR))
-	buttonlist.insert(1, Button(usercmd.FINISH_LINK, TBICON_FINISH_LINK))
-	buttonlist.insert(2, Separator(6))
+    buttonlist.insert(0, Button(usercmd.CREATEANCHOR, TBICON_CREATE_ANCHOR))
+    buttonlist.insert(1, Button(usercmd.FINISH_LINK, TBICON_FINISH_LINK))
+    buttonlist.insert(2, Separator(6))
 
 LINKING_TEMPLATE = (
-	('Linking and Timing', 'toolbar', 'docked', wndusercmd.TOOLBAR_LINKING, IDW_TOOLBAR_LINKING, grinsRC.IDR_TB_EDITOR, 0, tuple(buttonlist)
-	)
+        ('Linking and Timing', 'toolbar', 'docked', wndusercmd.TOOLBAR_LINKING, IDW_TOOLBAR_LINKING, grinsRC.IDR_TB_EDITOR, 0, tuple(buttonlist)
+        )
 )
 
 
 ALIGN_TEMPLATE = (
-	('Region alignment', 'toolbar', 'docked', wndusercmd.TOOLBAR_ALIGNMENT, IDW_TOOLBAR_ALIGNMENT, grinsRC.IDR_TB_EDITOR, 0, (
-		Button(usercmd.ALIGN_LEFT, TBICON_ALIGN_LEFT),
-		Button(usercmd.ALIGN_CENTER, TBICON_ALIGN_VERTICAL),
-		Button(usercmd.ALIGN_RIGHT, TBICON_ALIGN_RIGHT),
-		Separator(6),
-		Button(usercmd.ALIGN_TOP, TBICON_ALIGN_TOP),
-		Button(usercmd.ALIGN_MIDDLE, TBICON_ALIGN_HORIZONTAL),
-		Button(usercmd.ALIGN_BOTTOM, TBICON_ALIGN_BOTTOM),
-		Separator(6),
-		Button(usercmd.DISTRIBUTE_HORIZONTALLY, TBICON_DISTRIBUTE_HORIZONTAL),
-		Button(usercmd.DISTRIBUTE_VERTICALLY, TBICON_DISTRIBUTE_VERTICAL),
-		)
-	)
+        ('Region alignment', 'toolbar', 'docked', wndusercmd.TOOLBAR_ALIGNMENT, IDW_TOOLBAR_ALIGNMENT, grinsRC.IDR_TB_EDITOR, 0, (
+                Button(usercmd.ALIGN_LEFT, TBICON_ALIGN_LEFT),
+                Button(usercmd.ALIGN_CENTER, TBICON_ALIGN_VERTICAL),
+                Button(usercmd.ALIGN_RIGHT, TBICON_ALIGN_RIGHT),
+                Separator(6),
+                Button(usercmd.ALIGN_TOP, TBICON_ALIGN_TOP),
+                Button(usercmd.ALIGN_MIDDLE, TBICON_ALIGN_HORIZONTAL),
+                Button(usercmd.ALIGN_BOTTOM, TBICON_ALIGN_BOTTOM),
+                Separator(6),
+                Button(usercmd.DISTRIBUTE_HORIZONTALLY, TBICON_DISTRIBUTE_HORIZONTAL),
+                Button(usercmd.DISTRIBUTE_VERTICALLY, TBICON_DISTRIBUTE_VERTICAL),
+                )
+        )
 )
 
 buttonlist = [
-	Button(usercmd.DRAG_PAR, TBICON_PAR),
-	Button(usercmd.DRAG_SEQ, TBICON_SEQ),
-	Button(usercmd.DRAG_SWITCH, TBICON_SWITCH),
-	Button(usercmd.DRAG_EXCL, TBICON_EXCL),
-	Button(usercmd.DRAG_PRIO, TBICON_PRIO),
-	Separator(6),
-	Button(usercmd.DRAG_MEDIA, TBICON_MEDIA),
-	Button(usercmd.DRAG_BRUSH, TBICON_BRUSH),
-	Separator(6),
-	Button(usercmd.DRAG_TOPLAYOUT, TBICON_TOPLAYOUT),
-	Button(usercmd.DRAG_REGION, TBICON_REGION),
-	]
+        Button(usercmd.DRAG_PAR, TBICON_PAR),
+        Button(usercmd.DRAG_SEQ, TBICON_SEQ),
+        Button(usercmd.DRAG_SWITCH, TBICON_SWITCH),
+        Button(usercmd.DRAG_EXCL, TBICON_EXCL),
+        Button(usercmd.DRAG_PRIO, TBICON_PRIO),
+        Separator(6),
+        Button(usercmd.DRAG_MEDIA, TBICON_MEDIA),
+        Button(usercmd.DRAG_BRUSH, TBICON_BRUSH),
+        Separator(6),
+        Button(usercmd.DRAG_TOPLAYOUT, TBICON_TOPLAYOUT),
+        Button(usercmd.DRAG_REGION, TBICON_REGION),
+        ]
 if features.ANIMATE in features.feature_set:
-	# add animate button between media and brush
-	buttonlist.insert(7, Button(usercmd.DRAG_ANIMATE, TBICON_ANIMATE))
+    # add animate button between media and brush
+    buttonlist.insert(7, Button(usercmd.DRAG_ANIMATE, TBICON_ANIMATE))
 
 CONTAINERS_TEMPLATE = (
-	('Containers', 'toolbar', 'docked', wndusercmd.TOOLBAR_CONTAINERS, IDW_TOOLBAR_CONTAINERS, grinsRC.IDR_TB_EDITOR, 1, tuple(buttonlist)
-	)
+        ('Containers', 'toolbar', 'docked', wndusercmd.TOOLBAR_CONTAINERS, IDW_TOOLBAR_CONTAINERS, grinsRC.IDR_TB_EDITOR, 1, tuple(buttonlist)
+        )
 )
 del buttonlist
 
 TOOLBARS=[
-	GENERAL_TEMPLATE,
-##	PLAYER_TEMPLATE,
-	CONTAINERS_TEMPLATE,
-	LINKING_TEMPLATE,
+        GENERAL_TEMPLATE,
+##     PLAYER_TEMPLATE,
+        CONTAINERS_TEMPLATE,
+        LINKING_TEMPLATE,
 ]
 if features.ALIGNTOOL in features.feature_set:
-	TOOLBARS.append(ALIGN_TEMPLATE)
+    TOOLBARS.append(ALIGN_TEMPLATE)
 
 TOOLBARS.reverse()  # For now...
--- mm/editor/win32/TopLevelDialog.py
+++ mm/editor/win32/TopLevelDialog.py
@@ -13,90 +13,90 @@ from wndusercmd import *
 # can display its various views and its source
 
 class TopLevelDialog:
-	def __init__(self):
-		pass
-	
-	def set_commandlist(self):
-		self.viewcommandlist = self.viewcommandlist + [
-			PAUSE(callback = (self.pause_callback, ())),
-			STOP(callback = (self.stop_callback, ())),
-			]
-		if __debug__:
-			self.commandlist.append(SCHEDDUMP(callback = (self.__dump, ())))
-	
-	if __debug__:
-		def __dump(self):
-			self.player.scheduler.dump()
+    def __init__(self):
+        pass
 
-	def show(self):
-		if self.window is not None:
-			return
-		
-		dict = self.getsettingsdict()
+    def set_commandlist(self):
+        self.viewcommandlist = self.viewcommandlist + [
+                PAUSE(callback = (self.pause_callback, ())),
+                STOP(callback = (self.stop_callback, ())),
+                ]
+        if __debug__:
+            self.commandlist.append(SCHEDDUMP(callback = (self.__dump, ())))
 
-		adornments = {
-			'pulldown': dict
-		}
+    if __debug__:
+        def __dump(self):
+            self.player.scheduler.dump()
 
-		self.window = windowinterface.newdocument(self, 
-			adornments = adornments,commandlist = self.commandlist)
-		import Player
-		self.setplayerstate(Player.STOPPED)
+    def show(self):
+        if self.window is not None:
+            return
 
-	def hide(self):
-		if self.window is None:
-			return
-		self.window.close()
-		self.window = None
+        dict = self.getsettingsdict()
 
-	def setbuttonstate(self, command, showing):
-		self.window.set_toggle(command, showing)
+        adornments = {
+                'pulldown': dict
+        }
 
-	def setplayerstate(self, state):
-		self.window.setplayerstate(state)
-		self.update_undocommandlist()
+        self.window = windowinterface.newdocument(self,
+                adornments = adornments,commandlist = self.commandlist)
+        import Player
+        self.setplayerstate(Player.STOPPED)
 
-	def setsettingsdict(self, dict):
-		if self.window:
-			self.window.settoolbarpulldowns(dict)
+    def hide(self):
+        if self.window is None:
+            return
+        self.window.close()
+        self.window = None
 
-	def showsource(self, source = None, optional=0, readonly = 0):
-		if source is None:
-			if self.source is not None:
-				self.source.close()
-				self.source = None
-		else:
-			if self.source is not None:
-				self.source.settext(source)
-			else:
-				self.source = self.window.textwindow(source, readonly=1)
-				self.source.set_mother(self)
+    def setbuttonstate(self, command, showing):
+        self.window.set_toggle(command, showing)
 
-	# doesn't seem to work
-	# kk: you must pass a context string as a second arg
-	def setcommands(self, commandlist):
-		self.window.set_commandlist(commandlist,'document')
+    def setplayerstate(self, state):
+        self.window.setplayerstate(state)
+        self.update_undocommandlist()
 
-	def do_edit(self, tmp):
-		import os
+    def setsettingsdict(self, dict):
+        if self.window:
+            self.window.settoolbarpulldowns(dict)
 
-		# use only notepad for now
-		editor='Notepad'
-		stat1 = os.stat(tmp)
-		import win32api,win32con
-		try:
-			win32api.WinExec('%s %s' % (editor, tmp),win32con.SW_SHOW)
-		except:	
-			# no editor found
-			self.edit_finished_callback()
+    def showsource(self, source = None, optional=0, readonly = 0):
+        if source is None:
+            if self.source is not None:
+                self.source.close()
+                self.source = None
+        else:
+            if self.source is not None:
+                self.source.settext(source)
+            else:
+                self.source = self.window.textwindow(source, readonly=1)
+                self.source.set_mother(self)
 
-		stat2 = os.stat(tmp)
-		from stat import ST_INO, ST_DEV, ST_MTIME, ST_SIZE
-		if stat1[ST_INO] == stat2[ST_INO] and \
-		   stat1[ST_DEV] == stat2[ST_DEV] and \
-		   stat1[ST_MTIME] == stat2[ST_MTIME] and \
-		   stat1[ST_SIZE] == stat2[ST_SIZE]:
-			# nothing changed
-			self.edit_finished_callback()
-			return
-		self.edit_finished_callback(tmp)
+    # doesn't seem to work
+    # kk: you must pass a context string as a second arg
+    def setcommands(self, commandlist):
+        self.window.set_commandlist(commandlist,'document')
+
+    def do_edit(self, tmp):
+        import os
+
+        # use only notepad for now
+        editor='Notepad'
+        stat1 = os.stat(tmp)
+        import win32api,win32con
+        try:
+            win32api.WinExec('%s %s' % (editor, tmp),win32con.SW_SHOW)
+        except:
+            # no editor found
+            self.edit_finished_callback()
+
+        stat2 = os.stat(tmp)
+        from stat import ST_INO, ST_DEV, ST_MTIME, ST_SIZE
+        if stat1[ST_INO] == stat2[ST_INO] and \
+           stat1[ST_DEV] == stat2[ST_DEV] and \
+           stat1[ST_MTIME] == stat2[ST_MTIME] and \
+           stat1[ST_SIZE] == stat2[ST_SIZE]:
+            # nothing changed
+            self.edit_finished_callback()
+            return
+        self.edit_finished_callback(tmp)
