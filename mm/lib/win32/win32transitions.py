482d71b2590203ab02fbb625ab7e4542dc0d7ea1
Sjoerd Mullender <sjoerdmullender@users.noreply.github.com>
2008-02-08 14:59:49 +0000

    Use more modern (and consistent) indentation.

--- mm/lib/win32/win32transitions.py
+++ mm/lib/win32/win32transitions.py
@@ -13,306 +13,306 @@ import ddraw
 import win32api
 
 class TransitionEngine:
-	def __init__(self, window, outtrans, runit, dict, cb):
-		self.windows = [window,]
-		self.outtrans = outtrans
-		
-		self.__duration = dict.get('dur', 1)
-		self.__multiElement = dict.get('coordinated')
-		self.__childrenClip = dict.get('clipBoundary', 'children') == 'children'
+    def __init__(self, window, outtrans, runit, dict, cb):
+        self.windows = [window,]
+        self.outtrans = outtrans
 
-		self.__callback = cb
+        self.__duration = dict.get('dur', 1)
+        self.__multiElement = dict.get('coordinated')
+        self.__childrenClip = dict.get('clipBoundary', 'children') == 'children'
 
-		trtype = dict.get('trtype', 'fade')
-		subtype = dict.get('subtype')
-		klass = Transitions.TransitionFactory(trtype, subtype)
-		self.__transitiontype = klass(self, dict)
+        self.__callback = cb
 
-		self.__fiber_id = None
-		self.__running = 0		
+        trtype = dict.get('trtype', 'fade')
+        subtype = dict.get('subtype')
+        klass = Transitions.TransitionFactory(trtype, subtype)
+        self.__transitiontype = klass(self, dict)
 
-		self.__startprogress = dict.get('startProgress', 0)
-		self.__endprogress = dict.get('endProgress', 1)
-		if self.__endprogress <= self.__startprogress:
-			self.__transperiod = 0
-			#raise AssertionError
-		elif self.__duration <= 0:
-			self.__transperiod = 0
-			self.__startprogress = self.__endprogress
-		else:
-			self.__transperiod = float(self.__endprogress - self.__startprogress) / self.__duration
+        self.__fiber_id = None
+        self.__running = 0
 
-		# rect for use with wnd._paintOnDDS
-		# offset windowpos with respect to VisibleWindowPos
-		self._paint_rect = None
+        self.__startprogress = dict.get('startProgress', 0)
+        self.__endprogress = dict.get('endProgress', 1)
+        if self.__endprogress <= self.__startprogress:
+            self.__transperiod = 0
+            #raise AssertionError
+        elif self.__duration <= 0:
+            self.__transperiod = 0
+            self.__startprogress = self.__endprogress
+        else:
+            self.__transperiod = float(self.__endprogress - self.__startprogress) / self.__duration
 
-	def __del__(self):
-		if self.__transitiontype:
-			self.endtransition()
-		del self._tosurf
-		del self._tmp
+        # rect for use with wnd._paintOnDDS
+        # offset windowpos with respect to VisibleWindowPos
+        self._paint_rect = None
 
-	def begintransition(self):
-		self.__createSurfaces()
-		self.__running = 1	
-		self.__start = time.time()
-		self.settransitionvalue(self.__startprogress)
-		if self.__duration<=0.0:
-			self.settransitionvalue(self.__endprogress)
-		else:	
-			self.__register_for_timeslices()
+    def __del__(self):
+        if self.__transitiontype:
+            self.endtransition()
+        del self._tosurf
+        del self._tmp
 
-	def endtransition(self):
-		if not self.__transitiontype: return
-		self.__unregister_for_timeslices()
-		if self.__callback:
-			apply(apply, self.__callback)
-			self.__callback = None
-		self.__transitiontype = None
-		self.__running = 0		
-		wnd = self.windows[0]
-		if wnd.is_closed():
-			return
-		else:
-			# XXX: patch
-			if self.outtrans and wnd._active_displist:
-				wnd._active_displist.close()
-		for win in self.windows:
-			win._transition = None
-			win._drawsurf = None
-		wnd.update(wnd.getwindowpos())
+    def begintransition(self):
+        self.__createSurfaces()
+        self.__running = 1
+        self.__start = time.time()
+        self.settransitionvalue(self.__startprogress)
+        if self.__duration<=0.0:
+            self.settransitionvalue(self.__endprogress)
+        else:
+            self.__register_for_timeslices()
 
-	def settransitionvalue(self, value):
-		if value<0.0 or value>1.0:
-			raise AssertionError
-		parameters = self.__transitiontype.computeparameters(value)
-		
-		# transition window
-		# or parent window in multiElement transitions
-		wnd = self.windows[0]
-		if wnd.is_closed():
-			return
-		
-		# assert that we paint the active surface the correct way 
-		# for each of the following cases:
-		# 1. multiElement==true, childrenClip==true
-		# 2. multiElement==true, childrenClip==false
-		# 3. multiElement==false
-		if self.__multiElement:
-			if self.__childrenClip:
-				# since children clipping will be done in wnd's paint method
-				# do a normal painting on active surface
-				wnd.paintOnDDS(self._tosurf, wnd)
-			else:
-				# do a normal painting on active surface
-				wnd.paintOnDDS(self._tosurf, wnd)
-		else:
-			if self.outtrans:
-				wnd._paintOnDDS(wnd._fromsurf, self._paint_rect)
-				wnd.updateBackDDS(self._tosurf, exclwnd=wnd) 
-			else:
-				wnd.updateBackDDS(wnd._fromsurf, exclwnd=wnd)
-				wnd._paintOnDDS(self._tosurf, self._paint_rect)
-	
-		fromsurf = 	wnd._fromsurf
-		tosurf = self._tosurf	
-		tmpsurf  = self._tmp
-		dstsurf  = wnd._drawsurf
-		dstrgn = None
+    def endtransition(self):
+        if not self.__transitiontype: return
+        self.__unregister_for_timeslices()
+        if self.__callback:
+            apply(apply, self.__callback)
+            self.__callback = None
+        self.__transitiontype = None
+        self.__running = 0
+        wnd = self.windows[0]
+        if wnd.is_closed():
+            return
+        else:
+            # XXX: patch
+            if self.outtrans and wnd._active_displist:
+                wnd._active_displist.close()
+        for win in self.windows:
+            win._transition = None
+            win._drawsurf = None
+        wnd.update(wnd.getwindowpos())
 
-		self.__transitiontype.updatebitmap(parameters, tosurf, fromsurf, tmpsurf, dstsurf, dstrgn)
-		wnd.update(wnd.getVisibleWindowPos())
+    def settransitionvalue(self, value):
+        if value<0.0 or value>1.0:
+            raise AssertionError
+        parameters = self.__transitiontype.computeparameters(value)
 
-	def join(self, window, ismaster, cb):
-		# Join this (sub or super) window to an existing transition
-		if ismaster:
-			self.__callback = cb
-			if self.isrunning():
-				self.windows.insert(0, window)
-				self.__createSurfaces()
-			else:
-				self.windows.insert(0, window)
-		else:
-			self.windows.append(window)
+        # transition window
+        # or parent window in multiElement transitions
+        wnd = self.windows[0]
+        if wnd.is_closed():
+            return
 
-		x, y, w, h = self.windows[0]._rect
-		self.__transitiontype.move_resize((0, 0, w, h))
+        # assert that we paint the active surface the correct way
+        # for each of the following cases:
+        # 1. multiElement==true, childrenClip==true
+        # 2. multiElement==true, childrenClip==false
+        # 3. multiElement==false
+        if self.__multiElement:
+            if self.__childrenClip:
+                # since children clipping will be done in wnd's paint method
+                # do a normal painting on active surface
+                wnd.paintOnDDS(self._tosurf, wnd)
+            else:
+                # do a normal painting on active surface
+                wnd.paintOnDDS(self._tosurf, wnd)
+        else:
+            if self.outtrans:
+                wnd._paintOnDDS(wnd._fromsurf, self._paint_rect)
+                wnd.updateBackDDS(self._tosurf, exclwnd=wnd)
+            else:
+                wnd.updateBackDDS(wnd._fromsurf, exclwnd=wnd)
+                wnd._paintOnDDS(self._tosurf, self._paint_rect)
 
-	def _ismaster(self, wnd):
-		return self.windows[0]==wnd
+        fromsurf =      wnd._fromsurf
+        tosurf = self._tosurf
+        tmpsurf  = self._tmp
+        dstsurf  = wnd._drawsurf
+        dstrgn = None
 
-	def _isrunning(self):
-		return self.__running
+        self.__transitiontype.updatebitmap(parameters, tosurf, fromsurf, tmpsurf, dstsurf, dstrgn)
+        wnd.update(wnd.getVisibleWindowPos())
 
-	def __createSurfaces(self):
-		# transition window
-		# or parent window in multiElement transitions
-		wnd = self.windows[0]
-		while 1:
-			try:
-				wnd._fromsurf = wnd.getBackDDS(exclwnd = wnd, dopaint = 1)
-				# use getBackDDS instead of createDDS
-				# just in case of an early repaint 
-				wnd._drawsurf = wnd.getBackDDS(exclwnd = wnd, dopaint = 0)
-				self._tosurf = wnd.getBackDDS(exclwnd = wnd, dopaint = 0)
-				self._tmp = wnd.getBackDDS(exclwnd = wnd, dopaint = 0)
-			except ddraw.error, arg:
-				print arg
-				win32api.Sleep(50)
-			else:
-				break
+    def join(self, window, ismaster, cb):
+        # Join this (sub or super) window to an existing transition
+        if ismaster:
+            self.__callback = cb
+            if self.isrunning():
+                self.windows.insert(0, window)
+                self.__createSurfaces()
+            else:
+                self.windows.insert(0, window)
+        else:
+            self.windows.append(window)
 
-		# paint rect
-		w, h = wnd._drawsurf.GetSurfaceDesc().GetSize()
+        x, y, w, h = self.windows[0]._rect
+        self.__transitiontype.move_resize((0, 0, w, h))
 
-		# new origin
-		x1, y1, w1, h1 = wnd.getVisibleWindowPos()
+    def _ismaster(self, wnd):
+        return self.windows[0]==wnd
 
-		x2, y2, w2, h2 = wnd.getwindowpos()
-		
-		# offset windowpos with respect to VisibleWindowPos
-		self._paint_rect = x2-x1, y2-y1, w2, h2
+    def _isrunning(self):
+        return self.__running
 
-		# resize to this surface
-		self.__transitiontype.move_resize((0, 0, w, h))
+    def __createSurfaces(self):
+        # transition window
+        # or parent window in multiElement transitions
+        wnd = self.windows[0]
+        while 1:
+            try:
+                wnd._fromsurf = wnd.getBackDDS(exclwnd = wnd, dopaint = 1)
+                # use getBackDDS instead of createDDS
+                # just in case of an early repaint
+                wnd._drawsurf = wnd.getBackDDS(exclwnd = wnd, dopaint = 0)
+                self._tosurf = wnd.getBackDDS(exclwnd = wnd, dopaint = 0)
+                self._tmp = wnd.getBackDDS(exclwnd = wnd, dopaint = 0)
+            except ddraw.error, arg:
+                print arg
+                win32api.Sleep(50)
+            else:
+                break
 
-	def __onIdle(self):
-		if self.windows[0].is_closed():
-			self.endtransition()
-			return
-		t_sec = time.time() - self.__start
-		if t_sec>=self.__duration:
-			try:
-				self.settransitionvalue(self.__endprogress)
-				self.endtransition()
-			except ddraw.error, arg:
-				print arg			
-		else:
-			try:
-				self.settransitionvalue(self.__startprogress + self.__transperiod * t_sec)
-			except ddraw.error, arg:
-				print arg			
-		win32api.Sleep(0)
-				
-	
-	def __register_for_timeslices(self):
-		if not self.__fiber_id:
-			self.__fiber_id = windowinterface.setidleproc(self.__onIdle)
+        # paint rect
+        w, h = wnd._drawsurf.GetSurfaceDesc().GetSize()
 
-	def __unregister_for_timeslices(self):
-		if self.__fiber_id is not None:
-			windowinterface.cancelidleproc(self.__fiber_id)
-			self.__fiber_id = None
+        # new origin
+        x1, y1, w1, h1 = wnd.getVisibleWindowPos()
+
+        x2, y2, w2, h2 = wnd.getwindowpos()
+
+        # offset windowpos with respect to VisibleWindowPos
+        self._paint_rect = x2-x1, y2-y1, w2, h2
+
+        # resize to this surface
+        self.__transitiontype.move_resize((0, 0, w, h))
+
+    def __onIdle(self):
+        if self.windows[0].is_closed():
+            self.endtransition()
+            return
+        t_sec = time.time() - self.__start
+        if t_sec>=self.__duration:
+            try:
+                self.settransitionvalue(self.__endprogress)
+                self.endtransition()
+            except ddraw.error, arg:
+                print arg
+        else:
+            try:
+                self.settransitionvalue(self.__startprogress + self.__transperiod * t_sec)
+            except ddraw.error, arg:
+                print arg
+        win32api.Sleep(0)
+
+
+    def __register_for_timeslices(self):
+        if not self.__fiber_id:
+            self.__fiber_id = windowinterface.setidleproc(self.__onIdle)
+
+    def __unregister_for_timeslices(self):
+        if self.__fiber_id is not None:
+            windowinterface.cancelidleproc(self.__fiber_id)
+            self.__fiber_id = None
 
 
 class InlineTransitionEngine:
-	def __init__(self, window, outtrans, runit, dict, cb):
-		self.window = window
-		self.outtrans = outtrans
-		self.dict = dict
-		self.cb = cb
+    def __init__(self, window, outtrans, runit, dict, cb):
+        self.window = window
+        self.outtrans = outtrans
+        self.dict = dict
+        self.cb = cb
 
-		trtype = dict.get('trtype')
-		subtype = dict.get('subtype')
-		klass = Transitions.TransitionFactory(trtype, subtype)
-		self.__transitiontype = klass(self, self.dict)
+        trtype = dict.get('trtype')
+        subtype = dict.get('subtype')
+        klass = Transitions.TransitionFactory(trtype, subtype)
+        self.__transitiontype = klass(self, self.dict)
 
-		self.__running = 0		
-		self._lastvalue = None
+        self.__running = 0
+        self._lastvalue = None
 
-	def __del__(self):
-		if self.__transitiontype:
-			self.endtransition()
-	
-	def begintransition(self):
-		self.__createSurfaces()
-		self.__running = 1	
-		self._lastvalue = None
+    def __del__(self):
+        if self.__transitiontype:
+            self.endtransition()
 
-	def endtransition(self):
-		if not self.__transitiontype: return
-		self.__transitiontype = None
-		self.__running = 0		
-		wnd = self.window
-		if wnd.is_closed():
-			return
-		wnd._transition = None
-		wnd._drawsurf = None
-		wnd.update(wnd.getwindowpos())
+    def begintransition(self):
+        self.__createSurfaces()
+        self.__running = 1
+        self._lastvalue = None
 
-	def settransitionvalue(self, value):
-		if value<0.0 or value>1.0:
-			raise AssertionError, 'settransitionvalue out of range %d', value
-		
-		# transition window
-		wnd = self.window
-		if wnd.is_closed():
-			return
-		
-		if self._lastvalue == value:
-			wnd.update(wnd.getwindowpos())
-			return
-		self._lastvalue = value
-			
-		# hack until i find the bug
-		if value == 1.0: 
-			if self.outtrans: 
-				wnd.updateBackDDS(wnd._drawsurf, exclwnd=wnd)
-			else:
-				wnd._paintOnDDS(wnd._drawsurf, wnd._rect)
-			wnd.update(wnd.getwindowpos())
-			return
-		elif value==0.0:
-			if self.outtrans: 
-				wnd._paintOnDDS(wnd._drawsurf, wnd._rect)
-			else:
-				wnd.updateBackDDS(wnd._drawsurf, exclwnd=wnd)
-			wnd.update(wnd.getwindowpos())
-			return
+    def endtransition(self):
+        if not self.__transitiontype: return
+        self.__transitiontype = None
+        self.__running = 0
+        wnd = self.window
+        if wnd.is_closed():
+            return
+        wnd._transition = None
+        wnd._drawsurf = None
+        wnd.update(wnd.getwindowpos())
 
-		# normal processing
-		parameters = self.__transitiontype.computeparameters(value)
-		if self.outtrans:
-			wnd._paintOnDDS(wnd._fromsurf, wnd._rect)
-			wnd.updateBackDDS(self._tosurf, exclwnd=wnd) 
-		else:
-			wnd.updateBackDDS(wnd._fromsurf, exclwnd=wnd) 
-			wnd._paintOnDDS(self._tosurf, wnd._rect)
+    def settransitionvalue(self, value):
+        if value<0.0 or value>1.0:
+            raise AssertionError, 'settransitionvalue out of range %d', value
 
-		fromsurf = 	wnd._fromsurf
-		tosurf = self._tosurf	
-		tmpsurf  = self._tmp
-		dstsurf  = wnd._drawsurf
-		dstrgn = None
+        # transition window
+        wnd = self.window
+        if wnd.is_closed():
+            return
 
-		self.__transitiontype.updatebitmap(parameters, tosurf, fromsurf, tmpsurf, dstsurf, dstrgn)
-		wnd.update(wnd.getwindowpos())
+        if self._lastvalue == value:
+            wnd.update(wnd.getwindowpos())
+            return
+        self._lastvalue = value
+
+        # hack until i find the bug
+        if value == 1.0:
+            if self.outtrans:
+                wnd.updateBackDDS(wnd._drawsurf, exclwnd=wnd)
+            else:
+                wnd._paintOnDDS(wnd._drawsurf, wnd._rect)
+            wnd.update(wnd.getwindowpos())
+            return
+        elif value==0.0:
+            if self.outtrans:
+                wnd._paintOnDDS(wnd._drawsurf, wnd._rect)
+            else:
+                wnd.updateBackDDS(wnd._drawsurf, exclwnd=wnd)
+            wnd.update(wnd.getwindowpos())
+            return
+
+        # normal processing
+        parameters = self.__transitiontype.computeparameters(value)
+        if self.outtrans:
+            wnd._paintOnDDS(wnd._fromsurf, wnd._rect)
+            wnd.updateBackDDS(self._tosurf, exclwnd=wnd)
+        else:
+            wnd.updateBackDDS(wnd._fromsurf, exclwnd=wnd)
+            wnd._paintOnDDS(self._tosurf, wnd._rect)
+
+        fromsurf =      wnd._fromsurf
+        tosurf = self._tosurf
+        tmpsurf  = self._tmp
+        dstsurf  = wnd._drawsurf
+        dstrgn = None
+
+        self.__transitiontype.updatebitmap(parameters, tosurf, fromsurf, tmpsurf, dstsurf, dstrgn)
+        wnd.update(wnd.getwindowpos())
 
 
-	def _ismaster(self, wnd):
-		return self.window==wnd
+    def _ismaster(self, wnd):
+        return self.window==wnd
 
-	def _isrunning(self):
-		return self.__running
+    def _isrunning(self):
+        return self.__running
 
-	def __createSurfaces(self):
-		# transition window
-		# or parent window in multiElement transitions
-		wnd = self.window
-		while 1:
-			try:
-				# use getBackDDS instead of createDDS
-				# just in case of an early repaint 
-				wnd._fromsurf = wnd.getBackDDS(dopaint = 1)
-				wnd._drawsurf = wnd.getBackDDS(dopaint = 0)
-				self._tosurf = wnd.getBackDDS(dopaint = 0)
-				self._tmp = wnd.getBackDDS(dopaint = 0)
-			except ddraw.error, arg:
-				print arg
-				win32api.Sleep(50)
-			else:
-				break
+    def __createSurfaces(self):
+        # transition window
+        # or parent window in multiElement transitions
+        wnd = self.window
+        while 1:
+            try:
+                # use getBackDDS instead of createDDS
+                # just in case of an early repaint
+                wnd._fromsurf = wnd.getBackDDS(dopaint = 1)
+                wnd._drawsurf = wnd.getBackDDS(dopaint = 0)
+                self._tosurf = wnd.getBackDDS(dopaint = 0)
+                self._tmp = wnd.getBackDDS(dopaint = 0)
+            except ddraw.error, arg:
+                print arg
+                win32api.Sleep(50)
+            else:
+                break
 
-		# resize to this window
-		x, y, w, h = wnd._rect
-		self.__transitiontype.move_resize((0, 0, w, h))
+        # resize to this window
+        x, y, w, h = wnd._rect
+        self.__transitiontype.move_resize((0, 0, w, h))
