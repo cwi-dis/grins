1f0f63574d76eff7cf43581b2e19b4cb116740b1
Sjoerd Mullender <sjoerdmullender@users.noreply.github.com>
2008-02-08 14:59:31 +0000

    Use more modern (and consistent) indentation.

--- mm/common/bitrates.py
+++ mm/common/bitrates.py
@@ -1,18 +1,18 @@
 __version__ = "$Id$"
 
 bitrates = [
-	(14400, '14.4K Modem'),
-	(19200, '19.2K Connection'),
-	(28800, '28.8K Modem'),
-	(33600, '33.6K Modem'),
-	(34400, '56K Modem'),
-	(57600, '56K Single ISDN'),
-	(115200, '112K Dual ISDN'),
-	(262200, '256Kbps DSL/Cable'),
-	(307200, '300Kbps DSL/Cable'),
-	(524300, '512Kbps DSL/Cable'),
-	(1544000, 'T1 / LAN'),
-	(10485800, '10Mbps LAN'),
+        (14400, '14.4K Modem'),
+        (19200, '19.2K Connection'),
+        (28800, '28.8K Modem'),
+        (33600, '33.6K Modem'),
+        (34400, '56K Modem'),
+        (57600, '56K Single ISDN'),
+        (115200, '112K Dual ISDN'),
+        (262200, '256Kbps DSL/Cable'),
+        (307200, '300Kbps DSL/Cable'),
+        (524300, '512Kbps DSL/Cable'),
+        (1544000, 'T1 / LAN'),
+        (10485800, '10Mbps LAN'),
 ]
 
 a2l = {}
@@ -20,7 +20,7 @@ l2a = {}
 rates = []
 names = []
 for _a, _l in bitrates:
-	a2l[_a] = _l
-	l2a[_l] = _a
-	rates.append(_a)
-	names.append(_l)
+    a2l[_a] = _l
+    l2a[_l] = _a
+    rates.append(_a)
+    names.append(_l)
--- mm/common/flags.py
+++ mm/common/flags.py
@@ -13,9 +13,9 @@ __version__ = "$Id$"
 # In the menu templates and attrdefs, all flags that are on must be present in the
 # set of flags that is returned by curflags().  In other words, the
 # test that is done is '(flags & curflags()) != 0'.  This means
-# that in the menu templates you should set a combinaison of LIGHT_G2_LIGHT, 
-# FLAG_G2_PRO, FLAG_QT_LIGHT, FLAG_QT_PRO, FLAG_CMIF, FLAG_SMIL_1_0, DBG. 
-# The predefinated combinaison flags are: 
+# that in the menu templates you should set a combinaison of LIGHT_G2_LIGHT,
+# FLAG_G2_PRO, FLAG_QT_LIGHT, FLAG_QT_PRO, FLAG_CMIF, FLAG_SMIL_1_0, DBG.
+# The predefinated combinaison flags are:
 # FLAG_G2, FLAG_QT and FLAG_ALL
 #
 # in the attrdefs file, flags are set in the same way for specify the visible attributes in formularies,
@@ -45,35 +45,35 @@ FLAG_ALL = FLAG_G2 | FLAG_QT | FLAG_CMIF | FLAG_SMIL_1_0 | FLAG_BOSTON
 _curflags = None
 
 def curflags():
-	global _curflags
-	if _curflags is None:
-		import settings
-		import features
-		flags = FLAG_BOSTON
-		if hasattr(features, 'feature_set'):
-			if features.EXPORT_REAL in features.feature_set:
-				flags = flags | FLAG_G2
-			if features.EXPORT_QT in features.feature_set:
-				flags = flags | FLAG_QT
-			if features.EXPORT_SMIL2 in features.feature_set:
-				flags = flags | FLAG_BOSTON
-		elif features.compatibility == features.G2:
-			if features.lightweight:
-				flags = flags | FLAG_G2_LIGHT
-			else:
-				flags = flags | FLAG_G2_PRO
-		elif features.compatibility == features.QT:
-			if features.lightweight:
-				flags = flags | FLAG_QT_LIGHT
-			else:
-				flags = flags | FLAG_QT_PRO 
-		elif features.compatibility == features.SMIL10:
-			flags = flags | FLAG_SMIL_1_0
-		elif features.compatibility == features.Boston:
-			flags = flags | FLAG_BOSTON
-		if settings.get('cmif'):
-			flags = flags | FLAG_CMIF
-		if settings.get('debug'):
-			flags = flags | FLAG_DBG
-		_curflags = flags
-	return _curflags
+    global _curflags
+    if _curflags is None:
+        import settings
+        import features
+        flags = FLAG_BOSTON
+        if hasattr(features, 'feature_set'):
+            if features.EXPORT_REAL in features.feature_set:
+                flags = flags | FLAG_G2
+            if features.EXPORT_QT in features.feature_set:
+                flags = flags | FLAG_QT
+            if features.EXPORT_SMIL2 in features.feature_set:
+                flags = flags | FLAG_BOSTON
+        elif features.compatibility == features.G2:
+            if features.lightweight:
+                flags = flags | FLAG_G2_LIGHT
+            else:
+                flags = flags | FLAG_G2_PRO
+        elif features.compatibility == features.QT:
+            if features.lightweight:
+                flags = flags | FLAG_QT_LIGHT
+            else:
+                flags = flags | FLAG_QT_PRO
+        elif features.compatibility == features.SMIL10:
+            flags = flags | FLAG_SMIL_1_0
+        elif features.compatibility == features.Boston:
+            flags = flags | FLAG_BOSTON
+        if settings.get('cmif'):
+            flags = flags | FLAG_CMIF
+        if settings.get('debug'):
+            flags = flags | FLAG_DBG
+        _curflags = flags
+    return _curflags
