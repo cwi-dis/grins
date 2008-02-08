7fa3ae585fd1af17a830f8c029a6b79bbc8fa345
Sjoerd Mullender <sjoerdmullender@users.noreply.github.com>
2008-02-08 14:59:31 +0000

    Use more modern (and consistent) indentation.

--- mm/bin/wince/wincon.py
+++ mm/bin/wince/wincon.py
@@ -2788,21 +2788,21 @@ LOADFSD_SYNCH = 0x0001
 # Manualy added
 SM_CXFRAME = 32
 SM_CYFRAME = 33
-SRCCOPY		= 0x00CC0020	# dest = source
-SRCPAINT	= 0x00EE0086	# dest = source OR dest
-SRCAND		= 0x008800C6	# dest = source AND dest
-SRCINVERT	= 0x00660046	# dest = source XOR dest
-SRCERASE	= 0x00440328	# dest = source AND (NOT dest )
-NOTSRCCOPY	= 0x00330008	# dest = (NOT source)
-NOTSRCERASE	= 0x001100A6	# dest = (NOT src) AND (NOT dest)
-MERGECOPY	= 0x00C000CA	# dest = (source AND pattern)
-MERGEPAINT	= 0x00BB0226	# dest = (NOT source) OR dest
-PATCOPY		= 0x00F00021	# dest = pattern
-PATPAINT	= 0x00FB0A09	# dest = DPSnoo
-PATINVERT	= 0x005A0049	# dest = pattern XOR dest
-DSTINVERT	= 0x00550009	# dest = (NOT dest)
-BLACKNESS	= 0x00000042	# dest = BLACK
-WHITENESS	= 0x00FF0062	# dest = WHITE
+SRCCOPY         = 0x00CC0020    # dest = source
+SRCPAINT        = 0x00EE0086    # dest = source OR dest
+SRCAND          = 0x008800C6    # dest = source AND dest
+SRCINVERT       = 0x00660046    # dest = source XOR dest
+SRCERASE        = 0x00440328    # dest = source AND (NOT dest )
+NOTSRCCOPY      = 0x00330008    # dest = (NOT source)
+NOTSRCERASE     = 0x001100A6    # dest = (NOT src) AND (NOT dest)
+MERGECOPY       = 0x00C000CA    # dest = (source AND pattern)
+MERGEPAINT      = 0x00BB0226    # dest = (NOT source) OR dest
+PATCOPY         = 0x00F00021    # dest = pattern
+PATPAINT        = 0x00FB0A09    # dest = DPSnoo
+PATINVERT       = 0x005A0049    # dest = pattern XOR dest
+DSTINVERT       = 0x00550009    # dest = (NOT dest)
+BLACKNESS       = 0x00000042    # dest = BLACK
+WHITENESS       = 0x00FF0062    # dest = WHITE
 
 # commctrl.h
 PBM_SETRANGE = WM_USER+1
@@ -2810,5 +2810,3 @@ PBM_SETPOS  =  WM_USER+2
 PBM_DELTAPOS = WM_USER+3
 PBM_SETSTEP =  WM_USER+4
 PBM_STEPIT =  WM_USER+5
-
-
--- mm/common/BrushChannel.py
+++ mm/common/BrushChannel.py
@@ -4,20 +4,20 @@ from Channel import *
 from windowinterface import UNIT_SCREEN
 
 class BrushChannel(ChannelWindow):
-	node_attrs = ChannelWindow.node_attrs + ['fgcolor']
+    node_attrs = ChannelWindow.node_attrs + ['fgcolor']
 
-	def do_arm(self, node, same=0):
-		if same and self.armed_display:
-			return 1
-		color = MMAttrdefs.getattr(node, 'fgcolor')
-		self.armed_display.drawfbox(color, (0,0,1,1), units = UNIT_SCREEN)
-		return 1
+    def do_arm(self, node, same=0):
+        if same and self.armed_display:
+            return 1
+        color = MMAttrdefs.getattr(node, 'fgcolor')
+        self.armed_display.drawfbox(color, (0,0,1,1), units = UNIT_SCREEN)
+        return 1
 
-	def do_updateattr(self, node, name, value):
-		if name != 'fgcolor':
-			return
-		d = self.played_display.clone()
-		d.drawfbox(value, (0,0,1,1), units = UNIT_SCREEN)
-		d.render()
-		self.played_display.close()
-		self.played_display = d
+    def do_updateattr(self, node, name, value):
+        if name != 'fgcolor':
+            return
+        d = self.played_display.clone()
+        d.drawfbox(value, (0,0,1,1), units = UNIT_SCREEN)
+        d.render()
+        self.played_display.close()
+        self.played_display = d
