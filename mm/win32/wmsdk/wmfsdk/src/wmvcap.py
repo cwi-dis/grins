129403fb6e68ae472ee13077649b4ecce08fd1ac
Sjoerd Mullender <sjoerdmullender@users.noreply.github.com>
2008-02-08 14:59:57 +0000

    Use more modern (and consistent) indentation.

--- mm/win32/wmsdk/wmfsdk/src/wmvcap.py
+++ mm/win32/wmsdk/wmfsdk/src/wmvcap.py
@@ -8,32 +8,32 @@ import wmwriter
 # wmriter configuartion profile
 profile = 20
 
-###########################################
+## #########################################
 
 def LoadImageOn(dds, filename):
-	import gear32sd
-	img = gear32sd.load_file(filename)
-	if img>=0:
-		cx, cy, bpp = gear32sd.image_dimensions_get(img)
-		hdc = dds.GetDC()
-		x, y = (w-cx)/2, (h-cy)/2
-		rc = x, y, x+cx, y+cy
-		gear32sd.device_rect_set(img, rc)
-		gear32sd.display_desktop_pattern_set(img,0)
-		gear32sd.display_image(img, hdc)
-		dds.ReleaseDC(hdc)
-		gear32sd.image_delete(img)
+    import gear32sd
+    img = gear32sd.load_file(filename)
+    if img>=0:
+        cx, cy, bpp = gear32sd.image_dimensions_get(img)
+        hdc = dds.GetDC()
+        x, y = (w-cx)/2, (h-cy)/2
+        rc = x, y, x+cx, y+cy
+        gear32sd.device_rect_set(img, rc)
+        gear32sd.display_desktop_pattern_set(img,0)
+        gear32sd.display_image(img, hdc)
+        dds.ReleaseDC(hdc)
+        gear32sd.image_delete(img)
 
 
 
-###########################################
+## #########################################
 
 # viewport dimensions
 w = 400
 h = 300
 
 
-###########################################
+## #########################################
 import ddraw
 
 # create direct draw stuff
@@ -58,7 +58,7 @@ dds.BltFill((0, 0, w, h), ddcolor)
 filename1 = r'D:\ufs\mmdocuments\interop2\interop2\images\frown.jpg'
 filename2 = r'D:\ufs\mmdocuments\interop2\interop2\images\smile.jpg'
 
-###########################################
+## #########################################
 # record manually coded presentation
 
 # create writer
@@ -84,5 +84,4 @@ writer.endWriting()
 
 del writer
 
-###########################################
-
+## #########################################
