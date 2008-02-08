8ad86a0b09bb0cd41f8b544e6f57bd5030619148
Sjoerd Mullender <sjoerdmullender@users.noreply.github.com>
2008-02-08 14:59:44 +0000

    Use more modern (and consistent) indentation.

--- mm/grins/win32/MenuTemplate.py
+++ mm/grins/win32/MenuTemplate.py
@@ -32,62 +32,62 @@ from wndusercmd import *
 from flags import *
 
 MENUBAR=(
-	('&File', (
-		(FLAG_ALL, ENTRY, '&Open...\tCtrl+O', 'O', OPENFILE),
-		(FLAG_ALL, ENTRY, 'Open &URL...\tCtrl+L', 'O', OPEN),
-		(FLAG_ALL, DYNAMICCASCADE, 'Open &recent', OPEN_RECENT),
-		(FLAG_ALL, ENTRY, 'R&eload', 'R', RELOAD),
-		(FLAG_ALL, ENTRY, '&Close Document', None, CLOSE),
-		(FLAG_ALL, SEP,),
-		(FLAG_ALL, ENTRY, '&Preferences...', None, PREFERENCES),
-		(FLAG_DBG, SEP,),
-		(FLAG_DBG, CASCADE, '&Debug', (
-			(FLAG_DBG, ENTRY, 'Dump &scheduler data', None, SCHEDDUMP),
-			(FLAG_DBG, TOGGLE, 'Enable call &tracing', None, TRACE),
-			(FLAG_DBG, ENTRY, 'Enter &debugger', None, DEBUG),
-			(FLAG_DBG, ENTRY, '&Abort', None, CRASH),
-			(FLAG_DBG, TOGGLE, 'Show &log/debug window', None, CONSOLE),
-			)),
-		(FLAG_DBG, SEP,),
-		(FLAG_ALL, ENTRY, 'C&heck for GRiNS update...', None, CHECKVERSION),
-		(FLAG_ALL, SEP,),
-##		(FLAG_ALL, ENTRY, 'Choose skin...', None, CHOOSESKIN),
-##		(FLAG_ALL, ENTRY, 'Choose components config file...', None, CHOOSECOMPONENTS)
-##		(FLAG_ALL, SEP,),
-		(FLAG_ALL, ENTRY, 'E&xit', None, EXIT))),
+        ('&File', (
+                (FLAG_ALL, ENTRY, '&Open...\tCtrl+O', 'O', OPENFILE),
+                (FLAG_ALL, ENTRY, 'Open &URL...\tCtrl+L', 'O', OPEN),
+                (FLAG_ALL, DYNAMICCASCADE, 'Open &recent', OPEN_RECENT),
+                (FLAG_ALL, ENTRY, 'R&eload', 'R', RELOAD),
+                (FLAG_ALL, ENTRY, '&Close Document', None, CLOSE),
+                (FLAG_ALL, SEP,),
+                (FLAG_ALL, ENTRY, '&Preferences...', None, PREFERENCES),
+                (FLAG_DBG, SEP,),
+                (FLAG_DBG, CASCADE, '&Debug', (
+                        (FLAG_DBG, ENTRY, 'Dump &scheduler data', None, SCHEDDUMP),
+                        (FLAG_DBG, TOGGLE, 'Enable call &tracing', None, TRACE),
+                        (FLAG_DBG, ENTRY, 'Enter &debugger', None, DEBUG),
+                        (FLAG_DBG, ENTRY, '&Abort', None, CRASH),
+                        (FLAG_DBG, TOGGLE, 'Show &log/debug window', None, CONSOLE),
+                        )),
+                (FLAG_DBG, SEP,),
+                (FLAG_ALL, ENTRY, 'C&heck for GRiNS update...', None, CHECKVERSION),
+                (FLAG_ALL, SEP,),
+##         (FLAG_ALL, ENTRY, 'Choose skin...', None, CHOOSESKIN),
+##         (FLAG_ALL, ENTRY, 'Choose components config file...', None, CHOOSECOMPONENTS)
+##         (FLAG_ALL, SEP,),
+                (FLAG_ALL, ENTRY, 'E&xit', None, EXIT))),
 
 
-	('&View', (
-		(FLAG_ALL, CASCADE, 'T&oolbars', (
-			(FLAG_ALL, ENTRY, '&General', None, TOOLBAR_GENERAL),
-##			(FLAG_ALL, ENTRY, '&Player Controls', None, TOOLBAR_PLAYER),
-			(FLAG_ALL, ENTRY, '&Player Controls', None, PLAYER_PANEL),
-			)),
-		(FLAG_ALL, TOGGLE, '&Source', None, SOURCEVIEW))),
+        ('&View', (
+                (FLAG_ALL, CASCADE, 'T&oolbars', (
+                        (FLAG_ALL, ENTRY, '&General', None, TOOLBAR_GENERAL),
+##             (FLAG_ALL, ENTRY, '&Player Controls', None, TOOLBAR_PLAYER),
+                        (FLAG_ALL, ENTRY, '&Player Controls', None, PLAYER_PANEL),
+                        )),
+                (FLAG_ALL, TOGGLE, '&Source', None, SOURCEVIEW))),
 
-	('&Play', (
-		(FLAG_ALL, ENTRY, '&Play\tCtrl+P', 'P', PLAY),
-		(FLAG_ALL, ENTRY, 'Pa&use\tCtrl+U', 'U', PAUSE),
-		(FLAG_ALL, ENTRY, '&Stop\tCtrl+H', 'H', STOP),
-		(FLAG_BOSTON|FLAG_CMIF, SEP,),
-		(FLAG_BOSTON, DYNAMICCASCADE, 'Custom &tests', USERGROUPS),
-		(FLAG_CMIF, DYNAMICCASCADE, 'Visible &channels', CHANNELS),
-		)),
+        ('&Play', (
+                (FLAG_ALL, ENTRY, '&Play\tCtrl+P', 'P', PLAY),
+                (FLAG_ALL, ENTRY, 'Pa&use\tCtrl+U', 'U', PAUSE),
+                (FLAG_ALL, ENTRY, '&Stop\tCtrl+H', 'H', STOP),
+                (FLAG_BOSTON|FLAG_CMIF, SEP,),
+                (FLAG_BOSTON, DYNAMICCASCADE, 'Custom &tests', USERGROUPS),
+                (FLAG_CMIF, DYNAMICCASCADE, 'Visible &channels', CHANNELS),
+                )),
 
-	('&Window', (
-		(FLAG_ALL, ENTRY, 'Cl&ose', None, CLOSE_ACTIVE_WINDOW),)),
+        ('&Window', (
+                (FLAG_ALL, ENTRY, 'Cl&ose', None, CLOSE_ACTIVE_WINDOW),)),
 
-	('&Help', (
-		(FLAG_ALL, ENTRY, '&Contents', None, HELP_CONTENTS),
-##		(FLAG_ALL, ENTRY, 'Context &Help', None, HELP),
-##		(FLAG_ALL, SEP,),
-		(FLAG_ALL, ENTRY, 'GRiNS on the &Web', None,GRINS_WEB),
-		(FLAG_ALL, SEP,),
-		(FLAG_ALL, ENTRY, '&About GRiNS...', None, ABOUT_GRINS))))
+        ('&Help', (
+                (FLAG_ALL, ENTRY, '&Contents', None, HELP_CONTENTS),
+##         (FLAG_ALL, ENTRY, 'Context &Help', None, HELP),
+##         (FLAG_ALL, SEP,),
+                (FLAG_ALL, ENTRY, 'GRiNS on the &Web', None,GRINS_WEB),
+                (FLAG_ALL, SEP,),
+                (FLAG_ALL, ENTRY, '&About GRiNS...', None, ABOUT_GRINS))))
 
 MAIN_FRAME_POPUP = (
-		(FLAG_ALL, ENTRY, '&Paste document', None, PASTE_DOCUMENT),
-		(FLAG_ALL, SEP,),
-		(FLAG_ALL, ENTRY, '&Open...\tCtrl+O', 'O', OPEN),
-		(FLAG_ALL, ENTRY, '&Close document', None, CLOSE),
+                (FLAG_ALL, ENTRY, '&Paste document', None, PASTE_DOCUMENT),
+                (FLAG_ALL, SEP,),
+                (FLAG_ALL, ENTRY, '&Open...\tCtrl+O', 'O', OPEN),
+                (FLAG_ALL, ENTRY, '&Close document', None, CLOSE),
 )
