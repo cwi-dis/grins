0518da21b7f3c17bfed0d392a39dff72554606b3
Sjoerd Mullender <sjoerdmullender@users.noreply.github.com>
2008-02-08 14:59:54 +0000

    Use more modern (and consistent) indentation.

--- mm/pytools/grinsdb/src/grpasswd.py
+++ mm/pytools/grinsdb/src/grpasswd.py
@@ -7,41 +7,41 @@ import sys
 import whrandom
 
 def main():
-	if len(sys.argv) not in (2,3):
-		print "Usage: grpasswd user [passwd]"
-		sys.exit(1)
-	user = sys.argv[1]
-	passwd = None
-	if len(sys.argv) > 2:
-		passwd = sys.argv[2]
-	dbase = grinsdb.Database()
-	if not passwd:
-		passwd = getfield(dbase, user, 'Password')
-		if passwd:
-			print 'User: %s'%user
-			print 'Password: %s'%passwd
-		else:
-			print 'User %s has no password'%user
-	else:
-		if passwd == '-':
-			passwd = invent_passwd()
-		addfield(dbase, user, 'Password', passwd, override=1)
-	dbase.close()
+    if len(sys.argv) not in (2,3):
+        print "Usage: grpasswd user [passwd]"
+        sys.exit(1)
+    user = sys.argv[1]
+    passwd = None
+    if len(sys.argv) > 2:
+        passwd = sys.argv[2]
+    dbase = grinsdb.Database()
+    if not passwd:
+        passwd = getfield(dbase, user, 'Password')
+        if passwd:
+            print 'User: %s'%user
+            print 'Password: %s'%passwd
+        else:
+            print 'User %s has no password'%user
+    else:
+        if passwd == '-':
+            passwd = invent_passwd()
+        addfield(dbase, user, 'Password', passwd, override=1)
+    dbase.close()
 
 def addpasswd(obj, passwd='-'):
-	"""Helper for register: add password to a record"""
-	if passwd == '-':
-		passwd = invent_passwd()
-	obj['password'] = passwd
-	
+    """Helper for register: add password to a record"""
+    if passwd == '-':
+        passwd = invent_passwd()
+    obj['password'] = passwd
+
 
 def invent_passwd(length=6):
-	"""Invent a password. Not industry-strenght but good enough for us"""
-	ok = "abcdefghijkmnopqrstuvwxyz023456789"
-	rv = ''
-	for i in range(length):
-		rv = rv + whrandom.choice(ok)
-	return rv
+    """Invent a password. Not industry-strenght but good enough for us"""
+    ok = "abcdefghijkmnopqrstuvwxyz023456789"
+    rv = ''
+    for i in range(length):
+        rv = rv + whrandom.choice(ok)
+    return rv
 
 if __name__ == '__main__':
-	main()
+    main()
