26a3c0e011ba889cecc3ec00cec86aa06b8625b8
Sjoerd Mullender <sjoerdmullender@users.noreply.github.com>
2008-02-08 14:59:55 +0000

    Use more modern (and consistent) indentation.

--- mm/win32/Boost/Bridge/wintk/SystemColors.py
+++ mm/win32/Boost/Bridge/wintk/SystemColors.py
@@ -12,46 +12,46 @@ colors = {
 }
 
 def __add_color(name, sysindex):
-	rgb = None
-	# make sure it doesn't crash if there is a problem
-	try:
-		color = winuser.GetSysColor(sysindex)
-		if color != None and color >= 0:
-			rgb = wingdi.GetRGBValues(color)
-	except:
-		print 'systemcolor: error to get color ',name
-		
-	if type(rgb) is type(()):
-		colors[name] = rgb
-	
+    rgb = None
+    # make sure it doesn't crash if there is a problem
+    try:
+        color = winuser.GetSysColor(sysindex)
+        if color != None and color >= 0:
+            rgb = wingdi.GetRGBValues(color)
+    except:
+        print 'systemcolor: error to get color ',name
+
+    if type(rgb) is type(()):
+        colors[name] = rgb
+
 def init_colors():
-	__add_color('activeborder', win32con.COLOR_ACTIVEBORDER)
-	__add_color('activecaption', win32con.COLOR_ACTIVECAPTION)
-	__add_color('appworkspace', win32con.COLOR_APPWORKSPACE)
-	__add_color('background', win32con.COLOR_BACKGROUND)
-	__add_color('buttonface', win32con.COLOR_BTNFACE)
-	__add_color('buttonhighlight', win32con.COLOR_BTNHIGHLIGHT)
-	__add_color('buttonshadow', win32con.COLOR_BTNSHADOW)
-	__add_color('buttontext', win32con.COLOR_BTNTEXT)
-	__add_color('captiontext', win32con.COLOR_CAPTIONTEXT)
-	__add_color('graytext', win32con.COLOR_GRAYTEXT)
-	__add_color('highlight', win32con.COLOR_HIGHLIGHT)
-	__add_color('highlighttext', win32con.COLOR_HIGHLIGHTTEXT)
-	__add_color('inactiveborder', win32con.COLOR_INACTIVEBORDER)
-	__add_color('inactivecaption', win32con.COLOR_INACTIVECAPTION)
-	__add_color('inactivecaptiontext', win32con.COLOR_INACTIVECAPTIONTEXT)
-	__add_color('infobackground', win32con.COLOR_INFOBK)
-	__add_color('infotext', win32con.COLOR_INFOTEXT)
-	__add_color('menu', win32con.COLOR_MENU)
-	__add_color('menutext', win32con.COLOR_MENUTEXT)
-	__add_color('scrollbar', win32con.COLOR_SCROLLBAR)
-	__add_color('threeddarkshadow', win32con.COLOR_3DDKSHADOW)
-	__add_color('threedface', win32con.COLOR_3DFACE)
-	__add_color('threedhighlight', win32con.COLOR_3DHIGHLIGHT)
-	__add_color('threedlightshadow', win32con.COLOR_3DLIGHT)
-	__add_color('threedshadow', win32con.COLOR_3DSHADOW)
-	__add_color('window', win32con.COLOR_WINDOW)
-	__add_color('windowframe', win32con.COLOR_WINDOWFRAME)
-	__add_color('windowtext', win32con.COLOR_WINDOWTEXT)
-	
+    __add_color('activeborder', win32con.COLOR_ACTIVEBORDER)
+    __add_color('activecaption', win32con.COLOR_ACTIVECAPTION)
+    __add_color('appworkspace', win32con.COLOR_APPWORKSPACE)
+    __add_color('background', win32con.COLOR_BACKGROUND)
+    __add_color('buttonface', win32con.COLOR_BTNFACE)
+    __add_color('buttonhighlight', win32con.COLOR_BTNHIGHLIGHT)
+    __add_color('buttonshadow', win32con.COLOR_BTNSHADOW)
+    __add_color('buttontext', win32con.COLOR_BTNTEXT)
+    __add_color('captiontext', win32con.COLOR_CAPTIONTEXT)
+    __add_color('graytext', win32con.COLOR_GRAYTEXT)
+    __add_color('highlight', win32con.COLOR_HIGHLIGHT)
+    __add_color('highlighttext', win32con.COLOR_HIGHLIGHTTEXT)
+    __add_color('inactiveborder', win32con.COLOR_INACTIVEBORDER)
+    __add_color('inactivecaption', win32con.COLOR_INACTIVECAPTION)
+    __add_color('inactivecaptiontext', win32con.COLOR_INACTIVECAPTIONTEXT)
+    __add_color('infobackground', win32con.COLOR_INFOBK)
+    __add_color('infotext', win32con.COLOR_INFOTEXT)
+    __add_color('menu', win32con.COLOR_MENU)
+    __add_color('menutext', win32con.COLOR_MENUTEXT)
+    __add_color('scrollbar', win32con.COLOR_SCROLLBAR)
+    __add_color('threeddarkshadow', win32con.COLOR_3DDKSHADOW)
+    __add_color('threedface', win32con.COLOR_3DFACE)
+    __add_color('threedhighlight', win32con.COLOR_3DHIGHLIGHT)
+    __add_color('threedlightshadow', win32con.COLOR_3DLIGHT)
+    __add_color('threedshadow', win32con.COLOR_3DSHADOW)
+    __add_color('window', win32con.COLOR_WINDOW)
+    __add_color('windowframe', win32con.COLOR_WINDOWFRAME)
+    __add_color('windowtext', win32con.COLOR_WINDOWTEXT)
+
 init_colors()
--- mm/win32/Boost/Bridge/wintk/TransitionBitBlit.py
+++ mm/win32/Boost/Bridge/wintk/TransitionBitBlit.py
@@ -6,167 +6,165 @@ __version__ = "$Id$"
 
 import ddraw
 import wingdi
-import win32con 
+import win32con
 
 class BlitterClass:
-	SHOW_UNIMPLEMENTED_TRANSITION_NAME=1
-	
-	def __init__(self, engine, dict):
-		self._engine = engine
-		self._dict = dict
-	
-	def updatebitmap(self, parameters, src1, src2, tmp, dst, dstrgn):
-		# Called by the engine when the parameters have changed and the
-		# destination bitmap has to be recomputed. Also called for other redraw reasons.
-		pass
-					
-	def needtmpbitmap(self):
-		# Return true if this blitter needs the temporary bitmap.
-		return 1
-		
-	def _convertrect(self, ltrb):
-		# Convert an lrtb-style rectangle to the local convention
-		l,t,r,b = ltrb
-		return l,t,r-l,b-t
-	
-	def isempty(self, ltrb):
-		l,t,r,b = ltrb
-		return r==l or b==t
+    SHOW_UNIMPLEMENTED_TRANSITION_NAME=1
 
-	def copyBits(self, src, dst, rcsrc, rcdst, flags=0, rgn=None):
-		if not self.isempty(rcsrc) and not self.isempty(rcdst):
-			if rgn==None:
-				flags = flags | ddraw.DDBLT_WAIT 
-				try:	
-					dst.Blt(rcdst, src, rcsrc, flags)
-				except ddraw.error, arg:
-					print arg
-			else:
-				try:
-					dstDC = self.getDC(dst)
-				except ddraw.error, arg:
-					print arg
-					return
-				
-				try:
-					srcDC = self.getDC(src)
-				except ddraw.error, arg:
-					self.releaseDC(dst,dstDC)
-					print arg
-					return
-				
-				if dstDC and srcDC:
-					try:
-						dstDC.SelectClipRgn(rgn)			
-						ls, ts, rs, bs = rcsrc
-						ld, td, rd, bd = rcdst
-						dstDC.BitBlt((ld,td),(rd-ld,bd-td),srcDC,(ls, ts), win32con.SRCCOPY)
-					except ddraw.error, arg:
-						print arg
+    def __init__(self, engine, dict):
+        self._engine = engine
+        self._dict = dict
 
-				self.releaseDC(dst,dstDC)
-				self.releaseDC(src,srcDC)
+    def updatebitmap(self, parameters, src1, src2, tmp, dst, dstrgn):
+        # Called by the engine when the parameters have changed and the
+        # destination bitmap has to be recomputed. Also called for other redraw reasons.
+        pass
 
-	def getDC(self, dds):
-		hdc = dds.GetDC()
-		if not hdc: return None
-		return wingdi.CreateDCFromHandle(hdc)
+    def needtmpbitmap(self):
+        # Return true if this blitter needs the temporary bitmap.
+        return 1
 
-	def releaseDC(self, dds, dc):
-		if dc:
-			hdc = dc.Detach()
-			dds.ReleaseDC(hdc)
+    def _convertrect(self, ltrb):
+        # Convert an lrtb-style rectangle to the local convention
+        l,t,r,b = ltrb
+        return l,t,r-l,b-t
 
-	def createRegion(self, pointlist):
-		npl = []
-		for p in pointlist:
-			x = int(p[0]+0.5)
-			y = int(p[1]+0.5)
-			npl.append((x,y))
-		rgn = None
-		try:
-			rgn = wingdi.CreatePolygonRgn(npl)
-		except:
-			print 'CreatePolygonRgn exception:', npl			
-			succeeded = 0
-			rgn = None
-		else:
-			if not succeeded:
-				print 'CreatePolygonRgn failed:', npl
-				rgn = None
-		return rgn
+    def isempty(self, ltrb):
+        l,t,r,b = ltrb
+        return r==l or b==t
+
+    def copyBits(self, src, dst, rcsrc, rcdst, flags=0, rgn=None):
+        if not self.isempty(rcsrc) and not self.isempty(rcdst):
+            if rgn==None:
+                flags = flags | ddraw.DDBLT_WAIT
+                try:
+                    dst.Blt(rcdst, src, rcsrc, flags)
+                except ddraw.error, arg:
+                    print arg
+            else:
+                try:
+                    dstDC = self.getDC(dst)
+                except ddraw.error, arg:
+                    print arg
+                    return
+
+                try:
+                    srcDC = self.getDC(src)
+                except ddraw.error, arg:
+                    self.releaseDC(dst,dstDC)
+                    print arg
+                    return
+
+                if dstDC and srcDC:
+                    try:
+                        dstDC.SelectClipRgn(rgn)
+                        ls, ts, rs, bs = rcsrc
+                        ld, td, rd, bd = rcdst
+                        dstDC.BitBlt((ld,td),(rd-ld,bd-td),srcDC,(ls, ts), win32con.SRCCOPY)
+                    except ddraw.error, arg:
+                        print arg
+
+                self.releaseDC(dst,dstDC)
+                self.releaseDC(src,srcDC)
+
+    def getDC(self, dds):
+        hdc = dds.GetDC()
+        if not hdc: return None
+        return wingdi.CreateDCFromHandle(hdc)
+
+    def releaseDC(self, dds, dc):
+        if dc:
+            hdc = dc.Detach()
+            dds.ReleaseDC(hdc)
+
+    def createRegion(self, pointlist):
+        npl = []
+        for p in pointlist:
+            x = int(p[0]+0.5)
+            y = int(p[1]+0.5)
+            npl.append((x,y))
+        rgn = None
+        try:
+            rgn = wingdi.CreatePolygonRgn(npl)
+        except:
+            print 'CreatePolygonRgn exception:', npl
+            succeeded = 0
+            rgn = None
+        else:
+            if not succeeded:
+                print 'CreatePolygonRgn failed:', npl
+                rgn = None
+        return rgn
 
 class R1R2BlitterClass(BlitterClass):
-	# parameter is 2 rects, first copy rect2 from src2, then rect1 from src1
-	def updatebitmap(self, parameters, src1, src2, tmp, dst, dstrgn):
-		rect1, rect2 = parameters
-		self.copyBits(src2, dst, rect2, rect2)
-		self.copyBits(src1, dst, rect1, rect1)
-		
+    # parameter is 2 rects, first copy rect2 from src2, then rect1 from src1
+    def updatebitmap(self, parameters, src1, src2, tmp, dst, dstrgn):
+        rect1, rect2 = parameters
+        self.copyBits(src2, dst, rect2, rect2)
+        self.copyBits(src1, dst, rect1, rect1)
+
 class R1R2R3R4BlitterClass(BlitterClass):
-	# Parameter is 4 rects. Copy src1[rect1] to rect2, src2[rect3] to rect4
-	def updatebitmap(self, parameters, src1, src2, tmp, dst, dstrgn):
-		srcrect1, dstrect1, srcrect2, dstrect2 = parameters
-		self.copyBits(src1, dst, srcrect1, dstrect1)
-		self.copyBits(src2, dst, srcrect2, dstrect2)
-		
+    # Parameter is 4 rects. Copy src1[rect1] to rect2, src2[rect3] to rect4
+    def updatebitmap(self, parameters, src1, src2, tmp, dst, dstrgn):
+        srcrect1, dstrect1, srcrect2, dstrect2 = parameters
+        self.copyBits(src1, dst, srcrect1, dstrect1)
+        self.copyBits(src2, dst, srcrect2, dstrect2)
+
 class R1R2OverlapBlitterClass(BlitterClass):
-	# Like R1R2BlitterClass but rects may overlap, so copy via the temp bitmap
-	
-	def updatebitmap(self, parameters, src1, src2, tmp, dst, dstrgn):
-		rect1, rect2 = parameters
-		self.copyBits(src2, tmp, rect2, rect2)
-		self.copyBits(src1, tmp, rect1, rect1)
-		self.copyBits(tmp, dst, self.ltrb, self.ltrb)
-			
-				
+    # Like R1R2BlitterClass but rects may overlap, so copy via the temp bitmap
+
+    def updatebitmap(self, parameters, src1, src2, tmp, dst, dstrgn):
+        rect1, rect2 = parameters
+        self.copyBits(src2, tmp, rect2, rect2)
+        self.copyBits(src1, tmp, rect1, rect1)
+        self.copyBits(tmp, dst, self.ltrb, self.ltrb)
+
+
 class RlistR2OverlapBlitterClass(BlitterClass):
-	# Like R1R2OverlapBlitterClass, but first item is a list of rects
-	def updatebitmap(self, parameters, src1, src2, tmp, dst, dstrgn):
-		rectlist, rect2 = parameters
-		self.copyBits(src2, tmp, rect2, rect2)
-		x0, y0, x1, y1 = self.ltrb
-		for rect in rectlist:
-			self.copyBits(src1, tmp, rect, rect)
-		self.copyBits(tmp, dst, self.ltrb, self.ltrb)
-	
+    # Like R1R2OverlapBlitterClass, but first item is a list of rects
+    def updatebitmap(self, parameters, src1, src2, tmp, dst, dstrgn):
+        rectlist, rect2 = parameters
+        self.copyBits(src2, tmp, rect2, rect2)
+        x0, y0, x1, y1 = self.ltrb
+        for rect in rectlist:
+            self.copyBits(src1, tmp, rect, rect)
+        self.copyBits(tmp, dst, self.ltrb, self.ltrb)
+
 class PolyR2OverlapBlitterClass(BlitterClass):
-	# Like R1R2OverlapBlitterClass, but first item is a polygon (list of points)
-	def updatebitmap(self, parameters, src1, src2, tmp, dst, dstrgn):
-		pointlist, rect2 = parameters
-		self.copyBits(src2, tmp, rect2, rect2)
-		if pointlist:
-			rgn = self.createRegion(pointlist)
-			self.copyBits(src1, tmp, self.ltrb, self.ltrb, 0, rgn)
-			if rgn: rgn.DeleteObject()
-		self.copyBits(tmp, dst, self.ltrb, self.ltrb)
+    # Like R1R2OverlapBlitterClass, but first item is a polygon (list of points)
+    def updatebitmap(self, parameters, src1, src2, tmp, dst, dstrgn):
+        pointlist, rect2 = parameters
+        self.copyBits(src2, tmp, rect2, rect2)
+        if pointlist:
+            rgn = self.createRegion(pointlist)
+            self.copyBits(src1, tmp, self.ltrb, self.ltrb, 0, rgn)
+            if rgn: rgn.DeleteObject()
+        self.copyBits(tmp, dst, self.ltrb, self.ltrb)
 
 class PolylistR2OverlapBlitterClass(BlitterClass):
-	# Like PolyR2OverlapBlitterClass, but first item is a list of polygons
-		
-	def updatebitmap(self, parameters, src1, src2, tmp, dst, dstrgn):
-		pointlist, rect2 = parameters
-		self.copyBits(src2, tmp, rect2, rect2)
-		if pointlist:
-			rgn = self.createRegion(pointlist[0])
-			if rgn:
-				for pl in pointlist[1:]:
-					newrgn = self.createRegion(pl)
-					if newrgn:
-						rgn.CombineRgn(rgn,newrgn,win32con.RGN_OR)
-						newrgn.DeleteObject()
-				self.copyBits(src1, tmp, self.ltrb, self.ltrb, 0, rgn)
-				rgn.DeleteObject()
-		self.copyBits(tmp, dst, self.ltrb, self.ltrb)
+    # Like PolyR2OverlapBlitterClass, but first item is a list of polygons
 
-	
+    def updatebitmap(self, parameters, src1, src2, tmp, dst, dstrgn):
+        pointlist, rect2 = parameters
+        self.copyBits(src2, tmp, rect2, rect2)
+        if pointlist:
+            rgn = self.createRegion(pointlist[0])
+            if rgn:
+                for pl in pointlist[1:]:
+                    newrgn = self.createRegion(pl)
+                    if newrgn:
+                        rgn.CombineRgn(rgn,newrgn,win32con.RGN_OR)
+                        newrgn.DeleteObject()
+                self.copyBits(src1, tmp, self.ltrb, self.ltrb, 0, rgn)
+                rgn.DeleteObject()
+        self.copyBits(tmp, dst, self.ltrb, self.ltrb)
+
+
 class FadeBlitterClass(BlitterClass):
-	# Parameter is float in range 0..1, use this as blend value
-	def updatebitmap(self, parameters, src1, src2, tmp, dst, dstrgn):
-		value = parameters
-		try:
-			dst.BltBlend(src2, src1, value)
-		except ddraw.error, arg:
-			print arg
-
-
+    # Parameter is float in range 0..1, use this as blend value
+    def updatebitmap(self, parameters, src1, src2, tmp, dst, dstrgn):
+        value = parameters
+        try:
+            dst.BltBlend(src2, src1, value)
+        except ddraw.error, arg:
+            print arg
