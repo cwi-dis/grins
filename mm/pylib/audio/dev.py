da58a79bcab82ac2e275f9734489f443ef129aed
Sjoerd Mullender <sjoerdmullender@users.noreply.github.com>
2008-02-08 14:59:52 +0000

    Use more modern (and consistent) indentation.

--- mm/pylib/audio/dev.py
+++ mm/pylib/audio/dev.py
@@ -5,26 +5,26 @@ Error = 'audio.dev.Error'
 import sys
 
 if sys.platform[:4] == 'irix':
-	from audio.devsgi import AudioDevSGI
-	writer = AudioDevSGI
-	del AudioDevSGI
+    from audio.devsgi import AudioDevSGI
+    writer = AudioDevSGI
+    del AudioDevSGI
 elif sys.platform[:5] == 'sunos':
-	from audio.devsun import AudioDevSUN
-	writer = AudioDevSUN
-	del AudioDevSUN
+    from audio.devsun import AudioDevSUN
+    writer = AudioDevSUN
+    del AudioDevSUN
 elif sys.platform[:5] == 'linux':
-	from audio.devlinux import AudioDevLINUX
-	writer = AudioDevLINUX
-	del AudioDevLINUX
+    from audio.devlinux import AudioDevLINUX
+    writer = AudioDevLINUX
+    del AudioDevLINUX
 elif sys.platform in ('mac', 'darwin'):
-	from audio.devmac import AudioDevMAC
-	writer = AudioDevMAC
-	del AudioDevMAC
+    from audio.devmac import AudioDevMAC
+    writer = AudioDevMAC
+    del AudioDevMAC
 elif sys.platform == 'win32':
-	from audio.devwin import AudioDevWin
-	writer = AudioDevWin
-	del AudioDevWin
+    from audio.devwin import AudioDevWin
+    writer = AudioDevWin
+    del AudioDevWin
 else:
-	raise ImportError('No module named audio.dev')
+    raise ImportError('No module named audio.dev')
 
 del sys
