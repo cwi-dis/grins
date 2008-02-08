3a3784a9cf023f173345dba2c7102846d7d80d6a
Sjoerd Mullender <sjoerdmullender@users.noreply.github.com>
2008-02-08 14:59:39 +0000

    Use more modern (and consistent) indentation.

--- mm/editor/smilboston/features.py
+++ mm/editor/smilboston/features.py
@@ -7,33 +7,33 @@ from FeatureSet import *
 import sys
 
 feature_set = [
-	EXPORT_SMIL2,
-##	EXPORT_SMIL1,
-	EXPORT_REAL,
-##	EXPORT_QT, EXPORT_WMP,
-	EXPORT_HTML_TIME,
-	EXPORT_3GPP,			# unimplemented
-	EXPORT_WINCE,
-	PLAYER_VIEW, STRUCTURE_VIEW, LAYOUT_VIEW, HYPERLINKS_VIEW,
-	SOURCE_VIEW, ASSETS_VIEW,
-	SOURCE_VIEW_EDIT,
-	ERRORS_VIEW,
-	USER_GROUPS,
-	TRANSITION_VIEW,
-	UNIFIED_FOCUS,
-	H_MODIFY_STRUCTURE, CUSTOM_REGIONS,
-	H_ASSIGN_NAME,
-	CREATE_TEMPLATES,
-	ADVANCED_PROPERTIES,
-	PREFERENCES,
-	ALIGNTOOL,
-	SHOW_MEDIA_CHILDREN,
-	MULTIPLE_TOPLAYOUT,
-	INTERNAL_LINKS,
-	AUTO_EVALUATE,
-	]
+        EXPORT_SMIL2,
+##     EXPORT_SMIL1,
+        EXPORT_REAL,
+##     EXPORT_QT, EXPORT_WMP,
+        EXPORT_HTML_TIME,
+        EXPORT_3GPP,                    # unimplemented
+        EXPORT_WINCE,
+        PLAYER_VIEW, STRUCTURE_VIEW, LAYOUT_VIEW, HYPERLINKS_VIEW,
+        SOURCE_VIEW, ASSETS_VIEW,
+        SOURCE_VIEW_EDIT,
+        ERRORS_VIEW,
+        USER_GROUPS,
+        TRANSITION_VIEW,
+        UNIFIED_FOCUS,
+        H_MODIFY_STRUCTURE, CUSTOM_REGIONS,
+        H_ASSIGN_NAME,
+        CREATE_TEMPLATES,
+        ADVANCED_PROPERTIES,
+        PREFERENCES,
+        ALIGNTOOL,
+        SHOW_MEDIA_CHILDREN,
+        MULTIPLE_TOPLAYOUT,
+        INTERNAL_LINKS,
+        AUTO_EVALUATE,
+        ]
 
-auto_evaluate_period = 7	# 7 days auto-evaluate
+auto_evaluate_period = 7        # 7 days auto-evaluate
 version = 'pro'
 compatibility = Boston
 compatibility_short = 'SMIL2'
