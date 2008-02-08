5ca598e63be3b418aa75fbce428de66d80e2aee6
Sjoerd Mullender <sjoerdmullender@users.noreply.github.com>
2008-02-08 14:59:46 +0000

    Use more modern (and consistent) indentation.

--- mm/lib/svggraphics.py
+++ mm/lib/svggraphics.py
@@ -1,180 +1,178 @@
 __version__ = "$Id$"
 
 #
-#	SVG Graphics
+#       SVG Graphics
 #
 
 import svgtypes
 import svgpath
 
 class SVGGraphics:
-	def __init__(self):
-		# current point
-		self.cpoint = 0, 0
-		
-		# current path
-		self.cpath = svgpath.Path()
+    def __init__(self):
+        # current point
+        self.cpoint = 0, 0
 
-		# current transformation matrix (CTM)
-		self.ctm = svgtypes.TM()
+        # current path
+        self.cpath = svgpath.Path()
 
-		# current style attributes:
-		# font, color, line, weight, line cap, line join
-		# line miter limit, dash pattern
-		# clipping region
-		# ...
-		# XXX:  initialize according to spec
-		self.cstyle = {
-			'stroke-width': svgtypes.SVGLength(None, 'stroke-width', '1'), 
-			'font-family': 'Verdana', 
-			'font-size':svgtypes.SVGLength(None, 'font-size', '15'),
-			'fill':svgtypes.SVGColor(None, 'fill', 'black'),
-			}
+        # current transformation matrix (CTM)
+        self.ctm = svgtypes.TM()
 
-	def __repr__(self):
-		style = ''
-		for prop, val in self.cstyle.items():
-			try:
-				style = style + prop + ':' + val + '; '
-			except:
-				print  prop, val
-		style = style + 'ctm' + ':' + `self.ctm` + '; '
-		style = style + 'cp' + ':' + `self.cpoint` + '; '
-		return style
+        # current style attributes:
+        # font, color, line, weight, line cap, line join
+        # line miter limit, dash pattern
+        # clipping region
+        # ...
+        # XXX:  initialize according to spec
+        self.cstyle = {
+                'stroke-width': svgtypes.SVGLength(None, 'stroke-width', '1'),
+                'font-family': 'Verdana',
+                'font-size':svgtypes.SVGLength(None, 'font-size', '15'),
+                'fill':svgtypes.SVGColor(None, 'fill', 'black'),
+                }
 
-	def applyStyle(self, style):
-		if style:
-			self.cstyle.update(style)
+    def __repr__(self):
+        style = ''
+        for prop, val in self.cstyle.items():
+            try:
+                style = style + prop + ':' + val + '; '
+            except:
+                print  prop, val
+        style = style + 'ctm' + ':' + `self.ctm` + '; '
+        style = style + 'cp' + ':' + `self.cpoint` + '; '
+        return style
 
-	def applyTfList(self, tflist):
-		if tflist:
-			self.ctm.applyTfList(tflist)
+    def applyStyle(self, style):
+        if style:
+            self.cstyle.update(style)
 
-	# style arg overrides current graphics style
-	def getStyleAttr(self, prop, style=None):
-		if style:
-			val = style.get(prop)
-			if val is not None:
-				return self.toVal(val)
-		return self.toVal(self.cstyle.get(prop))
+    def applyTfList(self, tflist):
+        if tflist:
+            self.ctm.applyTfList(tflist)
 
-	def getStyleAttrObj(self, prop, style=None):
-		if style:
-			val = style.get(prop)
-			if val is not None:
-				return val
-		return self.cstyle.get(prop)
+    # style arg overrides current graphics style
+    def getStyleAttr(self, prop, style=None):
+        if style:
+            val = style.get(prop)
+            if val is not None:
+                return self.toVal(val)
+        return self.toVal(self.cstyle.get(prop))
 
-	def getStyle(self):
-		return self.cstyle
+    def getStyleAttrObj(self, prop, style=None):
+        if style:
+            val = style.get(prop)
+            if val is not None:
+                return val
+        return self.cstyle.get(prop)
 
-	def toVal(self, obj):
-		if obj is None:
-			return None
-		if type(obj) == type(''):
-			if obj == 'none':
-				return None
-			else:
-				return obj
-		else:
-			if isinstance(obj, svgtypes.Animateable):
-				return obj.getPresentValue()
-			else:
-				return obj.getValue()
+    def getStyle(self):
+        return self.cstyle
 
-	#
-	#  clone for save/restore
-	#
-	def clone(self):
-		new = self.__class__()
-		new.cpoint = self.cpoint
-		new.cpath = self.cpath.copy()
-		new.ctm = self.ctm.copy()
-		new.cstyle = self.cstyle.copy()
-		self.tkInitInstance(new)
-		return new
+    def toVal(self, obj):
+        if obj is None:
+            return None
+        if type(obj) == type(''):
+            if obj == 'none':
+                return None
+            else:
+                return obj
+        else:
+            if isinstance(obj, svgtypes.Animateable):
+                return obj.getPresentValue()
+            else:
+                return obj.getValue()
 
-	#
-	#  renderer notifications interface
-	#
+    #
+    #  clone for save/restore
+    #
+    def clone(self):
+        new = self.__class__()
+        new.cpoint = self.cpoint
+        new.cpath = self.cpath.copy()
+        new.ctm = self.ctm.copy()
+        new.cstyle = self.cstyle.copy()
+        self.tkInitInstance(new)
+        return new
 
-	def onBeginRendering(self):
-		self.tkOnBeginRendering()
+    #
+    #  renderer notifications interface
+    #
 
-	def onEndRendering(self):
-		self.tkOnEndRendering()
+    def onBeginRendering(self):
+        self.tkOnBeginRendering()
 
-
-	#
-	#  platform toolkit renderer notifications interface
-	#
-
-	# called to intialize platform toolkit before rendering
-	def tkStartup(self, params):
-		pass
-
-	# called to dispose platform toolkit objects after rendering
-	def tkShutdown(self):
-		pass
-
-	# we start rendering
-	def tkOnBeginRendering(self, size):
-		pass
-
-	# we finished rendering
-	def tkOnEndRendering(self):
-		pass
-
-	# new graphics context
-	# self state reflects this new context
-	def tkOnBeginContext(self):
-		pass
-
-	# end of context
-	# self state reflects the restored context
-	def tkOnEndContext(self):
-		pass
-
-	# copy platform toolkit objects to 'other' 
-	def tkInitInstance(self, other):
-		pass
-
-	# clip box
-	def tkClipBox(self, clipbox):
-		pass
-
-	def delRegion(self, rgn):
-		pass
-
-	def inside(self, rgn, pt):
-		return 0
-
-	#
-	#  plaform line art interface
-	#  renderers requests for execution
-	#
-	def drawRect(self, pos, size, rxy, style, tf, a=0):
-		print 'rect', pos, size, style, tf
-
-	def drawCircle(self, center, r, style, tf, a=0):
-		print 'circle', center, r, style, tf
-
-	def drawEllipse(self, center, rxy, style, tf, a=0):
-		print 'ellipse', center, rxy, style, tf
-
-	def drawLine(self, pt1, pt2, style, tf, a=0):
-		print 'line', pt1, pt2, style, tf
-
-	def drawPolyline(self, points, style, tf, a=0):
-		print 'polyline', points, style, tf
-
-	def drawPolygon(self, points, style, tf, a=0):
-		print 'polygon', points, style, tf
-
-	def drawPath(self, path, style, tf, a=0):
-		print 'path', path, style, tf
-
-	def drawText(self, text, pos, style, tf, a=0):
-		print text, pos, style, tf
+    def onEndRendering(self):
+        self.tkOnEndRendering()
 
 
+    #
+    #  platform toolkit renderer notifications interface
+    #
+
+    # called to intialize platform toolkit before rendering
+    def tkStartup(self, params):
+        pass
+
+    # called to dispose platform toolkit objects after rendering
+    def tkShutdown(self):
+        pass
+
+    # we start rendering
+    def tkOnBeginRendering(self, size):
+        pass
+
+    # we finished rendering
+    def tkOnEndRendering(self):
+        pass
+
+    # new graphics context
+    # self state reflects this new context
+    def tkOnBeginContext(self):
+        pass
+
+    # end of context
+    # self state reflects the restored context
+    def tkOnEndContext(self):
+        pass
+
+    # copy platform toolkit objects to 'other'
+    def tkInitInstance(self, other):
+        pass
+
+    # clip box
+    def tkClipBox(self, clipbox):
+        pass
+
+    def delRegion(self, rgn):
+        pass
+
+    def inside(self, rgn, pt):
+        return 0
+
+    #
+    #  plaform line art interface
+    #  renderers requests for execution
+    #
+    def drawRect(self, pos, size, rxy, style, tf, a=0):
+        print 'rect', pos, size, style, tf
+
+    def drawCircle(self, center, r, style, tf, a=0):
+        print 'circle', center, r, style, tf
+
+    def drawEllipse(self, center, rxy, style, tf, a=0):
+        print 'ellipse', center, rxy, style, tf
+
+    def drawLine(self, pt1, pt2, style, tf, a=0):
+        print 'line', pt1, pt2, style, tf
+
+    def drawPolyline(self, points, style, tf, a=0):
+        print 'polyline', points, style, tf
+
+    def drawPolygon(self, points, style, tf, a=0):
+        print 'polygon', points, style, tf
+
+    def drawPath(self, path, style, tf, a=0):
+        print 'path', path, style, tf
+
+    def drawText(self, text, pos, style, tf, a=0):
+        print text, pos, style, tf
