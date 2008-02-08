a5a662cd699bff6fa54be727e7ec2fba8fdd9ccd
Sjoerd Mullender <sjoerdmullender@users.noreply.github.com>
2008-02-08 14:59:42 +0000

    Use more modern (and consistent) indentation.

--- mm/grins/mac/TopLevelDialog.py
+++ mm/grins/mac/TopLevelDialog.py
@@ -4,26 +4,26 @@ import windowinterface, WMEVENTS
 from usercmd import *
 
 class TopLevelDialog:
-	#
-	# Note that the TopLevel class on the Mac does not use any commands, everything
-	# is done by the grins and player classes (the "view source" command is
-	# passed on to the player). So, currently the self.commandlist set
-	# by TopLevel is happily ignored.
-	
-	def show(self):
-		self.player.topcommandlist(self.commandlist)
+    #
+    # Note that the TopLevel class on the Mac does not use any commands, everything
+    # is done by the grins and player classes (the "view source" command is
+    # passed on to the player). So, currently the self.commandlist set
+    # by TopLevel is happily ignored.
 
-	def hide(self):
-		pass
+    def show(self):
+        self.player.topcommandlist(self.commandlist)
 
-	def showsource(self, source):
-		if self.source is not None and not self.source.is_closed():
-			self.source.show()
-			return
-		self.source = windowinterface.textwindow(self.root.source)
+    def hide(self):
+        pass
 
-	def setusergroupsmenu(self, menu):
-		self.player.setusergroupsmenu(menu)
+    def showsource(self, source):
+        if self.source is not None and not self.source.is_closed():
+            self.source.show()
+            return
+        self.source = windowinterface.textwindow(self.root.source)
 
-	def setsettingsdict(self, dict):
-		pass
+    def setusergroupsmenu(self, menu):
+        self.player.setusergroupsmenu(menu)
+
+    def setsettingsdict(self, dict):
+        pass
--- mm/grins/motif/PlayerDialog.py
+++ mm/grins/motif/PlayerDialog.py
@@ -6,102 +6,102 @@ from flags import *
 from PlayerDialogBase import PlayerDialogBase, STOPPED, PAUSING, PLAYING
 
 class PlayerDialog(PlayerDialogBase):
-	adornments = {
-		'shortcuts': {
-			'p': PLAY,
-			'P': PAUSE,
-			's': STOP,
-			' ': MAGIC_PLAY,
-			},
-		'menubar': [
-			(FLAG_ALL, 'File', [
-				(FLAG_ALL, 'Open...', OPEN),
-				(FLAG_ALL, 'Close Document', CLOSE),
-				(FLAG_ALL, None),
-				(FLAG_ALL, 'Preferences...', PREFERENCES),
-				(FLAG_ALL|FLAG_DBG, None),
-				(FLAG_ALL|FLAG_DBG, 'Debug', [
-					(FLAG_ALL|FLAG_DBG, 'Trace', TRACE, 't'),
-					(FLAG_ALL|FLAG_DBG, 'Debug', DEBUG),
-					(FLAG_ALL|FLAG_DBG, 'Crash CMIF', CRASH),
-					(FLAG_ALL|FLAG_DBG, 'Dump Scheduler Data', SCHEDDUMP),
-					]),
-				(FLAG_ALL, None),
-				(FLAG_ALL, 'Quit', EXIT),
-				]),
-			(FLAG_ALL, 'View', [
-				(FLAG_ALL, 'View Source...', SOURCEVIEW),
-				]),
-			(FLAG_ALL, 'Play', [
-				(FLAG_ALL, 'Play', PLAY, 't'),
-				(FLAG_ALL, 'Pause', PAUSE, 't'),
-				(FLAG_ALL, 'Stop', STOP, 't'),
-				(FLAG_BOSTON, None),
-				(FLAG_BOSTON, 'Custom Tests', USERGROUPS),
-				(FLAG_CMIF, 'Channels', CHANNELS),
-				]),
-			(FLAG_ALL, 'Help', [
-				(FLAG_ALL, 'Help', HELP),
-				]),
-			],
-		'toolbar': PlayerDialogBase.adornments['toolbar'],
-		'close': [ CLOSE_WINDOW, CLOSE, EXIT, ],
-		}
-	adornments2 = {
-		'shortcuts': {
-			'p': PLAY,
-			'P': PAUSE,
-			's': STOP,
-			' ': MAGIC_PLAY,
-			},
-		'close': [ CLOSE_WINDOW, ],
-		}
+    adornments = {
+            'shortcuts': {
+                    'p': PLAY,
+                    'P': PAUSE,
+                    's': STOP,
+                    ' ': MAGIC_PLAY,
+                    },
+            'menubar': [
+                    (FLAG_ALL, 'File', [
+                            (FLAG_ALL, 'Open...', OPEN),
+                            (FLAG_ALL, 'Close Document', CLOSE),
+                            (FLAG_ALL, None),
+                            (FLAG_ALL, 'Preferences...', PREFERENCES),
+                            (FLAG_ALL|FLAG_DBG, None),
+                            (FLAG_ALL|FLAG_DBG, 'Debug', [
+                                    (FLAG_ALL|FLAG_DBG, 'Trace', TRACE, 't'),
+                                    (FLAG_ALL|FLAG_DBG, 'Debug', DEBUG),
+                                    (FLAG_ALL|FLAG_DBG, 'Crash CMIF', CRASH),
+                                    (FLAG_ALL|FLAG_DBG, 'Dump Scheduler Data', SCHEDDUMP),
+                                    ]),
+                            (FLAG_ALL, None),
+                            (FLAG_ALL, 'Quit', EXIT),
+                            ]),
+                    (FLAG_ALL, 'View', [
+                            (FLAG_ALL, 'View Source...', SOURCEVIEW),
+                            ]),
+                    (FLAG_ALL, 'Play', [
+                            (FLAG_ALL, 'Play', PLAY, 't'),
+                            (FLAG_ALL, 'Pause', PAUSE, 't'),
+                            (FLAG_ALL, 'Stop', STOP, 't'),
+                            (FLAG_BOSTON, None),
+                            (FLAG_BOSTON, 'Custom Tests', USERGROUPS),
+                            (FLAG_CMIF, 'Channels', CHANNELS),
+                            ]),
+                    (FLAG_ALL, 'Help', [
+                            (FLAG_ALL, 'Help', HELP),
+                            ]),
+                    ],
+            'toolbar': PlayerDialogBase.adornments['toolbar'],
+            'close': [ CLOSE_WINDOW, CLOSE, EXIT, ],
+            }
+    adornments2 = {
+            'shortcuts': {
+                    'p': PLAY,
+                    'P': PAUSE,
+                    's': STOP,
+                    ' ': MAGIC_PLAY,
+                    },
+            'close': [ CLOSE_WINDOW, ],
+            }
 
-	def __init__(self, coords, title):
-		PlayerDialogBase.__init__(self, coords, title)
-		self.__topcommandlist = []
-		self.__has_window = 0
+    def __init__(self, coords, title):
+        PlayerDialogBase.__init__(self, coords, title)
+        self.__topcommandlist = []
+        self.__has_window = 0
 
-	def topcommandlist(self, list):
-		if list != self.__topcommandlist:
-			self.__topcommandlist = list
-			self.setstate()
+    def topcommandlist(self, list):
+        if list != self.__topcommandlist:
+            self.__topcommandlist = list
+            self.setstate()
 
-	def close(self):
-		PlayerDialogBase.close(self)
-		del self.__topcommandlist
+    def close(self):
+        PlayerDialogBase.close(self)
+        del self.__topcommandlist
 
-	def show(self):
-		if self.menu_created is None:
-			self.__has_window = 1
-			PlayerDialogBase.show(self)
-		else:
-			self.setchannels()
+    def show(self):
+        if self.menu_created is None:
+            self.__has_window = 1
+            PlayerDialogBase.show(self)
+        else:
+            self.setchannels()
 
-	def after_chan_show(self, channel=None):
-		pass
+    def after_chan_show(self, channel=None):
+        pass
 
-	def setstate(self, state = None):
-		commandlist = self.__topcommandlist + \
-			      self.toplevel.main.commandlist
-		savestoplist = self.stoplist
-		saveplaylist = self.playlist
-		savepauselist = self.pauselist
-		self.stoplist = commandlist + self.stoplist
-		self.playlist = commandlist + self.playlist
-		self.pauselist = commandlist + self.pauselist
+    def setstate(self, state = None):
+        commandlist = self.__topcommandlist + \
+                      self.toplevel.main.commandlist
+        savestoplist = self.stoplist
+        saveplaylist = self.playlist
+        savepauselist = self.pauselist
+        self.stoplist = commandlist + self.stoplist
+        self.playlist = commandlist + self.playlist
+        self.pauselist = commandlist + self.pauselist
 
-		PlayerDialogBase.setstate(self, state)
+        PlayerDialogBase.setstate(self, state)
 
-		self.stoplist = savestoplist
-		self.playlist = saveplaylist
-		self.pauselist = savepauselist
+        self.stoplist = savestoplist
+        self.playlist = saveplaylist
+        self.pauselist = savepauselist
 
-	def get_adornments(self, channel):
-		if self.menu_created is not None or \
-		   self.__has_window:
-			self.adornments2['flags'] = curflags()
-			return self.adornments2
-		self.menu_created = channel
-		self.adornments['flags'] = curflags()
-		return self.adornments
+    def get_adornments(self, channel):
+        if self.menu_created is not None or \
+           self.__has_window:
+            self.adornments2['flags'] = curflags()
+            return self.adornments2
+        self.menu_created = channel
+        self.adornments['flags'] = curflags()
+        return self.adornments
