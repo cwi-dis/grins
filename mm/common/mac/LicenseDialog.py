5a612ee39b35e53d9ee7a3874f8f75578c46cab5
Sjoerd Mullender <sjoerdmullender@users.noreply.github.com>
2008-02-08 14:59:32 +0000

    Use more modern (and consistent) indentation.

--- mm/common/mac/LicenseDialog.py
+++ mm/common/mac/LicenseDialog.py
@@ -27,75 +27,75 @@ ITEMLIST_ENTERKEY_ALL=ITEMrange(ITEM_OK, ITEM_KEY)
 
 
 class LicenseDialog(windowinterface.MACDialog):
-	def __init__(self):
-		windowinterface.MACDialog.__init__(self, "License", ID_DIALOG_LICENSE,
-				ITEMLIST_LICENSE_ALL, cancel=ITEM_QUIT)
-				
-	def close(self):
-		windowinterface.MACDialog.close(self)
-			
-	def setdialoginfo(self):
-			
-		self._setlabel(ITEM_MESSAGE, self.msg)
-		
-		self._setsensitive([ITEM_TRY], self.can_try)
-		self._setsensitive([ITEM_EVAL], self.can_eval)
-		
-	def do_itemhit(self, n, event):
-		if n == ITEM_QUIT:
-			self.cb_quit()
-		elif n == ITEM_TRY:
-			self.cb_try()
-		elif n == ITEM_BUY:
-			self.cb_buy()
-		elif n == ITEM_EVAL:
-			self.cb_eval()
-		elif n == ITEM_ENTERKEY:
-			self.cb_enterkey()
-		elif n == ITEM_OPEN:
-			self.cb_open()
-		else:
-			print 'Unknown LicenseDialog item', n
-		return 1
-			
-	def _optenable(self, onoff, item):
-		ctl = self.dialog.GetDialogItemAsControl(item)
-		if onoff:
-			ctl.ActivateControl()
-		else:
-			ctl.DeactivateControl()
+    def __init__(self):
+        windowinterface.MACDialog.__init__(self, "License", ID_DIALOG_LICENSE,
+                        ITEMLIST_LICENSE_ALL, cancel=ITEM_QUIT)
+
+    def close(self):
+        windowinterface.MACDialog.close(self)
+
+    def setdialoginfo(self):
+
+        self._setlabel(ITEM_MESSAGE, self.msg)
+
+        self._setsensitive([ITEM_TRY], self.can_try)
+        self._setsensitive([ITEM_EVAL], self.can_eval)
+
+    def do_itemhit(self, n, event):
+        if n == ITEM_QUIT:
+            self.cb_quit()
+        elif n == ITEM_TRY:
+            self.cb_try()
+        elif n == ITEM_BUY:
+            self.cb_buy()
+        elif n == ITEM_EVAL:
+            self.cb_eval()
+        elif n == ITEM_ENTERKEY:
+            self.cb_enterkey()
+        elif n == ITEM_OPEN:
+            self.cb_open()
+        else:
+            print 'Unknown LicenseDialog item', n
+        return 1
+
+    def _optenable(self, onoff, item):
+        ctl = self.dialog.GetDialogItemAsControl(item)
+        if onoff:
+            ctl.ActivateControl()
+        else:
+            ctl.DeactivateControl()
 
 class EnterkeyDialog(windowinterface.MACDialog):
-	def __init__(self, ok_callback, user='', org='', license=''):
-		windowinterface.MACDialog.__init__(self, "License", ID_DIALOG_ENTERKEY,
-				ITEMLIST_ENTERKEY_ALL, cancel=ITEM_CANCEL, default=ITEM_OK)
-		self.ok_callback = ok_callback
-		self._setlabel(ITEM_NAME, user)
-		self._setlabel(ITEM_ORGANIZATION, org)
-		self._setlabel(ITEM_KEY, license)
-		self.setdialoginfo()
-		self.show()
-				
-	def close(self):
-		del self.ok_callback
-		windowinterface.MACDialog.close(self)
-			
-	def setdialoginfo(self):
-##		nameok = self._getlabel(ITEM_NAME) or self._getlabel(ITEM_ORGANIZATION)
-		keyok = self._getlabel(ITEM_KEY)
-		self._setsensitive([ITEM_OK], keyok)
-		
-	def do_itemhit(self, n, event):
-		if n == ITEM_OK:
-			name = self._getlabel(ITEM_NAME)
-			org = self._getlabel(ITEM_ORGANIZATION)
-			key = self._getlabel(ITEM_KEY)
-			self.ok_callback(key, name, org)
-			self.close()
-		elif n == ITEM_CANCEL:
-			self.close()
-		elif n in (ITEM_NAME, ITEM_ORGANIZATION, ITEM_KEY):
-			self.setdialoginfo()
-		else:
-			print "Unknown EnterkeyItem", n
-		return 1
+    def __init__(self, ok_callback, user='', org='', license=''):
+        windowinterface.MACDialog.__init__(self, "License", ID_DIALOG_ENTERKEY,
+                        ITEMLIST_ENTERKEY_ALL, cancel=ITEM_CANCEL, default=ITEM_OK)
+        self.ok_callback = ok_callback
+        self._setlabel(ITEM_NAME, user)
+        self._setlabel(ITEM_ORGANIZATION, org)
+        self._setlabel(ITEM_KEY, license)
+        self.setdialoginfo()
+        self.show()
+
+    def close(self):
+        del self.ok_callback
+        windowinterface.MACDialog.close(self)
+
+    def setdialoginfo(self):
+##         nameok = self._getlabel(ITEM_NAME) or self._getlabel(ITEM_ORGANIZATION)
+        keyok = self._getlabel(ITEM_KEY)
+        self._setsensitive([ITEM_OK], keyok)
+
+    def do_itemhit(self, n, event):
+        if n == ITEM_OK:
+            name = self._getlabel(ITEM_NAME)
+            org = self._getlabel(ITEM_ORGANIZATION)
+            key = self._getlabel(ITEM_KEY)
+            self.ok_callback(key, name, org)
+            self.close()
+        elif n == ITEM_CANCEL:
+            self.close()
+        elif n in (ITEM_NAME, ITEM_ORGANIZATION, ITEM_KEY):
+            self.setdialoginfo()
+        else:
+            print "Unknown EnterkeyItem", n
+        return 1
